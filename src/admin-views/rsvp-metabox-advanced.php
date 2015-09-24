cfbcbf98de010b2a3eb3d860475f73be254e2895
Matthew Batchelder <borkweb@gmail.com>
2015-09-24 11:58:34 -0400

    Merge lines

--- src/admin-views/rsvp-metabox-advanced.php
+++ src/admin-views/rsvp-metabox-advanced.php
@@ -1,11 +1,9 @@
 <tr class="<?php $this->tr_class(); ?>">
 	<td><label for="ticket_woo_stock"><?php esc_html_e( 'Stock:', 'tribe-tickets' ); ?></label></td>
 	<td>
-		<input type='text' id='ticket_rsvp_stock' name='ticket_rsvp_stock' class="ticket_field" size='7'
-		       value='<?php echo esc_attr( $stock ); ?>'/>
+		<input type='text' id='ticket_rsvp_stock' name='ticket_rsvp_stock' class="ticket_field" size='7' value='<?php echo esc_attr( $stock ); ?>'/>
 
-		<p class="description"><?php esc_html_e( "(Total available # of this ticket type. Once they're gone, ticket type is sold out)",
-				'tribe-tickets' ); ?></p>
+		<p class="description"><?php esc_html_e( "(Total available # of this ticket type. Once they're gone, ticket type is sold out)", 'tribe-tickets' ); ?></p>
 	</td>
 </tr>
 
