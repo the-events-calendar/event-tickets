2bf95273da4cf65318dcbff4a2055a18b8c27fd4
Luca Tumedei <luca@theaveragedev.com>
2015-10-14 18:31:14 +0200

    scaffolded plugin tests

---
+++ tests/_bootstrap.dist.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @file Global bootstrap for all codeception tests
+ */
+
+Codeception\Util\Autoload::addNamespace( 'Tribe__Events__WP_UnitTestCase', __DIR__ . '/_support' );
+Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', __DIR__ . '/_support' );
