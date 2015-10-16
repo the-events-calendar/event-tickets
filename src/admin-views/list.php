23d7e5f05d79c1bc7163472ef92f20d13df6a6a9
Matthew Batchelder <borkweb@gmail.com>
2015-10-16 10:10:08 -0400

    Remove linebreak

--- src/admin-views/list.php
+++ src/admin-views/list.php
@@ -34,7 +34,6 @@
 		if ( $ticket->admin_link ) {
 			$controls[] = sprintf( "<span><a href='%s'>" . esc_html__( 'Edit in %s', 'event-tickets' ) . '</a></span>', esc_url( $ticket->admin_link ), $modules[ $ticket->provider_class ] );
 		}
-
 		if ( $ticket->frontend_link && get_post_status( $post_id ) == 'publish' ) {
 			$controls[] = sprintf( "<span><a href='%s'>" . esc_html__( 'View', 'event-tickets' ) . '</a></span>', esc_url( $ticket->frontend_link ) );
 		}
