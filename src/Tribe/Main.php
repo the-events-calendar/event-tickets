0b77cb9c388c8b29f495afb0a94855f1d194db54
Matthew Batchelder <borkweb@gmail.com>
2015-11-18 15:30:30 -0500

    4.0rc1

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -9,7 +9,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.0beta2';
+	const VERSION = '4.0rc1';
 
 	/**
 	 * Min required The Events Calendar version
