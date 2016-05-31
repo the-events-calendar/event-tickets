bd644532b67521fdcc455d235fc9a1210255fe91
barryhughes <barry@codingkills.me>
2016-05-31 15:05:09 -0700

    Bump version # to 4.2RC1 | #44413

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -9,12 +9,12 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.2beta1';
+	const VERSION = '4.2RC1';
 
 	/**
 	 * Min required The Events Calendar version
 	 */
-	const MIN_TEC_VERSION = '4.2beta1';
+	const MIN_TEC_VERSION = 'Bump version # to 4.2RC1 | #44413';
 
 	/**
 	 * Name of the provider
