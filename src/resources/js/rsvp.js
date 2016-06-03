d3249944ed9c198e1a0eec7e655b4be418e9da3a
trishasalas <trisha@trishasalas.com>
2016-06-03 15:51:18 -0500

    move toggle so that only a single instance shows for multiple meta details, fix a bit of css and html

--- src/resources/js/rsvp.js
+++ src/resources/js/rsvp.js
@@ -14,11 +14,11 @@ var tribe_tickets_rsvp = {
 
 		this.$rsvp.closest( '.cart' )
 			.on( 'submit', this.event.handle_submission );
-
-        $( '.tribe-rsvp-list' ).on( 'click', '.attendee-meta-row .toggle', function() {
+        
+        $( '.tribe-rsvp-list' ).on( 'click', '.attendee-meta.toggle', function() {
             $( this )
                 .toggleClass( 'on' )
-                .siblings( '.attendee-meta-details' )
+                .siblings( '.attendee-meta-row' )
                 .slideToggle();
         });
     };
