4d1bf9a1bbffff9576a02bc21602a66a5cc57d1d
barryhughes <barry@codingkills.me>
2016-06-07 09:23:44 -0700

    Update version # to 4.2 | #43937

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -9,12 +9,12 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.2rc1';
+	const VERSION = '4.2';
 
 	/**
 	 * Min required The Events Calendar version
 	 */
-	const MIN_TEC_VERSION = '4.2rc1';
+	const MIN_TEC_VERSION = '4.2';
 
 	/**
 	 * Name of the provider
