0139cbada33ae012f3694473748017d81140be49
trishasalas <trisha@trishasalas.com>
2016-06-15 09:27:52 -0500

    name change for code review fix

--- src/views/tickets/orders.php
+++ src/views/tickets/orders.php
@@ -45,12 +45,12 @@ $is_event_page = class_exists( 'Tribe__Events__Main' ) && Tribe__Events__Main::P
 
 	<?php tribe_tickets_get_template_part( 'tickets/orders-rsvp' ); ?>
 
-		<?php
-		/**
-		 * Adds ticket order form
-		 */
-		do_action( 'event_tickets_plus_orders_tickets' );
-		?>
+	<?php
+	/**
+	 * Fires before the process tickets submission button is rendered
+	 */
+	do_action( 'tribe_tickets_orders_before_submit' );
+	?>
 
 	<?php if ( $view->has_rsvp_attendees( $event_id ) || $view->has_ticket_attendees( $event_id ) ): ?>
 		<div class="tribe-submit-tickets-form">
