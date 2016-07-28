cac5949f1f43d41d98f8a20b3717796787467b5a
barryhughes <barry@codingkills.me>
2016-07-27 17:13:02 -0700

    Break attendee logic count out of template | #61992

--- src/Tribe/RSVP/Attendance_Totals.php
+++ src/Tribe/RSVP/Attendance_Totals.php
@@ -11,42 +11,10 @@
  * or if RSVPs are added/deleted later in the request, it would be necessary
  * to obtain a new object of this type to get accurate results.
  */
-class Tribe__Tickets__RSVP__Attendance_Totals {
-	protected $event_id = 0;
+class Tribe__Tickets__RSVP__Attendance_Totals extends Tribe__Tickets__Abstract_Attendance_Totals {
 	protected $total_rsvps = 0;
 	protected $total_going = 0;
 	protected $total_not_going = 0;
-
-	/**
-	 * Determines current RSVP totals for the current or specified event.
-	 *
-	 * @param int $event_id
-	 */
-	public function __construct( $event_id = 0 ) {
-		if ( ! $this->set_event_id( $event_id ) ) {
-			return;
-		}
-
-		$this->calculate_totals();
-	}
-
-	/**
-	 * Sets the event ID, based on the provided event ID but defaulting
-	 * to the value of the 'event_id' URL param, if set.
-	 *
-	 * @param int $event_id
-	 *
-	 * @return bool
-	 */
-	protected function set_event_id( $event_id = 0 ) {
-		if ( $event_id ) {
-			$this->event_id = absint( $event_id );
-		} elseif ( isset( $_GET[ 'event_id' ] ) ) {
-			$this->event_id = absint( $_GET[ 'event_id' ] );
-		}
-
-		return (bool) $this->event_id;
-	}
 
 	/**
 	 * Calculate total RSVP attendance for the current event.
@@ -60,13 +28,6 @@ class Tribe__Tickets__RSVP__Attendance_Totals {
 		}
 
 		$this->total_rsvps = $this->total_going + $this->total_not_going;
-	}
-
-	/**
-	 * Makes the RSVP totals available within the attendee summary screen.
-	 */
-	public function integrate_with_attendee_screen() {
-		add_action( 'tribe_tickets_attendees_totals', array( $this, 'print_totals' ) );
 	}
 
 	/**
