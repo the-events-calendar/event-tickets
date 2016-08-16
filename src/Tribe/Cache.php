4299ef2621b76f445aea28f3747287ddf2b4385b
Luca Tumedei <luca@theaveragedev.com>
2016-08-16 17:47:17 +0200

    implemented first version of tickets cache class and tests

--- src/Tribe/Cache.php
+++ src/Tribe/Cache.php
@@ -11,7 +11,15 @@ class Tribe__Tickets__Cache {
 	/**
 	 * @var array
 	 */
-	protected static $keys = array();
+	protected static $keys = array(
+		'posts_with_tickets',
+		'posts_without_tickets',
+	);
+
+	/**
+	 * @var int The expiration time in seconds.
+	 */
+	protected $expiration = 60;
 
 	/**
 	 * Resets all caches for the class.
@@ -26,7 +34,68 @@ class Tribe__Tickets__Cache {
 	 * @return array An array of post IDs of posts that have no tickets assigned.
 	 */
 	public function posts_without_tickets() {
+		$ids = wp_cache_get( __METHOD__, __CLASS__, null, $found );
 
-		return array();
+		if ( false === $found ) {
+			$supported_types = tribe_get_option( 'ticket-enabled-post-types', array() );
+
+			if ( empty( $supported_types ) ) {
+				$ids = array();
+			}
+
+			/** @var \wpdb $wpdb */
+			global $wpdb;
+
+			$post_types = "('" . implode( "','", $supported_types ) . "')";
+
+			$query = "SELECT DISTINCT(ID) FROM {$wpdb->posts}
+				WHERE post_type IN {$post_types}";
+
+			$posts_with_tickets = $this->posts_with_tickets();
+
+			if ( ! empty( $posts_with_tickets ) ) {
+				$excluded = '(' . implode( ',', $posts_with_tickets ) . ')';
+				$query .= " AND ID NOT IN {$excluded}";
+			}
+			$ids = $wpdb->get_col( $query );
+
+			$ids = is_array( $ids ) ? $ids : array();
+
+			wp_cache_set( __METHOD__, $ids, __CLASS__, $this->expiration );
+		}
+
+		return $ids;
+	}
+
+
+	public function posts_with_tickets() {
+		$ids = wp_cache_get( __METHOD__, __CLASS__, null, $found );
+
+		if ( false === $found ) {
+			$supported_types = tribe_get_option( 'ticket-enabled-post-types', array() );
+
+			if ( empty( $supported_types ) ) {
+				$ids = array();
+			}
+
+			/** @var \wpdb $wpdb */
+			global $wpdb;
+
+			$post_types = "('" . implode( "','", $supported_types ) . "')";
+
+			$query = "SELECT DISTINCT(pm.meta_value) FROM {$wpdb->postmeta} pm
+				LEFT JOIN {$wpdb->posts} p
+				ON pm.meta_value = p.ID
+				WHERE p.post_type IN {$post_types}
+				AND pm.meta_key LIKE '_tribe_%_for_event'
+				AND pm.meta_value IS NOT NULL";
+			$ids   = $wpdb->get_col( $query );
+
+			$ids = is_array( $ids ) ? $ids : array();
+
+			wp_cache_set( __METHOD__, $ids, __CLASS__, $this->expiration );
+		}
+
+		return $ids;
 	}
 }
--- tests/integration/Tribe/Tickets/CacheTest.php
+++ tests/integration/Tribe/Tickets/CacheTest.php
@@ -36,6 +36,7 @@ class CacheTest extends \Codeception\TestCase\WPTestCase {
 	 */
 	public function it_should_return_a_list_of_all_the_posts_without_a_ticket() {
 		tribe_update_option( 'ticket-enabled-post-types', [ 'page', 'post' ] );
+
 		$ids_without_tickets = $this->factory()->post->create_many( 3, [ 'post_type' => 'post' ] );
 		$ids_without_tickets = array_merge( $ids_without_tickets, $this->factory()->post->create_many( 3, [ 'post_type' => 'page' ] ) );
 
@@ -43,6 +44,153 @@ class CacheTest extends \Codeception\TestCase\WPTestCase {
 		$ids = $sut->posts_without_tickets();
 
 		$this->assertEqualSets( $ids_without_tickets, $ids );
+	}
+
+	/**
+	 * @test
+	 * it should return a list of all the posts with a ticket assigned
+	 */
+	public function it_should_return_a_list_of_all_the_posts_with_a_ticket_assigned() {
+		tribe_update_option( 'ticket-enabled-post-types', [ 'page', 'post' ] );
+
+		$posts_with_tickets = $this->factory()->post->create_many( 3, [ 'post_type' => 'post' ] );
+		$posts_with_tickets = array_merge( $posts_with_tickets, $this->factory()->post->create_many( 3, [ 'post_type' => 'page' ] ) );
+
+		// not relevant, any post type can be related to any other post type as "a ticket"
+		$ticket_post_type = 'ticket_type';
+		register_post_type( $ticket_post_type );
+
+		// create a ticket for each post and relate the ticket to the post
+		foreach ( $posts_with_tickets as $id ) {
+			$ticket_id = $this->factory()->post->create( [ 'post_type' => $ticket_post_type ] );
+			update_post_meta( $ticket_id, '_tribe_some_kind_of_ticket_for_event', $id );
+		}
+
+		$sut = $this->make_instance();
+		$ids = $sut->posts_with_tickets();
+
+		$this->assertEqualSets( $posts_with_tickets, $ids );
+	}
+
+	/**
+	 * @test
+	 * it should not list posts with tickets but unsupported
+	 */
+	public function it_should_not_list_posts_with_tickets_but_unsupported() {
+		tribe_update_option( 'ticket-enabled-post-types', [ 'page', 'post' ] );
+		register_post_type( 'unsupported_type' );
+
+		$batch_1 = $this->factory()->post->create_many( 3, [ 'post_type' => 'post' ] );
+		$batch_2 = $this->factory()->post->create_many( 3, [ 'post_type' => 'page' ] );
+		$batch_3 = $this->factory()->post->create_many( 3, [ 'post_type' => 'unsupported_type' ] );
+
+		// not relevant, any post type can be related to any other post type as "a ticket"
+		$ticket_post_type = 'ticket_type';
+		register_post_type( $ticket_post_type );
+
+		// create a ticket for each post and relate the ticket to the post
+		foreach ( array_merge( $batch_1, $batch_2, $batch_3 ) as $id ) {
+			$ticket_id = $this->factory()->post->create( [ 'post_type' => $ticket_post_type ] );
+			update_post_meta( $ticket_id, '_tribe_some_kind_of_ticket_for_event', $id );
+		}
+
+		$sut = $this->make_instance();
+		$ids = $sut->posts_with_tickets();
+
+		$this->assertEqualSets( array_merge( $batch_1, $batch_2 ), $ids );
+	}
+
+	/**
+	 * @test
+	 * it should not list posts without tickets but of unsupported
+	 * More of a sanity check here.
+	 */
+	public function it_should_not_list_posts_without_tickets_but_of_unsupported() {
+		tribe_update_option( 'ticket-enabled-post-types', [ 'page', 'post' ] );
+		register_post_type( 'unsupported_type' );
+
+		$batch_1 = $this->factory()->post->create_many( 3, [ 'post_type' => 'post' ] );
+		$batch_2 = $this->factory()->post->create_many( 3, [ 'post_type' => 'page' ] );
+		$batch_3 = $this->factory()->post->create_many( 3, [ 'post_type' => 'unsupported_type' ] );
+
+		// not relevant, any post type can be related to any other post type as "a ticket"
+		$ticket_post_type = 'ticket_type';
+		register_post_type( $ticket_post_type );
+
+		// create a ticket for each post and relate the ticket to the post
+		foreach ( array_merge( $batch_1, $batch_2 ) as $id ) {
+			$ticket_id = $this->factory()->post->create( [ 'post_type' => $ticket_post_type ] );
+			update_post_meta( $ticket_id, '_tribe_some_kind_of_ticket_for_event', $id );
+		}
+
+		$sut = $this->make_instance();
+		$ids = $sut->posts_without_tickets();
+
+		$this->assertEmpty( $ids );
+	}
+
+	/**
+	 * @test
+	 * it should only list posts that have a ticket among the supported ones
+	 */
+	public function it_should_only_list_posts_that_have_a_ticket_among_the_supported_ones() {
+		register_post_type( 'supported_type' );
+		tribe_update_option( 'ticket-enabled-post-types', [ 'supported_type' ] );
+
+		$with_tickets    = $this->factory()->post->create_many( 3, [ 'post_type' => 'supported_type' ] );
+		$without_tickets = $this->factory()->post->create_many( 3, [ 'post_type' => 'supported_type' ] );
+
+		// not relevant, any post type can be related to any other post type as "a ticket"
+		$ticket_post_type = 'ticket_type';
+		register_post_type( $ticket_post_type );
+
+		// create a ticket for each post and relate the ticket to the post
+		foreach ( $with_tickets as $id ) {
+			$ticket_id = $this->factory()->post->create( [ 'post_type' => $ticket_post_type ] );
+			update_post_meta( $ticket_id, '_tribe_some_kind_of_ticket_for_event', $id );
+		}
+
+		$sut = $this->make_instance();
+
+		$this->assertEqualSets( $with_tickets, $sut->posts_with_tickets() );
+		$this->assertEqualSets( $without_tickets, $sut->posts_without_tickets() );
+	}
+
+	/**
+	 * @test
+	 * it should correctly list posts with and without tickets across post types and ticket types
+	 */
+	public function it_should_correctly_list_posts_with_and_without_tickets_across_post_types_and_ticket_types() {
+		register_post_type( 'supported_type_1' );
+		register_post_type( 'supported_type_2' );
+		tribe_update_option( 'ticket-enabled-post-types', [ 'supported_type_1', 'supported_type_2' ] );
+
+		$with_tickets_1    = $this->factory()->post->create_many( 3, [ 'post_type' => 'supported_type_1' ] );
+		$without_tickets_1 = $this->factory()->post->create_many( 3, [ 'post_type' => 'supported_type_1' ] );
+		$with_tickets_2    = $this->factory()->post->create_many( 3, [ 'post_type' => 'supported_type_2' ] );
+		$without_tickets_2 = $this->factory()->post->create_many( 3, [ 'post_type' => 'supported_type_2' ] );
+
+		// not relevant, any post type can be related to any other post type as "a ticket"
+		$ticket_post_type_1 = 'ticket_type_1';
+		$ticket_post_type_2 = 'ticket_type_1';
+		register_post_type( $ticket_post_type_1 );
+		register_post_type( $ticket_post_type_2 );
+
+		// create a ticket for each post and relate the ticket to the post
+		// if the id of the post is even also add a ticket of the second type
+		foreach ( array_merge( $with_tickets_1, $with_tickets_2 ) as $id ) {
+			$ticket_id = $this->factory()->post->create( [ 'post_type' => $ticket_post_type_1 ] );
+			update_post_meta( $ticket_id, '_tribe_' . $ticket_post_type_1 . '_for_event', $id );
+			if ( $id % 2 === 0 ) {
+				$ticket_id = $this->factory()->post->create( [ 'post_type' => $ticket_post_type_1 ] );
+				update_post_meta( $ticket_id, '_tribe_' . $ticket_post_type_1 . '_for_event', $id );
+			}
+		}
+
+		$sut = $this->make_instance();
+
+		$this->assertEqualSets( array_merge( $with_tickets_1, $with_tickets_2 ), $sut->posts_with_tickets() );
+		$this->assertEqualSets( array_merge( $without_tickets_1, $without_tickets_2 ), $sut->posts_without_tickets() );
 	}
 
 	/**
