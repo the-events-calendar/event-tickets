dab7d559ba1afe554f5600d0995cb6eda4e8194e
Matthew Batchelder <borkweb@gmail.com>
2016-09-12 21:46:09 -0400

    Version update

--- event-tickets.php
+++ event-tickets.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Event Tickets
 Description: Event Tickets allows your guests to RSVP from any post, page, or event.
-Version: 4.3beta1
+Version: 4.3rc2
 Author: Modern Tribe, Inc.
 Author URI: http://m.tri.be/28
 License: GPLv2 or later
