e190b6fbd3bec24f1ba032afe4edcc5739f2e797
barryhughes <barry@codingkills.me>
2016-09-14 08:27:48 -0700

    Prep for release | 4.2.7

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -9,7 +9,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.2.7dev1';
+	const VERSION = '4.2.7';
 
 	/**
 	 * Min required The Events Calendar version
