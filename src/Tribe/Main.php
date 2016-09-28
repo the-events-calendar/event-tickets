1671c3e73372f648f4b2547aff0967fa637f5728
Matthew Batchelder <borkweb@gmail.com>
2016-09-28 14:58:14 -0400

    Upping version

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -9,12 +9,12 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.3rc3';
+	const VERSION = '4.3rc4';
 
 	/**
 	 * Min required The Events Calendar version
 	 */
-	const MIN_TEC_VERSION = '4.3rc3';
+	const MIN_TEC_VERSION = '4.3rc4';
 
 	/**
 	 * Name of the provider
