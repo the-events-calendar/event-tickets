4fc9cf3467b25427e29bc17d648d97dc4c203fe9
Luca Tumedei <luca@theaveragedev.com>
2016-10-12 16:14:01 +0200

    updated code and docs

--- src/Tribe/Tabbed_View/Attendee_Report_Tab.php
+++ src/Tribe/Tabbed_View/Attendee_Report_Tab.php
@@ -1,6 +1,11 @@
 <?php
 
 
+/**
+ * Class Tribe__Tickets__Tabbed_View__Attendee_Report_Tab
+ *
+ * Models the tab for the Attendees report on a post with RSVP tickets.
+ */
 class Tribe__Tickets__Tabbed_View__Attendee_Report_Tab extends Tribe__Tabbed_View__Tab {
 
 	/**
