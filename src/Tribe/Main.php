f583bb6bb206976f4f9124c375c3c9945d026d73
Matthew Batchelder <borkweb@gmail.com>
2016-10-14 09:36:16 -0400

    Setting version to 4.4dev1

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -9,12 +9,12 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.3';
+	const VERSION = '4.4dev1';
 
 	/**
 	 * Min required The Events Calendar version
 	 */
-	const MIN_TEC_VERSION = '4.3';
+	const MIN_TEC_VERSION = '4.4dev1';
 
 	/**
 	 * Name of the provider
