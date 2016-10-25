abcffd7ba290c1687854ee258108c18d44dc2033
Luca Tumedei <luca@theaveragedev.com>
2016-10-25 09:27:21 +0200

    store the column instances

--- src/Tribe/Admin/Columns.php
+++ src/Tribe/Admin/Columns.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * Class Tribe__Tickets__Admin__Columns
  *
@@ -12,6 +13,16 @@
  */
 class Tribe__Tickets__Admin__Columns {
 
+	/**
+	 * An array of ticket column objects by supported post type.
+	 *
+	 * @var Tribe__Tickets__Admin__Columns__Tickets[]
+	 */
+	protected $columns = array();
+
+	/**
+	 * Hooks the actions and filters needed to perform this class operations.
+	 */
 	public function hook() {
 		$this->add_custom_columns( (array) tribe_get_option( 'ticket-enabled-post-types', array() ) );
 	}
@@ -22,11 +33,28 @@ class Tribe__Tickets__Admin__Columns {
 		}
 
 		foreach ( $supported_types as $supported_type ) {
-			$tickets_column = new Tribe__Tickets__Admin__Columns__Tickets( $supported_type );
+			$tickets_column                   = new Tribe__Tickets__Admin__Columns__Tickets( $supported_type );
+
 			add_filter( "manage_{$supported_type}_posts_columns", array( $tickets_column, 'filter_manage_post_columns' ) );
 			add_action( "manage_{$supported_type}_posts_custom_column", array( $tickets_column, 'render_column' ), 10, 2 );
+
+			// store the column for later use
+			$this->columns[ $supported_type ] = $tickets_column;
 		}
 
 		return true;
+	}
+
+	/**
+	 * Returns the column associated with a post type.
+	 *
+	 * @param string $post_type The post type slug, e.g. 'post', 'page' or 'tribe_events'.
+	 *
+	 * @return Tribe__Tickets__Admin__Columns__Tickets|null Either the column object associated with the
+	 *                                                      post type or `null` if no column object is
+	 *                                                      associated with the post type.
+	 */
+	public function get_column_for_type( $post_type ) {
+		return isset( $this->columns[ $post_type ] ) ? $this->columns[ $post_type ] : null;
 	}
 }
