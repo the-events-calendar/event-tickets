1a22f0eeabfe13ac70d1874f7d89b2e4f5a7fa53
bordoni <bordoni.dev@gmail.com>
2016-11-03 21:42:29 -0200

    Bump to Version 4.3.3.

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -9,7 +9,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.3.2';
+	const VERSION = '4.3.3';
 
 	/**
 	 * Min required The Events Calendar version
