9d70f82369691f05f008102d558d69b7bb9a9500
barryhughes <barry@codingkills.me>
2017-06-14 10:16:12 -0700

    Provisionally set version #s to 4.5.1 | MR 17.12

--- event-tickets.php
+++ event-tickets.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Event Tickets
 Description: Event Tickets allows your guests to RSVP from any post, page, or event.
-Version: 4.4.10
+Version: 4.5.1
 Author: Modern Tribe, Inc.
 Author URI: http://m.tri.be/28
 License: GPLv2 or later
--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -5,7 +5,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.4.10';
+	const VERSION = '4.5.1';
 
 	/**
 	 * Min required The Events Calendar version
