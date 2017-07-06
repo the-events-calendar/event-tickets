4cc6cd3684c653909e5cf0ecf32f7ebdbf1c47b9
Brook <brook@eli.mn>
2017-07-06 12:11:58 -0600

    Added @since to docblock

--- src/admin-views/attendees-email.php
+++ src/admin-views/attendees-email.php
@@ -15,6 +15,7 @@ if ( ! current_user_can( 'list_users' ) ) {
  * Filters the args for the Email Users Dropdown menu
  *
  * @see wp_dropdown_users()
+ * @since TBD
  *
  * @param array $users_args Args that get passed to wp_dropdown_users()
  */
