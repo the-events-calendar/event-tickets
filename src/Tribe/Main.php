6fb720eec83459f70d0f240c9b1283eeafbe3942
Brook <brook@eli.mn>
2017-08-28 06:38:44 -0600

    Bumped version to 4.5.5

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -5,7 +5,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.5.4';
+	const VERSION = '4.5.5';
 
 	/**
 	 * Min required The Events Calendar version
