fb3e7e0fa5264a62078edf6d5a7cb91a61f0ae8b
camwyn <stephenjpage@gmail.com>
2017-08-29 15:53:23 -0400

    Correct inappropriate display of sale price field Tweaks to use new dependency functionality

--- src/resources/js/tickets.js
+++ src/resources/js/tickets.js
@@ -250,7 +250,7 @@ var ticketHeaderImage = window.ticketHeaderImage || {};
 		 * @return string
 		 */
 		function get_global_cap() {
-			return ( $global_capacity_edit.length > 0 ) ? $global_capacity_edit.val() : '';
+			return ( 0 <$global_capacity_edit.length && 0 <$global_capacity_edit.val() ) ? $global_capacity_edit.val() : '';
 		}
 
 		/**
@@ -549,6 +549,8 @@ var ticketHeaderImage = window.ticketHeaderImage || {};
 						if ( ! response ) {
 							return;
 						}
+
+						console.log(response.data);
 
 						var regularPrice = response.data.price;
 						var salePrice    = regularPrice;
