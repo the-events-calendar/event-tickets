d1e199de32e48bfe02cf503c59bfd1b297323065
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-08-30 17:35:34 +0200

    add missing escaping

--- src/admin-views/attendees-search-box.php
+++ src/admin-views/attendees-search-box.php
@@ -6,7 +6,7 @@
 ?>
 
 <p class="search-box" style="margin-bottom: 10px;">
-	<label for="s"><?php echo $text; ?>:</label>
-	<input type="search" style="float:none;" id="<?php echo $input_id; ?>" name="s" value="<?php _admin_search_query(); ?>"/>
+	<label for="s"><?php echo esc_html( $text ); ?>:</label>
+	<input type="search" style="float:none;" id="<?php echo esc_attr( $input_id ); ?>" name="s" value="<?php _admin_search_query(); ?>"/>
 	<?php submit_button( esc_html__( 'Search attendees', 'event-tickets' ), '', '', false, array( 'id' => 'search-submit' ) ); ?>
 </p>
