9a3508ab4c8247af4d8aecf06a939d17fb1f25ee
camwyn <stephenjpage@gmail.com>
2017-09-14 15:36:33 -0400

    single-quotes

--- src/admin-views/settings_admin_panel.php
+++ src/admin-views/settings_admin_panel.php
@@ -19,7 +19,7 @@
 				<p class="description"><?php esc_html_e( 'Select an image from your Media Library to display on emailed tickets. For best results, use a .jpg, .png, or .gif at least 1160px wide.', 'event-tickets' ); ?></p>
 			</div>
 			<input type="button" class="button" name="tribe_ticket_header_image" id="tribe_ticket_header_image" value="<?php esc_html_e( 'Select an Image', 'event-tickets' ); ?>" />
-			<span id="tribe_tickets_image_preview_filename" class="<?php echo ! empty( $header_filename )? esc_attr( "-active" ): ''; ?>"><span class="dashicons dashicons-format-image"></span><span class="filename"><?php echo esc_html( $header_filename ); ?></span></span>
+			<span id="tribe_tickets_image_preview_filename" class="<?php echo ! empty( $header_filename )? esc_attr( '-active' ): ''; ?>"><span class="dashicons dashicons-format-image"></span><span class="filename"><?php echo esc_html( $header_filename ); ?></span></span>
 		</div>
 		<div class="tribe-tickets-image-preview">
 				<a class="tribe_preview" id="tribe_ticket_header_preview">
