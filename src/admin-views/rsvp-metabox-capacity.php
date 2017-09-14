6ef4179993b678ee4025458679b7c15034ff3c6e
camwyn <stephenjpage@gmail.com>
2017-09-14 14:50:25 -0400

    Correct RSVP stock not displaying - change field ID Fix ticket title wrap. Had to tweak the icon positioning

--- src/admin-views/rsvp-metabox-capacity.php
+++ src/admin-views/rsvp-metabox-capacity.php
@@ -4,6 +4,6 @@
 	data-condition-is-checked
 >
 	<label for="Tribe__Tickets__RSVP_stock" class="ticket_form_label ticket_form_left"><?php esc_html_e( 'Capacity:', 'event-tickets' ); ?></label>
-	<input type='text' id='Tribe__Tickets__RSVP_stock' name='ticket_stock' class="ticket_field ticket_stock ticket_form_right" size='7' value='<?php echo esc_attr( $stock ); ?>'/>
+	<input type='text' id='Tribe__Tickets__RSVP_capacity' name='ticket_stock' class="ticket_field ticket_stock ticket_form_right" size='7' value='<?php echo esc_attr( $stock ); ?>'/>
 	<span class="tribe_soft_note ticket_form_right"><?php esc_html_e( 'Leave blank for unlimited', 'event-tickets' ); ?></span>
 </div>
