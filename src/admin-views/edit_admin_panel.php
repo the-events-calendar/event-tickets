ba6b6ed36c66447dae4da5a76bddd9df47fad256
bordoni <bordoni.dev@gmail.com>
2017-09-22 01:38:49 -0300

    Ticket Editor Add validation where required

--- src/admin-views/edit_admin_panel.php
+++ src/admin-views/edit_admin_panel.php
@@ -3,7 +3,7 @@ $timepicker_step = 30;
 $timepicker_round = '00:00:00';
 ?>
 
-<div id="tribe_panel_edit" class="ticket_panel panel_edit" aria-hidden="true" >
+<div id="tribe_panel_edit" class="ticket_panel panel_edit tribe-validation" aria-hidden="true">
 	<?php
 	/**
 	 * Allows for the insertion of additional elements into the main ticket edit panel
@@ -27,13 +27,17 @@ $timepicker_round = '00:00:00';
 					class="ticket_form_title tribe-dependent"
 					data-depends="#ticket_id"
 					data-condition-is-empty
-				><?php esc_html_e( 'Add new ticket', 'event-tickets' ); ?></h4>
+				>
+					<?php esc_html_e( 'Add new ticket', 'event-tickets' ); ?>
+				</h4>
 				<h4
 					id="ticket_title_edit"
 					class="ticket_form_title tribe-dependent"
 					data-depends="#ticket_id"
 					data-condition-is-not-empty
-				><?php esc_html_e( 'Edit ticket', 'event-tickets' ); ?></h4>
+				>
+					<?php esc_html_e( 'Edit ticket', 'event-tickets' ); ?>
+				</h4>
 			</div>
 			<div
 				class="tribe-dependent"
@@ -45,28 +49,44 @@ $timepicker_round = '00:00:00';
 					class="ticket_form_title tribe-dependent"
 					data-depends="#ticket_id"
 					data-condition-is-empty
-				><?php esc_html_e( 'Add new RSVP', 'event-tickets' ); ?></h4>
+				>
+					<?php esc_html_e( 'Add new RSVP', 'event-tickets' ); ?>
+				</h4>
 				<h4
 					id="rsvp_title_edit"
 					class="ticket_form_title tribe-dependent"
 					data-depends="#ticket_id"
 					data-condition-is-not-empty
-				><?php esc_html_e( 'Edit RSVP', 'event-tickets' ); ?></h4>
+				>
+					<?php esc_html_e( 'Edit RSVP', 'event-tickets' ); ?>
+				</h4>
 			</div>
 			<section id="ticket_form_main" class="main">
 				<div class="input_block">
 					<label class="ticket_form_label ticket_form_left" for="ticket_name"><?php esc_html_e( 'Type:', 'event-tickets' ); ?></label>
-					<input type='text' id='ticket_name' name='ticket_name' class="ticket_field ticket_form_right" size='25' value='' />
+					<input
+						type='text'
+						id='ticket_name'
+						name='ticket_name'
+						class="ticket_field ticket_form_right"
+						size='25'
+						value=''
+						data-validation-is-required
+						data-validation-error="<?php esc_attr_e( 'Ticket Type cannot be empty.', 'event-tickets' ); ?>"
+					/>
 					<span class="tribe_soft_note ticket_form_right"><?php esc_html_e( 'Ticket type name shows on the front end and emailed tickets', 'event-tickets' ); ?></span>
 				</div>
 				<fieldset id="tribe_ticket_provider_wrapper" class="input_block" aria-hidden="true" >
 					<legend class="ticket_form_label"><?php esc_html_e( 'Sell using:', 'event-tickets' ); ?></legend>
-					<?php
-					$default_module = Tribe__Tickets__Tickets::get_default_module();
-
-					foreach ( $modules as $class => $module ) {
-						?>
-						<input type="radio" name="ticket_provider" id="<?php echo esc_attr( $class . '_radio' ); ?>" value="<?php echo esc_attr( $class ); ?>" class="ticket_field ticket_provider" tabindex="-1">
+					<?php foreach ( $modules as $class => $module ) : ?>
+						<input
+							type="radio"
+							name="ticket_provider"
+							id="<?php echo esc_attr( $class . '_radio' ); ?>"
+							value="<?php echo esc_attr( $class ); ?>"
+							class="ticket_field ticket_provider"
+							tabindex="-1"
+						>
 						<span>
 							<?php
 							/**
@@ -79,10 +99,7 @@ $timepicker_round = '00:00:00';
 							echo esc_html( apply_filters( 'tribe_events_tickets_module_name', $module ) );
 							?>
 						</span>
-						<?php
-						$checked = false;
-					}
-					?>
+					<?php endforeach; ?>
 				</fieldset>
 				<?php
 				/**
@@ -216,50 +233,50 @@ $timepicker_round = '00:00:00';
 			do_action( 'tribe_events_tickets_post_accordion', $post_id );
 			?>
 			<div class="ticket_bottom">
-					<input type="hidden" name="ticket_id" id="ticket_id" class="ticket_field" />
-					<input
-						type="button"
-						id="ticket_form_save"
-						class="button-primary tribe-dependent"
-						name="ticket_form_save"
-						value="<?php esc_attr_e( 'Save ticket', 'event-tickets' ); ?>"
-						data-depends="#Tribe__Tickets__RSVP_radio"
-						data-condition-is-not-checked
-					/>
-					<input
-						type="button"
-						id="rsvp_form_save"
-						class="button-primary tribe-dependent"
-						name="ticket_form_save"
-						value="<?php esc_attr_e( 'Save RSVP', 'event-tickets' ); ?>"
-						data-depends="#Tribe__Tickets__RSVP_radio"
-						data-condition-is-checked
-					/>
-					<input type="button" id="ticket_form_cancel" class="button-secondary" name="ticket_form_cancel" value="<?php esc_attr_e( 'Cancel', 'event-tickets' ); ?>" />
+				<input type="hidden" name="ticket_id" id="ticket_id" class="ticket_field" />
+				<input
+					type="button"
+					id="ticket_form_save"
+					class="button-primary tribe-dependent tribe-validation-submit"
+					name="ticket_form_save"
+					value="<?php esc_attr_e( 'Save ticket', 'event-tickets' ); ?>"
+					data-depends="#Tribe__Tickets__RSVP_radio"
+					data-condition-is-not-checked
+				/>
+				<input
+					type="button"
+					id="rsvp_form_save"
+					class="button-primary tribe-dependent tribe-validation-submit"
+					name="ticket_form_save"
+					value="<?php esc_attr_e( 'Save RSVP', 'event-tickets' ); ?>"
+					data-depends="#Tribe__Tickets__RSVP_radio"
+					data-condition-is-checked
+				/>
+				<input type="button" id="ticket_form_cancel" class="button-secondary" name="ticket_form_cancel" value="<?php esc_attr_e( 'Cancel', 'event-tickets' ); ?>" />
 
+				<?php
+				/**
+				 * Allows for the insertion of additional content into the ticket edit form bottom (buttons) section
+				 *
+				 * @since TBD
+				 *
+				 * @param int Post ID
+				 */
+				do_action( 'tribe_events_tickets_bottom', $post_id );
+				?>
+
+				<div id="ticket_bottom_right">
 					<?php
 					/**
-					 * Allows for the insertion of additional content into the ticket edit form bottom (buttons) section
+					 * Allows for the insertion of additional content into the ticket edit form bottom (links on right) section
 					 *
 					 * @since TBD
 					 *
 					 * @param int Post ID
 					 */
-					do_action( 'tribe_events_tickets_bottom', $post_id );
+					do_action( 'tribe_events_tickets_bottom_right', $post_id );
 					?>
-
-					<div id="ticket_bottom_right">
-						<?php
-						/**
-						 * Allows for the insertion of additional content into the ticket edit form bottom (links on right) section
-						 *
-						 * @since TBD
-						 *
-						 * @param int Post ID
-						 */
-						do_action( 'tribe_events_tickets_bottom_right', $post_id );
-						?>
-					</div>
+				</div>
 			</div>
 
 		</div><!-- #ticket_form_table -->
