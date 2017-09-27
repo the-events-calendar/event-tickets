8240d7b9d310444a7d993952841e657d704f9594
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-09-27 15:58:08 +0200

    redirect to post with tickets page after registration

--- src/views/login-before-purchase.php
+++ src/views/login-before-purchase.php
@@ -12,16 +12,20 @@
 
 $login_url          = Tribe__Tickets__Tickets::get_login_url();
 $users_can_register = (bool) get_option( 'users_can_register' );
+$register_message = null;
+$registration_url = null;
+
 if ( ! $users_can_register ) {
-	$login_message = _x( 'Login before purchasing', 'Login link on Tribe Commerce checkout page', 'event-tickets-plus' );
+	$login_message = _x( 'Login before purchasing', 'Login link on Tribe Commerce checkout page', 'event-tickets' );
 } else {
-	$login_message = _x( 'Login', 'Login link on Tribe Commerce checkout page, shown as an alternative to the registration link', 'event-tickets-plus' );
+	$login_message    = _x( 'Login', 'Login link on Tribe Commerce checkout page, shown as an alternative to the registration link', 'event-tickets' );
+	$register_message = _x( 'create an account', 'Registration link on Tribe Commerce checkout page, shown as an alternative the login link', 'event-tickets' );
+	$registration_url = Tribe__Tickets__Tickets::get_registration_url();
 }
-$register_message = _x( 'register', 'Registration link on Tribe Commerce checkout page, shown as an alternative the login link', 'event-tickets-plus' );
 ?>
 
 <a href="<?php echo esc_attr( $login_url ); ?>"><?php echo esc_html( $login_message ); ?></a>
 
 <?php if ( $users_can_register ) : ?>
-	or <a href="<?php echo esc_attr( $login_url ); ?>"><?php echo esc_html( $register_message ); ?></a> before purchasing
+	or <a href="<?php echo esc_attr( $registration_url ); ?>"><?php echo esc_html( $register_message ); ?></a> before purchasing
 <?php endif ?>
