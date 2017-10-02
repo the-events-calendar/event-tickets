2d78b6b13694cbdf51d5926276c26bc1f3ae6c50
barryhughes <barry@codingkills.me>
2017-10-02 16:00:18 -0700

    Setup 4.5.7 | M17.20

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -5,7 +5,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.5.6';
+	const VERSION = '4.5.7';
 
 	/**
 	 * Min required The Events Calendar version
