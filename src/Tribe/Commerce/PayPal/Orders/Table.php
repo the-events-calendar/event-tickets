42e9f8cba32785275d7b6b5b7c41a220bfe61994
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-10-16 14:54:26 +0200

    scaffold the Orders Table class

--- src/Tribe/Commerce/PayPal/Orders/Table.php
+++ src/Tribe/Commerce/PayPal/Orders/Table.php
@@ -1,16 +1,3 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: luca
- * Date: 16/10/2017
- * Time: 11:47
- */
-
-class Tribe__Tickets__Commerce__PayPal__Orders__Table {
-
-	/**
-	 * Tribe__Tickets__Commerce__PayPal__Orders__Table constructor.
-	 */
-	public function __construct() {
-	}
+class Tribe__Tickets__Commerce__PayPal__Orders__Table extends WP_List_Table {
 }
