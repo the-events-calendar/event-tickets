a38f2a807df9e37c21e6f411006d7b4e25f51789
camwyn <stephenjpage@gmail.com>
2017-11-13 13:22:23 -0500

    i8n

--- src/admin-views/tickets-advanced.php
+++ src/admin-views/tickets-advanced.php
@@ -52,7 +52,7 @@
 				data-step="<?php echo esc_attr( $timepicker_step ); ?>"
 				data-round="<?php echo esc_attr( $timepicker_round ); ?>"
 				value=""
-				aria-label="Ticket start date"
+				aria-label="<?php esc_html_e( 'Ticket start date', 'event-tickets' ); ?>"
 			/>
 			<span class="helper-text hide-if-js"><?php esc_html_e( 'HH:MM', 'event-tickets' ) ?></span>
 			<span class="dashicons dashicons-editor-help" title="<?php esc_attr_e( 'If you do not set a start sale date, tickets will be available immediately.', 'event-tickets' ); ?>"></span>
@@ -81,7 +81,7 @@
 				data-step="<?php echo esc_attr( $timepicker_step ); ?>"
 				data-round="<?php echo esc_attr( $timepicker_round ); ?>"
 				value=""
-				aria-label="Ticket end date"
+				aria-label="aria-label="<?php esc_html_e( 'Ticket end date', 'event-tickets' ); ?>""
 			/>
 			<span class="helper-text hide-if-js"><?php esc_html_e( 'HH:MM', 'event-tickets' ) ?></span>
 			<span
