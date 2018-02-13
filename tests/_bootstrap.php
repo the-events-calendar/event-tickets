f0e386856e45e0f263e55eca6e194620e4f20708
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-02-13 16:06:06 +0100

    refactor `Ticket_Maker` test class

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -7,3 +7,4 @@ tad\FunctionMocker\FunctionMocker::init();
 
 Codeception\Util\Autoload::addNamespace( 'Tribe__Events__WP_UnitTestCase', __DIR__ . '/_support' );
 Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', __DIR__ . '/_support' );
+Codeception\Util\Autoload::addNamespace( 'Tribe\Tickets\Test', __DIR__ . '/_support' );
---
+++ tests/_support/Commerce/PayPal/Ticket_Maker.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tribe\Tickets\Test\Commerce\PayPal;
+
+
+trait Ticket_Maker {
+
+	/**
+	 * Generates a ticket for a post.
+	 *
+	 * @param       int $post_id
+	 * @param       int $price
+	 * @param array     $meta_input An array of values to override the `meta_input` entry (see method code)
+	 *
+	 * @return mixed
+	 */
+	protected function make_ticket( $post_id, $price, array $overrides = [] ) {
+		if ( ! isset( $this->factory ) || ! $this->factory instanceof \WP_UnitTest_Factory ) {
+			throw new \RuntimeException( 'This trait should be used in classes that extend the \\Codeception\\TestCase\\WPTestCase class' );
+		}
+
+		$meta_input = is_array( $overrides['meta_input'] ) ? $overrides['meta_input'] : array();
+		unset( $overrides['meta_input'] );
+
+		$ticket_id = $this->factory()->post->create( array_merge(
+				[
+					'post_title'   => "Test Ticket for {$post_id}",
+					'post_content' => "Ticket for {$post_id}",
+					'post_excerpt' => "Ticket for {$post_id}",
+					'post_type'    => tribe( 'tickets.commerce.paypal' )->ticket_object,
+					'meta_input'   => array_merge( [
+						'_tribe_tpp_for_event'                           => $post_id,
+						'_price'                                         => $price,
+						'_stock'                                         => 100,
+						'_capacity'                                      => 100,
+						'_manage_stock'                                  => 'yes',
+						'_ticket_start_date'                             => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
+						'_ticket_end_date'                               => date( 'Y-m-d H:i:s', strtotime( '+1 day' ) ),
+						\Tribe__Tickets__Global_Stock::TICKET_STOCK_MODE => 'own',
+					], $meta_input ),
+				], $overrides )
+		);
+
+		return $ticket_id;
+	}
+}
