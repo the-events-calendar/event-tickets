e9d0dcdf5f1433be499cb3a19cf41c37a20ae65d
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-04-25 12:46:08 +0200

    add the `setTribeOption` helper method to the functional helper

--- tests/_support/Helper/Functional.php
+++ tests/_support/Helper/Functional.php
@@ -1,10 +1,28 @@
 <?php
+
 namespace Helper;
 
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-class Functional extends \Codeception\Module
-{
+use Codeception\Module\WPDb;
 
+class Functional extends \Codeception\Module {
+	/**
+	 * Sets an option in the tribe option row.
+	 *
+	 * @param string $key
+	 * @param string|array $value
+	 */
+	public function setTribeOption( $key, $value ) {
+		$option_name = 'tribe_events_calendar_options';
+		/** @var WPDb $db */
+		$db      = $this->getModule( 'WPDb' );
+		$options = $db->grabOptionFromDatabase( $option_name );
+		if ( empty( $options ) ) {
+			$db->haveOptionInDatabase( $option_name, [ $key => $value ] );
+		} else {
+			$db->haveOptionInDatabase( $option_name, array_merge( $options, [ $key => $value ] ) );
+		}
+	}
 }
