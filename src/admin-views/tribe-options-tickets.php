933ff23f4755837c3afd7ea70819dbb9a429ff71
barryhughes <barry@codingkills.me>
2018-04-27 16:23:27 -0700

    Switch from esc_html() to esc_html__() | spotfix

--- src/admin-views/tribe-options-tickets.php
+++ src/admin-views/tribe-options-tickets.php
@@ -244,7 +244,7 @@ $paypal_fields            = array(
 			esc_html__( 'PayPal configuration status:' ),
 			esc_attr( tribe( 'tickets.commerce.paypal.handler.ipn' )->get_config_status( 'slug' ) ),
 			esc_html( tribe( 'tickets.commerce.paypal.handler.ipn' )->get_config_status( 'label' ) ),
-			esc_html( 'For help creating and configuring your account, call PayPal at 1-844-720-4038 (USA)', 'event-tickets' )
+			esc_html__( 'For help creating and configuring your account, call PayPal at 1-844-720-4038 (USA)', 'event-tickets' )
 		),
 		'size'            => 'large',
 		'default'         => 'no',
