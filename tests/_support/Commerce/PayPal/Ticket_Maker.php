b9b64054fad8da31baa2a6486a0ef28b3516fd91
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-05-24 17:57:01 +0200

    do not rely on smart casting

--- tests/_support/Commerce/PayPal/Ticket_Maker.php
+++ tests/_support/Commerce/PayPal/Ticket_Maker.php
@@ -20,25 +20,23 @@ trait Ticket_Maker {
 		$meta_input = isset( $overrides['meta_input'] ) && is_array( $overrides['meta_input'] ) ? $overrides['meta_input'] : array();
 		unset( $overrides['meta_input'] );
 
-		$ticket_id = $factory()->post->create( array_merge(
-				[
-					'post_title'   => "Test Ticket for {$post_id}",
-					'post_content' => "Ticket for {$post_id}",
-					'post_excerpt' => "Ticket for {$post_id}",
-					'post_type'    => tribe( 'tickets.commerce.paypal' )->ticket_object,
-					'meta_input'   => array_merge( [
-						'_tribe_tpp_for_event'                           => $post_id,
-						'_price'                                         => $price,
-						'_stock'                                         => 100,
-						'_capacity'                                      => 100,
-						'_manage_stock'                                  => 'yes',
-						'_ticket_start_date'                             => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
-						'_ticket_end_date'                               => date( 'Y-m-d H:i:s', strtotime( '+1 day' ) ),
-						\Tribe__Tickets__Global_Stock::TICKET_STOCK_MODE => 'own',
-					], $meta_input ),
-				], $overrides )
-		);
+		$defaults  = [
+			'post_title'   => "Test Ticket for {$post_id}",
+			'post_content' => "Ticket for {$post_id}",
+			'post_excerpt' => "Ticket for {$post_id}",
+			'post_type'    => tribe( 'tickets.commerce.paypal' )->ticket_object,
+			'meta_input'   => array_merge( [
+				'_tribe_tpp_for_event'                           => $post_id,
+				'_price'                                         => $price,
+				'_stock'                                         => 100,
+				'_capacity'                                      => 100,
+				'_manage_stock'                                  => 'yes',
+				'_ticket_start_date'                             => date( 'Y-m-d H:i:s', strtotime( '-1 day' ) ),
+				'_ticket_end_date'                               => date( 'Y-m-d H:i:s', strtotime( '+1 day' ) ),
+				\Tribe__Tickets__Global_Stock::TICKET_STOCK_MODE => 'own',
+			], $meta_input ),
+		];
 
-		return $ticket_id;
+		return $factory->post->create( array_merge( $defaults, $overrides ) );
 	}
 }
