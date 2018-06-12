518976c67e14023c7303e6b65a2e5bbe6671843c
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-06-12 09:10:17 +0200

    add more messages

--- src/Tribe/REST/V1/Messages.php
+++ src/Tribe/REST/V1/Messages.php
@@ -21,7 +21,10 @@ class Tribe__Tickets__REST__V1__Messages implements Tribe__REST__Messages_Interf
 			'attendee-not-accessible'     => __( 'The requested attendee is not accessible', 'event-tickets' ),
 			'attendee-check-in-not-found' => __( 'The requested attendee check in is not available', 'event-tickets' ),
 			'ticket-provider-not-found'   => __( 'The ticket provider for the requested ticket is not available', 'event-tickets' ),
-			'ticket-post-not-found'      => __( 'The post associated with the requested ticket was not found', 'post-tickets' ),
+			'ticket-post-not-found'       => __( 'The post associated with the requested ticket was not found', 'event-tickets' ),
+			'ticket-object-not-found'     => __( 'The requested ticket object could not be built or found', 'event-tickets' ),
+			'error-global-id-generation'  => __( 'The ticket global id could not be generated', 'event-tickets' ),
+			'error-ticket-post'           => __( 'There was a problem while fetching the requested ticket post', 'event-tickets' )
 		);
 	}
 
