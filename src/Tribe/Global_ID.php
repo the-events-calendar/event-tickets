efe53d51b4659795e14907b4f2a78d636f1a0548
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-06-12 09:10:06 +0200

    add a ticket-dedicated global ID class

---
+++ src/Tribe/Global_ID.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class Tribe__Tickets__Global_ID
+ *
+ * @since TBD
+ */
+class Tribe__Tickets__Global_ID extends Tribe__Utils__Global_ID {
+
+	/**
+	 * @var array An array of ticket types considered valid.
+	 */
+	protected $valid_types = array(
+		'rsvp',
+	);
+}
