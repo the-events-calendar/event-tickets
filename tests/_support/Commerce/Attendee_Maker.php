f2c843d869a6526c47492a397cb8c37e333e16fb
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-06-19 11:56:11 +0200

    require the `post_id` parameter when creating attendees

--- tests/_support/Commerce/Attendee_Maker.php
+++ tests/_support/Commerce/Attendee_Maker.php
@@ -12,16 +12,17 @@ trait Attendee_Maker {
 	 *
 	 * @param int   $count
 	 * @param int   $ticket_id
+	 * @param int   $post_id
 	 * @param array $overrides See single generation method code for overrides possibilities.
 	 *
 	 * @return array An array of generated attendees post IDs.
 	 */
-	protected function create_many_attendees_for_ticket( $count, $ticket_id, array $overrides = [] ) {
+	protected function create_many_attendees_for_ticket( int $count, int $ticket_id,int $post_id, array $overrides = [] ):array {
 
 		$attendes = [];
 
 		for ( $i = 0; $i < $count; $i ++ ) {
-			$attendes[] = $this->create_attendee_for_ticket( $ticket_id, $overrides );
+			$attendes[] = $this->create_attendee_for_ticket( $ticket_id,$post_id, $overrides );
 		}
 
 		return $attendes;
@@ -31,32 +32,35 @@ trait Attendee_Maker {
 	 * Generates an attendee for a ticket.
 	 *
 	 * @param int   $ticket_id
+	 * @param int   $post_id
 	 * @param array $overrides See code for overrides possibilities.
 	 *
 	 * @return int The generated attendee
 	 */
-	protected function create_attendee_for_ticket( $ticket_id, array $overrides = array() ) {
+	protected function create_attendee_for_ticket( int $ticket_id, int $post_id, array $overrides = array() ): int {
 		$faker = \Faker\Factory::create();
 
 		/** @var \Tribe__Tickets__Tickets $provider */
 		$provider            = tribe_tickets_get_ticket_provider( $ticket_id );
 		$provider_reflection = new \ReflectionClass( $provider );
 
+		$post_key        = $provider_reflection->getConstant( 'ATTENDEE_EVENT_KEY' );
 		$product_key     = $provider->attendee_product_key ?: $provider_reflection->getConstant( 'ATTENDEE_PRODUCT_KEY' );
 		$optout_key      = $provider->attendee_optout_key ?: $provider_reflection->getConstant( 'ATTENDEE_OPTOUT_KEY' );
 		$user_id_key     = $provider->attendee_user_id ?: $provider_reflection->getConstant( 'ATTENDEE_USER_ID' );
 		$ticket_sent_key = $provider->attendee_ticket_sent ?: $provider_reflection->getConstant( 'ATTENDEE_TICKET_SENT' );
 
 		$meta = [
-			$provider->checkin_key   => (bool) Arr::get( $overrides, 'checkin', false ),
+			$provider->checkin_key              => (bool) Arr::get( $overrides, 'checkin', false ),
 			$provider->checkin_key . '_details' => Arr::get( $overrides, 'checkin_details', false ),
-			$provider->security_code => Arr::get( $overrides, 'security_code', md5( time() ) ),
-			$product_key             => $ticket_id,
-			$optout_key              => Arr::get( $overrides, 'optout', false ),
-			$user_id_key             => Arr::get( $overrides, 'user_id', 0 ),
-			$ticket_sent_key         => Arr::get( $overrides, 'ticket_sent', true ),
-			$provider->full_name     => Arr::get( $overrides, 'full_name', $faker->name ),
-			$provider->email         => Arr::get( $overrides, 'email', $faker->email ),
+			$provider->security_code            => Arr::get( $overrides, 'security_code', md5( time() ) ),
+			$post_key                           => $post_id,
+			$product_key                        => $ticket_id,
+			$optout_key                         => Arr::get( $overrides, 'optout', false ),
+			$user_id_key                        => Arr::get( $overrides, 'user_id', 0 ),
+			$ticket_sent_key                    => Arr::get( $overrides, 'ticket_sent', true ),
+			$provider->full_name                => Arr::get( $overrides, 'full_name', $faker->name ),
+			$provider->email                    => Arr::get( $overrides, 'email', $faker->email ),
 		];
 
 		if ( $provider instanceof \Tribe__Tickets__RSVP ) {
