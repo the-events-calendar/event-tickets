2375f20c971732cff1858d31afb618693114f79d
Juanfra Aldasoro <juan@nicethemes.com>
2018-07-02 18:25:08 -0300

    Privacy Policy: Modify template so only the contents that we want are copied to the clipboard.

--- src/admin-views/privacy.php
+++ src/admin-views/privacy.php
@@ -13,17 +13,18 @@ $suggested_text = $descr ? '<strong>' . esc_html__( 'Suggested text:', 'event-ti
 ?>
 <div class="tribe-tickets-privacy">
 
-	<p><?php esc_html_e( 'Hello,', 'event-tickets' ); ?></p>
-	<p><?php esc_html_e( 'This information serves as a guide on what sections need to be modified due to usage of Event Tickets and its Add-ons.', 'event-tickets' ); ?></p>
-	<p><?php esc_html_e( 'You should include the information below in the correct sections of you privacy policy.', 'event-tickets' ); ?></p>
-	<p><strong> <?php esc_html_e( 'Disclaimer:', 'event-tickets' ); ?></strong> <?php esc_html_e( 'This information is only for guidance and not to be considered as legal advice.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php esc_html_e( 'Hello,', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php esc_html_e( 'This information serves as a guide on what sections need to be modified due to usage of Event Tickets and its Add-ons.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php esc_html_e( 'You should include the information below in the correct sections of you privacy policy.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><strong> <?php esc_html_e( 'Disclaimer:', 'event-tickets' ); ?></strong> <?php esc_html_e( 'This information is only for guidance and not to be considered as legal advice.', 'event-tickets' ); ?></p>
 
 	<h2><?php esc_html_e( 'What personal data we collect and why we collect it', 'event-tickets' ); ?></h2>
 
 	<h3><?php esc_html_e( 'Event, Attendee, and Ticket Purchaser Information', 'event-tickets' ); ?></h3>
 
-	<p><?php esc_html_e( 'Through the usage of Event Tickets, Event Tickets Plus, and Community Tickets, information may be collected and stored within your website’s database.', 'event-tickets' ); ?></p>
-	<p><?php echo $suggested_text . esc_html__( 'If you create, submit, import, save, or publish event ticket information, as well as RSVP or purchase tickets to events, such information is retained in the local database:', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php esc_html_e( 'Through the usage of Event Tickets, Event Tickets Plus, and Community Tickets, information may be collected and stored within your website’s database.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php echo $suggested_text; ?></p>
+	<p><?php esc_html_e( 'If you create, submit, import, save, or publish event ticket information, as well as RSVP or purchase tickets to events, such information is retained in the local database:', 'event-tickets' ); ?></p>
 
 	<ol>
 		<li><?php esc_html_e( 'Attendees information (RSVPs and Tickets): name and email address', 'event-tickets' ); ?></li>
@@ -36,22 +37,24 @@ $suggested_text = $descr ? '<strong>' . esc_html__( 'Suggested text:', 'event-ti
 
 	<h3><?php esc_html_e( 'API Keys', 'event-tickets' ); ?></h3>
 
-	<p><?php esc_html_e( 'Events Tickets suite offers the use of third-party API keys. The primary functions are to enhance the features we\'ve built in, some of which use Google Maps and PayPal. These API keys are not supplied by Modern Tribe.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php esc_html_e( 'Events Tickets suite offers the use of third-party API keys. The primary functions are to enhance the features we\'ve built in, some of which use Google Maps and PayPal. These API keys are not supplied by Modern Tribe.', 'event-tickets' ); ?></p>
 
-	<p><?php echo $suggested_text . esc_html__( 'We make use of certain API keys, in order to provide specific features.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php echo $suggested_text; ?></p>
+
+	<p><?php esc_html_e( 'We make use of certain API keys, in order to provide specific features.', 'event-tickets' ); ?></p>
 
 	<p><?php esc_html_e( 'These API keys may include the following third party services: Google Maps and PayPal.', 'event-tickets' ); ?></p>
 
-	<h3><?php esc_html_e( 'How Long You Retain this Data', 'event-tickets' ); ?></h3>
+	<h3 class="privacy-policy-tutorial"><?php esc_html_e( 'How Long You Retain this Data', 'event-tickets' ); ?></h3>
 
-	<p><?php esc_html_e( 'All information (data) is retained in the local database indefinitely, unless otherwise deleted.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php esc_html_e( 'All information (data) is retained in the local database indefinitely, unless otherwise deleted.', 'event-tickets' ); ?></p>
 
-	<p><?php esc_html_e( 'Certain data may be exported or removed upon users request via the existing Exporter or Eraser. Please note, however, that several “edge cases” exist in which we are unable to perfect the gathering and export of all data for your end users. We suggest running a search in your local database, as well as within the WordPress Dashboard, in order to identify all data collected and stored for your specific user requests.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php esc_html_e( 'Certain data may be exported or removed upon users request via the existing Exporter or Eraser. Please note, however, that several “edge cases” exist in which we are unable to perfect the gathering and export of all data for your end users. We suggest running a search in your local database, as well as within the WordPress Dashboard, in order to identify all data collected and stored for your specific user requests.', 'event-tickets' ); ?></p>
 
-	<h3><?php esc_html_e( 'Where We Send Your Data', 'event-tickets' ); ?></h3>
+	<h3 class="privacy-policy-tutorial"><?php esc_html_e( 'Where We Send Your Data', 'event-tickets' ); ?></h3>
 
-	<p><?php esc_html_e( 'Modern Tribe does not send any user data outside of your website by default.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php esc_html_e( 'Modern Tribe does not send any user data outside of your website by default.', 'event-tickets' ); ?></p>
 
-	<p><?php esc_html_e( 'If you have extended our plugin(s) to send data to a third-party service such as Eventbrite, Google Maps, or PayPal, user information may be passed to these external services. These services may be located abroad.', 'event-tickets' ); ?></p>
+	<p class="privacy-policy-tutorial"><?php esc_html_e( 'If you have extended our plugin(s) to send data to a third-party service such as Eventbrite, Google Maps, or PayPal, user information may be passed to these external services. These services may be located abroad.', 'event-tickets' ); ?></p>
 
 </div>
