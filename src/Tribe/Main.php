23c68377ba74c137edf319519a0d1ab14310ae90
Juanfra Aldasoro <juanfra@tri.be>
2018-07-05 19:33:16 -0300

    Update required common version to latest

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -14,7 +14,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Min required version of Tribe Common
 	 */
-	const MIN_COMMON_VERSION = '4.7.16';
+	const MIN_COMMON_VERSION = '4.7.17';
 
 	/**
 	 * Name of the provider
