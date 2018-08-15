2bb5f439a69a0ea6f592459f328f45f1b59297e6
jesseeproductions <info@jesseeproductions.com>
2018-08-15 14:17:24 -0400

    #108891 - add base classes in woocommerce and trigger status method

--- src/Tribe/Status/Manager.php
+++ src/Tribe/Status/Manager.php
@@ -31,38 +31,66 @@ class Tribe__Tickets__Status__Manager {
 	 *
 	 * @var array
 	 */
-	 protected $woo_statues = array();
+	protected $status_managers = array(
+		//'EDD' => 'Tribe__Tickets_Plus__Commerce__WooCommerce__Status_Manager',
+		//'RSVP' => 'Tribe__Tickets_Plus__Commerce__WooCommerce__Status_Manager',
+		//'Tribe Commerce' => 'Tribe__Tickets_Plus__Commerce__WooCommerce__Status_Manager',
+		'WooCommerce' => 'Tribe__Tickets_Plus__Commerce__WooCommerce__Status_Manager',
+	);
+
 
 	/**
-	 * An array of status objects for EDD Tickets
+	 * An array of status objects for WooCommerce Tickets
 	 *
 	 * @var array
 	 */
-	 protected $edd_statues = array();
+	protected $statues = array();
 
 	/**
-	 * An array of status objects for Tribe Commerce Tickets
+	 * Static Singleton Holder
 	 *
-	 * @var array
+	 * @var self
 	 */
-	 protected $tpp_statues = array();
+	protected $instance;
 
 	/**
-	 * An array of status objects for RSVP
+	 * Get (and instantiate, if necessary) the instance of the class
 	 *
-	 * @var array
+	 * @return self
 	 */
-	 protected $rsvp_statues = array();
+	public function instance() {
+		if ( ! $this->instance ) {
+			$this->instance = new self;
+		}
 
-
-	/**
-	 * Class constructor
-	 */
-	public function __construct() {
-		$this->active_modules = Tribe__Tickets__Tickets::modules();
-		//$this->setup_data();
+		return $this->instance;
 	}
 
+
+	public function hook() {
+		add_action( 'init', array( $this, 'setup' ), 10 );
+	}
+
+	public function setup() {
+		$this->active_modules = Tribe__Tickets__Tickets::modules();
+		$this->get_statuses_by_provider();
+
+		//log_me( $this->statues );
+	}
+
+	protected function get_statuses_by_provider() {
+
+		foreach ( $this->active_modules as $module_class => $module_name ) {
+
+			if ( ! isset( $this->status_managers[ $module_name ] ) ) {
+				continue;
+			}
+
+			$status_class                  = $this->status_managers[ $module_name ];
+			$this->statues[ $module_name ] = new $status_class();
+		}
+
+	}
 
 	/**
 	 * Gets the ticket provider class when passed an id
@@ -73,8 +101,31 @@ class Tribe__Tickets__Status__Manager {
 	 *
 	 * @return bool|object
 	 */
-	protected function get_ticket_provider( $id ) {
+	protected function get_provider_by_id( $id ) {
 		return tribe( 'tickets.data_api' )->get_ticket_provider( $id );
+	}
+
+	public function get_trigger_statuses( $commerce ) {
+
+		$trigger_statuses = array();
+
+		if ( ! isset( $this->statues[ $commerce ]->statuses ) ) {
+			return $trigger_statuses;
+		}
+
+		$filtered_statuses = wp_list_filter(
+			$this->statues[ $commerce ]->statuses,
+			array(
+		    'trigger_option' => true
+			)
+		);
+
+		foreach ( $filtered_statuses as $status ) {
+			$trigger_statuses[ $status->provider_name ] = $status->name;
+		}
+
+		return $trigger_statuses;
+
 	}
 
 }
