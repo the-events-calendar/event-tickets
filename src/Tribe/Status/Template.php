0499b3a9bb2c8085e70770fe21f75c6fb2e5f66f
jesseeproductions <info@jesseeproductions.com>
2018-08-15 17:12:25 -0400

    #108892 - add abstract status template and add all woocommerce statues

---
+++ src/Tribe/Status/Template.php
@@ -0,0 +1,27 @@
+<?php
+
+
+/**
+ * Class Tribe__Tickets__Status__Template
+ *
+ * @since TBD
+ *
+ */
+abstract class Tribe__Tickets__Status__Template {
+
+	public $name                = '';
+	public $provider_name       = '';
+	public $post_type           = '';
+	public $incomplete          = false;
+	public $warning             = false;
+	public $trigger_option      = false;
+	public $attendee_generation = false;
+	public $attendee_dispatch   = false;
+	public $stock_reduced       = false;
+	public $count_attendee      = false;
+	public $count_sales         = false;
+	public $count_completed     = false;
+	public $count_canceled      = false;
+	public $count_refunded      = false;
+
+}
