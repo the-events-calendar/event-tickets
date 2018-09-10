bbbdd5f01917eee88957f954e74e845abf7d9ed1
jesseeproductions <info@jesseeproductions.com>
2018-09-10 12:00:20 -0400

    M18.13 - update min version

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -14,7 +14,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Min required version of Tribe Common
 	 */
-	const MIN_COMMON_VERSION = '4.7.19';
+	const MIN_COMMON_VERSION = '4.7.20';
 
 	/**
 	 * Name of the provider
