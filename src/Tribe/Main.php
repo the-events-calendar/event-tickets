8df8353ada67d92cd16c819ef3cc53f0bd411a22
George Gecewicz <gecewicz.george@gmail.com>
2018-09-10 19:26:54 -0400

    setting up m18.14 branch

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -4,7 +4,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.8.1';
+	const VERSION = '4.8.2';
 
 	/**
 	 * Min required The Events Calendar version
