c954e224fce5500a41f9cb08029fbbf3c6937007
Juanfra Aldasoro <juanfraa@gmail.com>
2018-11-01 19:20:00 +0100

    Add an "RSVP Cart" implementation

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -229,6 +229,10 @@ class Tribe__Tickets__Main {
 		tribe_singleton( 'tickets.commerce.currency', 'Tribe__Tickets__Commerce__Currency', array( 'hook' ) );
 		tribe_singleton( 'tickets.commerce.paypal', new Tribe__Tickets__Commerce__PayPal__Main );
 		tribe_singleton( 'tickets.redirections', 'Tribe__Tickets__Redirections' );
+		tribe_singleton( 'tickets.rsvp.cart', 'Tribe__Tickets__RSVP__Cart' );
+
+		// Setup the cart here to avoid warnings
+		tribe( 'tickets.rsvp.cart' )->setup();
 
 		// REST API v1
 		tribe_register_provider( 'Tribe__Tickets__REST__V1__Service_Provider' );
