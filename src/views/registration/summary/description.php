da48fbe00463ed0a6b1176ab7c1b9b9f4ab1178c
Juanfra Aldasoro <juanfraa@gmail.com>
2018-11-04 17:11:06 +0100

    Update docblock, add check to see if TEC is active. We'll need to move this function to common probably.

--- src/views/registration/summary/description.php
+++ src/views/registration/summary/description.php
@@ -1,11 +1,14 @@
 <?php
 /**
- * This template renders the purchase summary description
+ * This template renders the event summary description
+ * for the registration page
  *
  * @version TBD
  *
  */
 ?>
+<?php if ( class_exists( 'Tribe__Events__Main' ) ) : ?>
 <div class="tribe-block__tickets__registration__description">
 	<?php echo tribe_events_event_schedule_details( $event_id ); ?>
 </div>
+<?php endif; ?>
