594d480bd526fb319b325365a8194401e6b790fc
mitogh <hello@crisoforo.com>
2018-11-14 21:27:58 -0600

    Move sagas to avoid duplicate execution

--- src/modules/data/index.js
+++ src/modules/data/index.js
@@ -5,8 +5,10 @@ import reducer from './reducers';
 
 import { actions, constants } from '@moderntribe/common/data/plugins';
 import { store } from '@moderntribe/common/store';
+import sagas from './sagas';
 
 export const initStore = () => {
+	sagas();
 	const { dispatch, injectReducers } = store;
 	const { TICKETS } = constants;
 	dispatch( actions.addPlugin( TICKETS ) );
