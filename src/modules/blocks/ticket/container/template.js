79d093e4fa56fa8a81158d45f6ff2d4435bc5192
Paul <kim.paul.ms@gmail.com>
2018-11-28 05:54:10 +0800

    fix import to ticket container

--- src/modules/blocks/ticket/container/template.js
+++ src/modules/blocks/ticket/container/template.js
@@ -9,7 +9,7 @@ import PropTypes from 'prop-types';
  */
 import { ContainerPanel } from '@moderntribe/tickets/elements';
 import TicketContainerHeader from '@moderntribe/tickets/blocks/ticket/container-header/template';
-import TicketContainerContent from '@moderntribe/tickets/blocks/ticket/container-content/template';
+import TicketContainerContent from '@moderntribe/tickets/blocks/ticket/container-content/container';
 import { LAYOUT } from '@moderntribe/tickets/elements/container-panel/element';
 import {
 	ClockActive,
