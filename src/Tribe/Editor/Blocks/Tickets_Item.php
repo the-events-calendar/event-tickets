3b7783ada6f8b742b88f69500e6c785704a3cf8b
mitogh <hello@crisoforo.com>
2018-11-28 21:11:59 -0600

    Add Block Ticket Item registration
    Register the render mechanism for the Ticket Item, used to populate the
    template argument "tickets" used to have access to the set of tickets
    used to render the block.
    
    So every ticket injected respects the order in which they are presented
    into the BE.

---
+++ src/Tribe/Editor/Blocks/Tickets_Item.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Tickets block Setup
+ */
+class Tribe__Tickets__Editor__Blocks__Tickets_Item extends Tribe__Editor__Blocks__Abstract {
+
+	/**
+	 * Which is the name/slug of this block
+	 *
+	 * @since TBD
+	 *
+	 * @return string
+	 */
+	public function slug() {
+		return 'tickets-item';
+	}
+
+	/**
+	 * Since we are dealing with a Dynamic type of Block we need a PHP method to render it
+	 *
+	 * @since TBD
+	 *
+	 * @param  array $attributes
+	 *
+	 * @return string
+	 */
+	public function render( $attributes = array() ) {
+		/** @var Tribe__Tickets__Editor__Template $template */
+		$template = tribe( 'tickets.editor.template' );
+
+		if (
+			empty( $attributes['ticketId'] )
+			|| empty( $attributes['hasBeenCreated'] )
+			|| $attributes['hasBeenCreated'] === false
+		) {
+			return;
+		}
+
+		$ticket_post = get_post( $attributes['ticketId'] );
+		// Prevent to attach blocks with tickets removed or under trash
+		if ( ! $ticket_post instanceof WP_Post || 'publish' !== $ticket_post->post_status ) {
+			return;
+		}
+
+		$tickets = $template->get( 'tickets', array(), false );
+		$ticket  = Tribe__Tickets__Tickets::load_ticket_object( $attributes['ticketId'] );
+		$args    = array(
+			'tickets' => array_merge( $tickets, array( $ticket ) )
+		);
+
+		// Add the rendering attributes into global context
+		$template->add_template_globals( $args );
+	}
+}
