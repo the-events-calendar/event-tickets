9b685d5022ee0745fd1359cc36aae9fc3f06e2ac
Juanfra Aldasoro <juanfraa@gmail.com>
2018-12-07 10:50:15 -0300

    Fix logic and rename variable to make it more readable.

--- src/Tribe/Commerce/PayPal/Frontend/Tickets_Form.php
+++ src/Tribe/Commerce/PayPal/Frontend/Tickets_Form.php
@@ -82,9 +82,15 @@ class Tribe__Tickets__Commerce__PayPal__Frontend__Tickets_Form {
 		}
 
 		$must_login = ! is_user_logged_in() && $this->main->login_required();
-		
-		// Controls the visibility of the "Log it before purchasing" link below the tickets form
-		$can_login  = apply_filters( 'tribe_tickets_show_login_before_purchasing_link', true );
+
+		/**
+		 * Controls the visibility of the "Log it before purchasing" link below the tickets form
+		 * for TPP tickets
+		 *
+		 * @since TBD
+		 *
+		 */
+		$display_login_link = apply_filters( 'tribe_tickets_show_login_before_purchasing_link', true );
 
 		ob_start();
 		include $this->main->getTemplateHierarchy( 'tickets/tpp' );
