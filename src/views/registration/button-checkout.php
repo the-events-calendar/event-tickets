ba75f3586868ccef61bc45dd0f91b4fe1ae39ebe
Paul <kim.paul.ms@gmail.com>
2019-01-03 00:01:38 +0800

    add confirm dialog to checkout button if there are changes

--- src/views/registration/button-checkout.php
+++ src/views/registration/button-checkout.php
@@ -11,12 +11,12 @@ if ( ! $checkout_url ) {
  ?>
 <?php if ( ! $cart_has_required_meta || $is_meta_up_to_date ) : ?>
 	<form
-		class="tribe-block__tickets__checkout"
+		class="tribe-block__tickets__registration__checkout"
 		action="<?php echo esc_url( $checkout_url ); ?>"
 		method="post"
 	>
 		<input type="hidden" name="tribe_tickets_checkout" value="1" />
-		<button type="submit" class="alignright button-primary tribe-block__tickets__checkout__submit">
+		<button type="submit" class="alignright button-primary tribe-block__tickets__registration__checkout__submit">
 			<?php esc_html_e( 'Checkout', 'event-tickets' ); ?>
 		</button>
 	</form>
