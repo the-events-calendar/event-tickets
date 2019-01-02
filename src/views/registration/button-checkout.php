399e476b051708fa219c7ed5dbcb380b224df09a
Paul <kim.paul.ms@gmail.com>
2019-01-02 22:47:14 +0800

    add classes to checkout form and button

--- src/views/registration/button-checkout.php
+++ src/views/registration/button-checkout.php
@@ -10,8 +10,14 @@ if ( ! $checkout_url ) {
 }
  ?>
 <?php if ( ! $cart_has_required_meta || $is_meta_up_to_date ) : ?>
-	<form action="<?php echo esc_url( $checkout_url ); ?>" method="post">
+	<form
+		class="tribe-block__tickets__checkout"
+		action="<?php echo esc_url( $checkout_url ); ?>"
+		method="post"
+	>
 		<input type="hidden" name="tribe_tickets_checkout" value="1" />
-		<button type="submit" class="alignright button-primary"><?php esc_html_e( 'Checkout', 'event-tickets' ); ?></button>
+		<button type="submit" class="alignright button-primary tribe-block__tickets__checkout__submit">
+			<?php esc_html_e( 'Checkout', 'event-tickets' ); ?>
+		</button>
 	</form>
 <?php endif;
