8ebbf8edb8f601dbad2cc703e9c5562739611fe2
Stephen Page <stephen@tri.be>
2019-01-03 13:31:58 -0500

    Rearrange the block as requested, fix bug in login link [120365]

--- src/views/blocks/rsvp/form/submit-login.php
+++ src/views/blocks/rsvp/form/submit-login.php
@@ -13,8 +13,10 @@
  * @version 4.9.3
  *
  */
-
+$ticket   = $this->get( 'ticket' );
+$ticket_data = tribe( 'tickets.handler' )->get_object_connections( $ticket->ID );
+$event_id    = $ticket_data->event;
 ?>
-<a href="<?php echo esc_url( Tribe__Tickets__Tickets::get_login_url() ); ?>">
+<a href="<?php echo esc_url( Tribe__Tickets__Tickets::get_login_url( $event_id ) ); ?>">
 	<?php esc_html_e( 'Login to RSVP', 'events-tickets' ); ?>
 </a>
