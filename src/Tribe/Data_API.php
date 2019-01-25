4fef90a334fb31a2fd176cb2cdfd02c222c44711
Scott Kingsley Clark <lol@scottkclark.com>
2019-01-25 10:26:22 -0500

    use a var for teh empty calls
    Co-Authored-By: Camwyn <stephen@tri.be>

--- src/Tribe/Data_API.php
+++ src/Tribe/Data_API.php
@@ -250,6 +250,7 @@ class Tribe__Tickets__Data_API {
 		if ( ! isset( $services['class'] ) ) {
 			$products = $this->get_product_ids_from_tickets( Tribe__Tickets__Tickets::get_all_event_tickets( $post_id ) );
 		}
+		$has_products = ! empty( $products );
 
 		// if no product ids and id is not ticket related return false
 		$is_ticket_related = array_intersect( array( 'order', 'ticket', 'attendee', 'product' ), $services );
