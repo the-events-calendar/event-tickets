4702e0626ce3adf47a1e4fe16fb1cd41a8ec2835
Paul <kim.paul.ms@gmail.com>
2019-02-14 01:24:45 +0800

    add tooltip for attendee registration icon, style

--- src/modules/blocks/ticket/container-header/title/template.js
+++ src/modules/blocks/ticket/container-header/title/template.js
@@ -13,6 +13,7 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
+import { Tooltip } from '@moderntribe/common/elements';
 import { Clipboard, Pencil } from '@moderntribe/common/icons';
 import './style.pcss';
 
@@ -23,33 +24,47 @@ const TicketContainerHeaderTitle = ( {
 	onTempTitleChange,
 	tempTitle,
 	title,
-} ) => (
-	<div className="tribe-editor__ticket__container-header-title">
-		{
-			isSelected
-				? (
-					<Fragment>
-						<AutosizeInput
-							className="tribe-editor__ticket__container-header-title-input"
-							value={ tempTitle }
-							placeholder={ __( 'Ticket Type', 'event-tickets' ) }
-							onChange={ onTempTitleChange }
-							disabled={ isDisabled }
-						/>
-						{ hasAttendeeInfoFields && <Clipboard /> }
-					</Fragment>
-				)
-				: (
-					<Fragment>
-						<h3 className="tribe-editor__ticket__container-header-title-label">
-							{ title }
-						</h3>
-						<Pencil />
-					</Fragment>
-				)
-		}
-	</div>
-);
+} ) => {
+	const clipboard = hasAttendeeInfoFields && (
+		<Tooltip
+			labelClassName="tribe-editor__ticket__container-header-clipboard-tooltip"
+			label={ <Clipboard /> }
+			text={ __(
+				'This ticket has Additional Fields configured.',
+				'event-tickets',
+			) }
+		/>
+	);
+
+	return (
+		<div className="tribe-editor__ticket__container-header-title">
+			{
+				isSelected
+					? (
+						<Fragment>
+							<AutosizeInput
+								className="tribe-editor__ticket__container-header-title-input"
+								value={ tempTitle }
+								placeholder={ __( 'Ticket Type', 'event-tickets' ) }
+								onChange={ onTempTitleChange }
+								disabled={ isDisabled }
+							/>
+							{ clipboard }
+						</Fragment>
+					)
+					: (
+						<Fragment>
+							<h3 className="tribe-editor__ticket__container-header-title-label">
+								{ title }
+							</h3>
+							{ clipboard }
+							<Pencil />
+						</Fragment>
+					)
+			}
+		</div>
+	);
+}
 
 TicketContainerHeaderTitle.propTypes = {
 	hasAttendeeInfoFields: PropTypes.bool,
