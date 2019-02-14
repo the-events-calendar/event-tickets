aed8aa26d8a4aa2e824ff17045b75fc13592dde9
Scott Kingsley Clark <lol@scottkclark.com>
2019-02-14 10:55:06 -0500

    Update src/admin-views/tribe-options-tickets.php
    use correct text domain
    
    Co-Authored-By: Camwyn <stephen@tri.be>

--- src/admin-views/tribe-options-tickets.php
+++ src/admin-views/tribe-options-tickets.php
@@ -95,7 +95,7 @@ $tickets_fields = array_merge( $tickets_fields, array(
 		),
 		'ticket-attendee-info-template' => array(
 			'type'            => 'dropdown',
-			'label'           => __( 'Attendee Registration template', 'the-events-calendar' ),
+			'label'           => __( 'Attendee Registration template', 'event-tickets' ),
 			'tooltip'         => __( 'Choose a page template to control the appearance of your attendee registration page.', 'event-tickets' ),
 			'validation_type' => 'options',
 			'size'            => 'large',
