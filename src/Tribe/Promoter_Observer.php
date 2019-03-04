dd1d335da03513a26b309f6d1b605c9e1b477924
mitogh <hello@crisoforo.com>
2019-03-04 17:46:06 -0600

    Update docblock

--- src/Tribe/Promoter_Observer.php
+++ src/Tribe/Promoter_Observer.php
@@ -1,5 +1,19 @@
 <?php
+
+/**
+ * Class Tribe__Tickets__Promoter_Observer
+ *
+ * Class used to observe hooks and actions happening to notify promoter of those actions.
+ *
+ * @since TBD
+ */
 class Tribe__Tickets__Promoter_Observer {
+
+	/**
+	 * Hooks on which this obseverver notifies promoter
+	 *
+	 * @since TBD
+	 */
 	public function hook() {
 		$this->registered_types();
 		// RSVP
@@ -22,6 +36,7 @@ class Tribe__Tickets__Promoter_Observer {
 	 * to attach a hook to `save_post_tribe_events` only attaches the action
 	 * if the post has supoort for tickets or if the support has been enabled for this type.
 	 *
+	 * @since TBD
 	 */
 	public function registered_types() {
 		$event_type = class_exists( 'Tribe__Events__Main' )
@@ -38,14 +53,27 @@ class Tribe__Tickets__Promoter_Observer {
 		add_action( 'save_post_' . $event_type, array( $this, 'notify' ), 10, 1 );
 	}
 
+	/**
+	 * Wrapper when the $post_id is passed as second argument of the hook
+	 *
+	 * @since TBD
+	 *
+	 * @param $ticket_id int The ID of the ticket
+	 * @param $event_id int The ID of the post/event
+	 */
 	public function notify_event_id( $ticket_id, $event_id ) {
 		$this->notify( $event_id );
 	}
 
+	/**
+	 * Function used to notify the promoter endpoint of a new change on an event
+	 *
+	 * @param $post_id int The ID of the post
+	 */
 	public function notify( $post_id ) {
 		/** @var Tribe__Promoter__Connector $connector */
 		$connector = tribe( 'promoter.connector' );
-		
+
 		if ( ! $connector ) {
 			return;
 		}
