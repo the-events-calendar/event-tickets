28da1f3a20da6ca45468757ed18c872258ea905f
Paul <kim.paul.ms@gmail.com>
2019-04-09 02:22:13 +0800

    update rsvp block icons

--- src/views/blocks/rsvp/status/going-icon.php
+++ src/views/blocks/rsvp/status/going-icon.php
@@ -11,13 +11,33 @@
  * @link {INSERT_ARTICLE_LINK_HERE}
  *
  * @since 4.9
- * @version 4.9.4
+ * @version TBD
  *
  */
 
 ?>
-<svg width="34" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 25">
-	<g data-name="Layer 2">
-		<path fill="#aeb4bb" fill-rule="evenodd" d="M12 24.357L.822 13.178l2.356-2.356L12 19.643 30.822.822l2.356 2.356" data-name="Layer 1"/>
-	</g>
+<svg width="18px" height="13px" viewBox="0 0 18 13" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+        <polyline id="path-1" points="7.49999986 16.1783338 1.91083332 10.589167 3.08916663 9.41083362 7.49999986 13.821667 16.910833 4.41083352 18.0891663 5.58916688"></polyline>
+    </defs>
+    <g id="RSVP-Enhancement" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Desktop-Default" transform="translate(-298.000000, -744.000000)">
+            <g id="FE/Ticket/RSVP/Large:Default" transform="translate(95.000000, 606.000000)">
+                <g id="Button/Going:Default" transform="translate(114.000000, 122.000000)">
+                    <g id="Gridicon-/-checkmark" transform="translate(88.000000, 12.000000)">
+                        <g id="gridicons-checkmark">
+                            <rect id="Rectangle-path" x="0" y="0" width="20" height="20"></rect>
+                            <mask id="mask-2" fill="white">
+                                <use xlink:href="#path-1"></use>
+                            </mask>
+                            <use id="Shape" fill="#000000" fill-rule="nonzero" xlink:href="#path-1"></use>
+                            <g id="color-dark-gray-900" mask="url(#mask-2)" fill="#A2AAB2">
+                                <rect id="light-gray-900" x="0" y="0" width="20" height="20"></rect>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </g>
+    </g>
 </svg>
