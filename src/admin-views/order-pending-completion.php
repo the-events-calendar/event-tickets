b3a678b6528e9ca4d16a8e040f46ffbf4f0b9e86
Scott Kingsley Clark <lol@scottkclark.com>
2019-04-18 12:16:19 -0400

    Update src/admin-views/order-pending-completion.php
    Co-Authored-By: jesseeproductions <jesseeproductions@users.noreply.github.com>

--- src/admin-views/order-pending-completion.php
+++ src/admin-views/order-pending-completion.php
@@ -4,7 +4,7 @@
  */
 ?>
 
-<?php echo esc_html__( 'Pending Order Completion refers to anything with the following statuses:', 'event-tickets' ); ?>
+<?php esc_html_e( 'Pending Order Completion refers to anything with the following statuses:', 'event-tickets' ); ?>
 <ul class="tooltip-list">
 	<li><?php echo implode( '</li><li>', $incomplete_statuses ) ?></li>
 </ul>
