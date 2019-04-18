f75e96b9be3629052ab8b1dff093eca32663c2fd
Scott Kingsley Clark <lol@scottkclark.com>
2019-04-18 12:16:14 -0400

    Update src/admin-views/order-available.php
    Co-Authored-By: jesseeproductions <jesseeproductions@users.noreply.github.com>

--- src/admin-views/order-available.php
+++ src/admin-views/order-available.php
@@ -12,6 +12,6 @@
 </p>
 <ul class="tooltip-list">
 	<li><?php esc_html_e( 'Inventory is the capacity minus generated attendees for that ticket.', 'event-tickets' ); ?></li>
-	<li><?php echo esc_html__( 'Stock is the lowest number of ticket stock or if active the shared stock.', 'event-tickets' ); ?></li>
+	<li><?php esc_html_e( 'Stock is the lowest number of ticket stock or if active the shared stock.', 'event-tickets' ); ?></li>
 	<li><?php esc_html_e( 'Capacity is based on the chosen shared, shared capped, or individual capacity for this ticket.', 'event-tickets' ); ?></li>
 </ul>
