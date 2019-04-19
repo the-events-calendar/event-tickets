5484f358a4f764f2abb82cab44c33069078faabd
Scott Kingsley Clark <lol@scottkclark.com>
2019-04-19 12:15:04 -0500

    Add more tests for Attendees Table class

--- tests/wpunit/Tribe/Tickets/Attendees_TableTest.php
+++ tests/wpunit/Tribe/Tickets/Attendees_TableTest.php
@@ -54,26 +54,26 @@ class Attendees_TableTest extends \Codeception\TestCase\WPTestCase {
 	}
 
 	/**
-	 * It should allow fetching ticket attendees.
+	 * It should allow fetching ticket attendees by event.
 	 *
 	 * @test
 	 */
-	public function should_allow_fetching_attendees() {
+	public function should_allow_fetching_attendees_by_event() {
 		$post_id  = $this->factory->post->create();
 		$post_id2 = $this->factory->post->create();
 
 		$paypal_ticket_id = $this->create_paypal_ticket( $post_id, 1 );
 		$rsvp_ticket_id   = $this->create_rsvp_ticket( $post_id );
 
-		$paypal_attendee_ids = $this->create_many_attendees_for_ticket( 5, $paypal_ticket_id, $post_id );
-		$rsvp_attendee_ids   = $this->create_many_attendees_for_ticket( 5, $rsvp_ticket_id, $post_id );
+		$paypal_attendee_ids = $this->create_many_attendees_for_ticket( 25, $paypal_ticket_id, $post_id );
+		$rsvp_attendee_ids   = $this->create_many_attendees_for_ticket( 25, $rsvp_ticket_id, $post_id );
 
 		// Add other ticket/attendees for another post so we can confirm we only returned the correct attendees.
 		$paypal_ticket_id2 = $this->create_paypal_ticket( $post_id2, 1 );
 		$rsvp_ticket_id2   = $this->create_rsvp_ticket( $post_id2 );
 
-		$paypal_attendee_ids2 = $this->create_many_attendees_for_ticket( 5, $paypal_ticket_id2, $post_id2 );
-		$rsvp_attendee_ids2   = $this->create_many_attendees_for_ticket( 5, $rsvp_ticket_id2, $post_id2 );
+		$paypal_attendee_ids2 = $this->create_many_attendees_for_ticket( 25, $paypal_ticket_id2, $post_id2 );
+		$rsvp_attendee_ids2   = $this->create_many_attendees_for_ticket( 25, $rsvp_ticket_id2, $post_id2 );
 
 		$sut = $this->make_instance();
 
@@ -81,15 +81,173 @@ class Attendees_TableTest extends \Codeception\TestCase\WPTestCase {
 		$sut->prepare_items();
 		$attendee_ids = wp_list_pluck( $sut->items, 'attendee_id' );
 
-		$this->assertEqualSets( array_merge( $paypal_attendee_ids, $rsvp_attendee_ids ), $attendee_ids );
+		$expected_attendee_ids = array_slice( array_merge( $paypal_attendee_ids, $rsvp_attendee_ids ), 0, $sut->get_pagination_arg( 'per_page' ) );
+
+		$this->assertEqualSets( $expected_attendee_ids, $attendee_ids );
 		$this->assertEquals( count( array_merge( $paypal_attendee_ids, $rsvp_attendee_ids ) ), $sut->get_pagination_arg( 'total_items' ) );
 
 		$_GET['event_id'] = $post_id2;
 		$sut->prepare_items();
 		$attendee_ids2 = wp_list_pluck( $sut->items, 'attendee_id' );
 
-		$this->assertEqualSets( array_merge( $paypal_attendee_ids2, $rsvp_attendee_ids2 ), $attendee_ids2 );
+		$expected_attendee_ids2 = array_slice( array_merge( $paypal_attendee_ids2, $rsvp_attendee_ids2 ), 0, $sut->get_pagination_arg( 'per_page' ) );
+
+		$this->assertEqualSets( $expected_attendee_ids2, $attendee_ids2 );
 		$this->assertEquals( count( array_merge( $paypal_attendee_ids2, $rsvp_attendee_ids2 ) ), $sut->get_pagination_arg( 'total_items' ) );
+	}
+
+	/**
+	 * It should allow fetching ticket attendees by name search.
+	 *
+	 * @test
+	 */
+	public function should_allow_fetching_attendees_by_name_search() {
+		$post_id  = $this->factory->post->create();
+
+		$rsvp_name_meta_key = tribe( 'tickets.rsvp' )->full_name;
+
+		$paypal_ticket_id = $this->create_paypal_ticket( $post_id, 1 );
+		$rsvp_ticket_id   = $this->create_rsvp_ticket( $post_id );
+
+		$paypal_attendee_ids = $this->create_many_attendees_for_ticket( 5, $paypal_ticket_id, $post_id );
+		$rsvp_attendee_ids   = $this->create_many_attendees_for_ticket( 5, $rsvp_ticket_id, $post_id );
+
+		$search_attendee_id = current( array_slice( $rsvp_attendee_ids, 0, 1 ) );
+
+		update_post_meta( $search_attendee_id, $rsvp_name_meta_key, 'Rob Tester' );
+
+		$sut = $this->make_instance();
+
+		$_REQUEST['s'] = 'rob';
+
+		$_GET['event_id'] = $post_id;
+		$sut->prepare_items();
+		$attendee_ids = wp_list_pluck( $sut->items, 'attendee_id' );
+
+		$this->assertEqualSets( [ $search_attendee_id ], $attendee_ids );
+		$this->assertEquals( 1, $sut->get_pagination_arg( 'total_items' ) );
+	}
+
+	/**
+	 * It should allow fetching ticket attendees by email search.
+	 *
+	 * @test
+	 */
+	public function should_allow_fetching_attendees_by_email_search() {
+		$post_id  = $this->factory->post->create();
+
+		$rsvp_email_meta_key = tribe( 'tickets.rsvp' )->email;
+
+		$paypal_ticket_id = $this->create_paypal_ticket( $post_id, 1 );
+		$rsvp_ticket_id   = $this->create_rsvp_ticket( $post_id );
+
+		$paypal_attendee_ids = $this->create_many_attendees_for_ticket( 5, $paypal_ticket_id, $post_id );
+		$rsvp_attendee_ids   = $this->create_many_attendees_for_ticket( 5, $rsvp_ticket_id, $post_id );
+
+		$search_attendee_id = current( array_slice( $rsvp_attendee_ids, 0, 1 ) );
+
+		update_post_meta( $search_attendee_id, $rsvp_email_meta_key, 'rob@likestests.com' );
+
+		$sut = $this->make_instance();
+
+		$_REQUEST['s'] = 'likestests';
+
+		$_GET['event_id'] = $post_id;
+		$sut->prepare_items();
+		$attendee_ids = wp_list_pluck( $sut->items, 'attendee_id' );
+
+		$this->assertEqualSets( [ $search_attendee_id ], $attendee_ids );
+		$this->assertEquals( 1, $sut->get_pagination_arg( 'total_items' ) );
+	}
+
+	/**
+	 * It should allow fetching ticket attendees by ticket search.
+	 *
+	 * @test
+	 */
+	public function should_allow_fetching_attendees_by_ticket_search() {
+		$post_id  = $this->factory->post->create();
+
+		$paypal_ticket_id = $this->create_paypal_ticket( $post_id, 1 );
+		$rsvp_ticket_id   = $this->create_rsvp_ticket( $post_id );
+
+		$paypal_attendee_ids = $this->create_many_attendees_for_ticket( 5, $paypal_ticket_id, $post_id );
+		$rsvp_attendee_ids   = $this->create_many_attendees_for_ticket( 5, $rsvp_ticket_id, $post_id );
+
+		$sut = $this->make_instance();
+
+		$_REQUEST['s'] = $rsvp_ticket_id;
+
+		$_GET['event_id'] = $post_id;
+		$sut->prepare_items();
+		$attendee_ids = wp_list_pluck( $sut->items, 'attendee_id' );
+
+		$this->assertEqualSets( $rsvp_attendee_ids, $attendee_ids );
+		$this->assertEquals( count( $rsvp_attendee_ids ), $sut->get_pagination_arg( 'total_items' ) );
+	}
+
+	/**
+	 * It should allow fetching ticket attendees by security code search.
+	 *
+	 * @test
+	 */
+	public function should_allow_fetching_attendees_by_security_code_search() {
+		$post_id  = $this->factory->post->create();
+
+		$rsvp_security_code_meta_key = tribe( 'tickets.rsvp' )->security_code;
+
+		$paypal_ticket_id = $this->create_paypal_ticket( $post_id, 1 );
+		$rsvp_ticket_id   = $this->create_rsvp_ticket( $post_id );
+
+		$paypal_attendee_ids = $this->create_many_attendees_for_ticket( 5, $paypal_ticket_id, $post_id );
+		$rsvp_attendee_ids   = $this->create_many_attendees_for_ticket( 5, $rsvp_ticket_id, $post_id );
+
+		$search_attendee_id = current( array_slice( $rsvp_attendee_ids, 0, 1 ) );
+
+		update_post_meta( $search_attendee_id, $rsvp_security_code_meta_key, 'robba' );
+
+		$sut = $this->make_instance();
+
+		$_REQUEST['s'] = 'robba';
+
+		$_GET['event_id'] = $post_id;
+		$sut->prepare_items();
+		$attendee_ids = wp_list_pluck( $sut->items, 'attendee_id' );
+
+		$this->assertEqualSets( [ $search_attendee_id ], $attendee_ids );
+		$this->assertEquals( 1, $sut->get_pagination_arg( 'total_items' ) );
+	}
+
+	/**
+	 * It should allow fetching ticket attendees by user search.
+	 *
+	 * @test
+	 */
+	public function should_allow_fetching_attendees_by_user_search() {
+		$post_id  = $this->factory->post->create();
+
+		$rsvp_user_meta_key = tribe( 'tickets.rsvp' )->attendee_user_id;
+
+		$paypal_ticket_id = $this->create_paypal_ticket( $post_id, 1 );
+		$rsvp_ticket_id   = $this->create_rsvp_ticket( $post_id );
+
+		$paypal_attendee_ids = $this->create_many_attendees_for_ticket( 5, $paypal_ticket_id, $post_id );
+		$rsvp_attendee_ids   = $this->create_many_attendees_for_ticket( 5, $rsvp_ticket_id, $post_id );
+
+		$search_attendee_id = current( array_slice( $rsvp_attendee_ids, 0, 1 ) );
+
+		update_post_meta( $search_attendee_id, $rsvp_user_meta_key, '1234567890' );
+
+		$sut = $this->make_instance();
+
+		$_REQUEST['s'] = '1234';
+
+		$_GET['event_id'] = $post_id;
+		$sut->prepare_items();
+		$attendee_ids = wp_list_pluck( $sut->items, 'attendee_id' );
+
+		$this->assertEqualSets( [ $search_attendee_id ], $attendee_ids );
+		$this->assertEquals( 1, $sut->get_pagination_arg( 'total_items' ) );
 	}
 
 }
