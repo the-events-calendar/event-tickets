4ac93acf815ec57826d1399ab5a359120e3da28e
Stephen Page <stephen@tri.be>
2019-05-01 13:01:46 -0400

    Tweak a few more.

--- src/admin-views/order-available.php
+++ src/admin-views/order-available.php
@@ -10,8 +10,3 @@
 <p>
 	<a href="m.tri.be/1aek"><?php esc_html_e( 'Learn more about how Availability is calculated.', 'event-tickets' ); ?></a>
 </p>
-<ul class="tooltip-list">
-	<li><?php esc_html_e( 'Inventory is the capacity minus generated attendees for that ticket.', 'event-tickets' ); ?></li>
-	<li><?php esc_html_e( 'Stock is the lowest number of ticket stock or if active the shared stock.', 'event-tickets' ); ?></li>
-	<li><?php esc_html_e( 'Capacity is based on the chosen shared, shared capped, or individual capacity for this ticket.', 'event-tickets' ); ?></li>
-</ul>
