73ed05481117d6cd53eea2eeec878c7030946988
Stephen Page <stephen@tri.be>
2019-05-02 15:05:00 -0400

    Fix a string and format the Orders tab availability tooltip.

--- src/Tribe/Abstract_Attendance_Totals.php
+++ src/Tribe/Abstract_Attendance_Totals.php
@@ -73,7 +73,7 @@ abstract class Tribe__Tickets__Abstract_Attendance_Totals {
 	 * @return string a string of html for the tooltip
 	 */
 	public function get_total_completed_tooltip() {
-		$message = _x( 'This pertains to Orders that have been marked Completed.', 'total complete tooltip', 'event-tickets' );
+		$message = _x( 'Includes ticketed attendees with orders marked Completed.', 'total complete tooltip', 'event-tickets' );
 		$args    = [ 'classes' => 'required' ];
 
 		return tribe( 'tooltip.view' )->render_tooltip( $message, $args );
