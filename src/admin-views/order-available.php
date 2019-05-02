ab698023da3664cd208e2cba15f8658d577a533e
Scott Kingsley Clark <lol@scottkclark.com>
2019-05-01 22:19:19 -0500

    Fix doc link and target
    https://central.tri.be/issues/126342

--- src/admin-views/order-available.php
+++ src/admin-views/order-available.php
@@ -8,5 +8,5 @@
 	<?php echo esc_html__( 'Availability for this ticket type is counted using', 'event-tickets' ) . ', ' . esc_html( array_search( min( $available ), $available ) . ' - ' . min( $available ) ); ?>
 </div>
 <p>
-	<a href="m.tri.be/1aek"><?php esc_html_e( 'Learn more about how Availability is calculated.', 'event-tickets' ); ?></a>
+	<a href="https://m.tri.be/1aek" target="_blank"><?php esc_html_e( 'Learn more about how Availability is calculated.', 'event-tickets' ); ?></a>
 </p>
