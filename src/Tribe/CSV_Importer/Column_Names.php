c51c59ad8a6109963c88765acf167faa2b83502b
Scott Kingsley Clark <lol@scottkclark.com>
2019-05-12 07:11:14 -0500

    Clean up formatting

--- src/Tribe/CSV_Importer/Column_Names.php
+++ src/Tribe/CSV_Importer/Column_Names.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Tribe__Tickets__CSV_Importer__Column_Names {
 
 	/**
@@ -29,21 +28,20 @@ class Tribe__Tickets__CSV_Importer__Column_Names {
 	 * @return array
 	 */
 	public function filter_rsvp_column_names( array $column_names ) {
-        $column_names = array_merge( $column_names,
-            array(
-                'event_name'              => esc_html__( 'Event Name or ID or Slug', 'event-tickets' ),
-                'ticket_name'             => esc_html__( 'Ticket Name', 'event-tickets' ),
-                'ticket_description'      => esc_html__( 'Ticket Description', 'event-tickets' ),
-                'ticket_show_description' => esc_html__( 'Ticket Show Description', 'event-tickets' ),
-                'ticket_start_sale_date'  => esc_html__( 'Ticket Start Sale Date', 'event-tickets' ),
-                'ticket_start_sale_time'  => esc_html__( 'Ticket Start Sale Time', 'event-tickets' ),
-                'ticket_end_sale_date'    => esc_html__( 'Ticket End Sale Date', 'event-tickets' ),
-                'ticket_end_sale_time'    => esc_html__( 'Ticket End Sale Time', 'event-tickets' ),
-                'ticket_stock'            => esc_html__( 'Ticket Stock', 'event-tickets' ),
-                'ticket_capacity'         => esc_html__( 'Ticket Capacity', 'event-tickets' ),
-            ) );
+		$column_names = array_merge( $column_names, [
+				'event_name'              => esc_html__( 'Event Name or ID or Slug', 'event-tickets' ),
+				'ticket_name'             => esc_html__( 'Ticket Name', 'event-tickets' ),
+				'ticket_description'      => esc_html__( 'Ticket Description', 'event-tickets' ),
+				'ticket_show_description' => esc_html__( 'Ticket Show Description', 'event-tickets' ),
+				'ticket_start_sale_date'  => esc_html__( 'Ticket Start Sale Date', 'event-tickets' ),
+				'ticket_start_sale_time'  => esc_html__( 'Ticket Start Sale Time', 'event-tickets' ),
+				'ticket_end_sale_date'    => esc_html__( 'Ticket End Sale Date', 'event-tickets' ),
+				'ticket_end_sale_time'    => esc_html__( 'Ticket End Sale Time', 'event-tickets' ),
+				'ticket_stock'            => esc_html__( 'Ticket Stock', 'event-tickets' ),
+				'ticket_capacity'         => esc_html__( 'Ticket Capacity', 'event-tickets' ),
+			] );
 
-        return $column_names;
+		return $column_names;
 	}
 
 	/**
@@ -53,8 +51,9 @@ class Tribe__Tickets__CSV_Importer__Column_Names {
 	 *
 	 * @return array
 	 */
-	public function filter_rsvp_column_mapping( $mapping ) {
-		$mapping['rsvp_tickets'] = get_option( 'tribe_events_import_column_mapping_rsvp_tickets', array() );
+	public function filter_rsvp_column_mapping( array $mapping ) {
+		$mapping['rsvp_tickets'] = get_option( 'tribe_events_import_column_mapping_rsvp_tickets', [] );
+
 		return $mapping;
 	}
 }
