95de2dfbe286dc669ba780d0e50157445e52a136
rafsuntaskin <rafsun.aiub@gmail.com>
2019-05-12 17:40:03 +0600

    Fixed text domains

--- src/Tribe/CSV_Importer/Column_Names.php
+++ src/Tribe/CSV_Importer/Column_Names.php
@@ -29,21 +29,21 @@ class Tribe__Tickets__CSV_Importer__Column_Names {
 	 * @return array
 	 */
 	public function filter_rsvp_column_names( array $column_names ) {
-		$column_names = array_merge( $column_names,
-			array(
-				'event_name'             => esc_html__( 'Event Name or ID or Slug', 'event-tickets' ),
-				'ticket_name'            => esc_html__( 'Ticket Name', 'event-tickets' ),
-				'ticket_description'     => esc_html__( 'Ticket Description', 'event-tickets' ),
-				'ticket_show_description' => esc_html__( 'Ticket Show Description', 'event-tickets-plus' ),
-				'ticket_start_sale_date' => esc_html__( 'Ticket Start Sale Date', 'event-tickets' ),
-				'ticket_start_sale_time' => esc_html__( 'Ticket Start Sale Time', 'event-tickets' ),
-				'ticket_end_sale_date'   => esc_html__( 'Ticket End Sale Date', 'event-tickets' ),
-				'ticket_end_sale_time'   => esc_html__( 'Ticket End Sale Time', 'event-tickets' ),
-				'ticket_stock'           => esc_html__( 'Ticket Stock', 'event-tickets' ),
-				'ticket_capacity'        => esc_html__( 'Ticket Capacity', 'event-tickets' ),
-			) );
+        $column_names = array_merge( $column_names,
+            array(
+                'event_name'              => esc_html__( 'Event Name or ID or Slug', 'event-tickets' ),
+                'ticket_name'             => esc_html__( 'Ticket Name', 'event-tickets' ),
+                'ticket_description'      => esc_html__( 'Ticket Description', 'event-tickets' ),
+                'ticket_show_description' => esc_html__( 'Ticket Show Description', 'event-tickets' ),
+                'ticket_start_sale_date'  => esc_html__( 'Ticket Start Sale Date', 'event-tickets' ),
+                'ticket_start_sale_time'  => esc_html__( 'Ticket Start Sale Time', 'event-tickets' ),
+                'ticket_end_sale_date'    => esc_html__( 'Ticket End Sale Date', 'event-tickets' ),
+                'ticket_end_sale_time'    => esc_html__( 'Ticket End Sale Time', 'event-tickets' ),
+                'ticket_stock'            => esc_html__( 'Ticket Stock', 'event-tickets' ),
+                'ticket_capacity'         => esc_html__( 'Ticket Capacity', 'event-tickets' ),
+            ) );
 
-		return $column_names;
+        return $column_names;
 	}
 
 	/**
