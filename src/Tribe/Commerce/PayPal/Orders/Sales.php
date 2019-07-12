7075cbcb30dc8dc02b0e21c8d217742b1f91e7ad
Clifford Paulick <tko@tourkick.com>
2019-07-12 18:45:05 -0500

    If unlimited, don't need to worry about Global Stock, since it'd also be unlimited

--- src/Tribe/Commerce/PayPal/Orders/Sales.php
+++ src/Tribe/Commerce/PayPal/Orders/Sales.php
@@ -363,7 +363,10 @@ class Tribe__Tickets__Commerce__PayPal__Orders__Sales {
 	 * @return int
 	 */
 	public function filter_available( $available, Tribe__Tickets__Ticket_Object $ticket, $sold, $stock ) {
-		if ( 'Tribe__Tickets__Commerce__PayPal__Main' !== $ticket->provider_class ) {
+		if (
+			'Tribe__Tickets__Commerce__PayPal__Main' !== $ticket->provider_class
+			|| -1 === $available
+		) {
 			return $available;
 		}
 
