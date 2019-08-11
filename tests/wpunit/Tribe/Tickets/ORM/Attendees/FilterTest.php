76ee7b7fa9c49c69406ab652b672d6339d59548d
Clifford Paulick <tko@tourkick.com>
2019-08-11 14:57:44 -0500

    Change Message so no need for debug

--- tests/wpunit/Tribe/Tickets/ORM/Attendees/FilterTest.php
+++ tests/wpunit/Tribe/Tickets/ORM/Attendees/FilterTest.php
@@ -19,9 +19,6 @@ class FilterTest extends ORMTestCase {
 	 * @dataProvider get_attendee_test_matrix
 	 */
 	public function test_attendees_orm_filters( $method ) {
-		// Tell codeception which filter we are testing. You can see these by adding -vvv when running tests.
-		codecept_debug( $method );
-
 		list( $filter_name, $filter_arguments, $assertions ) = $this->$method();
 
 		// Setup attendees.
@@ -40,10 +37,10 @@ class FilterTest extends ORMTestCase {
 		$attendees->by( ...$args );
 
 		// Assert that we get what we expected.
-		$this->assertEquals( $assertions['get_ids'], $attendees->get_ids(), $filter_name );
-		$this->assertEquals( $assertions['all'], $attendees->all(), $filter_name );
-		$this->assertEquals( $assertions['count'], $attendees->count(), $filter_name );
-		$this->assertEquals( $assertions['found'], $attendees->found(), $filter_name );
+		$this->assertEquals( $assertions['get_ids'], $attendees->get_ids(), $method );
+		$this->assertEquals( $assertions['all'], $attendees->all(), $method );
+		$this->assertEquals( $assertions['count'], $attendees->count(), $method );
+		$this->assertEquals( $assertions['found'], $attendees->found(), $method );
 	}
 
 }
