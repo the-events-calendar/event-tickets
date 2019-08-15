5f57d29d91def02573574ad836d6c4ae3852ae7d
Scott Kingsley Clark <lol@scottkclark.com>
2019-08-15 18:52:19 -0500

    Update version and readme

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -4,7 +4,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.10.8';
+	const VERSION = '4.11.0';
 
 	/**
 	 * Min required The Events Calendar version
