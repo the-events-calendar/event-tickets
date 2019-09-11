56b8edfde59ac1ccb3e4bec62304f739eabdf384
Clifford Paulick <tko@tourkick.com>
2019-09-11 17:13:15 -0500

    additional reformatting and array() to []

--- src/views/registration/content.php
+++ src/views/registration/content.php
@@ -27,7 +27,10 @@ foreach ( $events as $event_id => $tickets ) :
 	$provider_class = $passed_provider_class;
 	$providers = array_unique( wp_list_pluck( wp_list_pluck( $tickets, 'provider'), 'attendee_object') );
 
-	if (  empty( $provider_class ) && ! empty( $providers[ $event_id ] ) ) {
+	if (
+		empty( $provider_class )
+		&& ! empty( $providers[ $event_id ] )
+	) {
 		$provider_class = 'tribe-block__tickets__item__attendee__fields__form--' . $providers[ $event_id ];
 	}
 
@@ -38,18 +41,21 @@ foreach ( $events as $event_id => $tickets ) :
 		data-event-id="<?php echo esc_attr( $event_id ); ?>"
 		data-is-meta-up-to-date="<?php echo absint( $is_meta_up_to_date ); ?>"
 	>
-		<?php $this->template( 'registration/summary/content', array( 'event_id' => $event_id, 'tickets' => $tickets ) ); ?>
+		<?php $this->template( 'registration/summary/content', [ 'event_id' => $event_id, 'tickets' => $tickets ] ); ?>
 
 		<div class="tribe-block__tickets__registration__actions">
-			<?php $this->template( 'registration/button-cart', [
-				'event_id' => $event_id,
-				'provider' => $passed_provider,
-			] ); ?>
+			<?php $this->template(
+				'registration/button-cart',
+				[
+					'event_id' => $event_id,
+					'provider' => $passed_provider,
+				]
+			); ?>
 		</div>
 
 		<div class="tribe-block__tickets__item__attendee__fields">
 
-			<?php $this->template( 'registration/attendees/error', array( 'event_id' => $event_id, 'tickets' => $tickets ) ); ?>
+			<?php $this->template( 'registration/attendees/error', [ 'event_id' => $event_id, 'tickets' => $tickets ] ); ?>
 
 			<form
 				method="post"
@@ -57,7 +63,7 @@ foreach ( $events as $event_id => $tickets ) :
 				name="<?php echo 'event' . esc_attr( $event_id ); ?>"
 				novalidate
 			>
-				<?php $this->template( 'registration/attendees/content', array( 'event_id' => $event_id, 'tickets' => $tickets ) ); ?>
+				<?php $this->template( 'registration/attendees/content', [ 'event_id' => $event_id, 'tickets' => $tickets ] ); ?>
 				<input type="hidden" name="tribe_tickets_saving_attendees" value="1" />
 				<?php if ( $has_tpp ) : ?>
 					<button type="submit"><?php esc_html_e( 'Save and Checkout', 'event-tickets' ); ?></button>
@@ -67,10 +73,10 @@ foreach ( $events as $event_id => $tickets ) :
 
 			</form>
 
-			<?php $this->template( 'registration/attendees/error', array() ); ?>
-			<?php $this->template( 'registration/attendees/success', array() ); ?>
+			<?php $this->template( 'registration/attendees/error', [] ); ?>
+			<?php $this->template( 'registration/attendees/success', [] ); ?>
 
-			<?php $this->template( 'registration/attendees/loader', array() ); ?>
+			<?php $this->template( 'registration/attendees/loader', [] ); ?>
 
 		</div>
 
@@ -78,4 +84,11 @@ foreach ( $events as $event_id => $tickets ) :
 
 <?php endforeach; ?>
 
-<?php $this->template( 'registration/button-checkout', array( 'checkout_url' => $checkout_url, 'cart_has_required_meta' => $cart_has_required_meta, 'is_meta_up_to_date' => $is_meta_up_to_date ) );
+<?php $this->template(
+	'registration/button-checkout',
+	[
+		'checkout_url'           => $checkout_url,
+		'cart_has_required_meta' => $cart_has_required_meta,
+		'is_meta_up_to_date'     => $is_meta_up_to_date,
+	]
+);
