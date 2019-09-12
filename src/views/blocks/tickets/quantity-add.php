2386ce4305aba70b626d2bd079ba5321098ae63a
Scott Kingsley Clark <lol@scottkclark.com>
2019-09-11 22:29:27 -0500

    Apply suggestions from code review

--- src/views/blocks/tickets/quantity-add.php
+++ src/views/blocks/tickets/quantity-add.php
@@ -18,7 +18,7 @@
 
 $ticket = $this->get( 'ticket' );
 $button_title = sprintf(
-	_x('Increase ticket quantity for %s', '%s: ticket name.', 'event-tickets'),
+	_x( 'Increase ticket quantity for %s', '%s: ticket name.', 'event-tickets' ),
 	$ticket->name
 );
 ?>
