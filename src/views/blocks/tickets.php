e2c2c1e44313f82b302d79baabc667679fc04bb0
Scott Kingsley Clark <lol@scottkclark.com>
2019-09-19 10:27:54 -0500

    Update src/views/blocks/tickets.php

--- src/views/blocks/tickets.php
+++ src/views/blocks/tickets.php
@@ -56,7 +56,7 @@ echo $html;
 >
 	<h2 class="tribe-common-h4 tribe-common-h--alt tribe-tickets__title"><?php esc_html_e( 'Tickets', 'event-tickets' ); ?></h2>
 	<p id="tribe-tickets__notice__tickets-in-cart" class="tribe-common-b3 tribe-notice">
-	The numbers below include tickets already in your cart. You can update your ticket quantities on this page to the number of tickets you want in the cart. Clicking "Get Tickets" will allow you to edit/adjust any existing Attendee Information.
+		<?php esc_html_e( 'The numbers below include tickets already in your cart. You can update your ticket quantities on this page to the number of tickets you want in the cart. Clicking "Get Tickets" will allow you to edit/adjust any existing Attendee Information.', 'event-tickets' ); ?>
 	</p>
 	<?php $this->template( 'blocks/tickets/commerce/fields', [ 'provider' => $provider, 'provider_id' => $provider_id ] ); ?>
 	<?php if ( $has_tickets_on_sale ) : ?>
