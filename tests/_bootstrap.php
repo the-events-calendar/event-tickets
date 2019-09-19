0d08c648102ff632f00eea74d3412fb35c8a5370
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-19 17:00:19 +0200

    Move TEC test inclusion to `views_integration`

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -5,23 +5,8 @@
 
 use Codeception\Util\Autoload;
 
-$tec_tests_dir = __DIR__ . '/../../the-events-calendar/tests';
-
-if ( ! is_dir( $tec_tests_dir ) ) {
-	throw new RuntimeException( "Tickets tests require The Events Calendar installed in a \"the-events-calendar\" sibling folder: {$tec_tests_dir} not found." );
-}
-
-$tec_support_dir = $tec_tests_dir . '/_support';
-
-/**
- * Manually include the file here as we might need it in a suite configuration file.
- * Suites fire before the autoload below is used so we need to gather what we need without
- * autoloading.
- */
-require_once $tec_support_dir . '/Helper/TribeDb.php';
-
 Codeception\Util\Autoload::addNamespace( 'Tribe__Events__WP_UnitTestCase', __DIR__ . '/_support' );
-Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', $tec_support_dir );
+Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', __DIR__ . '/_support' );
 Codeception\Util\Autoload::addNamespace( 'Tribe\Tickets\Test', __DIR__ . '/_support' );
 
 /**
--- tests/views_integration/_bootstrap.php
+++ tests/views_integration/_bootstrap.php
@@ -2,6 +2,24 @@
 
 use Tribe\Events\Views\V2\Service_Provider as Events_Provider;
 use Tribe\Events\Tickets\Views\V2\Service_Provider as Tickets_Provider;
+use Codeception\Util\Autoload;
+
+$tec_tests_dir = __DIR__ . '/../../../the-events-calendar/tests';
+
+if ( ! is_dir( $tec_tests_dir ) ) {
+	throw new RuntimeException( "Tickets tests require The Events Calendar installed in a \"the-events-calendar\" sibling folder: {$tec_tests_dir} not found." );
+}
+
+$tec_support_dir = $tec_tests_dir . '/_support';
+
+/**
+ * Manually include the file here as we might need it in a suite configuration file.
+ * Suites fire before the autoload below is used so we need to gather what we need without
+ * autoloading.
+ */
+require_once $tec_support_dir . '/Helper/TribeDb.php';
+
+Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', $tec_support_dir );
 
 // Let's  make sure Views v2 are activated if not.
 putenv( 'TRIBE_EVENTS_V2_VIEWS=1' );
