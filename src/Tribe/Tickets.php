bf4d7644a4dfdd18adf8340c76f2a015a9c3beb4
Clifford Paulick <tko@tourkick.com>
2019-09-20 22:03:38 -0500

    Scott requested sprintf change

--- src/Tribe/Tickets.php
+++ src/Tribe/Tickets.php
@@ -490,7 +490,7 @@ if ( ! class_exists( 'Tribe__Tickets__Tickets' ) ) {
 			// Make sure Thickbox is available regardless of which admin page we're on.
 			add_thickbox();
 
-			$move_link = sprintf( '<a href="%1$s" class="thickbox tribe-ticket-move-link">' . esc_html( $button_text ) . '</a>', $move_url );
+			$move_link = sprintf( '<a href="%1$s" class="thickbox tribe-ticket-move-link">%2$s</a>', $move_url, esc_html( $button_text ) );
 
 			return $move_link;
 		}
