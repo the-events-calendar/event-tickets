97c14eef5f374ceb1b78d23632b4e722008fbfd7
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-23 12:46:23 +0200

    Add templates.

--- src/views/v2/week/mobile-events/day/event/cost.php
+++ src/views/v2/week/mobile-events/day/event/cost.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * View: List Single Event Cost
+ * View: Week View - Mobile Event Cost
  *
  * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/events/views/v2/list/event/cost.php
+ * [your-theme]/tribe/events-pro/views/v2/week/mobile-events/day/event/cost.php
  *
  * See more documentation about our views templating system.
  *
@@ -20,9 +20,8 @@
 if ( empty( $event->cost ) ) {
 	return;
 }
-
 ?>
-<div class="tribe-events-c-small-cta tribe-common-b3 tribe-events-calendar-list__event-cost">
+<div class="tribe-events-c-small-cta tribe-common-b3 tribe-events-pro-week-mobile-events__event-cost">
 	<?php if ( $event->tickets->exist ) : ?>
 		<a
 			href="<?php echo esc_url( $event->tickets->link->anchor ); ?>"
@@ -34,7 +33,9 @@ if ( empty( $event->cost ) ) {
 	<span class="tribe-events-c-small-cta__price">
 		<?php echo esc_html( $event->cost ) ?>
 	</span>
-	<span class="tribe-events-c-small-cta__stock">
-		<?php echo esc_html( $event->tickets->stock->available ) ?>
-	</span>
+	<?php if ( ! empty( $event->tickets->stock->available ) ) : ?>
+		<span class="tribe-events-c-small-cta__stock">
+			<?php echo esc_html( $event->tickets->stock->available ) ?>
+		</span>
+	<?php endif; ?>
 </div>
