021889a23e1ac1e13e04e371351c2e0232847873
Juanfra Aldasoro <juanfraa@gmail.com>
2019-09-23 12:24:21 +0200

    Test cleanup.

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -3,20 +3,6 @@
  * @file Global bootstrap for all codeception tests
  */
 
-use Codeception\Util\Autoload;
-
 Codeception\Util\Autoload::addNamespace( 'Tribe__Events__WP_UnitTestCase', __DIR__ . '/_support' );
 Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', __DIR__ . '/_support' );
 Codeception\Util\Autoload::addNamespace( 'Tribe\Tickets\Test', __DIR__ . '/_support' );
-
-/**
- * Codeception will regenerate snapshots on `--debug`, while the `spatie/snapshot-assertions`
- * library will do the same on `--update-snapshots`.
- * Since Codeception has strict check on the CLI arguments appending `--update-snapshots` to the
- * `vendor/bin/codecept run` command will throw an error.
- * We handle that intention here.
- */
-if ( in_array( '--debug', $_SERVER['argv'], true ) ) {
-	$_SERVER['argv'][] = '--update-snapshots';
-}
-
--- tests/views_integration/_bootstrap.php
+++ tests/views_integration/_bootstrap.php
@@ -2,7 +2,6 @@
 
 use Tribe\Events\Views\V2\Service_Provider as Events_Provider;
 use Tribe\Events\Tickets\Views\V2\Service_Provider as Tickets_Provider;
-use Codeception\Util\Autoload;
 
 $tec_tests_dir = __DIR__ . '/../../../the-events-calendar/tests';
 
@@ -30,4 +29,15 @@ tribe_register_provider( Tickets_Provider::class );
 global $wp_rewrite;
 $wp_rewrite->permalink_structure = '/%postname%/';
 $wp_rewrite->rewrite_rules();
+
+/**
+ * Codeception will regenerate snapshots on `--debug`, while the `spatie/snapshot-assertions`
+ * library will do the same on `--update-snapshots`.
+ * Since Codeception has strict check on the CLI arguments appending `--update-snapshots` to the
+ * `vendor/bin/codecept run` command will throw an error.
+ * We handle that intention here.
+ */
+if ( in_array( '--debug', $_SERVER['argv'], true ) ) {
+	$_SERVER['argv'][] = '--update-snapshots';
+}
 
