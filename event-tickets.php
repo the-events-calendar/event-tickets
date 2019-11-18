91e5c53ee6e818b5e6a7d37618c7831a79268171
bordoni <bordoni.dev@gmail.com>
2019-11-18 10:48:40 -0500

    Version Bump to 4.10.12-beta1

--- event-tickets.php
+++ event-tickets.php
@@ -3,7 +3,7 @@
 Plugin Name: Event Tickets
 Plugin URI:  http://m.tri.be/1acb
 Description: Event Tickets allows you to sell basic tickets and collect RSVPs from any post, page, or event.
-Version: 4.10.11
+Version: 4.10.12-beta1
 Author: Modern Tribe, Inc.
 Author URI: http://m.tri.be/28
 License: GPLv2 or later
