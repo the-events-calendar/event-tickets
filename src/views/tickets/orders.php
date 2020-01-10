2f935099eeed7c6f946958babce3f8883f8cb2c6
Scott Kingsley Clark <lol@scottkclark.com>
2020-01-10 13:45:28 -0600

    Update src/views/tickets/orders.php

--- src/views/tickets/orders.php
+++ src/views/tickets/orders.php
@@ -10,7 +10,7 @@
  * @since   4.10.2 Only show Update button if ticket has meta.
  * @since   4.10.8 Show Update button if current user has either RSVP or Ticket with meta. Do not use the now-deprecated third parameter of `get_description_rsvp_ticket()`.
  * @since   4.10.9 Use function for text.
- * @since   TBD Correct getting `$event_id` when using The Events Calendar's "Default Page Template" display template.
+ * @since   TBD Correct getting `$event_id` when using The Events Calendar's "Default Page Template" display template. `$event_id` now relies on the `WP_Query` queried object ID instead of the global `$post` object.
  *
  * @version TBD
  */
