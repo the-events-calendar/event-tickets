20e1afa516eb44f90ac9e37949017143769e5be2
Scott Kingsley Clark <lol@scottkclark.com>
2020-01-21 15:50:20 -0600

    Update src/views/blocks/rsvp/form/opt-out.php

--- src/views/blocks/rsvp/form/opt-out.php
+++ src/views/blocks/rsvp/form/opt-out.php
@@ -25,6 +25,7 @@ $ticket   = $this->get( 'ticket' );
  * @param bool
  */
 $hide_attendee_list_optout = apply_filters( 'tribe_tickets_plus_hide_attendees_list_optout', $modal );
+
 if (
 	$hide_attendee_list_optout
 	 || ! class_exists( 'Tribe__Tickets_Plus__Attendees_List' )
