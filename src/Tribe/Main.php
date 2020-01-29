3f93c8e30591028e9699ae82fea8605acae989d5
Matthew Batchelder <borkweb@gmail.com>
2020-01-29 09:24:13 -0500

    Updating version numbers

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -4,7 +4,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin
 	 */
-	const VERSION = '4.11.2';
+	const VERSION = '4.11.2.1';
 
 	/**
 	 * Min required The Events Calendar version
