be346969fe43d91f12b0dee73fef29a26457e4f2
Paul <kim.paul.ms@gmail.com>
2020-02-06 17:54:29 +0800

    fix override paths

--- src/views/v2/map/event-cards/event-card/actions/cost-spacer.php
+++ src/views/v2/map/event-cards/event-card/actions/cost-spacer.php
@@ -3,7 +3,7 @@
  * View: Map View - Single Event Actions - Cost Spacer
  *
  * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/event-tickets/views/v2/map/event-cards/event-card/event/actions/cost-spacer.php
+ * [your-theme]/tribe/event-tickets/views/v2/map/event-cards/event-card/actions/cost-spacer.php
  *
  * See more documentation about our views templating system.
  *
