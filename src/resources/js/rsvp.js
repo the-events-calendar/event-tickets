f35f02d7fb7c128b9d93ed7e604ddfbe9edf5476
Clifford Paulick <tko@tourkick.com>
2020-02-15 00:45:23 -0600

    rsvp.js to handle form submission and errors properly, including ECP Attendee Info fields

--- src/resources/js/rsvp.js
+++ src/resources/js/rsvp.js
@@ -38,16 +38,32 @@ var tribe_tickets_rsvp = {
 		}
 	};
 
-	my.validate_submission = function( $form ) {
+	my.validate_rsvp_info = function( $form ) {
+		var rsvp_qty = 0;
 		var $qty = $form.find( 'input.tribe-tickets-quantity' );
-		var $name = $form.find( 'input.tribe-tickets-full-name' );
-		var $email = $form.find( 'input.tribe-tickets-email' );
+		var $name = $form.find( 'input#tribe-tickets-full-name' );
+		var $email = $form.find( 'input#tribe-tickets-email' );
 
-		return (
-			$.trim( $name.val() ).length &&
-			$.trim( $email.val() ).length &&
-			parseInt( $qty.val() ) > 0
+		$qty.each( function() {
+			rsvp_qty = rsvp_qty + parseInt( $( this ).val() );
+		} );
+
+		return !!(
+			$.trim( $name.val() ).length
+			&& $.trim( $email.val() ).length
+			&& rsvp_qty
 		);
+	};
+
+	my.validate_meta = function( $form ) {
+		var is_meta_valid = true;
+		var has_tickets_plus = !!window.tribe_event_tickets_plus;
+
+		if( has_tickets_plus ) {
+			is_meta_valid = window.tribe_event_tickets_plus.meta.validate_meta( $form );
+		}
+
+		return is_meta_valid;
 	};
 
 	my.event.quantity_changed = function() {
@@ -55,18 +71,38 @@ var tribe_tickets_rsvp = {
 	};
 
 	my.event.handle_submission = function( e ) {
+		var $form = $( this ).closest( 'form' );
 
-		if ( ! my.validate_submission(  $( this ).closest( 'form' ) ) ) {
-			e.preventDefault();
-			var $form = $( this ).closest( 'form' );
+		var $rsvp_msgs = $form.find( '.tribe-rsvp-messages, .tribe-rsvp-message-confirmation-error' );
+		var $etp_meta_msgs = $form.find( '.tribe-event-tickets-meta-required-message' );
 
-			$form.addClass( 'tribe-rsvp-message-display' );
-			$form.find( '.tribe-rsvp-message-confirmation-error' ).show();
+		var is_rsvp_info_valid = !!my.validate_rsvp_info( $form );
+		var is_attendee_meta_valid = !!my.validate_meta( $form );
 
-			$( 'html, body').animate({
-				scrollTop: $form.offset().top
+		// Show/Hide message about missing RSVP details (name, email, going/not) and/or missing ETP fields (if applicable).
+		if(
+			!is_rsvp_info_valid
+			|| !is_attendee_meta_valid
+		) {
+			is_rsvp_info_valid
+				? $rsvp_msgs.hide()
+				: $rsvp_msgs.show();
+
+			if( is_attendee_meta_valid ) {
+				$etp_meta_msgs.hide();
+				$form.removeClass( 'tribe-event-tickets-plus-meta-missing-required' );
+			} else {
+				$form.addClass( 'tribe-event-tickets-plus-meta-missing-required' );
+				$etp_meta_msgs.show();
+			}
+
+			$( 'html, body' ).animate( {
+				scrollTop: $form.offset().top - 100,
 			}, 300 );
+
 			return false;
+		} else {
+			return true;
 		}
 	};
 
