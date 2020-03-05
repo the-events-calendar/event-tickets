8cdf073472f2a11b3f84696d50fcd598e097ce2e
Clifford Paulick <tko@tourkick.com>
2020-03-05 17:10:09 -0600

    docblock corrections and array() to []

--- src/views/blocks/rsvp/form/details.php
+++ src/views/blocks/rsvp/form/details.php
@@ -4,17 +4,18 @@
  * Form Details
  *
  * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/tickets/blocks/rsvp/form/submit.php
+ * [your-theme]/tribe/tickets/blocks/rsvp/form/details.php
  *
  * See more documentation about our Blocks Editor templating system.
  *
- * @link {INSERT_ARTICLE_LINK_HERE}
+ * @link    {INSERT_ARTICLE_LINK_HERE}
  *
- * @since 4.9
- * @version 4.9.4
+ * @since   4.9
+ * @since   TBD Added template override instructions in template comments.
  *
+ * @version TBD
  */
 
-$this->template( 'blocks/rsvp/form/name', array( 'ticket' => $ticket ) );
-$this->template( 'blocks/rsvp/form/email', array( 'ticket' => $ticket ) );
-$this->template( 'blocks/rsvp/form/opt-out', array( 'ticket' => $ticket ) );
+$this->template( 'blocks/rsvp/form/name', [ 'ticket' => $ticket ] );
+$this->template( 'blocks/rsvp/form/email', [ 'ticket' => $ticket ] );
+$this->template( 'blocks/rsvp/form/opt-out', [ 'ticket' => $ticket ] );
