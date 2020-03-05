660cec12c0ae9f7e39593eeda107960f39744970
Juanfra Aldasoro <juanfraa@gmail.com>
2020-03-05 15:09:46 +0100

    Add snapshots.

---
+++ tests/wpunit/Tribe/Tickets/Partials/Tickets/__snapshots__/ContentDescription__test_should_render_regular_wo_modal__1.php
@@ -0,0 +1,3 @@
+<?php return '<div id="tribe__details__content--7" class="tribe-common-b2 tribe-common-b3--min-medium tribe-tickets__item__details__content">
+	Ticket PayPal ticket excerpt for 8</div>
+';
---
+++ tests/wpunit/Tribe/Tickets/Partials/Tickets/__snapshots__/SubmitTest__test_should_render_must_login__1.php
@@ -0,0 +1,3 @@
+<?php return '<a class="tribe-common-c-btn tribe-common-c-btn--small" href="http://tribe/wp-login.php">
+	Log in to purchase</a>
+';
