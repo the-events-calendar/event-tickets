07d47408ace08403b665a21446aeb7a8b90daebc
Scott Kingsley Clark <lol@scottkclark.com>
2020-03-10 15:13:44 -0500

    Regenerate snapshots

---
+++ tests/wpunit/Tribe/Tickets/Partials/Tickets/__snapshots__/ContentTitle__test_should_render_title__1.php
@@ -0,0 +1,3 @@
+<?php return '<div  class="tribe-common-h7 tribe-common-h6--min-medium tribe-tickets__item__content__title"  >
+		Test PayPal ticket for 8</div>
+';
---
+++ tests/wpunit/Tribe/Tickets/Partials/Tickets/__snapshots__/SubmitTest__test_should_render_submit_button__1.php
@@ -0,0 +1,7 @@
+<?php return '<button
+	 class="tribe-common-c-btn tribe-common-c-btn--small tribe-tickets__buy" 	id="tribe-tickets__buy"
+	type="submit"
+			name="cart-button"
+		disabled aria-disabled="true">
+	Get Tickets</button>
+';
