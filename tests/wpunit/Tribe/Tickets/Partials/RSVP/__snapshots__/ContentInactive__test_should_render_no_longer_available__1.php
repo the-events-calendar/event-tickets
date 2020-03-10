e0a9801c25862218db878dcf69a2e34302c7be21
Juanfra Aldasoro <juanfraa@gmail.com>
2020-03-10 17:37:34 -0300

    Add snapshots.

---
+++ tests/wpunit/Tribe/Tickets/Partials/RSVP/__snapshots__/ContentInactive__test_should_render_no_longer_available__1.php
@@ -0,0 +1,7 @@
+<?php return '<div class="tribe-block__rsvp__content tribe-block__rsvp__content--inactive">
+	<div class="tribe-block__rsvp__details__status">
+		<div class="tribe-block__rsvp__details">
+			RSVPs are no longer available		</div>
+	</div>
+</div>
+';
