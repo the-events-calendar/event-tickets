9513b56b4155414fd983d2d2f5f8daf6bf79bb0f
Scott Kingsley Clark <lol@scottkclark.com>
2020-03-10 15:38:38 -0500

    Remove snapshots

--- tests/wpunit/Tribe/Tickets/Commerce/PayPal/__snapshots__/LinksTest__test_ipn_notification_history_snapshot__1.php
+++
@@ -1,1 +0,0 @@
-<?php return 'https://www.paypal.com/customerprofileweb?cmd=_display-ipns-history';
--- tests/wpunit/Tribe/Tickets/Commerce/PayPal/__snapshots__/LinksTest__test_ipn_notification_history_snapshot__2.php
+++
@@ -1,1 +0,0 @@
-<?php return '<a href="https://www.paypal.com/customerprofileweb?cmd=_display-ipns-history" target="_blank">Profile and Settings &gt; My selling tools &gt; Instant Payment Notification &gt; IPN History Page</a>';
