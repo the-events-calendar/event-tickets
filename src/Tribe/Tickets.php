d1d5e43fd9bd91e418223f66929b7bc479adce93
Clifford Paulick <tko@tourkick.com>
2020-04-07 03:11:13 -0500

    correct docblock, help IDE, empty()

--- src/Tribe/Tickets.php
+++ src/Tribe/Tickets.php
@@ -610,9 +610,9 @@ if ( ! class_exists( 'Tribe__Tickets__Tickets' ) ) {
 		 *
 		 * @abstract
 		 *
-		 * @param int   $post_id
-		 * @param int   $ticket
-		 * @param array $raw_data
+		 * @param int                           $post_id
+		 * @param Tribe__Tickets__Ticket_Object $ticket
+		 * @param array                         $raw_data
 		 *
 		 * @return int|false The updated/created ticket post ID or false if no ticket ID.
 		 */
@@ -2912,7 +2912,10 @@ if ( ! class_exists( 'Tribe__Tickets__Tickets' ) ) {
 				update_post_meta( $ticket->ID, $tickets_handler->key_end_date, $event_start );
 			}
 
-			tribe( 'tickets.version' )->update( $ticket->ID );
+			/** @var Tribe__Tickets__Version $version */
+			$version = tribe( 'tickets.version' );
+
+			$version->update( $ticket->ID );
 
 			return $save_ticket;
 		}
