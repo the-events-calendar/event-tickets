8f31f0bb2712a31af3b2aa5004a3ade0dff73239
Lucas B <lucasfbustamante@gmail.com>
2020-04-09 15:08:07 -0300

    CR: Remove etplus_wpunit suite to add the tests in ET+ itself

--- tests/etplus_wpunit/_bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Here you can initialize variables that will be available to your tests
-
-$tec_support = dirname( __DIR__, 3 ) . '/the-events-calendar/tests/_support';
-Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', $tec_support );
