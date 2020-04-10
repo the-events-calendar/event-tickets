8b34b16261074d5acf66d18ae598f5d73a992a28
Lucas B <lucasfbustamante@gmail.com>
2020-04-10 13:06:41 -0300

    Move shortcode identifier to static property

--- tests/functional/Tribe/Tickets/Attendee_RegistrationCest.php
+++ tests/functional/Tribe/Tickets/Attendee_RegistrationCest.php
@@ -2,6 +2,8 @@
 
 class Attendee_RegistrationCest {
 
+	public static $shortcode_identifier = 'div.tribe-tickets__registration';
+
 	public function should_not_see_shortcode_in_page( FunctionalTester $I ) {
 		$page_id = $I->havePageInDatabase( [
 			"post_content" => 'Foo',
@@ -10,10 +12,9 @@ class Attendee_RegistrationCest {
 		$I->setTribeOption( "ticket-attendee-page-id", $page_id );
 
 		$page_url             = get_post_permalink( $page_id );
-		$shortcode_identifier = "div.tribe-tickets__registration";
 
 		$I->amOnUrl( $page_url );
-		$I->dontSeeElement( $shortcode_identifier );
+		$I->dontSeeElement( static::$shortcode_identifier );
 	}
 
 	public function should_see_shortcode_in_page( FunctionalTester $I ) {
@@ -24,10 +25,9 @@ class Attendee_RegistrationCest {
 		$I->setTribeOption( "ticket-attendee-page-id", $page_id );
 
 		$page_url             = get_post_permalink( $page_id );
-		$shortcode_identifier = "div.tribe-tickets__registration";
 
 		$I->amOnUrl( $page_url );
-		$I->seeElement( $shortcode_identifier );
+		$I->seeElement( static::$shortcode_identifier );
 	}
 
 	public function should_see_shortcode_and_content_in_page( FunctionalTester $I ) {
@@ -41,10 +41,9 @@ class Attendee_RegistrationCest {
 		$I->setTribeOption( "ticket-attendee-page-id", $page_id );
 
 		$page_url             = get_post_permalink( $page_id );
-		$shortcode_identifier = "div.tribe-tickets__registration";
 
 		$I->amOnUrl( $page_url );
-		$I->seeElement( $shortcode_identifier );
+		$I->seeElement( static::$shortcode_identifier );
 		$I->see( $before );
 		$I->see( $after );
 	}
