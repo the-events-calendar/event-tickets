99240bcfd5920fb3da3d5a0f0e6cb967ec958b5f
Lucas B <lucasfbustamante@gmail.com>
2020-04-10 12:57:02 -0300

    Fix method_exists calls

--- src/views/registration-js/content.php
+++ src/views/registration-js/content.php
@@ -28,7 +28,7 @@ if ( empty( $provider ) ) {
 	$provider     = $provider_obj->attendee_object;
 }
 
-if ( method_exists( $provider_obj, 'get_cart_provider' ) ) {
+if ( method_exists( $provider_obj, 'get_checkout_url' ) ) {
 	$checkout_url = $provider_obj->get_checkout_url();
 } else {
 	$checkout_url = '';
