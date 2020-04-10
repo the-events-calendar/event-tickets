a631d5e50ddc27046655d547c4dcb6300126cf62
Scott Kingsley Clark <lol@scottkclark.com>
2020-04-10 16:48:36 -0500

    Apply suggestions from code review

--- src/Tribe/Events/Attendee_List_Display.php
+++ src/Tribe/Events/Attendee_List_Display.php
@@ -13,13 +13,13 @@ use Tribe__Tickets_Plus__Attendees_List;
 use WP_Post;
 
 /**
- * Class Attendee_List
+ * Class Attendees_List
  *
  * @since TBD
  *
  * @package Tribe\Tickets\Events
  */
-class Attendee_List {
+class Attendees_List {
 
 	/**
 	 * @since TBD
