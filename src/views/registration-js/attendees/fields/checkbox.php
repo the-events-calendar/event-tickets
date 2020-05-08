6d8739f8c5cb643903084395a290fa0bfdba735b
Scott Kingsley Clark <lol@scottkclark.com>
2020-05-08 11:31:34 -0500

    Apply suggestions from code review

--- src/views/registration-js/attendees/fields/checkbox.php
+++ src/views/registration-js/attendees/fields/checkbox.php
@@ -7,7 +7,7 @@
  *
  * @since 4.11.0
  * @since TBD Updated phpdoc for which field class to reference.
- * @version 4.11.0
+ * @version TBD
  *
  * @see Tribe__Tickets_Plus__Meta__Field__Checkbox
  *
--- src/views/registration-js/attendees/fields/number.php
+++ src/views/registration-js/attendees/fields/number.php
@@ -6,6 +6,7 @@
  * Override this template in your own theme by creating a file at:
  * [your-theme]/tribe/tickets/registration-js/attendees/fields/url.php
  *
+ * @since  TBD
  * @version TBD
  *
  * @see Tribe__Tickets_Plus__Meta__Field__Number
