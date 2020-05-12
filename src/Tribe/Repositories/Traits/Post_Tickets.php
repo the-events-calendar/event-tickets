8c9263a42b34f7b1ce74c483b8e76c8d09754450
Scott Kingsley Clark <lol@scottkclark.com>
2020-05-11 23:50:03 -0500

    Update src/Tribe/Repositories/Traits/Post_Tickets.php
    Co-authored-by: Gustavo Bordoni <bordoni.dev@gmail.com>

--- src/Tribe/Repositories/Traits/Post_Tickets.php
+++ src/Tribe/Repositories/Traits/Post_Tickets.php
@@ -63,10 +63,13 @@ trait Post_Tickets {
 			throw Tribe__Repository__Usage_Error::because_this_comparison_operator_is_not_supported( $operator, 'filter_by_cost' );
 		}
 
-		if ( in_array( $operator, [
-				'BETWEEN',
-				'NOT BETWEEN',
-			], true ) && ! ( is_array( $value ) && 2 === count( $value ) ) ) {
+		if (
+			in_array( $operator, [ 'BETWEEN', 'NOT BETWEEN' ], true ) 
+			&& ! (
+				is_array( $value ) 
+				&& 2 === count( $value )
+			) 
+		) {
 			throw Tribe__Repository__Usage_Error::because_this_comparison_operator_requires_an_value_of_type( $operator, 'filter_by_cost', 'array' );
 		}
 
