7030e96522a1d8cd6b33cdd302d01580f3bda2e0
Clifford Paulick <tko@tourkick.com>
2020-05-13 18:02:34 -0500

    don't use deprecated methods for paypal

--- tests/_support/Commerce/Test_Case.php
+++ tests/_support/Commerce/Test_Case.php
@@ -311,7 +311,7 @@ class Test_Case extends WPTestCase {
 		$test_data['rsvp_tickets'] = array_merge( [ $rsvp_id_one ], $this->create_many_rsvp_tickets( 3, $event_id_one ) );
 
 		// Create test PayPal1 ticket
-		$paypal_id_one = $this->create_paypal_ticket_basic( $event_id_one, 5 );
+		$paypal_id_one = $this->create_paypal_ticket( $event_id_one, 5 );
 
 		// Add User3 (Attendee5) and User4 (Attendee6) as Tribe Commerce PayPal Ticket attendees
 
@@ -342,12 +342,12 @@ class Test_Case extends WPTestCase {
 			$this->create_attendee_for_ticket( $paypal_id_one, $event_id_one );
 
 		// Create 3 more PayPal tickets that will never have any attendees
-		$test_data['paypal_tickets'] = array_merge( [ $paypal_id_one ], $this->create_many_paypal_tickets_basic( 3, $event_id_one ) );
+		$test_data['paypal_tickets'] = array_merge( [ $paypal_id_one ], $this->create_many_paypal_tickets( 3, $event_id_one ) );
 
 		// Create test PayPal5 ticket
 		$test_data['paypal_tickets'][] =
 		$paypal_id_five =
-			$this->create_paypal_ticket_basic( $event_id_one, 12 );
+			$this->create_paypal_ticket( $event_id_one, 12 );
 
 		// Add User2 (Attendee9) as Tribe Commerce PayPal Ticket attendee
 		$test_data['attendees_event_1'][] =
