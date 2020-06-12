f63481decbdadc627acd97514b098a0391e6d9bb
Juanfra Aldasoro <juanfraa@gmail.com>
2020-06-12 15:00:26 -0300

    Implement `guest_label` on ARI for guest list.

--- src/views/v2/rsvp/ari/sidebar/guest-list.php
+++ src/views/v2/rsvp/ari/sidebar/guest-list.php
@@ -16,7 +16,15 @@
 		<button>
 			<em class="tribe-common-svgicon tribe-common-svgicon--guest"></em>
 			<span class="tribe-tickets__rsvp-ar-guest-list-item-title tribe-common-a11y-visual-hide">
-				<?php esc_html_e( 'Main Guest', 'event-tickets' ); ?>
+				<?php
+				echo wp_kses_post(
+					sprintf(
+						/* Translators: %s Guest label for RSVP attendee registration sidebar. */
+						__( 'Main %s', 'event-tickets' ),
+						tribe_get_guest_label_singular( 'RSVP attendee registration sidebar guest button' )
+					)
+				);
+				?>
 			</span>
 		</button>
 	</li>
@@ -24,7 +32,15 @@
 		<button>
 			<em class="tribe-common-svgicon tribe-common-svgicon--guest-disabled"></em>
 			<span class="tribe-tickets__rsvp-ar-guest-list-item-title tribe-common-a11y-visual-hide">
-				<?php esc_html_e( 'Guest 2', 'event-tickets' ); ?>
+				<?php
+				echo wp_kses_post(
+					sprintf(
+						/* Translators: %s Guest label for RSVP attendee registration sidebar. */
+						__( '%s 2', 'event-tickets' ),
+						tribe_get_guest_label_singular( 'RSVP attendee registration sidebar guest button' )
+					)
+				);
+				?>
 			</span>
 		</button>
 	</li>
@@ -32,8 +48,16 @@
 		<button>
 			<em class="tribe-common-svgicon tribe-common-svgicon--guest-disabled"></em>
 			<span class="tribe-tickets__rsvp-ar-guest-list-item-title tribe-common-a11y-visual-hide">
-				<?php esc_html_e( 'Guest 3', 'event-tickets' ); ?>
-				</span>
+				<?php
+				echo wp_kses_post(
+					sprintf(
+						/* Translators: %s Guest label for RSVP attendee registration sidebar. */
+						__( '%s 3', 'event-tickets' ),
+						tribe_get_guest_label_singular( 'RSVP attendee registration sidebar guest button' )
+					)
+				);
+				?>
+			</span>
 		</button>
 	</li>
 </ul>
