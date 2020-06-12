47e68c4dfdcc7dfef10845e42c73e2ba888a69d5
Juanfra Aldasoro <juanfraa@gmail.com>
2020-06-12 15:00:11 -0300

    Implement `guest_label` on ARI for title.

--- src/views/v2/rsvp/ari/form/title.php
+++ src/views/v2/rsvp/ari/form/title.php
@@ -12,5 +12,13 @@
 
 ?>
 <h3 class="tribe-tickets__rsvp-ar-form-title tribe-common-h5">
-	<?php esc_html_e( 'Main Guest', 'event-tickets' ); ?>
+	<?php
+	echo wp_kses_post(
+		sprintf(
+			/* Translators: %s Guest label for RSVP attendee registration form title. */
+			__( 'Main %s', 'event-tickets' ),
+			tribe_get_guest_label_singular( 'RSVP attendee registration form title' )
+		)
+	);
+	?>
 </h3>
