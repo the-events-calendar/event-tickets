f7201a00a2458d6e602c7abff4ef4285c1c52d27
Juanfra Aldasoro <juanfraa@gmail.com>
2020-06-12 11:19:37 -0300

    Update docblock.

--- src/views/v2/rsvp/ari/sidebar/quantity/plus.php
+++ src/views/v2/rsvp/ari/sidebar/quantity/plus.php
@@ -4,7 +4,7 @@
  * Form Quantity Plus
  *
  * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/tickets/v2/rsvp/ari/sidebar/quantity/quantity-plus.php
+ * [your-theme]/tribe/tickets/v2/rsvp/ari/sidebar/quantity/plus.php
  *
  * See more documentation about our Blocks Editor templating system.
  *
