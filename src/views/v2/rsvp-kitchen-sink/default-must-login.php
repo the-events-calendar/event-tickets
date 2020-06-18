83ac1aa2a438cf7b52c6e73f3262e53f79f9f630
Juanfra Aldasoro <juanfraa@gmail.com>
2020-06-18 15:20:26 -0300

    Add kitchen sink must login

---
+++ src/views/v2/rsvp-kitchen-sink/default-must-login.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Block: RSVP default must login
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/tickets/v2/rsvp-kitchen-sink/default-must-login.php
+ *
+ * See more documentation about our Blocks Editor templating system.
+ *
+ * @link {INSERT_ARTICLE_LINK_HERE}
+ *
+ * @var Tribe__Tickets__Ticket_Object $rsvp The rsvp ticket object.
+ *
+ * @since TBD
+ *
+ * @version TBD
+ */
+?>
+<div class="tribe-tickets__rsvp-wrapper" data-rsvp-id="16">
+
+
+<div class="tribe-tickets__rsvp-message tribe-tickets__rsvp-message--must-login tribe-common-b3">
+	<svg class="tribe-tickets-svgicon tribe-tickets__rsvp-message--must-login-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="8" cy="8" r="7.467" stroke="#141827" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"></circle><circle id="dot" cx="8" cy="11.733" r="1.067" fill="#141827" fill-rule="nonzero"></circle><path stroke="#141827" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8 3.733v4.8"></path></g></svg>
+	<span class="tribe-tickets__rsvp-message-text">
+		<strong>
+			You must be logged in to RSVP.
+			<a href="http://localhost:10025/wp-login.php?redirect_to=http://localhost:10025/event/rsvp-test/?tribe-tickets__rsvp16" class="tribe-tickets__rsvp-message-link">
+				Log in here			</a>
+		</strong>
+	</span>
+</div>
+
+
+
+	<div class="tribe-tickets__rsvp tribe-common-g-row tribe-common-g-row--gutters">
+
+		<div class="tribe-tickets__rsvp-details-wrapper tribe-common-g-col">
+	<div class="tribe-tickets__rsvp-details">
+		<h3 class="tribe-tickets__rsvp-title tribe-common-h4">
+	Job &amp; Career Fair</h3>
+
+		<div class="tribe-tickets__rsvp-description tribe-common-b3">
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+
+		<div class="tribe-tickets__rsvp-attendance">
+	<span class="tribe-tickets__rsvp-attendance-number tribe-common-h4">
+		0	</span>
+	<span class="tribe-tickets__rsvp-attendance-going tribe-common-b3">
+		Going	</span>
+</div>
+
+		<div class="tribe-tickets__rsvp-availability tribe-common-b3">
+			<span class="tribe-tickets__rsvp-availability-quantity tribe-common-b2--bold"> 100 </span> remaining,
+	<span class="tribe-tickets__rsvp-availability-days-left tribe-common-b2--bold"> 48 </span> days left to RSVP
+</div>
+	</div>
+</div>
+
+		<div class="tribe-tickets__rsvp-actions-wrapper tribe-common-g-col">
+	<div class="tribe-tickets__rsvp-actions">
+
+
+			<div class="tribe-tickets__rsvp-actions-rsvp">
+	<span class="tribe-common-h6">
+		RSVP Here	</span>
+
+
+<div class="tribe-tickets__rsvp-actions-rsvp-going">
+	<button class="tribe-common-c-btn tribe-tickets__rsvp-actions-button-going" type="submit" disabled="" aria-disabled="true">
+		Going	</button>
+</div>
+
+	<div class="tribe-tickets__rsvp-actions-rsvp-not-going">
+	<button class="tribe-common-cta tribe-common-cta--alt" disabled="" aria-disabled="true">
+		Can't go	</button>
+</div>
+
+</div>
+
+			</div>
+
+</div>
+
+	</div>
+
+			</div>
