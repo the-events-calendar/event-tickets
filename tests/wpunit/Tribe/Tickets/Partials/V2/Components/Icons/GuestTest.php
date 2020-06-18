af092088d246f92aa2d263243111314a3cf30cdc
Juanfra Aldasoro <juanfraa@gmail.com>
2020-06-18 17:45:15 -0300

    Add guest icon test.

---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Components/Icons/GuestTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tribe\Tickets\Partials\V2\Components\Icons;
+
+use Codeception\TestCase\WPTestCase;
+use Spatie\Snapshots\MatchesSnapshots;
+use tad\WP\Snapshots\WPHtmlOutputDriver;
+
+class Guest extends WPTestCase {
+
+	use MatchesSnapshots;
+
+	protected $partial_path = 'v2/components/icons/guest';
+
+	/**
+	 * @test
+	 */
+	public function test_should_render_icon() {
+
+		$template = tribe( 'tickets.editor.template' );
+		$html     = $template->template( $this->partial_path, [], false );
+
+		$this->assertMatchesSnapshot( $html, $driver );
+	}
+}
