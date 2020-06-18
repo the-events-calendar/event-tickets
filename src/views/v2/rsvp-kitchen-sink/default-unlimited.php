b5a3d9eb0f07fc6104b634cba3235516bd73d285
Juanfra Aldasoro <juanfraa@gmail.com>
2020-06-18 15:20:11 -0300

    Add kitchen sink unlimited

---
+++ src/views/v2/rsvp-kitchen-sink/default-unlimited.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Block: RSVP default unlimited
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/tickets/v2/rsvp-kitchen-sink/default-unlimited.php
+ *
+ * See more documentation about our Blocks Editor templating system.
+ *
+ * @link {INSERT_ARTICLE_LINK_HERE}
+ *
+ * @var Tribe__Tickets__Ticket_Object $rsvp The rsvp ticket object.
+ *
+ * @since TBD
+ *
+ * @version TBD
+ */
+?>
+<div class="tribe-tickets__rsvp-wrapper" data-rsvp-id="27">
+
+
+
+
+
+<div class="tribe-tickets__rsvp tribe-common-g-row tribe-common-g-row--gutters">
+
+	<div class="tribe-tickets__rsvp-details-wrapper tribe-common-g-col">
+<div class="tribe-tickets__rsvp-details">
+	<h3 class="tribe-tickets__rsvp-title tribe-common-h4">
+Job &amp; Career Fair Unlimited</h3>
+
+	<div class="tribe-tickets__rsvp-description tribe-common-b3">
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</div>
+
+	<div class="tribe-tickets__rsvp-attendance">
+<span class="tribe-tickets__rsvp-attendance-number tribe-common-h4">
+	0	</span>
+<span class="tribe-tickets__rsvp-attendance-going tribe-common-b3">
+	Going	</span>
+</div>
+
+	<div class="tribe-tickets__rsvp-availability tribe-common-b3">
+
+<span class="tribe-tickets__rsvp-availability-days-left tribe-common-b2--bold"> 41 </span> days left to RSVP
+</div>
+</div>
+</div>
+
+	<div class="tribe-tickets__rsvp-actions-wrapper tribe-common-g-col">
+<div class="tribe-tickets__rsvp-actions">
+
+
+		<div class="tribe-tickets__rsvp-actions-rsvp">
+<span class="tribe-common-h6">
+	RSVP Here	</span>
+
+
+<div class="tribe-tickets__rsvp-actions-rsvp-going">
+<button class="tribe-common-c-btn tribe-tickets__rsvp-actions-button-going" type="submit">
+	Going	</button>
+</div>
+
+
+</div>
+
+		</div>
+
+</div>
+
+</div>
+
+		</div>
