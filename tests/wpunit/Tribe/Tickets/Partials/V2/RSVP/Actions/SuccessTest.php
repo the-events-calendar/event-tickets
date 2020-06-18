6968168b1da6391d5f2b7aa7e1a8e6bfb636e8be
Juanfra Aldasoro <juanfraa@gmail.com>
2020-06-18 17:46:50 -0300

    Add rsvp action success test.

---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/RSVP/Actions/SuccessTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tribe\Tickets\Partials\V2\RSVP\Actions;
+
+use Codeception\TestCase\WPTestCase;
+use Spatie\Snapshots\MatchesSnapshots;
+use tad\WP\Snapshots\WPHtmlOutputDriver;
+
+class Success extends WPTestCase {
+
+	use MatchesSnapshots;
+
+	protected $partial_path = 'v2/rsvp/actions/success';
+
+	/**
+	 * @test
+	 */
+	public function test_should_render_success_message() {
+		$template     = tribe( 'tickets.editor.template' );
+		$_GET['step'] = 'success';
+
+		$html   = $template->template( $this->partial_path, [], false );
+		$driver = new WPHtmlOutputDriver( home_url(), 'http://test.tribe.dev' );
+
+		$this->assertMatchesSnapshot( $html, $driver );
+	}
+
+}
