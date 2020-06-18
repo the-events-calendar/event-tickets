940d4398ad8711b0c76a606b91abb08678e9f428
Juanfra Aldasoro <juanfraa@gmail.com>
2020-06-18 17:53:30 -0300

    Add RSVP messages must login test.

---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/RSVP/Messages/MustLoginTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tribe\Tickets\Partials\V2\RSVP\Messages;
+
+use Codeception\TestCase\WPTestCase;
+use Spatie\Snapshots\MatchesSnapshots;
+use tad\WP\Snapshots\WPHtmlOutputDriver;
+use Tribe\Test\PHPUnit\Traits\With_Post_Remapping;
+
+use Tribe\Tickets\Test\Commerce\RSVP\Ticket_Maker as RSVP_Ticket_Maker;
+
+
+class MustLogin extends WPTestCase {
+
+	use MatchesSnapshots;
+	use With_Post_Remapping;
+
+	use RSVP_Ticket_Maker;
+
+	protected $partial_path = 'v2/rsvp/messages/must-login';
+
+	/**
+	 * @test
+	 */
+	public function test_should_render_must_login() {
+		$template  = tribe( 'tickets.editor.template' );
+		$event     = $this->get_mock_event( 'events/single/1.json' );
+		$event_id  = $event->ID;
+		$ticket_id = $this->create_rsvp_ticket(
+			$event_id,
+			[
+				'meta_input' => [
+					'_capacity' => 5,
+				],
+			]
+		);
+
+		$ticket = tribe( 'tickets.rsvp' )->get_ticket( $event_id, $ticket_id );
+
+		$args = [
+			'must_login' => true,
+			'login_url'  => 'http://wordpress.test/wp-login.php',
+			'rsvp'       => $ticket,
+		];
+
+		$html   = $template->template( $this->partial_path, $args, false );
+		$driver = new WPHtmlOutputDriver( home_url(), 'http://wordpress.test' );
+		$driver->setTolerableDifferences( [ $ticket_id, $event_id ] );
+
+		$this->assertMatchesSnapshot( $html, $driver );
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_render_empty_if_must_login_is_false() {
+		$template = tribe( 'tickets.editor.template' );
+
+		$html   = $template->template( $this->partial_path, [ 'must_login' => false ], false );
+		$driver = new WPHtmlOutputDriver( home_url(), 'http://wordpress.test' );
+
+		$this->assertMatchesSnapshot( $html, $driver );
+	}
+}
