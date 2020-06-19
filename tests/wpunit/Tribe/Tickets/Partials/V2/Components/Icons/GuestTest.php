c69800734751fb24048b4c4657757759b86ad3b4
Juanfra Aldasoro <juanfraa@gmail.com>
2020-06-19 14:40:32 -0300

    Test icons with CSS classes.

--- tests/wpunit/Tribe/Tickets/Partials/V2/Components/Icons/GuestTest.php
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Components/Icons/GuestTest.php
@@ -18,7 +18,19 @@ class Guest extends WPTestCase {
 	public function test_should_render_icon() {
 
 		$template = tribe( 'tickets.editor.template' );
-		$html     = $template->template( $this->partial_path, [], false );
+		$html     = $template->template( $this->partial_path, [ 'classes' => [] ], false );
+		$driver   = new WPHtmlOutputDriver( home_url(), 'http://test.tribe.dev' );
+
+		$this->assertMatchesSnapshot( $html, $driver );
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_render_icon_with_custom_classes() {
+
+		$template = tribe( 'tickets.editor.template' );
+		$html     = $template->template( $this->partial_path, [ 'classes' => [ 'css-test-class' ] ], false );
 		$driver   = new WPHtmlOutputDriver( home_url(), 'http://test.tribe.dev' );
 
 		$this->assertMatchesSnapshot( $html, $driver );
