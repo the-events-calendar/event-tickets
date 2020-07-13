08bf3dcec2a704e6de300435600048f833fcb67c
Juanfra Aldasoro <juanfraa@gmail.com>
2020-07-13 12:08:56 -0300

    Test for form error template.

---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/RSVP/Form/__snapshots__/ErrorTest__test_should_render_error__1.php
@@ -0,0 +1,8 @@
+<?php return '<div class="tribe-tickets__form-message tribe-tickets__form-message--error tribe-common-b3 tribe-common-a11y-hidden">
+	<svg  class="tribe-tickets-svgicon tribe-tickets__form-message--error-icon"  xmlns="http://www.w3.org/2000/svg" width="18" height="18"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="8" cy="8" r="7.467" stroke="#141827" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/><circle id="dot" cx="8" cy="11.733" r="1.067" fill="#141827" fill-rule="nonzero"/><path stroke="#141827" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8 3.733v4.8"/></g></svg>	<span class="tribe-tickets__form-message-text">
+		<strong>
+			Whoops		</strong>
+		<p>There is a field that requires information.</p>
+	</span>
+</div>
+';
