8dae6dfbd5ad11ee403f8f48b30df8eafecc3810
Scott Kingsley Clark <lol@scottkclark.com>
2020-07-15 19:53:09 -0500

    [ET-808] Add "Not Going" option to classic editor if using new RSVP views
    [ET-808]

---
+++ src/admin-views/rsvp-metabox-not-going.php
@@ -0,0 +1,20 @@
+<div
+	class="input_block ticket_advanced_Tribe__Tickets__RSVP tribe-dependent"
+	data-depends="#Tribe__Tickets__RSVP_radio"
+	data-condition-is-checked
+>
+	<label
+		for="Tribe__Tickets__RSVP_not_going"
+		class="ticket_form_label ticket_form_left"
+	>
+		<?php esc_html_e( 'Not Going:', 'event-tickets' ); ?>
+	</label>
+	<input
+		type='checkbox' id='Tribe__Tickets__RSVP_not_going'
+		name='tribe-ticket[not_going]'
+		class="ticket_field tribe-rsvp-field-not-going ticket_form_right"
+		value='yes'
+		<?php checked( $not_going ); ?>
+	/>
+	<span class="tribe_soft_note ticket_form_right"><?php esc_html_e( 'Enable "Not Going" responses', 'event-tickets' ); ?></span>
+</div>
