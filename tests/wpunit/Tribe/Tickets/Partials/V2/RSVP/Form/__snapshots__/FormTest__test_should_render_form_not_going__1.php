a19d0e8003851d023bae7098da970918248390a3
Juanfra Aldasoro <juanfraa@gmail.com>
2020-07-17 11:37:00 -0300

    Update snapshots.

--- tests/wpunit/Tribe/Tickets/Partials/V2/RSVP/Form/__snapshots__/FormTest__test_should_render_form_not_going__1.php
+++ tests/wpunit/Tribe/Tickets/Partials/V2/RSVP/Form/__snapshots__/FormTest__test_should_render_form_not_going__1.php
@@ -15,14 +15,6 @@
 
 		<div class="tribe-tickets__rsvp-form-content tribe-tickets__form">
 
-			<div class="tribe-tickets__form-message tribe-tickets__form-message--error tribe-common-b3 tribe-common-a11y-hidden">
-	<svg  class="tribe-tickets-svgicon tribe-tickets__form-message--error-icon"  xmlns="http://www.w3.org/2000/svg" width="18" height="18"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="8" cy="8" r="7.467" stroke="#141827" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/><circle id="dot" cx="8" cy="11.733" r="1.067" fill="#141827" fill-rule="nonzero"/><path stroke="#141827" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8 3.733v4.8"/></g></svg>	<span class="tribe-tickets__form-message-text">
-		<strong>
-			Whoops		</strong>
-		<p>There is a field that requires information.</p>
-	</span>
-</div>
-
 			<div class="tribe-common-b1 tribe-tickets__form-field tribe-tickets__form-field--required">
 	<label
 		class="tribe-common-b2--min-medium tribe-tickets__form-field-label"
