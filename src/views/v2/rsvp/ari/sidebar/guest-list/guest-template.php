d642a262c10ee9a50bec21836fbc2bb2b3de2953
Juanfra Aldasoro <juanfraa@gmail.com>
2020-07-30 16:57:00 -0300

    Remove unused data attribute

--- src/views/v2/rsvp/ari/sidebar/guest-list/guest-template.php
+++ src/views/v2/rsvp/ari/sidebar/guest-list/guest-template.php
@@ -16,10 +16,7 @@
 	id="tmpl-tribe-tickets__rsvp-ar-guest-list-item-template-<?php echo esc_attr( $rsvp->ID ); ?>"
 	type="text/template"
 >
-	<li
-		class="tribe-tickets__rsvp-ar-guest-list-item"
-		data-guest-number="{{data.attendee_id + 1}}"
-	>
+	<li class="tribe-tickets__rsvp-ar-guest-list-item">
 		<button
 			class="tribe-tickets__rsvp-ar-guest-list-item-button tribe-tickets__rsvp-ar-guest-list-item-button--inactive"
 			type="button"
