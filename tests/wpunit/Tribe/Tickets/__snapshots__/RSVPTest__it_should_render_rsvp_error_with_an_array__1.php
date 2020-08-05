8ca44f610df99e81174f9e3b52d7f40173b57df5
Juanfra Aldasoro <juanfraa@gmail.com>
2020-08-05 12:05:25 -0300

    Update snapshots.

--- tests/wpunit/Tribe/Tickets/__snapshots__/RSVPTest__it_should_render_rsvp_error_with_an_array__1.php
+++ tests/wpunit/Tribe/Tickets/__snapshots__/RSVPTest__it_should_render_rsvp_error_with_an_array__1.php
@@ -1,5 +1,5 @@
 <?php return '<div class="tribe-tickets__rsvp-message tribe-tickets__rsvp-message--error tribe-common-b3">
-	<svg  class="tribe-tickets-svgicon tribe-tickets__rsvp-message--error-icon"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 15 15"><defs/><path stroke="#111" d="M14 1L6.5 8.5"/><path stroke="#111" stroke-linecap="square" d="M14 1L9.14286 13 6.85714 8.14286 2 5.85714 14 1z" clip-rule="evenodd"/></svg>
+	<svg  class="tribe-tickets-svgicon tribe-tickets__rsvp-message--error-icon"  xmlns="http://www.w3.org/2000/svg" width="18" height="18"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="8" cy="8" r="7.467" stroke="#141827" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/><circle id="dot" cx="8" cy="11.733" r="1.067" fill="#141827" fill-rule="nonzero"/><path stroke="#141827" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M8 3.733v4.8"/></g></svg>
 			<span class="tribe-tickets__rsvp-message-text">
 			<strong>
 				There was an error here			</strong>
