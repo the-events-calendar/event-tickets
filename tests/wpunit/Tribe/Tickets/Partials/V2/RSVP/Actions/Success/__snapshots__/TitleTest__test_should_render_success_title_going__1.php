dabd6682a93994baab56334100dbb783c70ac8c2
Juanfra Aldasoro <juanfraa@gmail.com>
2020-08-10 15:34:19 -0300

    Update snapshots.

--- tests/wpunit/Tribe/Tickets/Partials/V2/RSVP/Actions/Success/__snapshots__/TitleTest__test_should_render_success_title_going__1.php
+++ tests/wpunit/Tribe/Tickets/Partials/V2/RSVP/Actions/Success/__snapshots__/TitleTest__test_should_render_success_title_going__1.php
@@ -1,6 +1,6 @@
 <?php return '<div class="tribe-tickets__rsvp-actions-success-going">
 	<em class="tribe-tickets__rsvp-actions-success-going-check-icon"></em>
 	<span class="tribe-tickets__rsvp-actions-success-going-text tribe-common-h4 tribe-common-h6--min-medium">
-					You are going			</span>
+		You are going	</span>
 </div>
 ';
