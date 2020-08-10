48230debe8bd4dad283eb97c1651c5eb68d52dfe
Juanfra Aldasoro <juanfraa@gmail.com>
2020-08-10 14:48:57 -0300

    Add text for succes (not-going and going)

--- src/views/v2/rsvp/actions/success/title.php
+++ src/views/v2/rsvp/actions/success/title.php
@@ -11,13 +11,17 @@
  * @link {INSERT_ARTICLE_LINK_HERE}
  *
  * @since 4.12.3
- * @version 4.12.3
+ * @version TBD
  */
 
 ?>
 <div class="tribe-tickets__rsvp-actions-success-going">
 	<em class="tribe-tickets__rsvp-actions-success-going-check-icon"></em>
 	<span class="tribe-tickets__rsvp-actions-success-going-text tribe-common-h4 tribe-common-h6--min-medium">
-		<?php esc_html_e( 'You are going', 'event-tickets' ); ?>
+		<?php if ( ! empty( $is_going ) ) : ?>
+			<?php esc_html_e( 'You are going', 'event-tickets' ); ?>
+		<?php else : ?>
+			<?php esc_html_e( "Can't go", 'event-tickets' ); ?>
+		<?php endif; ?>
 	</span>
 </div>
