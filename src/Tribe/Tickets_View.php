c43b0ad802888597b3b76748a1716502e4e61915
Juanfra Aldasoro <juanfraa@gmail.com>
2020-09-11 15:52:03 +0200

    Add template global vars for the tickets block.

--- src/Tribe/Tickets_View.php
+++ src/Tribe/Tickets_View.php
@@ -1012,22 +1012,60 @@ class Tribe__Tickets__Tickets_View {
 		/** @var Tribe__Tickets__Editor__Blocks__Tickets $blocks_tickets */
 		$blocks_tickets = tribe( 'tickets.editor.blocks.tickets' );
 
+		/** @var Tribe__Settings_Manager $settings_manager */
+		$settings_manager = tribe( 'settings.manager' );
+
+		$threshold = $settings_manager::get_option( 'ticket-display-tickets-left-threshold', null );
+
+		/**
+		 * Overwrites the threshold to display "# tickets left".
+		 *
+		 * @since 4.11.1
+		 *
+		 * @param int   $threshold Stock threshold to trigger display of "# tickets left"
+		 * @param int   $post_id   WP_Post/Event ID.
+		 */
+		$threshold = absint( apply_filters( 'tribe_display_tickets_block_tickets_left_threshold', $threshold, $post_id ) );
+
+		/**
+		 * Allow filtering of the button name for the tickets block.
+		 *
+		 * @since 4.11.0
+		 *
+		 * @param string $button_name The button name. Set to cart-button to send to cart on submit, or set to checkout-button to send to checkout on submit.
+		 */
+		$submit_button_name = apply_filters( 'tribe_tickets_ticket_block_submit', 'cart-button' );
+
+		/**
+		 * Show original price on sale.
+		 *
+		 * @param bool Wether the original price should be shown on sale or not. Default is true.
+		 *
+		 * @return bool Wether the original price should be shown on sale or not.
+		 */
+		$show_original_price_on_sale = apply_filters( 'tribe_tickets_show_original_price_on_sale', true );
+
 		// Load assets manually.
 		$blocks_tickets->assets();
 
 		$tickets = $provider->get_tickets( $post_id );
 
 		$args = [
-			'post_id'             => $post_id,
-			'provider'            => $provider,
-			'provider_id'         => $provider->class_name,
-			'tickets'             => $tickets,
-			'cart_classes'        => [ 'tribe-block', 'tribe-tickets' ],
-			'tickets_on_sale'     => $blocks_tickets->get_tickets_on_sale( $tickets ),
-			'has_tickets_on_sale' => tribe_events_has_tickets_on_sale( $post_id ),
-			'is_sale_past'        => $blocks_tickets->get_is_sale_past( $tickets ),
-			'is_sale_future'      => $blocks_tickets->get_is_sale_future( $tickets ),
-			'currency'            => tribe( 'tickets.commerce.currency' ),
+			'post_id'                     => $post_id,
+			'provider'                    => $provider,
+			'provider_id'                 => $provider->class_name,
+			'tickets'                     => $tickets,
+			'cart_classes'                => [ 'tribe-block', 'tribe-tickets' ], // @todo: deprecate with V1.
+			'tickets_on_sale'             => $blocks_tickets->get_tickets_on_sale( $tickets ),
+			'has_tickets_on_sale'         => tribe_events_has_tickets_on_sale( $post_id ),
+			'is_sale_past'                => $blocks_tickets->get_is_sale_past( $tickets ),
+			'is_sale_future'              => $blocks_tickets->get_is_sale_future( $tickets ),
+			'currency'                    => tribe( 'tickets.commerce.currency' ),
+			'threshold'                   => $threshold,
+			'must_login'                  => ! is_user_logged_in() && $provider->login_required(),
+			'show_original_price_on_sale' => $show_original_price_on_sale,
+			'is_mini'                     => null,
+			'is_modal'                    => null,
 		];
 
 		// Add the rendering attributes into global context.
