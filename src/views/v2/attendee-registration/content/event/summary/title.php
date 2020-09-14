b9c7d5332f0d39461924250a7f97c203e33c51ee
Juanfra Aldasoro <juanfraa@gmail.com>
2020-09-14 18:14:21 +0200

    Add Attendee Registration v2 templates.

---
+++ src/views/v2/attendee-registration/content/event/summary/title.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Attendee registration
+ * Content > Event > Summary > Title
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/tickets/v2/attendee-registration/event/summary/title.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link http://m.tri.be/1amp
+ *
+ * @since TBD
+ * @version TBD
+ *
+ * @var int $post_id The event/post ID.
+ */
+
+?>
+<div class="tribe-tickets__registration__title">
+	<header>
+		<h2 class="tribe-common-h4 tribe-common-h3--min-medium">
+			<a href="<?php the_permalink( $post_id ); ?>">
+				<?php echo get_the_title( $post_id ); ?>
+			</a>
+		</h2>
+	</header>
+</div>
