75fcf1b7450ce632b7ffa3c8f117a3da20da19a1
Juanfra Aldasoro <juanfraa@gmail.com>
2020-09-14 18:08:09 +0200

    Add modal v2 templates.

---
+++ src/views/v2/modal/attendee-registration/title.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Modal: Attendee Registration > Title
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/tickets/v2/modal/attendee-registration/title.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link http://m.tri.be/1amp
+ *
+ * @since TBD
+ *
+ * @version TBD
+ */
+
+?>
+<h2 class="tribe-common-h3 tribe-common-h4--min-medium tribe-common-h--alt tribe-tickets__item__attendee__fields__title">
+	<?php esc_html_e( 'Attendee Details', 'event-tickets' ); ?>
+</h2>
