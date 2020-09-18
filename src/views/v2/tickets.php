940dc8ee798aeb277c51d2f1afed7bf630d4d6d7
Juanfra Aldasoro <juanfraa@gmail.com>
2020-09-18 16:58:02 +0200

    Add wrapper class. Use post id as data attribute.

--- src/views/v2/tickets.php
+++ src/views/v2/tickets.php
@@ -35,6 +35,7 @@ if ( ! $is_sale_future && ( ! $provider || ! $tickets ) ) {
 $classes = [
 	'tribe-common',
 	'event-tickets',
+	'tribe-tickets__tickets-wrapper',
 ];
 
 ?>
@@ -49,6 +50,7 @@ $classes = [
 		data-provider="<?php echo esc_attr( $provider->class_name ); ?>"
 		autocomplete="off"
 		data-provider-id="<?php echo esc_attr( $provider->orm_provider ); ?>"
+		data-post-id="<?php echo esc_attr( $post_id ); ?>"
 		novalidate
 	>
 
