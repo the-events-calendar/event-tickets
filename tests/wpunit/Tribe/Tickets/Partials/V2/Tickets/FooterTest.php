1a49e88b73f6e54f9c6e8f5bd189a118ff7b52b4
rafsuntaskin <rafsun.aiub@gmail.com>
2020-09-23 19:55:57 +0600

    Added more empty return tests

--- tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/FooterTest.php
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/FooterTest.php
@@ -21,8 +21,42 @@ class FooterTest extends WPTestCase {
 		$template = tribe( 'tickets.editor.template' );
 
 		$args = [
-			'is_mini' => null,
+			'is_mini' => false,
 			'tickets_on_sale' => [],
+		];
+
+		$html   = $template->template( $this->partial_path, $args, false );
+		$driver = new WPHtmlOutputDriver( home_url(), 'http://wordpress.test' );
+
+		$this->assertMatchesSnapshot( $html, $driver );
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_not_render_if_is_mini_and_empty_ticket_on_sale() {
+		$template = tribe( 'tickets.editor.template' );
+
+		$args = [
+			'is_mini' => true,
+			'tickets_on_sale' => [],
+		];
+
+		$html   = $template->template( $this->partial_path, $args, false );
+		$driver = new WPHtmlOutputDriver( home_url(), 'http://wordpress.test' );
+
+		$this->assertMatchesSnapshot( $html, $driver );
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_not_render_if_not_is_mini_and_not_empty_ticket_on_sale() {
+		$template = tribe( 'tickets.editor.template' );
+
+		$args = [
+			'is_mini' => false,
+			'tickets_on_sale' => [ 'Heck', 'Yeah' ],
 		];
 
 		$html   = $template->template( $this->partial_path, $args, false );
