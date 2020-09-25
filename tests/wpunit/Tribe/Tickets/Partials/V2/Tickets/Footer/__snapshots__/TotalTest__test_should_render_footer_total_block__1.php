797e6022d86d3020593da61ed6e3d7b9d9f84ae6
rafsuntaskin <rafsun.aiub@gmail.com>
2020-09-25 15:38:34 +0600

    Added tests for 'footer/total'

---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Footer/__snapshots__/TotalTest__test_should_render_footer_total_block__1.php
@@ -0,0 +1,12 @@
+<?php return '<div class="tribe-common-b2 tribe-tickets__footer__total">
+	<span class="tribe-tickets__footer__total__label">
+		Total:	</span>
+	<span class="tribe-tickets__footer__total__wrap">
+		
+				<span class="tribe-formatted-currency-wrap tribe-currency-prefix">
+					<span class="tribe-currency-symbol">$</span>
+					<span class="tribe-amount">0.00</span>
+				</span>
+				</span>
+</div>
+';
