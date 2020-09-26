ae2b251779faf8e48ca0f36dab3ee729e6746844
rafsuntaskin <rafsun.aiub@gmail.com>
2020-09-26 15:37:27 +0600

    added default driver and simplify snapshot assertion

--- tests/_support/Partials/V2TestCase.php
+++ tests/_support/Partials/V2TestCase.php
@@ -3,6 +3,7 @@
 namespace Tribe\Tickets\Test\Partials;
 
 use Codeception\TestCase\WPTestCase;
+use tad\WP\Snapshots\WPHtmlOutputDriver;
 use Spatie\Snapshots\MatchesSnapshots;
 use Tribe\Test\PHPUnit\Traits\With_Post_Remapping;
 
@@ -12,9 +13,32 @@ use Tribe\Test\PHPUnit\Traits\With_Post_Remapping;
  */
 abstract class V2TestCase extends WPTestCase {
 
-	use MatchesSnapshots;
+	use MatchesSnapshots {
+		assertMatchesSnapshot as _assertMatchesSnapshot;
+	}
+
 	use With_Post_Remapping;
 
 	/** @var string Test site home URL (not HTTPS). */
 	public $base_url = 'http://wordpress.test/';
+
+	/**
+	 * Get the HTML Driver.
+	 *
+	 * @return WPHtmlOutputDriver
+	 */
+	public function get_html_output_driver() {
+		return new WPHtmlOutputDriver( home_url(), $this->base_url );
+	}
+
+	/**
+	 * Override snapshot assertion with support for default driver
+	 *
+	 * @param $html
+	 * @param WPHtmlOutputDriver|null $driver
+	 */
+	public function assertMatchesSnapshot( $html, WPHtmlOutputDriver $driver = null ) {
+		$driver = $driver ? $driver : $this->get_html_output_driver();
+		$this->_assertMatchesSnapshot( $html, $driver );
+	}
 }
