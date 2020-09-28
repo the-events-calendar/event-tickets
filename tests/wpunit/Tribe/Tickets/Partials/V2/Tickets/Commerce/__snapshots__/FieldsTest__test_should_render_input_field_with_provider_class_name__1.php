447e80ee7998d6aca49f350bed058e69214ccdc4
rafsuntaskin <rafsun.aiub@gmail.com>
2020-09-28 12:11:57 +0600

    added test for commerce/fields

---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Commerce/__snapshots__/FieldsTest__test_should_render_input_field_with_provider_class_name__1.php
@@ -0,0 +1,8 @@
+<?php return '<input
+	type="hidden"
+	id="add"
+	name="add"
+	value="1"
+/>
+<input name="provider" value="Tribe__Tickets__Commerce__PayPal__Main" class="tribe-tickets-provider" type="hidden">
+';
