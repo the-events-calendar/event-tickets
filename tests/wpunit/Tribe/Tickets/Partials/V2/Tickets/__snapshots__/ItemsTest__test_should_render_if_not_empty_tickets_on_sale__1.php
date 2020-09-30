c9fd73c1517f0866418aa7a0f1948bce57cb9e28
rafsuntaskin <rafsun.aiub@gmail.com>
2020-09-30 13:25:02 +0600

    Added snapshots for 'tickets/items/item.php'

---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/__snapshots__/ItemsTest__test_should_render_if_not_empty_tickets_on_sale__1.php
@@ -0,0 +1,141 @@
+<?php return '<div
+	id="tribe-block-tickets-item-8"
+	 class="tribe-tickets__item post-8 tribe_tpp_tickets type-tribe_tpp_tickets status-publish hentry" 	data-ticket-id="8"
+	data-available="false"
+	data-has-shared-cap="false"
+	>
+
+	
+	<div  class="tribe-common-h7 tribe-common-h6--min-medium tribe-tickets__item__content__title"  >
+		Test ticket for 8</div>
+
+
+<div
+	id="tribe__details__content--8"
+	class="tribe-common-b2 tribe-common-b3--min-medium tribe-tickets__item__details__content"
+>
+	Test ticket description for 8</div>
+<div  class="tribe-tickets__item__extra" >
+
+	<div  class="tribe-common-b2 tribe-common-b1--min-medium tribe-tickets__item__extra__price" >
+		<span class="tribe-tickets__sale_price">
+		
+				<span class="tribe-formatted-currency-wrap tribe-currency-prefix">
+					<span class="tribe-currency-symbol">$</span>
+					<span class="tribe-amount">6.00</span>
+				</span>
+						</span>
+</div>
+
+	<div class="tribe-common-b3 tribe-tickets__item__extra__available">
+	
+	<span class="tribe-tickets__item__extra__available__quantity"> 100 </span> available</div>
+
+	<div class="tribe-tickets__item__details__summary">
+	<button
+		type="button"
+		class="tribe-common-b3 tribe-tickets__item__details__summary--more"
+		aria-controls="tribe__details__content--8"
+		tabindex="0"
+	>
+		<span class="screen-reader-text tribe-common-a11y-visual-hide">
+			Open the ticket description.		</span>
+		More	</button>
+	<button
+		type="button"
+		class="tribe-common-b3 tribe-tickets__item__details__summary--less"
+		aria-controls="tribe__details__content--8"
+		tabindex="0"
+	>
+		<span class="screen-reader-text tribe-common-a11y-visual-hide">Close the ticket description.</span>
+		Less	</button>
+</div>
+
+</div>
+
+	<div  class="tribe-common-h4 tribe-tickets__item__quantity" >
+			<div class="tribe-common-b2 tribe-common-b2--bold tribe-tickets__item__quantity__unavailable">
+	Sold Out</div>
+	</div>
+
+	
+	
+		<input
+		name="attendee[optout]"
+		value="1"
+		type="hidden"
+	/>
+	
+	
+</div>
+<div
+	id="tribe-block-tickets-item-9"
+	 class="tribe-tickets__item post-9 tribe_tpp_tickets type-tribe_tpp_tickets status-publish hentry" 	data-ticket-id="9"
+	data-available="false"
+	data-has-shared-cap="false"
+	>
+
+	
+	<div  class="tribe-common-h7 tribe-common-h6--min-medium tribe-tickets__item__content__title"  >
+		Test ticket for 8</div>
+
+
+<div
+	id="tribe__details__content--9"
+	class="tribe-common-b2 tribe-common-b3--min-medium tribe-tickets__item__details__content"
+>
+	Test ticket description for 8</div>
+<div  class="tribe-tickets__item__extra" >
+
+	<div  class="tribe-common-b2 tribe-common-b1--min-medium tribe-tickets__item__extra__price" >
+		<span class="tribe-tickets__sale_price">
+		
+				<span class="tribe-formatted-currency-wrap tribe-currency-prefix">
+					<span class="tribe-currency-symbol">$</span>
+					<span class="tribe-amount">5.00</span>
+				</span>
+						</span>
+</div>
+
+	<div class="tribe-common-b3 tribe-tickets__item__extra__available">
+	
+	<span class="tribe-tickets__item__extra__available__quantity"> 100 </span> available</div>
+
+	<div class="tribe-tickets__item__details__summary">
+	<button
+		type="button"
+		class="tribe-common-b3 tribe-tickets__item__details__summary--more"
+		aria-controls="tribe__details__content--9"
+		tabindex="0"
+	>
+		<span class="screen-reader-text tribe-common-a11y-visual-hide">
+			Open the ticket description.		</span>
+		More	</button>
+	<button
+		type="button"
+		class="tribe-common-b3 tribe-tickets__item__details__summary--less"
+		aria-controls="tribe__details__content--9"
+		tabindex="0"
+	>
+		<span class="screen-reader-text tribe-common-a11y-visual-hide">Close the ticket description.</span>
+		Less	</button>
+</div>
+
+</div>
+
+	<div  class="tribe-common-h4 tribe-tickets__item__quantity" >
+			<div class="tribe-common-b2 tribe-common-b2--bold tribe-tickets__item__quantity__unavailable">
+	Sold Out</div>
+	</div>
+
+	
+	
+		<input
+		name="attendee[optout]"
+		value="1"
+		type="hidden"
+	/>
+	
+	
+</div>
+';
