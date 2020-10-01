6a578f259a484e00294cd1f1e79f74cb29d59918
rafsuntaskin <rafsun.aiub@gmail.com>
2020-10-01 14:51:13 +0600

    added tests for 'tickets/item/extra/available/unlimited'

---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Item/Extra/Available/UnlimitedTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tribe\Tickets\Partials\V2\Tickets\Item\Extra\Available;
+
+use Tribe\Tickets\Test\Partials\V2TestCase;
+use Tribe\Tickets\Test\Commerce\PayPal\Ticket_Maker as PayPal_Ticket_Maker;
+
+class UnlimitedTest extends V2TestCase {
+
+	use PayPal_Ticket_Maker;
+
+	protected $partial_path = 'v2/tickets/item/extra/available/unlimited';
+
+	/**
+	 * Get all the default args required for this template
+	 *
+	 * @return array
+	 */
+	public function get_default_args() {
+		/**
+		 * @var \Tribe__Tickets__Commerce__PayPal__Main
+		 */
+		$provider = tribe_get_class_instance( 'Tribe__Tickets__Commerce__PayPal__Main' );
+
+		$event = $this->get_mock_event( 'events/single/1.json' );
+		$ids   = $this->create_many_paypal_tickets( 1, $event->ID );
+
+		$ticket = $provider->get_ticket( $event->ID, $ids[0] );
+
+		// default Ticket scenario.
+		$ticket->capacity = - 1;
+
+		return [
+			'ticket' => $ticket,
+		];
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_render_unlimited_text() {
+		$template = tribe( 'tickets.editor.template' );
+		$html     = $template->template( $this->partial_path, $this->get_default_args(), false );
+		$this->assertMatchesSnapshot( $html );
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_not_render_if_show_unlimited_is_false() {
+		add_filter( 'tribe_tickets_block_show_unlimited_availability', '__return_false' );
+
+		$template = tribe( 'tickets.editor.template' );
+		$html     = $template->template( $this->partial_path, $this->get_default_args(), false );
+		$this->assertMatchesSnapshot( $html );
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_not_render_if_available_quantity_is_not_unlimited() {
+		$template = tribe( 'tickets.editor.template' );
+
+		$args = $this->get_default_args();
+
+		// set a fixed capacity
+		$args['ticket']->capacity = 25;
+
+		$html = $template->template( $this->partial_path, $args, false );
+		$this->assertMatchesSnapshot( $html );
+	}
+}
