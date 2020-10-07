98ad7c68801a21c595bb957fd8f25d83e9f3abf3
Juanfra Aldasoro <juanfraa@gmail.com>
2020-10-07 12:08:57 +0200

    Include tickets v2 if it's enabled.

--- src/Tribe/Editor/Blocks/Tickets.php
+++ src/Tribe/Editor/Blocks/Tickets.php
@@ -107,7 +107,7 @@ extends Tribe__Editor__Blocks__Abstract {
 			$tickets_block_dependencies[] = 'wp-i18n';
 		}
 
-		// Check wether we use v1 or v2. We need to update this when we deprecate tickets v1.
+		// Check whether we use v1 or v2. We need to update this when we deprecate tickets v1.
 		$tickets_js = tribe_tickets_new_views_is_enabled() ? 'v2/tickets-block.js' : 'tickets-block.js';
 
 		tribe_asset(
