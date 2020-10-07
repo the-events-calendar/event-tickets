c32a4bec467a64841498b765bfc13aad74c3b494
rafsuntaskin <rafsun.aiub@gmail.com>
2020-10-08 00:53:26 +0600

    add wp_kses and fix doc format

--- src/views/v2/tickets/item/content/description.php
+++ src/views/v2/tickets/item/content/description.php
@@ -38,5 +38,5 @@ $ticket_details_id .= '--' . $ticket->ID;
 	id="<?php echo esc_attr( $ticket_details_id ); ?>"
 	class="tribe-common-b2 tribe-common-b3--min-medium tribe-tickets__item__details__content"
 >
-	<?php echo esc_html( $ticket->description ); ?>
+	<?php echo wp_kses_post( $ticket->description ); ?>
 </div>
