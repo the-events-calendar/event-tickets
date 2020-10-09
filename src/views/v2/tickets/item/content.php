155e65519d58a52b79ef7ccfc2ae54e331d62026
Clifford Paulick <tko@tourkick.com>
2020-10-08 22:56:19 -0500

    short array syntax and formatting

--- src/views/v2/tickets/item/content.php
+++ src/views/v2/tickets/item/content.php
@@ -8,13 +8,13 @@
  *
  * See more documentation about our views templating system.
  *
- * @link http://m.tri.be/1amp
+ * @link    http://m.tri.be/1amp
  *
- * @since TBD
+ * @since   TBD
  * @version TBD
  *
- * @var Tribe__Tickets__Ticket_Object   $ticket Ticket Object.
- * @var int                             $key Ticket Item index
+ * @var Tribe__Tickets__Ticket_Object $ticket Ticket Object.
+ * @var int                           $key    Ticket Item index
  */
 
 if ( empty( $ticket ) ) {
