4f056f1f6e073db0926e68eb92371d03d7b29a2e
rafsuntaskin <rafsun.aiub@gmail.com>
2020-10-14 12:55:08 +0600

    Add snapshots 'Item/quantity/add' , 'Item/quantity/remove', 'Item/quantity/unavailable'

---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Item/Quantity/__snapshots__/AddTest__test_should_render_add_ticket_html__1.php
@@ -0,0 +1,8 @@
+<?php return '<button
+	class="tribe-tickets__item__quantity__add"
+	title="Increase ticket quantity for Test ticket for 8"
+	type="button"
+>
+	<span class="screen-reader-text tribe-common-a11y-visual-hide">Increase ticket quantity for Test ticket for 8</span>
+	+</button>
+';
---
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Item/Quantity/__snapshots__/RemoveTest__test_should_render_remove_ticket_html__1.php
@@ -0,0 +1,8 @@
+<?php return '<button
+	class="tribe-tickets__item__quantity__remove"
+	title="Decrease ticket quantity for Test ticket for 8"
+	type="button"
+>
+	<span class="screen-reader-text tribe-common-a11y-visual-hide">Decrease ticket quantity for Test ticket for 8</span>
+	-</button>
+';
