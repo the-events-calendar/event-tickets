42242f8fe96f0916d33ffa3e3448092a58b13d30
Clifford Paulick <tko@tourkick.com>
2020-10-17 08:33:05 -0500

    use method instead of protected property

--- src/views/v2/rsvp/details/attendance.php
+++ src/views/v2/rsvp/details/attendance.php
@@ -41,7 +41,7 @@ if ( ! $rsvp->show_description() || empty( $rsvp->description ) ) {
 ?>
 <div class="tribe-tickets__rsvp-attendance">
 	<span <?php tribe_classes( $classes ); ?>>
-		<?php echo esc_html( $rsvp->qty_sold ); ?>
+		<?php echo esc_html( $rsvp->qty_sold() ); ?>
 	</span>
 	<span class="tribe-tickets__rsvp-attendance-going tribe-common-h7 tribe-common-h--alt tribe-common-b3--min-medium">
 		<?php echo esc_html_x( 'Going', 'Label below the attendance number', 'event-tickets' ); ?>
