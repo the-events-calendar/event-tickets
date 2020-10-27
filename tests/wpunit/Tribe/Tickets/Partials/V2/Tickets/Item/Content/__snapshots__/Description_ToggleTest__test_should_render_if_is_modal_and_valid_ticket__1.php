7ff855a44adf979740dc3505f508dd95eddcd907
Juanfra Aldasoro <juanfraa@gmail.com>
2020-10-27 13:14:42 +0100

    Update snapshots.

--- tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Item/Content/__snapshots__/Description_ToggleTest__test_should_render_if_is_modal_and_valid_ticket__1.php
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Item/Content/__snapshots__/Description_ToggleTest__test_should_render_if_is_modal_and_valid_ticket__1.php
@@ -2,7 +2,7 @@
 	<button
 		type="button"
 		class="tribe-common-b3 tribe-tickets__tickets-item-details-summary-button--more"
-		aria-controls="tribe__details__content1--12699"
+		aria-controls="tribe__details__content__modal--21"
 		tabindex="0"
 	>
 		<span class="screen-reader-text tribe-common-a11y-visual-hide">Open the ticket description.</span>
@@ -10,7 +10,7 @@
 	<button
 		type="button"
 		class="tribe-common-b3 tribe-tickets__tickets-item-details-summary-button--less"
-		aria-controls="tribe__details__content1--12699"
+		aria-controls="tribe__details__content__modal--21"
 		tabindex="0"
 	>
 		<span class="screen-reader-text tribe-common-a11y-visual-hide">Close the ticket description.</span>
