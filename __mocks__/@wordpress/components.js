5a5e42c2ce4d01a01a60d6e49647e77f820323c9
Paul <kim.paul.ms@gmail.com>
2020-10-30 02:19:53 +0800

    add radio control to wordpress components mocks

--- __mocks__/@wordpress/components.js
+++ __mocks__/@wordpress/components.js
@@ -14,4 +14,5 @@ export const Modal = ( { title, children } ) => (
 export const Dashicon = ( { className, icon } ) => <span className={ className }>{ icon }</span>;
 export const Dropdown = () => <span>Dropdown</span>;
 export const Tooltip = () => <span>Tooltip</span>;
-export const PanelBody = ({ children }) => <span className="PanelBody">{ children }</span>
+export const PanelBody = ({ children }) => <span className="PanelBody">{ children }</span>;
+export const RadioControl = () => <input type="radio" value="" />;
