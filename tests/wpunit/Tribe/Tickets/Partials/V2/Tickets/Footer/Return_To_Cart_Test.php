8d260e1bdde69f5e085bbb0c27f6875dd663db36
Clifford Paulick <tko@tourkick.com>
2020-10-30 00:16:36 -0500

    Return To Cart Test use V2TestCase & add to cart

--- tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Footer/Return_To_Cart_Test.php
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Footer/Return_To_Cart_Test.php
@@ -2,13 +2,12 @@
 
 namespace Tribe\Tickets\Partials\V2\Tickets\Footer;
 
-use Codeception\TestCase\WPTestCase;
-use Spatie\Snapshots\MatchesSnapshots;
-use tad\WP\Snapshots\WPHtmlOutputDriver;
+use Tribe\Tickets\Test\Partials\V2TestCase;
+use Tribe\Tickets\Test\Commerce\PayPal\Ticket_Maker as PayPal_Ticket_Maker;
 
-class Return_To_Cart_Test extends WPTestCase {
+class Return_To_Cart_Test extends V2TestCase {
 
-	use MatchesSnapshots;
+	use PayPal_Ticket_Maker;
 
 	protected $partial_path = 'v2/tickets/footer/return-to-cart';
 
@@ -18,10 +17,22 @@ class Return_To_Cart_Test extends WPTestCase {
 	 * @return array
 	 */
 	public function get_default_args() {
+		/**
+		 * @var \Tribe__Tickets__Commerce__PayPal__Main
+		 */
+		$provider = tribe_get_class_instance( 'Tribe__Tickets__Commerce__PayPal__Main' );
+
+		$event_id = $this->factory()->event->create();
+
+		$ticket_ids = $this->create_many_paypal_tickets( 1, $event_id );
+
 		return [
-			'is_mini'      => true,
-			'cart_url'     => 'http://wordpress.test/cart/?foo',
-			'checkout_url' => 'http://wordpress.test/checkout/?bar',
+			'provider'       => $provider,
+			'post_id'        => $event_id,
+			'test_ticket_id' => $ticket_ids[0],
+			'is_mini'        => true,
+			'cart_url'       => 'http://wordpress.test/cart/?foo',
+			'checkout_url'   => 'http://wordpress.test/checkout/?bar',
 		];
 	}
 
@@ -37,10 +48,9 @@ class Return_To_Cart_Test extends WPTestCase {
 
 		$args = array_merge( $this->get_default_args(), $override );
 
-		$html   = $template->template( $this->partial_path, $args, false );
-		$driver = new WPHtmlOutputDriver( home_url(), 'http://wordpress.test' );
+		$html = $template->template( $this->partial_path, $args, false );
 
-		$this->assertMatchesSnapshot( $html, $driver );
+		$this->assertMatchesSnapshot( $html );
 	}
 
 	/**
@@ -56,10 +66,9 @@ class Return_To_Cart_Test extends WPTestCase {
 
 		$args = array_merge( $this->get_default_args(), $override );
 
-		$html   = $template->template( $this->partial_path, $args, false );
-		$driver = new WPHtmlOutputDriver( home_url(), 'http://wordpress.test' );
+		$html = $template->template( $this->partial_path, $args, false );
 
-		$this->assertMatchesSnapshot( $html, $driver );
+		$this->assertMatchesSnapshot( $html );
 	}
 
 	/**
@@ -70,13 +79,36 @@ class Return_To_Cart_Test extends WPTestCase {
 
 		$args = $this->get_default_args();
 
-		$html   = $template->template( $this->partial_path, $args, false );
-		$driver = new WPHtmlOutputDriver( home_url(), 'http://wordpress.test' );
+		$invoice_number = 'foo';
+
+		/** @var \Tribe__Tickets__Commerce__PayPal__Cart__Unmanaged $cart */
+		$cart = tribe( 'tickets.commerce.paypal.cart' );
+		$cart->set_id( $invoice_number );
+		$cart->add_item( $args['test_ticket_id'], 3 );
+		$cart->save();
+
+		// Make PayPal Gateway find our mocked items in cart.
+		add_filter(
+			'tribe_tickets_commerce_paypal_invoice_number',
+			static function () use ( $invoice_number ) {
+				return $invoice_number;
+			}
+		);
+
+		// Filter Checkout URL (not Cart, since Checkout starts from there) to force it different.
+		add_filter(
+			'tribe_tickets_tribe-commerce_checkout_url',
+			static function () use ( $args ) {
+				return $args['checkout_url'];
+			}
+		);
+
+		$html = $template->template( $this->partial_path, $args, false );
 
 		// Check Cart URL is showing.
-		$this->assertContains( 'href="' . $args['cart_url'], $html );
+		$this->assertContains( 'href="https://www.paypal.com/cgi-bin/webscr/_cart', $html );
 
-		$this->assertMatchesSnapshot( $html, $driver );
+		$this->assertMatchesSnapshot( $html );
 	}
 
 }
