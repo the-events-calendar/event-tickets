fd31619d31c51aa4a3e72745cbaddd2fb24bd799
rafsuntaskin <rafsun.aiub@gmail.com>
2020-11-26 18:41:08 +0600

    add default args for rsvp repository meta handler

--- src/Tribe/Repositories/Attendee/RSVP.php
+++ src/Tribe/Repositories/Attendee/RSVP.php
@@ -110,21 +110,20 @@ class Tribe__Tickets__Repositories__Attendee__RSVP extends Tribe__Tickets__Atten
 	 */
 	public function update_additional_data( $attendee, $ticket, $attendee_data ) {
 
-		/** @var Tribe__Tickets__RSVP $provider */
-		$provider = tribe( 'tickets.rsvp' );
+		$attendee_data = $this->format_attendee_data( $attendee, $ticket, $attendee_data );
 
 		try {
 			$query = $this->where( 'id', $attendee->ID )
 			              ->set( 'ticket_id', $ticket->ID )
-			              ->set( 'event_id', $provider->get_event_for_ticket( $ticket ) )
-			              ->set( 'security_code', $provider->generate_security_code( $attendee->ID ) )
-			              ->set( 'order_id', $attendee_data['order_id'] )
-			              ->set( 'optout', $attendee_data['optout'] )
-			              ->set( 'user_id', $attendee_data['user_id'] )
-			              ->set( 'full_name', $attendee_data['full_name'] )
-			              ->set( 'email', $attendee_data['email'] )
-			              ->set( 'attendee_status', $attendee_data['attendee_status'] )
-			              ->set( 'price_paid', 0 );
+						  ->set( 'full_name', $attendee_data['full_name'] )
+						  ->set( 'email', $attendee_data['email'] )
+						  ->set( 'event_id', $attendee_data['event_id'] )
+						  ->set( 'security_code', $attendee_data['security_code'] )
+						  ->set( 'order_id', $attendee_data['order_id'] )
+						  ->set( 'optout', $attendee_data['optout'] )
+						  ->set( 'order_status', $attendee_data['order_status'] )
+						  ->set( 'price_paid', 0 )
+						  ->set( 'user_id', $attendee_data['user_id'] );
 		}
 		catch ( Tribe__Repository__Usage_Error $e ) {
 			do_action( 'tribe_log', 'error', __CLASS__, [
@@ -133,6 +132,45 @@ class Tribe__Tickets__Repositories__Attendee__RSVP extends Tribe__Tickets__Atten
 		}
 		finally {
 			$query->save();
+
+
 		}
+	}
+
+	/**
+	 * Format attendee data for meta updates.
+	 *
+	 * @param WP_Post                       $attendee Attendee Post Object.
+	 * @param Tribe__Tickets__Ticket_Object $ticket Ticket Object.
+	 * @param array                         $attendee_data Array of attendee data.
+	 *
+	 * @return array
+	 */
+	public function format_attendee_data( $attendee, $ticket, $attendee_data ) {
+
+		/** @var Tribe__Tickets__RSVP $provider */
+		$provider = tribe( 'tickets.rsvp' );
+
+		$defaults = [
+			'event_id'      => $provider->get_event_for_ticket( $ticket->ID )->ID,
+			'security_code' => $provider->generate_security_code( $attendee->ID ),
+			'order_id'      => $provider->generate_order_id(),
+			'optout'        => 1,
+			'order_status'  => 'yes',
+			'price_paid'    => 0,
+			'user_id'       => 0,
+		];
+
+		/**
+		 * Filter the formatted defaults for RSVP attendee.
+		 *
+		 * @since TBD
+		 *
+		 * @param array $data Attendee data.
+		 * @param Tribe__Tickets__Ticket_Object $ticket Ticket for attendee.
+		 */
+		$attendee_data = apply_filters( 'event_tickets_attendee_rsvp_data_before_insert', wp_parse_args( $attendee_data, $defaults ), $ticket );
+
+		return $attendee_data;
 	}
 }
