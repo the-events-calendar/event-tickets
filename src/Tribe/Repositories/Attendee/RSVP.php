6cae03af2f737d48d818363c2fd6a047f250d5a3
rafsuntaskin <rafsun.aiub@gmail.com>
2020-11-26 19:27:34 +0600

    moved action hooks to rsvp repo

--- src/Tribe/Repositories/Attendee/RSVP.php
+++ src/Tribe/Repositories/Attendee/RSVP.php
@@ -133,7 +133,7 @@ class Tribe__Tickets__Repositories__Attendee__RSVP extends Tribe__Tickets__Atten
 		finally {
 			$query->save();
 
-
+			$this->trigger_actions( $attendee, $ticket, $attendee_data );
 		}
 	}
 
@@ -172,5 +172,58 @@ class Tribe__Tickets__Repositories__Attendee__RSVP extends Tribe__Tickets__Atten
 		$attendee_data = apply_filters( 'event_tickets_attendee_rsvp_data_before_insert', wp_parse_args( $attendee_data, $defaults ), $ticket );
 
 		return $attendee_data;
+	}
+
+	/**
+	 * Trigger actions.
+	 *
+	 * @since TBD
+	 *
+	 * @param WP_Post                       $attendee Attendee Post Object.
+	 * @param Tribe__Tickets__Ticket_Object $ticket Ticket Object.
+	 * @param array                         $attendee_data Array of attendee data.
+	 */
+	public function trigger_actions( $attendee, $ticket, $attendee_data ) {
+
+		$attendee_id       = $attendee->ID;
+		$post_id           = $attendee['event_id'];
+		$order_id          = $attendee['order_id'];
+		$product_id        = $ticket->ID;
+		$order_attendee_id = $attendee_data['order_attendee_id'];
+
+		/**
+		 * RSVP specific action fired when a RSVP-driven attendee ticket for an event is generated.
+		 * Used to assign a unique ID to the attendee.
+		 *
+		 * @param int    $attendee_id ID of attendee ticket.
+		 * @param int    $post_id     ID of event.
+		 * @param string $order_id    RSVP order ID (hash).
+		 * @param int    $product_id  RSVP product ID.
+		 */
+		do_action( 'event_tickets_rsvp_attendee_created', $attendee_id, $post_id, $order_id, $product_id );
+
+		/**
+		 * Action fired when an RSVP attendee ticket is created.
+		 * Used to store attendee meta.
+		 *
+		 * @param int $attendee_id       ID of the attendee post.
+		 * @param int $post_id           Event post ID.
+		 * @param int $product_id        RSVP ticket post ID.
+		 * @param int $order_attendee_id Attendee # for order.
+		 */
+		do_action( 'event_tickets_rsvp_ticket_created', $attendee_id, $post_id, $product_id, $order_attendee_id );
+
+		if ( null === $order_attendee_id ) {
+			/**
+			 * Action fired when an RSVP ticket has had attendee tickets generated for it.
+			 *
+			 * @param int    $product_id RSVP ticket post ID.
+			 * @param string $order_id   ID (hash) of the RSVP order.
+			 * @param int    $qty        Quantity ordered.
+			 */
+			do_action( 'event_tickets_rsvp_tickets_generated_for_product', $product_id, $order_id, 1 );
+
+			$ticket->get_provider()->clear_attendees_cache( $post_id );
+		}
 	}
 }
