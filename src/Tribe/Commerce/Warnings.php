ca3fd1d2c77fb10a03fe2745ecacd7a1a261919d
rafsuntaskin <rafsun.aiub@gmail.com>
2020-11-27 09:10:23 +0600

    Update recurring warning notice to new design

--- src/Tribe/Commerce/Warnings.php
+++ src/Tribe/Commerce/Warnings.php
@@ -2,7 +2,7 @@
 /**
  * Conditional Warnings on Ticket Features
  *
- * Class Tribe__Tickets_Plus__Commerce__Warnings
+ * Class Tribe__Tickets__Commerce__Warnings
  *
  * @since TBD
  *
@@ -10,49 +10,7 @@
 class Tribe__Tickets__Commerce__Warnings {
 
 	public function hook() {
-		add_action( 'tribe_events_tickets_new_ticket_buttons', [ $this, 'recurring_event_warning' ] );
-		add_action( 'tribe_events_tickets_after_new_ticket_panel', [ $this, 'recurring_event_warning_msg' ] );
-	}
-
-	/**
-	 * Create Recurring Event Warning Label
-	 *
-	 * @since 4.6
-	 *
-	 */
-	public function recurring_event_warning() {
-		if ( ! class_exists( 'Tribe__Events__Pro__Main' ) ) {
-			return;
-		}
-		if ( ! function_exists( 'tribe_is_recurring_event' ) ) {
-			return;
-		}
-		?>
-		<div
-			class="tribe-warning-toggle tribe-dependent"
-			data-depends="#tribe-recurrence-active"
-			data-condition-is-checked
-		>
-			<label for="tribe-tickets-warning">
-			<span
-				class="event-warnings tribe-dependent"
-				data-depends="#tribe-tickets-warning"
-				data-condition-is-not-checked
-			>
-				<span class="dashicons dashicons-warning"></span><?php esc_html_e( 'Warning', 'event-tickets-plus' ); ?>
-			</span>
-				<span
-					class="event-warnings-close tribe-dependent"
-					data-depends="#tribe-tickets-warning"
-					data-condition-is-checked
-				>
-				<span class="dashicons dashicons-no"></span><?php esc_html_e( 'Hide Warning', 'event-tickets-plus' ); ?>
-			</span>
-			</label>
-			<input id="tribe-tickets-warning" type="checkbox">
-		</div>
-		<?php
-
+		add_action( 'tribe_events_tickets_new_ticket_buttons', [ $this, 'recurring_event_warning_msg' ] );
 	}
 
 	/**
@@ -60,24 +18,41 @@ class Tribe__Tickets__Commerce__Warnings {
 	 *
 	 * @since 4.6
 	 *
+	 * @param int $post_id Post ID.
+	 *
 	 */
-	public function recurring_event_warning_msg() {
+	public function recurring_event_warning_msg( $post_id ) {
 		if ( ! class_exists( 'Tribe__Events__Pro__Main' ) ) {
 			return;
 		}
+
 		if ( ! function_exists( 'tribe_is_recurring_event' ) ) {
 			return;
 		}
 
-		$warning = __( 'This is a recurring event. If you add tickets they will only show up on the next upcoming event in the recurrence pattern. The same ticket form will appear across all events in the series. Please configure your events accordingly.', 'event-tickets-plus' );
+		if ( ! tribe_is_recurring_event( $post_id ) ) {
+			return;
+		}
+
+		$warning = __( 'This is a recurring event. If you add tickets, they will only show up on first event in the recurrence series. Please carefully configure your recurring events.', 'event-tickets' );
+
+		$this->show_notice( $warning );
+	}
+
+	/**
+	 * Render the notice block.
+	 *
+	 * @since TBD
+	 *
+	 * @param string $msg Message to show.
+	 * @param string $type Type of message.
+	 */
+	public function show_notice( $msg, $type = 'info' ) {
+		$icon = 'dashicons-' . $type;
 		?>
-		<div
-			class="recurring-warning tribe-dependent"
-			data-depends="#tribe-tickets-warning"
-			data-condition-is-checked
-		>
-			<span class="dashicons dashicons-warning"></span>
-			<div class="recurring-warning-msg"><?php echo esc_attr( $warning ); ?></div>
+		<div class="ticket-editor-notice <?php echo esc_attr( $type ) ?>">
+			<span class="dashicons <?php echo esc_attr( $icon ); ?>"></span>
+			<span class="message"><?php esc_html_e( $msg, 'event-tickets' ); ?></span>
 		</div>
 		<?php
 	}
