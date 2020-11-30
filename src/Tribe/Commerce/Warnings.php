719c6d992b58c4d71ad0fac7c7193decb210149e
rafsuntaskin <rafsun.aiub@gmail.com>
2020-11-30 17:45:10 +0600

    apply CR changes

--- src/Tribe/Commerce/Warnings.php
+++ src/Tribe/Commerce/Warnings.php
@@ -5,10 +5,12 @@
  * Class Tribe__Tickets__Commerce__Warnings
  *
  * @since TBD
- *
  */
 class Tribe__Tickets__Commerce__Warnings {
 
+	/**
+	 * Hooks actions for showing warnings
+	 */
 	public function hook() {
 		add_action( 'tribe_events_tickets_new_ticket_buttons', [ $this, 'recurring_event_warning_msg' ] );
 	}
@@ -19,7 +21,6 @@ class Tribe__Tickets__Commerce__Warnings {
 	 * @since 4.6
 	 *
 	 * @param int $post_id Post ID.
-	 *
 	 */
 	public function recurring_event_warning_msg( $post_id ) {
 		if ( ! class_exists( 'Tribe__Events__Pro__Main' ) ) {
@@ -50,7 +51,7 @@ class Tribe__Tickets__Commerce__Warnings {
 	public function show_notice( $msg, $type = 'info' ) {
 		$icon = 'dashicons-' . $type;
 		?>
-		<div class="ticket-editor-notice <?php echo esc_attr( $type ) ?>">
+		<div class="ticket-editor-notice <?php echo esc_attr( $type ); ?>">
 			<span class="dashicons <?php echo esc_attr( $icon ); ?>"></span>
 			<span class="message"><?php esc_html_e( $msg, 'event-tickets' ); ?></span>
 		</div>
