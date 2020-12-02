1a65fa979c02c383cdc8185918a6e2d3ebf75110
rafsuntaskin <rafsun.aiub@gmail.com>
2020-12-03 03:34:32 +0600

    fix phpcs warnings

--- src/Tribe/Repositories/Attendee/RSVP.php
+++ src/Tribe/Repositories/Attendee/RSVP.php
@@ -125,8 +125,7 @@ class Tribe__Tickets__Repositories__Attendee__RSVP extends Tribe__Tickets__Atten
 		} catch ( Tribe__Repository__Usage_Error $e ) {
 			do_action( 'tribe_log', 'error', __CLASS__, [ 'message' => $e->getMessage() ] );
 			return;
-		}
-		finally {
+		} finally {
 			$query->save();
 
 			$this->trigger_actions( $attendee, $ticket, $attendee_data );
