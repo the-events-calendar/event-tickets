3c02427619737bb27fa31844f947b0de72e523bf
Juanfra Aldasoro <juanfraa@gmail.com>
2021-02-09 18:00:30 +0100

    Update snapshots.

--- tests/wpunit/Tribe/Tickets/__snapshots__/RSVPTest__it_should_render_rsvp_step with data set opt-in__1.php
+++ tests/wpunit/Tribe/Tickets/__snapshots__/RSVPTest__it_should_render_rsvp_step with data set opt-in__1.php
@@ -37,7 +37,7 @@
 </div>
 
 		<div class="tribe-tickets__rsvp-availability tribe-common-h6 tribe-common-h--alt tribe-common-b3--min-medium">
-			<span class="tribe-tickets__rsvp-availability-quantity tribe-common-b2--bold"> 10 </span> remaining,	
+			<span class="tribe-tickets__rsvp-availability-quantity tribe-common-b2--bold"> 5 </span> remaining,	
 			
 <span class="tribe-tickets__rsvp-availability-days-to-rsvp">
 	Last day to RSVP</span>
