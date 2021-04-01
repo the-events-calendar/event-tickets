6e30bbbbcdbb7a08ab27442f11709bef868298fa
rafsuntaskin <rafsun.aiub@gmail.com>
2021-04-01 10:01:00 +0600

    fixed typo

--- src/admin-views/settings/getting-started.php
+++ src/admin-views/settings/getting-started.php
@@ -14,7 +14,7 @@ $help_text = $etp_enabled
 	: __( 'Thank you for using Event Tickets! We recommend looking through the settings below so that you can fine tune your specific ticketing needs. Here are some resources that can help.', 'event-tickets' );
 
 ?>
-<div class="event-tickets__admin-banner notice is-dismissible>
+<div class="event-tickets__admin-banner notice is-dismissible">
 	<h3><?php echo esc_html__( 'Getting Started With Tickets', 'event-tickets' ); ?></h3>
 	<p class="event-tickets__admin-banner-help-text"><?php echo esc_html__( $help_text ); ?></p>
 
