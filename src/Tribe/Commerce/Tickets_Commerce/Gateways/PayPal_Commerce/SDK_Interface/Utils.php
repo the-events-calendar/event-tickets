d0e36e9aecac5956a7688f39c84b71dc3cffce97
Scott Kingsley Clark <lol@scottkclark.com>
2021-04-19 21:51:30 -0500

    Add utils back

---
+++ src/Tribe/Commerce/Tickets_Commerce/Gateways/PayPal_Commerce/SDK_Interface/Utils.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tribe\Tickets\Commerce\Tickets_Commerce\Gateways\PayPal_Commerce\SDK_Interface;
+
+use Tribe\Tickets\Commerce\Tickets_Commerce\Gateways\PayPal_Commerce\SDK\Models\MerchantDetail;
+
+/**
+ * Class Utils
+ *
+ * @since TBD
+ */
+class Utils {
+
+	/**
+	 * Returns whether or not the PayPal Commerce gateway is active
+	 *
+	 * @since TBD
+	 *
+	 * @return bool
+	 */
+	public static function gatewayIsActive() {
+		// @todo Add something to ET to check if a provider's "gateway" is active.
+		return give_is_gateway_active( PayPalCommerce::GATEWAY_ID );
+	}
+
+	/**
+	 * Return whether or not payment gateway accept payment.
+	 *
+	 * @since TBD
+	 *
+	 * @return bool
+	 */
+	public static function isAccountReadyToAcceptPayment() {
+		/* @var MerchantDetail $merchantDetail */
+		$merchantDetail = tribe( MerchantDetail::class );
+
+		return (bool) $merchantDetail->accountIsReady;
+	}
+}
