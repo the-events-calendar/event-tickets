a8be08ffda140052722c90b5ecdeb93678f995be
Scott Kingsley Clark <lol@scottkclark.com>
2021-05-05 20:47:23 -0500

    Fix api url used in testing

--- src/Tribe/Commerce/Tickets_Commerce/Gateways/PayPal_Commerce/Connect_Client.php
+++ src/Tribe/Commerce/Tickets_Commerce/Gateways/PayPal_Commerce/Connect_Client.php
@@ -21,7 +21,7 @@ class Connect_Client {
 	 * @todo Replace with final API URL.
 	 */
 	//public $api_url = 'https://tickets.theeventscalendar.com/connect';
-	public $api_url = 'http://test.tribe.local/wp-content/plugins/event-tickets/src/Tribe/Commerce/Tickets_Commerce/Gateways/PayPal_Commerce/Service';
+	public $api_url = 'http://test.tec.local/wp-content/plugins/event-tickets/src/Tribe/Commerce/Tickets_Commerce/Gateways/PayPal_Commerce/Service';
 
 	/**
 	 * Get REST API endpoint URL for requests.
