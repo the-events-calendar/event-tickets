2192ebb6a07a8dd6d99e5a4f32370d01ac852786
Brian Krane <redscar@gmail.com>
2021-08-20 10:03:31 -0400

    Added template tag `tribe_tickets_get_provider_slug` for calling `Tribe__Tickets__Tickets::get_provider_url_slug()`

--- src/template-tags/tickets.php
+++ src/template-tags/tickets.php
@@ -1809,3 +1809,16 @@ if ( ! function_exists( 'tribe_tickets_new_views_is_enabled' ) ) {
 		return (bool) apply_filters( 'tribe_tickets_new_views_is_enabled', $enabled );
 	}
 }
+
+if ( ! function_exists( 'tribe_tickets_get_provider_slug' ) ) {
+	/**
+	 * Returns the slug for providers
+	 *
+	 * @since TBD
+	 *
+	 * @return string String for which the slug should be named.
+	 */
+	function tribe_tickets_get_provider_slug() {
+		return Tribe__Tickets__Tickets::get_provider_url_slug();
+	}
+}
