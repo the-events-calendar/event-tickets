ea97ec1b24a72141a01643a2eff616658ca1c3ca
Juanfra Aldasoro <juanfraa@gmail.com>
2021-08-24 14:37:00 +0200

    tests and snapshots for TC.

--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/Item/__snapshots__/ItemPriceTest__test_should_render_cart_item_price__1.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/Item/__snapshots__/ItemPriceTest__test_should_render_cart_item_price__1.php
@@ -1,3 +1,3 @@
 <?php return '<div class="tribe-tickets__commerce-checkout-cart-item-price">
-	<span class="tribe-tickets-price-amount amount">&#x24;99.00</span></div>
+	<span class="tribe-tickets-price-amount amount">&#x24;100.00</span></div>
 ';
--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/__snapshots__/CartFooterTest__test_should_render_cart_footer__1.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/__snapshots__/CartFooterTest__test_should_render_cart_footer__1.php
@@ -1,15 +1,10 @@
-<?php return '<div  class="tribe-tickets__commerce-checkout-cart-footer tribe-common-b1" >
+<?php return '<footer  class="tribe-tickets__commerce-checkout-cart-footer tribe-common-b1" >
+
 	<div class="tribe-tickets__commerce-checkout-footer-quantity">
-		<span class="tribe-tickets__commerce-checkout-footer-quantity-label">
-			Quantity: 		</span>
-		<span class="tribe-tickets__commerce-checkout-footer-quantity-number">
-			2		</span>
-	</div>
+	<span class="tribe-tickets__commerce-checkout-footer-quantity-label">Quantity: </span><span class="tribe-tickets__commerce-checkout-footer-quantity-number">2</span></div>
 
 	<div class="tribe-tickets__commerce-checkout-footer-total">
-		<span class="tribe-tickets__commerce-checkout-footer-total-label">
-			Total: 		</span>
-		<span class="tribe-tickets__commerce-checkout-footer-total-wrap">
-			&#x24;198.00		</div>
-</div>
+	<span class="tribe-tickets__commerce-checkout-footer-total-label">Total: </span><span class="tribe-tickets__commerce-checkout-footer-total-wrap">$198.00</span></div>
+
+</footer>
 ';
