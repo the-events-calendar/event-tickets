392b6b54dd0dc96742b0769512ef8f69f1d9d8c9
Juanfra Aldasoro <juanfraa@gmail.com>
2021-08-26 13:17:47 +0200

    Update snapshots.

--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/Item/__snapshots__/ItemDetailsTest__test_should_render_cart_item_details__1.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/Item/__snapshots__/ItemDetailsTest__test_should_render_cart_item_details__1.php
@@ -7,7 +7,8 @@
 	<button
 		type="button"
 		class="tribe-common-b2 tribe-common-b3--min-medium tribe-tickets__commerce-checkout-cart-item-details-button--more"
-		aria-controls="tribe-tickets__commerce-checkout-cart-item-details-description--14"	>
+		aria-controls="tribe-tickets__commerce-checkout-cart-item-details-description--14"
+	>
 		<span class="screen-reader-text tribe-common-a11y-visual-hide">
 			Open the ticket description in checkout.		</span>
 		<span class="tribe-tickets__commerce-checkout-cart-item-details-button-text">
@@ -26,7 +27,18 @@
 </div>
 
 	<div id="tribe-tickets__commerce-checkout-cart-item-details-description--14"  class="tribe-common-b2 tribe-common-b3--min-medium tribe-tickets__commerce-checkout-cart-item-details-description tribe-common-a11y-hidden" >
-	Test ticket description for 13</div>
+	Test ticket description for 13
+	<div class="tribe-tickets__commerce-checkout-cart-item-details-description-attendee">
+		<div class="tribe-tickets__commerce-checkout-cart-item-details-description-attendee-name">John Smith</div>
+		<div class="tribe-tickets__commerce-checkout-cart-item-details-description-attendee-fields">t-shirt: large, meal: vegan, eyes: crossed</div>
+	</div>
+
+	<div class="tribe-tickets__commerce-checkout-cart-item-details-description-attendee">
+		<div class="tribe-tickets__commerce-checkout-cart-item-details-description-attendee-name">Karen Schmidt</div>
+		<div class="tribe-tickets__commerce-checkout-cart-item-details-description-attendee-fields">t-shirt: small, meal: very carnivorous, eyes: dotted</div>
+	</div>
+
+</div>
 
 </div>
 ';
