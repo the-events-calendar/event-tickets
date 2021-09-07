048a18326d23037d35fa9f9c38a0e7e39a81a3f9
Juanfra Aldasoro <juanfraa@gmail.com>
2021-09-07 12:26:48 +0200

    Add tests for PayPal settings.

---
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Admin/PayPal/Connect/InactiveTest.php
@@ -0,0 +1,55 @@
+<?php
+namespace TEC\Tickets\Commerce\Partials\Admin\PayPal\Connect;
+
+use Tribe__Tickets__Main;
+use Tribe\Tickets\Test\Partials\V2AdminTestCase;
+use TEC\Tickets\Commerce\Gateways\PayPal\Merchant;
+use TEC\Tickets\Commerce\Gateways\PayPal\Signup;
+
+class InactiveTest extends V2AdminTestCase {
+
+	public $partial_path = 'settings/tickets-commerce/paypal/connect/inactive';
+
+	/**
+	 * Get all the default args required for this template
+	 *
+	 * @return array
+	 */
+	public function get_default_args() {
+		$merchant = tribe( Merchant::class );
+		$signup   = tribe( SignUp::class );
+
+		$args = [
+			'plugin_url'         => Tribe__Tickets__Main::instance()->plugin_url,
+			'merchant'           => $merchant,
+			'is_merchant_active' => false,
+			'signup'             => $signup,
+		];
+
+		return $args;
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_render() {
+		$args   = $this->get_default_args();
+		$html   = $this->template_class()->template( $this->partial_path, $args, false );
+		$driver = $this->get_html_output_driver();
+
+		$this->assertMatchesSnapshot( $html, $driver );
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_render_empty() {
+		$args                       = $this->get_default_args();
+		$args['is_merchant_active'] = true;
+		$html                       = $this->template_class()->template( $this->partial_path, $args, false );
+		$driver                     = $this->get_html_output_driver();
+
+		$this->assertMatchesSnapshot( $html, $driver );
+	}
+
+}
