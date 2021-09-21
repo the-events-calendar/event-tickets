1f00e1a44bb3c90825df33c36038f46f1c3a28d0
Juanfra Aldasoro <juanfraa@gmail.com>
2021-09-21 16:19:55 +0200

    Add tests and snapshots.

---
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Footer/__snapshots__/GatewayErrorTest__test_should_render_notice__1.php
@@ -0,0 +1,7 @@
+<?php return '<div id="my-notice"  class="tribe-tickets__notice tribe-tickets__notice--error tribe-tickets__commerce-checkout-footer-notice-error--no-gateway" >
+			<h3 class="tribe-common-h7 tribe-tickets-notice__title">Checkout Unavailable!</h3>
+	
+	<div  class="tribe-common-b2 tribe-tickets-notice__content" >
+		Checkout is not available at this time because a payment method has not been set up. Please notify the site administrator.	</div>
+</div>
+';
