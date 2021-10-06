db3e108f63103b3675b579aa205ce48289fe709a
rafsuntaskin <rafsun.aiub@gmail.com>
2021-10-06 18:38:57 +0600

    doc blocks updated as per CR

--- src/resources/js/commerce/notice.js
+++ src/resources/js/commerce/notice.js
@@ -18,7 +18,7 @@ tribe.tickets = tribe.tickets || {};
 tribe.tickets.commerce = tribe.tickets.commerce || {};
 
 /**
- * Configures ET Tickets Commerce Object in the Global Tribe variable
+ * Configures ET Tickets Commerce Notice Object in the Global Tribe variable
  *
  * @since TBD
  *
@@ -27,23 +27,22 @@ tribe.tickets.commerce = tribe.tickets.commerce || {};
 tribe.tickets.commerce.notice = tribe.tickets.commerce.notice || {};
 
 /**
- * Initializes in a Strict env the code that manages the plugin tickets commerce.
+ * Initializes in a Strict env the code that manages the plugin tickets commerce notice.
  *
  * @since TBD
  *
  * @param  {Object} $   jQuery
- * @param  {Object} obj tribe.tickets.commerce
+ * @param  {Object} obj tribe.tickets.commerce.notice
  *
  * @return {void}
  */
 ( function( $, obj ) {
 	'use strict';
-	const $document = $( document );
 
 	/*
 	 * Tickets Commerce Selectors.
 	 *
-	 * @since 5.1.9
+	 * @since TBD
 	 */
 	obj.selectors = {
 		hiddenElement: '.tribe-common-a11y-hidden',
@@ -54,11 +53,11 @@ tribe.tickets.commerce.notice = tribe.tickets.commerce.notice || {};
 	};
 
 	/**
-	 * Display the notice component.
+	 * Show the populated notice component.
 	 *
 	 * @since TBD
 	 *
-	 * @param {jQuery} $item Data for notice.
+	 * @param {jQuery} $item Container element for notice to be shown.
 	 */
 	obj.show = ( $item ) => {
 		if ( ! $item.length  ) {
@@ -77,6 +76,8 @@ tribe.tickets.commerce.notice = tribe.tickets.commerce.notice || {};
 	 * Hide the notice component.
 	 *
 	 * @since TBD
+	 *
+	 * @param {jQuery} $item Container element for notice.
 	 */
 	obj.hide = ( $item ) => {
 		if ( ! $item.length  ) {
@@ -96,9 +97,9 @@ tribe.tickets.commerce.notice = tribe.tickets.commerce.notice || {};
 	 *
 	 * @since TBD
 	 *
-	 * @param {jQuery} $item Data for notice.
-	 * @param {string} title Data for notice.
-	 * @param {string} content Data for notice.
+	 * @param {jQuery} $item Target container for notice component.
+	 * @param {string} title Notice title data.
+	 * @param {string} content Message that is shown.
 	 */
 	obj.populate = ( $item, title, content ) => {
 		const $content = $item.find( obj.selectors.content );
@@ -120,18 +121,5 @@ tribe.tickets.commerce.notice = tribe.tickets.commerce.notice || {};
 
 		$item.trigger( 'afterPopulateNotice.tecTicketsCommerce', [ $container ] );
 	};
-
-	/**
-	 * Handles the initialization of the tickets commerce events when Document is ready.
-	 *
-	 * @since TBD
-	 *
-	 * @return {void}
-	 */
-	obj.ready = () => {
-
-	};
-
-	$( obj.ready );
 
 } )( jQuery, tribe.tickets.commerce.notice );
