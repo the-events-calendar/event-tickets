091c45bd7a0ba7029a7ee567bb3bc3a14aeaffe3
Juanfra Aldasoro <juanfraa@gmail.com>
2021-10-14 16:00:12 +0200

    Implementing new designs for the PayPal connect section for `tickets commerce`

--- src/admin-views/settings/tickets-commerce/paypal/signup-link.php
+++ src/admin-views/settings/tickets-commerce/paypal/signup-link.php
@@ -14,15 +14,19 @@
 	}
 </script>
 
-<div class="tec-tickets__admin-settings-tickets-commerce-paypal-connect-button">
-	<a
-		target="_blank"
-		data-paypal-onboard-complete="onboardedCallback"
-		href="<?php echo esc_url( $url ) ?>&displayMode=minibrowser"
-		data-paypal-button="true"
-		id="connect_to_paypal"
-		class="tec-tickets__admin-settings-tickets-commerce-paypal-connect-button-link"
-	>
-		<?php echo wp_kses( __( 'Connect Automatically with <i>PayPal</i>', 'event-tickets' ), 'post' ); ?>
-	</a>
+<div
+	class="tec-tickets__admin-settings-tickets-commerce-paypal-signup-settings"
+>
+	<div class="tec-tickets__admin-settings-tickets-commerce-paypal-connect-button">
+		<a
+			target="_blank"
+			data-paypal-onboard-complete="onboardedCallback"
+			href="<?php echo esc_url( $url ) ?>&displayMode=minibrowser"
+			data-paypal-button="true"
+			id="connect_to_paypal"
+			class="tec-tickets__admin-settings-tickets-commerce-paypal-connect-button-link"
+		>
+			<?php echo wp_kses( __( 'Connect Automatically with <i>PayPal</i>', 'event-tickets' ), 'post' ); ?>
+		</a>
+	</div>
 </div>
