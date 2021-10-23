aba4aa365eb580b561220bd598f15b9df87ce24a
Mike Cotton <codingmusician@gmail.com>
2021-10-23 15:33:24 -0500

    add notices

---
+++ src/admin-views/settings/tickets-commerce/notices/success-page-not-set.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Success page set up notice.
+ *
+ * @since TBD
+ *
+ */
+
+?>
+<div class="event-tickets__admin-banner">
+	<h3><?php echo esc_html__( 'Set up your order success page', 'event-tickets' ); ?></h3>
+	<p class="event-tickets__admin-banner-help-text">
+		<?php 
+			echo sprintf( 
+				esc_html__( 
+					"In order to start selling with Tickets Commerce, you'll need to set up your " .
+					"order success page. Please configure the setting on Settings > Payments and " .
+					"confirm that the page you have selected has the proper shortcode. " . 
+					"%sLearn more%s" 
+				),
+				'<a href="https://evnt.is/1axv" target="_blank" rel="noopener noreferrer">',
+				'</a>'
+			); 
+		?>
+	</p>
+</div>
