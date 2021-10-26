772acce020614bc5289090ab0da58e8ad007d21d
Ricardo Moraleida <moraleida@gmail.com>
2021-10-26 15:44:37 -0300

    ActiveTest

--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Admin/PayPal/Connect/ActiveTest.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Admin/PayPal/Connect/ActiveTest.php
@@ -3,54 +3,33 @@
 namespace TEC\Tickets\Commerce\Partials\Admin\PayPal\Connect;
 
 use Tribe__Tickets__Main;
-use Tribe\Tickets\Test\Partials\V2AdminTestCase;
 use TEC\Tickets\Commerce\Gateways\PayPal\Merchant;
 use TEC\Tickets\Commerce\Gateways\PayPal\Signup;
 
-class ActiveTest extends V2AdminTestCase {
+use Tribe\Tickets\Test\Testcases\Html_Partial_Test_Case;
 
-	public $partial_path = 'settings/tickets-commerce/paypal/connect/active';
+class ActiveTest extends Html_Partial_Test_Case {
 
-	/**
-	 * Get all the default args required for this template
-	 *
-	 * @return array
-	 */
-	public function get_default_args() {
-		$merchant = tribe( Merchant::class );
-		$signup   = tribe( Signup::class );
+	protected $partial_path = 'src/admin-views/settings/tickets-commerce/paypal/connect/active';
 
-		$args = [
-			'plugin_url'            => Tribe__Tickets__Main::instance()->plugin_url,
-			'merchant'              => $merchant,
-			'is_merchant_connected' => true,
-			'signup'                => $signup,
-		];
-
-		return $args;
-	}
-
-	/**
-	 * @test
-	 */
 	public function test_should_render() {
-		$args   = $this->get_default_args();
-		$html   = $this->template_class()->template( $this->partial_path, $args, false );
-		$driver = $this->get_html_output_driver();
+		$merchant = tribe( Merchant::class );
+		$signup   = tribe( SignUp::class );
 
-		$this->assertMatchesSnapshot( $html, $driver );
+		$this->assertMatchesHtmlSnapshot( $this->get_partial_html( [
+				'plugin_url'            => Tribe__Tickets__Main::instance()->plugin_url,
+				'merchant'              => $merchant,
+				'is_merchant_connected' => true,
+				'signup'                => $signup,
+			]
+		) );
 	}
 
-	/**
-	 * @test
-	 */
 	public function test_should_render_empty() {
-		$args                          = $this->get_default_args();
-		$args['is_merchant_connected'] = false;
-		$html                          = $this->template_class()->template( $this->partial_path, $args, false );
-		$driver                        = $this->get_html_output_driver();
-
-		$this->assertMatchesSnapshot( $html, $driver );
+		$this->assertEmpty( $this->get_partial_html( [
+				'plugin_url'            => Tribe__Tickets__Main::instance()->plugin_url,
+				'is_merchant_connected' => false,
+			]
+		) );
 	}
-
 }
