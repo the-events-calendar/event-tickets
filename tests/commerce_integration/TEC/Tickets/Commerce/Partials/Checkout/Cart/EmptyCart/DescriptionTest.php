b92b7300a6d52b76fc7481e08a587a2c9bc5a8e5
Ricardo Moraleida <moraleida@gmail.com>
2021-10-26 11:12:23 -0300

    fix DescriptionTest so all tests pass

--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/EmptyCart/DescriptionTest.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/EmptyCart/DescriptionTest.php
@@ -2,47 +2,32 @@
 
 namespace TEC\Tickets\Commerce\Partials\Checkout\Cart\EmptyCart;
 
-use Tribe\Tickets\Test\Partials\V2CommerceTestCase;
+use Tribe\Tickets\Test\Testcases\Html_Partial_Test_Case;
 
-class DescriptionTest extends V2CommerceTestCase {
+class DescriptionTest extends Html_Partial_Test_Case {
 
-	public $partial_path = 'checkout/cart/empty/description';
-
-	/**
-	 * Get all the default args required for this template
-	 *
-	 * @return array
-	 */
-	public function get_default_args() {
-		$args = [
-			'items'      => [],
-			'is_tec_active' => class_exists( 'Tribe__Events__Main' ),
-		];
-
-		return $args;
-	}
+	protected $partial_path = 'src/views/v2/commerce/checkout/cart/empty/description';
 
 	/**
 	 * @test
 	 */
 	public function test_should_render() {
-		$args   = $this->get_default_args();
-		$html   = $this->template_class()->template( $this->partial_path, $args, false );
-		$driver = $this->get_html_output_driver();
-
-		$this->assertMatchesSnapshot( $html, $driver );
+		$this->assertMatchesHtmlSnapshot( $this->get_partial_html( [
+				'items' => [],
+				'is_tec_active' => class_exists( 'Tribe__Events__Main' ),
+			]
+		) );
 	}
 
 	/**
 	 * @test
 	 */
 	public function test_should_render_empty() {
-		$args          = $this->get_default_args();
-		$args['items'] = [ 'Ticket 1', 'Ticket 2' ];
-		$html          = $this->template_class()->template( $this->partial_path, $args, false );
-		$driver        = $this->get_html_output_driver();
-
-		$this->assertMatchesSnapshot( $html, $driver );
+		$this->assertEmpty( $this->get_partial_html( [
+				'items' => [ 'Ticket 1', 'Ticket 2' ],
+				'is_tec_active' => class_exists( 'Tribe__Events__Main' ),
+			]
+		) );
 	}
 
 }
