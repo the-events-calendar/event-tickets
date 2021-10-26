ca19b3d93fdf6e296bbed44d2042eb1a69684fb5
Ricardo Moraleida <moraleida@gmail.com>
2021-10-26 18:02:33 -0300

    All Gateway Tests

--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Gateway/PayPal/AdvancedPayments/SeparatorTest.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Gateway/PayPal/AdvancedPayments/SeparatorTest.php
@@ -2,11 +2,11 @@
 
 namespace TEC\Tickets\Commerce\Partials\Gateway\PayPal\AdvancedPayments;
 
-use Tribe\Tickets\Test\Testcases\Html_Partial_Test_Case;
+use Tribe\Tickets\Test\Testcases\TicketsCommerceSnapshotTestCase;
 
-class SeparatorTest extends Html_Partial_Test_Case {
+class SeparatorTest extends TicketsCommerceSnapshotTestCase {
 
-	protected $partial_path = 'src/views/v2/commerce/gateway/paypal/advanced-payments/separator';
+	protected $partial_path = 'gateway/paypal/advanced-payments/separator';
 
 	public function test_should_render() {
 		$this->assertMatchesHtmlSnapshot( $this->get_partial_html( [] ) );
--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Gateway/PayPal/AdvancedPaymentsTest.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Gateway/PayPal/AdvancedPaymentsTest.php
@@ -2,11 +2,11 @@
 
 namespace TEC\Tickets\Commerce\Partials\Gateway\PayPal;
 
-use Tribe\Tickets\Test\Testcases\Html_Partial_Test_Case;
+use Tribe\Tickets\Test\Testcases\TicketsCommerceSnapshotTestCase;
 
-class AdvancedPaymentsTest extends Html_Partial_Test_Case {
+class AdvancedPayments extends TicketsCommerceSnapshotTestCase {
 
-	protected $partial_path = 'src/views/v2/commerce/gateway/paypal/advanced-payments';
+	protected $partial_path = 'gateway/paypal/advanced-payments';
 
 	public function test_should_render() {
 		$this->assertMatchesHtmlSnapshot( $this->get_partial_html( [
@@ -24,49 +24,5 @@ class AdvancedPaymentsTest extends Html_Partial_Test_Case {
 				'active_custom_payments'   => false,
 			]
 		) );
-	}
-}
-
-/*
-class AdvancedPayments extends V2CommerceTestCase {
-
-	public $partial_path = 'gateway/paypal/advanced-payments';
-
-	/**
-	 * Get all the default args required for this template
-	 *
-	 * @return array
-	 *
-	public function get_default_args() {
-		$args = [
-			'must_login'               => false,
-			'supports_custom_payments' => true,
-			'active_custom_payments'   => true,
-		];
-
-		return $args;
-	}
-
-	/**
-	 * @test
-	 *
-	public function test_should_render() {
-		$args   = $this->get_default_args();
-		$html   = $this->template_class()->template( $this->partial_path, $args, false );
-		$driver = $this->get_html_output_driver();
-
-		$this->assertMatchesSnapshot( $html, $driver );
-	}
-
-	/**
-	 * @test
-	 *
-	public function test_should_render_empty_if_no_custom_payments_support() {
-		$args                           = $this->get_default_args();
-		$args['active_custom_payments'] = false;
-		$html                           = $this->template_class()->template( $this->partial_path, $args, false );
-		$driver                         = $this->get_html_output_driver();
-
-		$this->assertMatchesSnapshot( $html, $driver );
 	}
 }
