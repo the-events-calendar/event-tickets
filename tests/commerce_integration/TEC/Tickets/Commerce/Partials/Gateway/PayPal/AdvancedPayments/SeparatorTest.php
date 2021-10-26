2160e9e9c3bf4448bdee617e45f72b66c4a10d91
Ricardo Moraleida <moraleida@gmail.com>
2021-10-26 15:57:26 -0300

    SeparatorTest

--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Gateway/PayPal/AdvancedPayments/SeparatorTest.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Gateway/PayPal/AdvancedPayments/SeparatorTest.php
@@ -1,31 +1,14 @@
 <?php
+
 namespace TEC\Tickets\Commerce\Partials\Gateway\PayPal\AdvancedPayments;
 
-use Tribe\Tickets\Test\Partials\V2CommerceTestCase;
+use Tribe\Tickets\Test\Testcases\Html_Partial_Test_Case;
 
-class Separator extends V2CommerceTestCase {
+class SeparatorTest extends Html_Partial_Test_Case {
 
-	public $partial_path = 'gateway/paypal/advanced-payments/separator';
+	protected $partial_path = 'src/views/v2/commerce/gateway/paypal/advanced-payments/separator';
 
-	/**
-	 * Get all the default args required for this template
-	 *
-	 * @return array
-	 */
-	public function get_default_args() {
-		$args = [];
-
-		return $args;
-	}
-
-	/**
-	 * @test
-	 */
 	public function test_should_render() {
-		$args   = $this->get_default_args();
-		$html   = $this->template_class()->template( $this->partial_path, $args, false );
-		$driver = $this->get_html_output_driver();
-
-		$this->assertMatchesSnapshot( $html, $driver );
+		$this->assertMatchesHtmlSnapshot( $this->get_partial_html( [] ) );
 	}
 }
