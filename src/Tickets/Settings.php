ab9c1e9163f1b9024ed40b697bc2ad723f7c4497
rafsuntaskin <rafsun.aiub@gmail.com>
2021-10-28 23:51:31 +0600

    avoid force enabling tickets commerce

--- src/Tickets/Settings.php
+++ src/Tickets/Settings.php
@@ -27,23 +27,22 @@ class Settings {
 	 */
 	public static $tickets_commerce_enabled = 'tickets_commerce_enabled';
 
-	/**
-	 * Force enable TicketsCommerce for new users.
-	 *
-	 * @since TBD
-	 *
-	 * @param bool $enabled
-	 *
-	 * @return bool|mixed
-	 */
-	public static function force_enable_tickets_commerce( $enabled ) {
-
-		return true;
-		// If the tickets_commerce setting was saved before, then maintain the setting value.
-		if ( '' != tribe_get_option( Payments_Tab::$option_enable ) ) {
-			return $enabled;
-		}
-
-		return tribe_installed_after( 'Tribe__Tickets__Main', '5.1.10' );
-	}
+//	/**
+//	 * Force enable TicketsCommerce for new users.
+//	 *
+//	 * @since TBD
+//	 *
+//	 * @param bool $enabled
+//	 *
+//	 * @return bool|mixed
+//	 */
+//	public static function force_enable_tickets_commerce( $enabled ) {
+//
+//		// If the tickets_commerce setting was saved before, then maintain the setting value.
+//		if ( '' != tribe_get_option( Payments_Tab::$option_enable ) ) {
+//			return $enabled;
+//		}
+//
+//		return tribe_installed_after( 'Tribe__Tickets__Main', '5.1.10' );
+//	}
 }
