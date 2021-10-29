9f02221c3b36a9858013efd960aa1cbb201dc954
rafsuntaskin <rafsun.aiub@gmail.com>
2021-10-30 00:40:51 +0600

    remove unused codes

--- src/Tickets/Settings.php
+++ src/Tickets/Settings.php
@@ -26,23 +26,4 @@ class Settings {
 	 * @var string
 	 */
 	public static $tickets_commerce_enabled = 'tickets_commerce_enabled';
-
-//	/**
-//	 * Force enable TicketsCommerce for new users.
-//	 *
-//	 * @since TBD
-//	 *
-//	 * @param bool $enabled
-//	 *
-//	 * @return bool|mixed
-//	 */
-//	public static function force_enable_tickets_commerce( $enabled ) {
-//
-//		// If the tickets_commerce setting was saved before, then maintain the setting value.
-//		if ( '' != tribe_get_option( Payments_Tab::$option_enable ) ) {
-//			return $enabled;
-//		}
-//
-//		return tribe_installed_after( 'Tribe__Tickets__Main', '5.1.10' );
-//	}
 }
