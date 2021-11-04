d0e1f64de1148ca9d443946cfe80b67e3ccc626a
Ricardo Moraleida <moraleida@gmail.com>
2021-11-03 23:38:03 -0300

    adds todo

--- src/Tickets/Commerce/Flag_Actions/Send_Email.php
+++ src/Tickets/Commerce/Flag_Actions/Send_Email.php
@@ -57,6 +57,8 @@ class Send_Email extends Flag_Action_Abstract {
 			 * make sure the email only goes out after all the work to register the order and attendees is
 			 * finished, so we hook it to the same hook used to process everything, but make sure it's the last
 			 * function to run.
+			 *
+			 * @todo TribeLegacyCommerce
 			 */
 			if ( doing_action( 'wp_ajax_tribe_tickets_admin_manager' ) ) {
 				add_filter( 'tribe_tickets_admin_manager_request', static function( $response ) use ( $order, $event ) {
