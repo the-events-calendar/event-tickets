c2ff35fe144438a7a89430e3e3dbbf1cd7aa3c51
Mike Cotton <codingmusician@gmail.com>
2021-11-30 09:36:43 -0600

    delete individual keys when using object cache

--- src/Tribe/Cache/Transient_Cache.php
+++ src/Tribe/Cache/Transient_Cache.php
@@ -25,13 +25,16 @@ class Tribe__Tickets__Cache__Transient_Cache extends Tribe__Tickets__Cache__Abst
 		}
 
 		$hash = md5( serialize( $post_types ) );
-		foreach ( $this->keys as $key ) {
+		$is_using_object_cache = wp_using_ext_object_cache();
+
+		array_walk( $this->keys, function( $key ) use ( $hash, $is_using_object_cache ) {
 			delete_transient( __CLASS__ . $key );
 			delete_transient( __CLASS__ . $key . $hash );
-		}
-
-		// Flush WordPress cache for cache compatibility
-		wp_cache_flush();
+			if ( $is_using_object_cache ) {
+				wp_cache_delete( __CLASS__ . $key );
+				wp_cache_delete( __CLASS__ . $key . $hash );
+			}
+		} );
 
 	}
 
