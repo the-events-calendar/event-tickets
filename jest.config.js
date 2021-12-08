e55e69804e06c11c8dfda8989a3974181eae79f9
Paul <kim.paul.ms@gmail.com>
2021-12-08 17:03:31 +0800

    update product taskmaster to 2.1.1, update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,8 @@
 var sharedConfig = require( '@the-events-calendar/product-taskmaster/config/jest.config.js' );
+var pkg = require( './package.json' );
 
 module.exports = {
 	...sharedConfig,
-	displayName: 'common',
-	testMatch: [
-		'**/__tests__/**/*.js',
-	],
+	displayName: 'tickets',
+	testMatch: pkg._filePath.jest.map( ( path ) => `<rootDir>/${ path }` ),
 };
