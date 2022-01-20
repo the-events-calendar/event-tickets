89a003b8d000fa504d4979f0353554ffbd0612fc
Gustavo Bordoni <bordoni.dev@gmail.com>
2022-01-20 00:13:39 -0500

    Modifications to Stripe Checkout so it's more ES6 compliant

--- src/resources/js/commerce/gateway/stripe/checkout.js
+++ src/resources/js/commerce/gateway/stripe/checkout.js
@@ -16,83 +16,11 @@ tribe.tickets.commerce.gateway.stripe = tribe.tickets.commerce.gateway.stripe ||
  */
 tribe.tickets.commerce.gateway.stripe.checkout = {};
 
-(async function( $, obj, Stripe ) {
+(( $, obj, Stripe ) => {
 	'use strict';
 	const $document = $( document );
 
 	obj.checkout = tecTicketsCommerceGatewayStripeCheckout;
-
-	// Fetch Publishable API Key and Initialize Stripe Elements on Ready
-	var response = await fetch( obj.checkout.keyEndpoint, {
-		method: 'POST',
-		headers: {
-			'Accept': 'application/json',
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify( { nonce: obj.checkout.keyNonce } )
-	} ).then( function( response ) {
-		return response.json();
-	} );
-
-	obj.stripeLib = Stripe( response );
-	obj.stripeElements = obj.stripeLib.elements();
-
-	obj.submitPayment = async function( secret ) {
-
-		obj.stripeLib.confirmCardPayment( secret, {
-			payment_method: {
-				card: obj.checkout.card,
-				billing_details: {
-					name: 'user name' // @todo get this value
-				}
-			}
-		} ).then( function( result ) {
-			console.log( result );
-			if ( result.error ) {
-				console.log( result.error.message );
-				return false;
-			}
-
-			if ( result.paymentIntent.status === 'succeeded' ) {
-				console.log( 'great success!' );
-			}
-		} );
-
-	};
-
-	/**
-	 * Create an order
-	 */
-	obj.createOrder = async function() {
-
-		// Fetch Publishable API Key and Initialize Stripe Elements on Ready
-		var response = await fetch( obj.checkout.orderEndpoint, {
-			method: 'POST',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify( { nonce: obj.checkout.orderNonce } )
-		} ).then( function( response ) {
-			return response.json();
-		} );
-
-		if ( true === response.success ) {
-			obj.submitPayment( response.client_secret );
-		}
-
-		console.log( response );
-	};
-
-	/**
-	 * Starts the process to submit a payment
-	 *
-	 * @param event
-	 */
-	obj.handlePayment = function( event ) {
-		event.preventDefault();
-		obj.createOrder();
-	};
 
 	/**
 	 * Checkout Selectors.
@@ -113,42 +41,123 @@ tribe.tickets.commerce.gateway.stripe.checkout = {};
 	 */
 	obj.callbacks = {
 		submit: obj.handlePayment
-	}
+	};
+
+	obj.setupStripe = async () => {
+		// Fetch Publishable API Key and Initialize Stripe Elements on Ready
+		let response = await fetch( obj.checkout.keyEndpoint, {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify( { nonce: obj.checkout.keyNonce } )
+			} )
+			.then( response => response.json() );
+
+		obj.stripeLib = Stripe( response );
+		obj.stripeElements = obj.stripeLib.elements();
+	};
+
+	obj.submitPayment = async ( secret ) => {
+
+		obj.stripeLib.confirmCardPayment( secret, {
+			payment_method: {
+				card: obj.checkout.card,
+				billing_details: {
+					name: 'user name' // @todo get this value
+				}
+			}
+		} ).then( ( result ) => {
+			console.log( result );
+			if ( result.error ) {
+				console.log( result.error.message );
+				return false;
+			}
+
+			if ( result.paymentIntent.status === 'succeeded' ) {
+				console.log( 'great success!' );
+			}
+		} );
+
+	};
+
+	/**
+	 * Create an order
+	 *
+	 * @since TBD
+	 */
+	obj.createOrder = async () => {
+
+		// Fetch Publishable API Key and Initialize Stripe Elements on Ready
+		let response = await fetch( obj.checkout.orderEndpoint, {
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify( { nonce: obj.checkout.orderNonce } )
+		} ).then( response => response.json() );
+
+		if ( true === response.success ) {
+			obj.submitPayment( response.client_secret );
+		}
+
+		console.log( response );
+	};
+
+	/**
+	 * Starts the process to submit a payment
+	 *
+	 * @since TBD
+	 *
+	 * @param event
+	 */
+	obj.handlePayment = ( event ) => {
+		event.preventDefault();
+		obj.createOrder();
+	};
 
 	/**
 	 * Bind script loader to trigger script dependent methods.
 	 *
 	 * @since TBD
 	 */
-	obj.bindEvents = function() {
-
-		// Load CardElement
-		window.onload = ( event ) => {
-			var style = {
-				base: {
-					color: "#32325d"
-				}
-			};
-
-			obj.checkout.card = obj.stripeElements.create( "card", { style: style } );
-			obj.checkout.card.mount( document.getElementById( obj.selectors.cardElementDiv ) );
-
-			obj.checkout.card.on( 'change', ( { error } ) => {
-				let displayError = document.getElementById( obj.selectors.cardErrorsDiv );
-				if ( error ) {
-					displayError.textContent = error.message;
-				} else {
-					displayError.textContent = '';
-				}
-			} );
-		};
-
+	obj.bindEvents = () => {
 		// Handle submit
-		var paymentButton = document.getElementById( obj.selectors.submitButton );
-		paymentButton.addEventListener( 'click', obj.callbacks.submit );
+		$( obj.selectors.submitButton ).on( 'click', obj.callbacks.submit );
 	};
 
-	// Initialize
-	obj.bindEvents();
+	obj.ready = () => {
+		obj.setupStripe();
 
-})( jQuery, tribe.tickets.commerce.gateway.stripe, Stripe );
+		// Initialize
+		obj.bindEvents();
+	};
+
+	obj.onCardChange = ( { error } ) => {
+		let displayError = document.getElementById( obj.selectors.cardErrorsDiv );
+		if ( error ) {
+			displayError.textContent = error.message;
+		} else {
+			displayError.textContent = '';
+		}
+	};
+
+	obj.onLoad =  ( event ) => {
+		const style = {
+			base: {
+				color: "#32325d"
+			}
+		};
+
+		obj.checkout.card = obj.stripeElements.create( 'card', { style: style } );
+		obj.checkout.card.mount( document.getElementById( obj.selectors.cardElementDiv ) );
+		obj.checkout.card.on( 'change', obj.onCardChange );
+	};
+
+	// Bind the onload of the page.
+	$( window ).load( obj.onLoad );
+
+	$( obj.ready );
+} )( jQuery, tribe.tickets.commerce.gateway.stripe, Stripe );
