f839a4966fd812ae251d3629047074534fedd34a
Ricardo Moraleida <moraleida@gmail.com>
2022-02-02 16:19:42 -0300

    remove mention to account_status

--- src/Tickets/Commerce/Gateways/Stripe/Client.php
+++ src/Tickets/Commerce/Gateways/Stripe/Client.php
@@ -34,8 +34,6 @@ class Client {
 	 */
 	public $payment_intent_transient_name;
 
-	public $account_status;
-
 	/**
 	 * Get environment base URL.
 	 *
@@ -322,8 +320,6 @@ class Client {
 
 			$return['request_error'] = $response;
 		}
-
-		$this->account_status = $return;
 
 		return $return;
 	}
