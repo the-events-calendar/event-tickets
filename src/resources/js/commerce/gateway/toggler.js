23713873689da89633ac77241194bcbd1446c9e1
Mike Cotton <mike@theeventscalendar.com>
2022-02-09 06:47:11 -0600

    Apply suggestions from code review
    Co-authored-by: Gustavo Bordoni <bordoni.dev@gmail.com>

--- src/resources/js/commerce/gateway/toggler.js
+++ src/resources/js/commerce/gateway/toggler.js
@@ -113,7 +113,7 @@ tribe.tickets.commerce.gateway = tribe.tickets.commerce.gateway || {};
 	 * @param {Element} gateway Gateway element to show.
 	 */
 	obj.toggler.showGateway = gateway => {
-		$( gateway ).show().attr('aria-expanded', 'true');
+		$( gateway ).show().attr( 'aria-expanded', 'true' );
 	};
 
 	/**
@@ -124,7 +124,7 @@ tribe.tickets.commerce.gateway = tribe.tickets.commerce.gateway || {};
 	 * @param {Element} gateway Gateway element to hide.
 	 */
 	obj.toggler.hideGateway = gateway => {
-		$( gateway ).hide().attr('aria-expanded', 'false');
+		$( gateway ).hide().attr( 'aria-expanded', 'false' );
 	};
 
 	/**
@@ -140,7 +140,7 @@ tribe.tickets.commerce.gateway = tribe.tickets.commerce.gateway || {};
 			}
 			obj.toggler.hideGateway( gateway );
 		});
-		obj.toggler.toggles.default.addClass(obj.toggler.classes.toggleOpen).hide().attr('aria-hidden', 'true');
+		obj.toggler.toggles.default.addClass(obj.toggler.classes.toggleOpen).hide().attr( 'aria-hidden', 'true' );
 		obj.toggler.toggles.additional.removeClass(obj.toggler.classes.toggleOpen);
 	};
 
@@ -158,7 +158,7 @@ tribe.tickets.commerce.gateway = tribe.tickets.commerce.gateway || {};
 			obj.toggler.showGateway( gateway );
 		});
 		obj.toggler.toggles.additional.addClass(obj.toggler.classes.toggleOpen);
-		obj.toggler.toggles.default.removeClass(obj.toggler.classes.toggleOpen).show().attr('aria-hidden', 'false');
+		obj.toggler.toggles.default.removeClass(obj.toggler.classes.toggleOpen).show().attr( 'aria-hidden', 'false' );
 	};
 
 	/**
