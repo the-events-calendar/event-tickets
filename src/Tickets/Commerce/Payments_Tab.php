7c4fb1e97d4919702a2591871bb9862750ed7dbd
Juanfra Aldasoro <juanfraa@gmail.com>
2022-02-10 15:58:45 +0100

    Update Tickets Commerce link.

--- src/Tickets/Commerce/Payments_Tab.php
+++ src/Tickets/Commerce/Payments_Tab.php
@@ -251,20 +251,15 @@ class Payments_Tab extends tad_DI52_ServiceProvider {
 
 		if ( empty( $section_gateway ) ) {
 			// If no gateway section is selected, show main settings.
-			$plus_link    = sprintf(
+			$plus_link = sprintf(
 				'<a href="https://evnt.is/19zl" target="_blank" rel="noopener noreferrer">%s</a>',
 				esc_html__( 'Event Tickets Plus', 'event-tickets' )
 			);
-			$plus_link_2  = sprintf(
-				'<a href="https://evnt.is/19zl" target="_blank" rel="noopener noreferrer">%s</a>',
-				esc_html__( 'Check it out!', 'event-tickets' )
-			);
+
 			$plus_message = sprintf(
-				// Translators: %1$s: The Event Tickets Plus link, %2$s: The word "ticket" in lowercase, %3$s: The "Check it out!" link.
-				esc_html_x( 'Tickets Commerce is a light implementation of a commerce gateway using PayPal and simplified stock handling. If you need more advanced features, take a look at %1$s. In addition to integrating with your favorite ecommerce provider, Event Tickets Plus includes options to collect custom information for attendees, check attendees in via QR codes, and share stock between %2$s. %3$s', 'about Tickets Commerce', 'event-tickets' ),
-				$plus_link,
-				esc_html( tribe_get_ticket_label_singular_lowercase( 'tickets_fields_settings_about_tribe_commerce' ) ),
-				$plus_link_2
+				// Translators: %1$s: The Event Tickets Plus link.
+				esc_html_x( 'Tickets Commerce provides a simple and flexible ecommerce checkout for purchasing tickets.  Just choose your payment gateway and configure checkout options and you\'re all set.  If you need more advanced features like custom attendee information, QR code check in, and stock sharing between tickets, take a look at %1$s for these features and more.', 'about Tickets Commerce', 'event-tickets' ),
+				$plus_link
 			);
 			$is_tickets_commerce_enabled                   = tec_tickets_commerce_is_enabled();
 			$top_level_settings['tickets-commerce-header'] = [
