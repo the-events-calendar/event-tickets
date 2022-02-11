7fcec9ab280c5758747860e2528041776452be7b
Ricardo Moraleida <moraleida@gmail.com>
2022-02-10 23:48:31 -0300

    remove errors column from Orders

--- src/Tickets/Commerce/Admin_Tables/Orders.php
+++ src/Tickets/Commerce/Admin_Tables/Orders.php
@@ -3,7 +3,6 @@
 namespace TEC\Tickets\Commerce\Admin_Tables;
 
 use TEC\Tickets\Commerce\Gateways\Manager;
-use TEC\Tickets\Commerce\Gateways\Stripe\Logger;
 use TEC\Tickets\Commerce\Status\Completed;
 use TEC\Tickets\Commerce\Status\Refunded;
 use TEC\Tickets\Commerce\Status\Status_Handler;
@@ -108,7 +107,6 @@ class Orders extends WP_List_Table {
 			'gateway_order_id' => __( 'Gateway ID', 'event-tickets' ),
 			'status'           => __( 'Status', 'event-tickets' ),
 			'total'            => __( 'Total', 'event-tickets' ),
-			'errors'           => __( 'Errors', 'event-tickets' ),
 		];
 
 		return $columns;
@@ -336,32 +334,5 @@ class Orders extends WP_List_Table {
 		}
 
 		return $gateway::get_label();
-	}
-
-	/**
-	 * Handler for the errors column
-	 *
-	 * @since TBD
-	 *
-	 * @param WP_Post $item
-	 *
-	 * @return string
-	 */
-	public function column_errors( $item ) {
-		$logs = $item->gateway_errors;
-
-		if ( empty( $logs ) ) {
-			return '';
-		}
-
-		if ( ! is_array( $logs ) ) {
-			$logs = unserialize( $logs );
-		}
-
-		$count = count( $logs );
-
-		// @todo figure out how we should present these errors to the site admin.
-		// the errors are in $item->gateway_errors
-		return esc_html__( sprintf( '%d errors', $count ), 'event-tickets' );
 	}
 }
