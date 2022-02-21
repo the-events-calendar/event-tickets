a327d95b57002b5a4f0bc0ab6b8d56dee07cc659
rafsuntaskin <rafsun.aiub@gmail.com>
2022-02-21 13:38:40 +0600

    tests updated

--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Footer/GatewayErrorTest.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Footer/GatewayErrorTest.php
@@ -12,18 +12,16 @@ class GatewayErrorTest extends TicketsCommerceSnapshotTestCase {
 	 */
 	public function test_should_render_notice() {
 		$this->assertMatchesHtmlSnapshot( $this->get_partial_html( [
-				'items'            => [ 'Ticket 1', 'Ticket 2' ],
-				'gateways_active'  => 0,
-				'gateways_enabled' => 0,
+				'items'              => [ 'Ticket 1', 'Ticket 2' ],
+				'gateways_connected' => 0,
 			]
 		) );
 	}
 
 	public function test_should_render_empty_if_no_tickets() {
 		$this->assertEmpty( $this->get_partial_html( [
-				'items'            => [],
-				'gateways_active'  => 0,
-				'gateways_enabled' => 0,
+				'items'               => [],
+				'gateways_connected'  => 0,
 			]
 		) );
 	}
@@ -31,8 +29,7 @@ class GatewayErrorTest extends TicketsCommerceSnapshotTestCase {
 	public function test_should_render_empty_if_gateway_active() {
 		$this->assertEmpty( $this->get_partial_html( [
 				'items'            => [ 'Ticket 1', 'Ticket 2' ],
-				'gateways_active'  => 1,
-				'gateways_enabled' => 1,
+				'gateways_connected'  => 1,
 			]
 		) );
 	}
