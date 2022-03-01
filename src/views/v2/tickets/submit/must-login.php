f296c223dc5cb42ae76c202a203f3510366daacc
Matt Calvert <64328076+XanderCalvert@users.noreply.github.com>
2022-03-01 22:50:56 +0000

    Update must-login.php
    Fixing template routes

--- src/views/v2/tickets/submit/must-login.php
+++ src/views/v2/tickets/submit/must-login.php
@@ -4,7 +4,7 @@
  * Submit Login
  *
  * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/tickets/v2/tickets/submit/login.php
+ * [your-theme]/tribe/tickets/v2/tickets/submit/must-login.php
  *
  * See more documentation about our views templating system.
  *
