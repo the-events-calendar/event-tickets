706e1934508d36dadcc9e88ee83f53a8d2ec97b0
rafsuntaskin <rafsun.aiub@gmail.com>
2022-03-09 14:11:03 +0600

    updated snapshots for items test

--- tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/__snapshots__/ItemTest__test_should_render_wit_whatever_data_attribute__1.php
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/__snapshots__/ItemTest__test_should_render_wit_whatever_data_attribute__1.php
@@ -1,20 +1,20 @@
 <?php return '<div
-	id="tribe-block-tickets-item-117"
-	 class="tribe-tickets__tickets-item post-117 tribe_tpp_tickets type-tribe_tpp_tickets status-publish hentry" 	 data-ticket-id="117" data-available="false" data-has-shared-cap="false" data-ticket-price="99" data-whatever="value" >
+	id="tribe-block-tickets-item-11"
+	 class="tribe-tickets__tickets-item post-11 tribe_tpp_tickets type-tribe_tpp_tickets status-publish hentry" 	 data-ticket-id="11" data-available="false" data-has-shared-cap="false" data-ticket-price="99" data-whatever="value" >
 
 	<div  class="tribe-common-h7 tribe-common-h6--min-medium tribe-tickets__tickets-item-content-title"  >
 		Test ticket for 8</div>
 
 
 <div
-	id="tribe__details__content--117"
+	id="tribe__details__content--11"
 	 class="tribe-common-b2 tribe-common-b3--min-medium tribe-tickets__tickets-item-details-content" >
 	Test ticket description for 8</div>
 <div  class="tribe-tickets__tickets-item-extra" >
 
 	<div  class="tribe-common-b2 tribe-common-b1--min-medium tribe-tickets__tickets-item-extra-price" >
 		<span class="tribe-tickets__tickets-sale-price">
-
+		
 				<span class="tribe-formatted-currency-wrap tribe-currency-prefix">
 					<span class="tribe-currency-symbol">$</span>
 					<span class="tribe-amount">99.00</span>
@@ -22,14 +22,14 @@
 						</span>
 </div>
 
-
+	
 <div class="tribe-common-b3 tribe-tickets__tickets-item-extra-available">
 
-
+	
 	<span class="tribe-tickets__tickets-item-extra-available-quantity"> 100 </span> available
 </div>
 
-
+	
 </div>
 
 	<div  class="tribe-common-h4 tribe-tickets__tickets-item-quantity" >
@@ -37,12 +37,12 @@
 	Sold Out</div>
 	</div>
 
-
+	
 		<input
 		name="attendee[optout]"
 		value="1"
 		type="hidden"
 	/>
-
+	
 </div>
 ';
