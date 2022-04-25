7b9b90ee8cd8c01081a3446f62173971c3f5972f
Mike Cotton <codingmusician@gmail.com>
2022-04-25 18:06:10 -0500

    show upsell if no et+

--- src/Tickets/Admin/Upsell.php
+++ src/Tickets/Admin/Upsell.php
@@ -20,6 +20,7 @@ class Upsell {
 	 */
 	public function hooks() {
 		add_action( 'tribe_events_tickets_pre_edit', [ $this, 'maybe_show_capacity_arf' ] );
+		add_action( 'tec_events_tickets_attendees_event_summary_table_extra', [ $this, 'maybe_show_manual_attendees' ] );
 	}
 	
 	/**
@@ -46,6 +47,34 @@ class Upsell {
 				'url'     => 'https://evnt.is/et-in-app-capacity-arf',
 			],
 		] );
+	}
+	
+	/**
+	 * Maybe show upsell for Manual Attendees.
+	 * 
+	 * @since TBD
+	 */
+	public function maybe_show_manual_attendees() {
+		// If they already have ET+ activated, then bail.
+		if ( class_exists( 'Tribe__Tickets_Plus__Main' ) ) {
+			return;
+		}
+
+		echo '<div class="welcome-panel-column welcome-panel-extra">';
+		tribe( Upsell_Notice\Main::class )->render( [
+			'classes' => [
+				'tec-admin__upsell-event_tickets-manual_attendees'
+			],
+			'text'    => esc_html__( 'Manually add attendees with' , 'event-tickets' ),
+			'link'    => [
+				'classes' => [
+					'tec-admin__upsell-link--underlined'
+				],
+				'text'    => 'Event Tickets Plus',
+				'url'     => 'https://evnt.is/et-in-app-manual-attendees',
+			],
+		] );
+		echo '</div>';
 	}
 
 }
