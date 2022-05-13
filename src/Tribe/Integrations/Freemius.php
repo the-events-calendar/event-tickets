e6ff6338f84f2ca62eabac646fda72bfd428d6b4
Juan Aldasoro <juanfraa@gmail.com>
2022-05-13 15:39:21 +0200

    Add settings page to Freemius.

--- src/Tribe/Integrations/Freemius.php
+++ src/Tribe/Integrations/Freemius.php
@@ -96,9 +96,10 @@ class Tribe__Tickets__Integrations__Freemius {
 		$page = tribe_get_request_var( 'page' );
 
 		$valid_page = [
-			Tribe__Settings::$parent_slug => true,
-			Tribe__App_Shop::MENU_SLUG    => true,
-			'tribe-help'                  => true,
+			Tribe__Settings::$parent_slug                    => true,
+			Tribe__App_Shop::MENU_SLUG                       => true,
+			'tribe-help'                                     => true,
+			\Tribe\Tickets\Admin\Settings::$settings_page_id => true,
 		];
 
 		if ( class_exists( 'Tribe__Events__Aggregator__Page' ) ) {
