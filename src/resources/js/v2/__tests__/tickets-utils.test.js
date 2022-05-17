b74fd5697d94d4074931744f1f6884c57d8f7a55
rafsuntaskin <rafsun.aiub@gmail.com>
2022-05-17 18:41:42 +0600

    added dataset testing

--- src/resources/js/v2/__tests__/tickets-utils.test.js
+++ src/resources/js/v2/__tests__/tickets-utils.test.js
@@ -34,4 +34,19 @@ test( 'Testing demo rest endpoint', () => {
 test( 'Testing formats are not empty', () => {
 	expect( utils.getCurrencyFormatting('default') ).not.toBe( {} );
 } );
+
+describe( 'Format testing', () => {
+
+	const dataset = [
+		{ format: 'default', number: '12.22', output: '12.22' },
+		{ format: 'default', number: '1112.22', output: '1,112.22' },
+		{ format: 'default', number: '1111111', output: '1,111,111.00' },
+		{ format: 'german', number: '12.22', output: '12,22' },
+		{ format: 'german', number: '1212.22', output: '1.212,22' },
+		{ format: 'german', number: '393939', output: '393.939,00' },
+	];
+
+	it.each(dataset)('Testing number format ( $format, $number, $output )', ({ format, number, output }) => {
+		expect(utils.numberFormat( number, format )).toBe(output);
+	});
 } );
