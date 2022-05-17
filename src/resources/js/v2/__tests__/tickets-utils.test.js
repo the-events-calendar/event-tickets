146f3f1b2af2b59979e13f3a7d384b8e33b78863
rafsuntaskin <rafsun.aiub@gmail.com>
2022-05-17 22:08:09 +0600

    test methods

--- src/resources/js/v2/__tests__/tickets-utils.test.js
+++ src/resources/js/v2/__tests__/tickets-utils.test.js
@@ -1,5 +1,6 @@
 global.TribeCartEndpoint = { url : 'test' };
 
+// Follows the formatting found here: `Tribe__Tickets__Tickets::get_asset_localize_data_for_currencies()`
 const formats = {
 	'default' : {
 		"symbol":"$",
@@ -40,13 +41,22 @@ describe( 'Format testing', () => {
 	const dataset = [
 		{ format: 'default', number: '12.22', output: '12.22' },
 		{ format: 'default', number: '1112.22', output: '1,112.22' },
-		{ format: 'default', number: '1111111', output: '1,111,111.00' },
+		{ format: 'default', number: '1111111.00', output: '1,111,111.00' },
 		{ format: 'german', number: '12.22', output: '12,22' },
 		{ format: 'german', number: '1212.22', output: '1.212,22' },
-		{ format: 'german', number: '393939', output: '393.939,00' },
+		{ format: 'german', number: '393939.00', output: '393.939,00' },
+		{ format: 'german', number: '2011.00', output: '2.011,00' },
 	];
 
-	it.each(dataset)('Testing number format ( $format, $number, $output )', ({ format, number, output }) => {
-		expect(utils.numberFormat( number, format )).toBe(output);
-	});
+	describe( 'Testing Number format', () => {
+		it.each(dataset)('values :( $format, $number, $output )', ({ format, number, output }) => {
+			expect(utils.numberFormat( number, format )).toBe(output);
+		});
+	} );
+
+	describe( 'Testing Clean Number', () => {
+		it.each(dataset)('values : ( $format, $number, $output )', ({ format, number, output }) => {
+			expect(utils.cleanNumber( output, format )).toBe(number);
+		});
+	} );
 } );
