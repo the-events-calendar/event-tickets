6fcf650c869d8b5c963b5501ed46e89119d66fe7
Juan Aldasoro <juanfraa@gmail.com>
2022-05-17 15:08:52 +0200

    Back to `tec-tickets-settings`.

--- src/Tribe/Integrations/Freemius.php
+++ src/Tribe/Integrations/Freemius.php
@@ -68,7 +68,7 @@ class Tribe__Tickets__Integrations__Freemius {
 	 *
 	 * @var string
 	 */
-	private $page = 'tec-tickets';
+	private $page = 'tec-tickets-settings';
 
 	/**
 	 * Tribe__Tickets__Integrations__Freemius constructor.
