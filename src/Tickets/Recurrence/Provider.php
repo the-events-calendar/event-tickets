bf80e9bacd094f5556620f2e40976245b64255eb
Edwin Daniels <stratease@gmail.com>
2022-05-26 11:05:10 -0700

    Fix for migration state. Check if migration complete.

--- src/Tickets/Recurrence/Provider.php
+++ src/Tickets/Recurrence/Provider.php
@@ -3,6 +3,7 @@
 namespace TEC\Tickets\Recurrence;
 
 use tad_DI52_ServiceProvider;
+use TEC\Events\Custom_Tables\V1\Migration\State;
 
 class Provider extends tad_DI52_ServiceProvider {
 
@@ -11,11 +12,15 @@ class Provider extends tad_DI52_ServiceProvider {
 	 */
 	public function register() {
 
-		if ( ! class_exists( 'TEC\Events_Pro\Custom_Tables\V1\Provider' ) ) {
+		if ( ! class_exists( 'TEC\Events_Pro\Custom_Tables\V1\Provider' ) || ! class_exists( 'TEC\Events\Custom_Tables\V1\Migration\State' ) ) {
 			return;
 		}
 
 		if ( ! \TEC\Events\Custom_Tables\V1\Provider::is_active() ) {
+			return;
+		}
+
+		if ( ! tribe( State::class )->is_migrated() ) {
 			return;
 		}
 
