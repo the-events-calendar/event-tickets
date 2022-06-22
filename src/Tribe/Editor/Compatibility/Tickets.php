320c7843438783ad56f0c34ede2a1f14bff4f8f4
Mike Cotton <codingmusician@gmail.com>
2022-06-22 15:47:40 -0500

    update comment

--- src/Tribe/Editor/Compatibility/Tickets.php
+++ src/Tribe/Editor/Compatibility/Tickets.php
@@ -32,7 +32,7 @@ class Tribe__Tickets__Editor__Compatibility__Tickets {
 			return $content;
 		}
 
-		// If editing via Elementor, return.
+		// If edited via Elementor, return.
 		if ( class_exists( '\Elementor\Plugin' ) && \Elementor\Plugin::$instance->documents->get( get_the_ID() )->is_built_with_elementor() ) {
 			return $content;
 		}
