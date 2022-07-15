3cc24f7c32ec1c8de13a4827e5e2bd723f9ec7b2
rafsuntaskin <rafsun.aiub@gmail.com>
2022-07-15 18:30:24 +0600

    add capacity test

---
+++ tests/wpunit/Tickets/Commerce/Stock/CapacityTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tribe\Tickets\Commerce;
+
+use Tribe\Tickets\Test\Commerce\TicketsCommerce\Ticket_Maker;
+
+class CapacityTest extends \Codeception\TestCase\WPTestCase {
+
+	use Ticket_Maker;
+
+}
