411b8e49ded4d5e089242dff8494b4471c3511ae
rafsuntaskin <rafsun.aiub@gmail.com>
2022-09-13 18:00:37 +0600

    follow the same standard as TEC

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -24,8 +24,6 @@ if( ! defined( 'TRIBE_TESTS_HOME_URL' ) ) {
 	define( 'TRIBE_TESTS_HOME_URL', 'http://wordpress.test/' );
 }
 
-define( 'TEC_CUSTOM_TABLES_V1_DISABLED', true );
-
 /**
  * Codeception will regenerate snapshots on `--debug`, while the `spatie/snapshot-assertions`
  * library will do the same on `--update-snapshots`.
@@ -36,3 +34,7 @@ define( 'TEC_CUSTOM_TABLES_V1_DISABLED', true );
 if ( in_array( '--debug', $_SERVER['argv'], true ) ) {
 	$_SERVER['argv'][] = '--update-snapshots';
 }
+
+// By default, do not enable the Custom Tables v1 implementation in tests.
+putenv( 'TEC_CUSTOM_TABLES_V1_DISABLED=1' );
+$_ENV['TEC_CUSTOM_TABLES_V1_DISABLED'] = 1;
