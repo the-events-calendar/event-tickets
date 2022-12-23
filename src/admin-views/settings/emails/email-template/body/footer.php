d789354f33b243c3c839f360c96988c93c7a7ac8
Mike Cotton <codingmusician@gmail.com>
2022-12-23 11:15:04 -0600

    split up footer and some escapings

--- src/admin-views/settings/emails/email-template/body/footer.php
+++ src/admin-views/settings/emails/email-template/body/footer.php
@@ -4,6 +4,7 @@
  *
  * @since  TBD   Email template footer.
  * 
+ * @var Tribe__Template  $this  Parent template object.
  * @var string $footer_content    HTML of footer content.
  * @var bool   $footer_credit     Show the footer credit?
  * @var string $header_bg_color   Header background color.
@@ -18,32 +19,8 @@ if ( empty( $footer_content ) && ! tribe_is_truthy( $footer_credit ) ) {
 <tr>
 	<td style="padding:0px 20px 10px 20px;border-top:1px solid #efefef;background:<?php echo esc_attr( $header_bg_color ); ?>;">
 		<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-			<?php if ( ! empty( $footer_content ) ) : ?>
-				<tr>
-					<td style="padding:10px 0px 0px 0px;color:<?php echo esc_attr( $header_text_color ); ?>;">
-						<?php echo wp_kses( $footer_content, 'post' ); ?>
-					</td>
-				</tr>
-			<?php endif; ?>
-			<?php if ( tribe_is_truthy( $footer_credit ) ) : ?>
-				<tr>
-					<td style="padding:10px 0px 0px 0px;text-align:right;color:<?php echo $header_text_color; ?>;" align="right">
-					<?php
-						$et_link = sprintf( 
-							'<a href="%s" style="color:%s;">%s</a>',
-							'#', // @todo Update link to ET.
-							esc_attr( $header_text_color ),
-							esc_html__( 'Event Tickets', 'event-tickets' )
-						);
-						echo sprintf(
-							// Translators: %s - HTML link to `Event Tickets` website.
-							__( 'Ticket powered by %s', 'event-tickets' ),
-							$et_link
-						);
-					?>
-					</td>
-				</tr>
-			<?php endif; ?>
+			<?php $this->template( 'email-template/footer-content' ); ?>
+			<?php $this->template( 'email-template/footer-credit' ); ?>
 		</table>
 	</td>
 </tr>
--- src/admin-views/settings/emails/email-template/body/ticket-info.php
+++ src/admin-views/settings/emails/email-template/body/ticket-info.php
@@ -16,7 +16,7 @@
 	<td style="padding:20px 25px;background:<?php echo esc_attr( $ticket_bg_color ); ?>">
 		<?php $this->template( 'email-template/body/qr-image' ); ?>
 		<?php $this->template( 'email-template/body/recipient-name' ); ?>
-		<p style="font-size: 16px;margin:0;padding:0;color:<?php echo $ticket_text_color; ?>;">
+		<p style="font-size: 16px;margin:0;padding:0;color:<?php echo esc_attr( $ticket_text_color ); ?>;">
 			<?php echo esc_html( $ticket_name ); ?>
 		</p>
 		<p style="font-size: 14px;font-weight: 400;margin:0;padding:15px 0 0 0;color:<?php echo esc_attr( $ticket_text_color ); ?>;">
