2b3c2ea66c018b57d6db73ff36a198cd638f0f29
Mike Cotton <codingmusician@gmail.com>
2023-03-21 12:31:14 -0500

    doc blocks

--- src/views/v2/emails/admin-failed-order/body.php
+++ src/views/v2/emails/admin-failed-order/body.php
@@ -1,5 +1,20 @@
 <?php
-
+/**
+ * Event Tickets Emails: Failed Order Template.
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/tickets/v2/emails/admin-failed-order/body.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link https://evnt.is/tickets-emails-tpl Help article for Tickets Emails template files.
+ *
+ * @version TBD
+ *
+ * @since TBD
+ *
+ * @var Tribe_Template  $this  Current template object.
+ */
 
 
 $this->template( 'template-parts/body/title' );
