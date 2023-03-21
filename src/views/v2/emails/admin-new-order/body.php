388e4a47dfc09a9c033474d26f6d7deb6eddf2ff
Mike Cotton <codingmusician@gmail.com>
2023-03-21 15:35:04 -0500

    doc blocks

--- src/views/v2/emails/admin-new-order/body.php
+++ src/views/v2/emails/admin-new-order/body.php
@@ -1,5 +1,25 @@
 <?php
-
+/**
+ * Event Tickets Emails: Completed Order Template.
+ *
+ * Override this template in your own theme by creating a file at:
+ * [your-theme]/tribe/tickets/v2/emails/admin-new-order/body.php
+ *
+ * See more documentation about our views templating system.
+ *
+ * @link https://evnt.is/tickets-emails-tpl Help article for Tickets Emails template files.
+ *
+ * @version TBD
+ *
+ * @since TBD
+ *
+ * @var Tribe_Template  $this  Current template object.
+ * @var Module           $provider              [Global] The tickets provider instance.
+ * @var string           $provider_id           [Global] The tickets provider class name.
+ * @var \WP_Post         $order                 [Global] The order object.
+ * @var int              $order_id              [Global] The order ID.
+ * @var bool             $is_tec_active         [Global] Whether `The Events Calendar` is active or not.
+ */
 
 
 $this->template( 'template-parts/body/title' );
