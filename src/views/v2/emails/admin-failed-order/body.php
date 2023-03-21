63e7b878ae93fc526c423526bce094bda89a0dc9
Mike Cotton <codingmusician@gmail.com>
2023-03-21 12:53:09 -0500

    copy variables from TC success page

--- src/views/v2/emails/admin-failed-order/body.php
+++ src/views/v2/emails/admin-failed-order/body.php
@@ -14,6 +14,11 @@
  * @since TBD
  *
  * @var Tribe_Template  $this  Current template object.
+ * @var Module           $provider              [Global] The tickets provider instance.
+ * @var string           $provider_id           [Global] The tickets provider class name.
+ * @var \WP_Post         $order                 [Global] The order object.
+ * @var int              $order_id              [Global] The order ID.
+ * @var bool             $is_tec_active         [Global] Whether `The Events Calendar` is active or not.
  */
 
 
