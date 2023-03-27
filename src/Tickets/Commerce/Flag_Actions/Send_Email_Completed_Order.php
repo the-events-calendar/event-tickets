56ec35935f4ba7a39526cc77dd760bd56e79a991
Juan Aldasoro <juanfraa@gmail.com>
2023-03-27 19:53:09 +0200

    Apply suggestions from code review
    Co-authored-by: Rafsun Chowdhury <rafsun.aiub@gmail.com>

--- src/Tickets/Commerce/Flag_Actions/Send_Email_Completed_Order.php
+++ src/Tickets/Commerce/Flag_Actions/Send_Email_Completed_Order.php
@@ -8,7 +8,7 @@ use TEC\Tickets\Commerce\Status\Status_Interface;
 /**
  * Class Send_Email_Completed_Order, normally triggered when an order is completed.
  *
- * @since   TBD
+ * @since TBD
  *
  * @package TEC\Tickets\Commerce\Flag_Actions
  */
