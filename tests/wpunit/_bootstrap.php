156608e95aa035736573352dc8390f5617470c20
Luca Tumedei <luca@theaveragedev.com>
2023-03-28 14:45:11 +0200

    test(wpunit) disable Promoter trigger in tests, avoid cost of throwing and catching hundreds of exceptions

--- tests/wpunit/_bootstrap.php
+++ tests/wpunit/_bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 
+use Tribe\Tickets\Promoter\Triggers\Dispatcher;
+
 $tec_support = dirname( __DIR__, 3 ) . '/the-events-calendar/tests/_support';
 Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', $tec_support );
 
@@ -18,3 +20,6 @@ if ( ! function_exists( 'tribe_is_recurring_event' ) ) {
 
 update_option( 'theme', 'twentytwenty' );
 update_option( 'stylesheet', 'twentytwenty' );
+
+// Disable Promoter.
+remove_action( 'tribe_tickets_promoter_trigger', [ tribe( Dispatcher::class ), 'trigger' ] );
