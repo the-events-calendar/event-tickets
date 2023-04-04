886f2fe1b42e9daf57db6921ac29ed8f528592a4
Luca Tumedei <luca@theaveragedev.com>
2023-04-04 13:43:42 +0200

    test(ft_integration) fix filtering removal check logic

--- tests/ft_integration/Controller_Test_Case.php
+++ tests/ft_integration/Controller_Test_Case.php
@@ -11,6 +11,7 @@ namespace TEC\Tickets\Flexible_Tickets;
 
 use Codeception\TestCase\WPTestCase;
 use TEC\Common\Provider\Controller;
+use Tribe\Tests\Traits\With_Uopz;
 
 /**
  * Class Controller_Test_Case.
@@ -20,6 +21,8 @@ use TEC\Common\Provider\Controller;
  * @package TEC\Tickets\Flexible_Tickets;
  */
 class Controller_Test_Case extends WPTestCase {
+	use With_Uopz;
+
 	/**
 	 * A reference to the container used to create the controller and run the tests.
 	 *
@@ -68,17 +71,47 @@ class Controller_Test_Case extends WPTestCase {
 	 * @test
 	 */
 	public function should_register_and_unregister_correctly(): void {
+		// Run this now to check the `controller_class` property is set.
 		$controller = $this->make_controller();
 
-		global $wp_filters, $wp_actions;
-		$wp_filters_before = $wp_filters;
-		$wp_actions_before = $wp_actions;
+		$added_filters    = [];
+		$controller_class = $this->controller_class;
+
+		$this->set_fn_return( 'add_filter', function (
+			string $tag, callable $callback, int $priority = 10, int $args = 1
+		) use (
+			$controller_class, &$added_filters
+		) {
+			if ( is_array( $callback ) && $callback[0] instanceof $controller_class ) {
+				$added_filters[] = [ $tag, $callback, $priority ];
+			}
+			add_filter( $tag, $callback, $priority, $args );
+		}, true );
+		$this->set_fn_return( 'remove_filter', function (
+			string $tag, callable $callback, int $priority = 10
+		) use (
+			$controller_class, &$added_filters
+		) {
+			if (
+				is_array( $callback )
+				&& $callback[0] instanceof $controller_class
+			) {
+				$found = array_search( [ $tag, $callback, $priority ], $added_filters, true );
+				if ( $found !== false ) {
+					unset( $added_filters[ $found ] );
+				}
+			}
+			remove_filter( $tag, $callback, $priority );
+		}, true );
 
 		$controller->register();
-
 		$controller->unregister();
 
-		$this->assertEquals( $wp_filters_before, $wp_filters );
-		$this->assertEquals( $wp_actions_before, $wp_actions );
+		$this->assertCount(
+			0,
+			$added_filters,
+			'The controller should have removed all its filters and actions: '
+			. PHP_EOL . json_encode( $added_filters, JSON_PRETTY_PRINT )
+		);
 	}
 }
