0a14eb63d8ec0fedcb65d28f3067543b06003631
Mike Cotton <codingmusician@gmail.com>
2023-04-05 10:51:12 -0500

    spacing tweaks

--- src/views/v2/emails/template-parts/body/greeting.php
+++ src/views/v2/emails/template-parts/body/greeting.php
@@ -13,7 +13,7 @@
  *
  * @since TBD
  *
- * @var Tribe_Template  $this  Current template object.
+ * @var Tribe_Template   $this  Current template object.
  * @var Module           $provider              [Global] The tickets provider instance.
  * @var string           $provider_id           [Global] The tickets provider class name.
  * @var \WP_Post         $order                 [Global] The order object.
