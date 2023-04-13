6629bb9cd879f74b9940db1d184c0369ad730f4f
Juan Aldasoro <juanfraa@gmail.com>
2023-04-13 10:36:00 +0200

    Update src/views/v2/emails/purchase-receipt/intro.php

--- src/views/v2/emails/purchase-receipt/intro.php
+++ src/views/v2/emails/purchase-receipt/intro.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Event Tickets Emails: Main template > Body > Greeting.
+ * Event Tickets Emails: Main template > Body > Intro.
  *
  * Override this template in your own theme by creating a file at:
- * [your-theme]/tribe/tickets/v2/emails/template-parts/body/greeting.php
+ * [your-theme]/tribe/tickets/v2/emails/template-parts/body/intro.php
  *
  * See more documentation about our views templating system.
  *
