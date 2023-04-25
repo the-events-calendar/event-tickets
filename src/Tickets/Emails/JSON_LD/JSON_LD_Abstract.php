b137332fe985015721481f8fa3ac9b2274175130
rafsuntaskin <rafsun.aiub@gmail.com>
2023-04-25 23:30:40 +0600

    updated abstract

--- src/Tickets/Emails/JSON_LD/JSON_LD_Abstract.php
+++ src/Tickets/Emails/JSON_LD/JSON_LD_Abstract.php
@@ -6,7 +6,45 @@ namespace TEC\Tickets\Emails\JSON_LD;
  *
  * @since TBD
  */
-class JSON_LD_Abstract {
+abstract class JSON_LD_Abstract {
 
 	// @todo @rafsuntaskin Fill in later if needed.
+
+	/**
+	 * The type of the schema.
+	 *
+	 * @since TBD
+	 *
+	 * @var string
+	 */
+	private string $type;
+
+	/**
+	 * Get the data for the schema.
+	 *
+	 * @since TBD
+	 *
+	 * @return array<string,mixed> The data for the schema.
+	 */
+	public function get_basic_data(): array {
+		$data = [
+			'@context' => 'https://schema.org',
+			'@type'    => $this->type,
+			'merchant' => [
+				'@type' => 'Organization',
+				'name'  => get_bloginfo( 'name' ),
+			],
+		];
+
+		return $data;
+	}
+
+	/**
+	 * Get the JSON data for the schema.
+	 *
+	 * @since TBD
+	 *
+	 * @return array<string,mixed> The data for the schema.
+	 */
+	abstract public function get_data() : array;
 }
