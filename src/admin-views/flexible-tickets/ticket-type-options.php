cfe00dff1e48f2d8e6c15c571d839f2fc30d34c5
Luca Tumedei <luca@theaveragedev.com>
2023-04-25 17:48:59 +0200

    fix(FT) align type section correctly

--- src/admin-views/flexible-tickets/ticket-type-options.php
+++ src/admin-views/flexible-tickets/ticket-type-options.php
@@ -5,9 +5,10 @@
  * @since TBD
  */
 
+$origin = \Tribe__Tickets__Main::instance();
 ?>
 
-<div id="ticket_type_options">
+<div id="ticket_type_options" class="input_block">
 	<label class="ticket_form_label ticket_form_left" id="ticket_type_label" for="ticket_type">
 		<?php echo esc_html_x( 'Type:', 'The label used in the ticket edit form for the type of the ticket.', 'event-tickets' ); ?>
 	</label>
@@ -21,7 +22,7 @@
 		 style="display: flex; align-items: center">
 		<img
 				class="tribe-tickets-svgicon tec-tickets-icon tec-tickets-icon__ticket-type"
-				src="<?php echo esc_url( tribe_resource_url( 'icons/series-pass.svg', false, null, \Tribe__Tickets__Main::instance() ) ); ?>"
+				src="<?php echo esc_url( tribe_resource_url( 'icons/series-pass.svg', false, null, $origin ) ); ?>"
 				alt="<?php echo esc_attr_x(
 						'Series Pass',
 						'The alt text for the Series Pass icon in the ticket form.',
@@ -34,12 +35,13 @@
 				'The name of the Series Pass ticket type.',
 				'event-tickets'
 		); ?>
-	</span>
+		</span>
 		<span class="dashicons dashicons-editor-help ticket-type__help-icon"
 			  title="<?php echo esc_attr_x(
 					  'A Series Pass provides an attendee with access to all events in a Series.',
 					  'The help text for the Series Pass icon in the ticket form.',
 					  'event-tickets'
 			  ); ?>">
+		</span>
 	</div>
 </div>
