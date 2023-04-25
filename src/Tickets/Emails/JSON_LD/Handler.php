26c464d5a181338c303842f20036dc84854597fc
rafsuntaskin <rafsun.aiub@gmail.com>
2023-04-25 23:44:00 +0600

    updated order schema and handler

--- src/Tickets/Emails/JSON_LD/Handler.php
+++ src/Tickets/Emails/JSON_LD/Handler.php
@@ -21,83 +21,24 @@ class Handler {
 	 *
 	 * @param Email_Abstract $email The email object.
 	 *
-	 * @return array The JSON LD data.
 	 * @since TBD
 	 *
+	 * @return array The JSON LD data.
 	 */
-	public static function get_data( \TEC\Tickets\Emails\Email_Abstract $email ): array {
+	public static function get_data( Email_Abstract $email ) : array {
 
 		$data = [];
 
-		if ( $email::$slug === Completed_Order::$slug
-		     || $email::$slug === Purchase_Receipt::$slug ) {
+		if (
+			$email::$slug === Completed_Order::$slug
+			|| $email::$slug === Purchase_Receipt::$slug ) {
 			$order = $email->__get( 'order' );
 
 			if ( empty( $order ) ) {
 				return [];
 			}
 
-			return ( new Handler )->get_completed_order_data( $order );
-		}
-
-		return $data;
-	}
-
-	/**
-	 * Get the JSON LD data for a completed order.
-	 *
-	 * @param $order \WP_Post The order post object.
-	 *
-	 * @return array<string,mixed>
-	 * @since TBD
-	 *
-	 */
-	public function get_completed_order_data( $order ): array {
-
-		$commerce    = tribe( Module::class );
-		$report_link = $commerce->get_event_reports_link( $order->events_in_order[0], true );
-
-		$data = [
-			'@context'        => 'https://schema.org',
-			'@type'           => 'Order',
-			'orderNumber'     => $order->ID,
-			'merchant'        => [
-				'@type' => 'Organization',
-				'name'  => get_bloginfo( 'name' ),
-			],
-			'priceCurrency'   => $order->currency,
-			'price'           => $order->total,
-			'orderStatus'     => 'https://schema.org/OrderDelivered',
-			'customer'        => [
-				'@type' => 'Person',
-				'name'  => $order->purchaser_name,
-				'email' => $order->purchaser_email,
-			],
-			'url'             => $report_link,
-			'potentialAction' => [
-				'@type' => 'ViewAction',
-				'url'   => $report_link,
-				'name'  => esc_html__( 'View Order', 'event-tickets' ),
-			],
-		];
-
-		// Add order items.
-		foreach ( $order->items as $ticket_id => $item ) {
-			$ticket                  = tec_tc_get_ticket( $ticket_id );
-			$data['acceptedOffer'][] = [
-				'@type'            => 'Offer',
-				'price'            => $item['price'],
-				'priceCurrency'    => $order->currency,
-				'itemOffered'      => [
-					'@type' => 'Ticket',
-					'name'  => $ticket->post_title,
-				],
-				'eligibleQuantity' => [
-					'@type' => 'QuantitativeValue',
-					'value' => $item['quantity'],
-				],
-
-			];
+			return ( new Order_Schema( $order ) )->get_data();
 		}
 
 		return $data;
---
+++ src/Tickets/Emails/JSON_LD/Order_Schema.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace TEC\Tickets\Emails\JSON_LD;
+
+use TEC\Tickets\Commerce\Module;
+
+/**
+ * Class Order_Schema
+ *
+ * @todo @rafsuntaskin Fill in later if needed.
+ */
+class Order_Schema extends JSON_LD_Abstract {
+
+	/**
+	 * The type of the schema.
+	 *
+	 * @since TBD
+	 *
+	 * @var string
+	 */
+	private string $type = 'Order';
+
+	/**
+	 * The order object.
+	 *
+	 * @since TBD
+	 *
+	 * @var \WP_Post
+	 */
+	private \WP_Post $order;
+
+	public function __construct( \WP_Post $order ) {
+		$this->order = $order;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function get_data() : array {
+		$order       = $this->order;
+		$commerce    = tribe( Module::class );
+		$report_link = $commerce->get_event_reports_link( $order->events_in_order[0], true );
+
+		$data = [
+			'orderNumber'     => $order->ID,
+			'priceCurrency'   => $order->currency,
+			'price'           => $order->total,
+			'orderStatus'     => 'https://schema.org/OrderDelivered',
+			'customer'        => [
+				'@type' => 'Person',
+				'name'  => $order->purchaser_name,
+				'email' => $order->purchaser_email,
+			],
+			'url'             => $report_link,
+			'potentialAction' => [
+				'@type' => 'ViewAction',
+				'url'   => $report_link,
+				'name'  => esc_html__( 'View Order', 'event-tickets' ),
+			],
+		];
+
+		// Add order items.
+		foreach ( $order->items as $ticket_id => $item ) {
+			$ticket                  = tec_tc_get_ticket( $ticket_id );
+			$data['acceptedOffer'][] = [
+				'@type'            => 'Offer',
+				'price'            => $item['price'],
+				'priceCurrency'    => $order->currency,
+				'itemOffered'      => [
+					'@type' => 'Ticket',
+					'name'  => $ticket->post_title,
+				],
+				'eligibleQuantity' => [
+					'@type' => 'QuantitativeValue',
+					'value' => $item['quantity'],
+				],
+			];
+		}
+
+		return array_merge( $this->get_basic_data(), $data );
+	}
+}
