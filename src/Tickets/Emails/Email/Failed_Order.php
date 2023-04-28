7cb7d64bc759ce5e64dd4a7dfa651767c2081f57
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-04-27 23:03:14 -0400

    Update TBD to 5.5.11:  src/Tickets/Emails/Email/Failed_Order.php

--- src/Tickets/Emails/Email/Failed_Order.php
+++ src/Tickets/Emails/Email/Failed_Order.php
@@ -60,7 +60,7 @@ class Failed_Order extends \TEC\Tickets\Emails\Email_Abstract {
 	/**
 	 * Get email to.
 	 *
-	 * @since TBD
+	 * @since 5.5.11
 	 *
 	 * @return string The email "to".
 	 */
@@ -178,7 +178,7 @@ class Failed_Order extends \TEC\Tickets\Emails\Email_Abstract {
 	/**
 	 * Get default preview context for email.
 	 *
-	 * @since TBD
+	 * @since 5.5.11
 	 *
 	 * @param array $args The arguments.
 	 * @return array $args The modified arguments
@@ -205,7 +205,7 @@ class Failed_Order extends \TEC\Tickets\Emails\Email_Abstract {
 	/**
 	 * Get default template context for email.
 	 *
-	 * @since TBD
+	 * @since 5.5.11
 	 *
 	 * @return array $args The default arguments
 	 */
@@ -242,7 +242,7 @@ class Failed_Order extends \TEC\Tickets\Emails\Email_Abstract {
 	/**
 	 * Send the email.
 	 *
-	 * @since TBD
+	 * @since 5.5.11
 	 *
 	 * @return bool Whether the email was sent or not.
 	 */
