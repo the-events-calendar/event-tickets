b09559718fb3ed5e1ee72bd498b2e74783894050
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-05-01 11:26:16 -0400

    Remove the wrong namespace

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,7 +4,6 @@
  */
 
 Codeception\Util\Autoload::addNamespace( 'Tribe__Events__WP_UnitTestCase', __DIR__ . '/_support' );
-Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', __DIR__ . '/_support' );
 Codeception\Util\Autoload::addNamespace( 'Tribe\Tickets\Test', __DIR__ . '/_support' );
 Codeception\Util\Autoload::addNamespace( 'TEC\Tickets\Tests', __DIR__ . '/_support' );
 
