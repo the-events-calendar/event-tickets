c0d33b49cfec9a95e1d58c517c413ec687ea57dc
Mike Cotton <codingmusician@gmail.com>
2023-05-01 16:21:52 -0500

    fixes preview issues

--- src/Tickets/Emails/Admin/Preview_Data.php
+++ src/Tickets/Emails/Admin/Preview_Data.php
@@ -278,8 +278,8 @@ class Preview_Data {
 		$tickets = self::get_tickets();
 		$order   = self::get_order();
 		$default = [
-			'{attendee_name}'  => $tickets[0]->purchaser_name,
-			'{attendee_email}' => $tickets[0]->purchaser_email,
+			'{attendee_name}'  => $tickets[0]['purchaser_name'],
+			'{attendee_email}' => $tickets[0]['purchaser_email'],
 			'{order_number}'   => $order->ID,
 		];
 		return wp_parse_args( $args, $default );
