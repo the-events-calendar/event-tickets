0c8da8d9681beb1a1a3125292398efb6c7aadead
Rafsun Chowdhury <rafsun.aiub@gmail.com>
2023-05-15 20:42:46 +0600

    Update src/views/v2/emails/template-parts/header/head/json-ld.php
    Co-authored-by: Gustavo Bordoni <bordoni.dev@gmail.com>

--- src/views/v2/emails/template-parts/header/head/json-ld.php
+++ src/views/v2/emails/template-parts/header/head/json-ld.php
@@ -19,12 +19,13 @@
  * @var string                             $title              The email title.
  * @var bool                               $preview            Whether the email is in preview mode or not.
  * @var string                             $additional_content The email additional content.
- * @var bool                               $is_tec_active      Whether `The Events Calendar` is active or not.
- * @var \TEC\Tickets\Emails\JSON_LD\JSON_LD_Abstract $json_ld  The JSON-LD object.
+ * @var bool             $is_tec_active      Whether `The Events Calendar` is active or not.
+ * @var JSON_LD_Abstract $json_ld  The JSON-LD object.
  *
  * @see tribe_get_event() For the format of the event object.
  */
 
+use \TEC\Tickets\Emails\JSON_LD\JSON_LD_Abstract;
 if ( ! empty( $preview ) || empty( $json_ld ) ) {
 	return;
 }
