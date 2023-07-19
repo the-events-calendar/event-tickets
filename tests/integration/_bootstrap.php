d92cb2a746e68312ae8dae65e5442e654275c75b
Luca Tumedei <luca@theaveragedev.com>
2023-07-19 09:04:35 -0700

    test(integration,commerce_integration) disconnect Promoter in tests
    (cherry picked from commit c83c6b1dc8ad5b9f3580993cea7cbb67a1bda6c4)

--- tests/integration/_bootstrap.php
+++ tests/integration/_bootstrap.php
@@ -1,1 +1,6 @@
 <?php
+
+// Disconnect Promoter to avoid license-related notices.
+use Tribe\Tickets\Promoter\Triggers\Dispatcher;
+
+remove_action( 'tribe_tickets_promoter_trigger', [ tribe( Dispatcher::class ), 'trigger' ] );
