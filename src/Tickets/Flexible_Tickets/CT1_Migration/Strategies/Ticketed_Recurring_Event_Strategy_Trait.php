f1cb9c2ebbd977cac64a85e62785772a49ec71b3
Luca Tumedei <luca@theaveragedev.com>
2023-08-17 14:27:37 +0200

    fix(FT) clean post caches correctly, more robust ticket provider handling

--- src/Tickets/Flexible_Tickets/CT1_Migration/Strategies/Ticketed_Recurring_Event_Strategy_Trait.php
+++ src/Tickets/Flexible_Tickets/CT1_Migration/Strategies/Ticketed_Recurring_Event_Strategy_Trait.php
@@ -9,6 +9,9 @@
 
 namespace TEC\Tickets\Flexible_Tickets\CT1_Migration\Strategies;
 
+use TEC\Events\Custom_Tables\V1\Migration\Migration_Exception;
+use TEC\Events\Custom_Tables\V1\Migration\Reports\Event_Report;
+use TEC\Events\Custom_Tables\V1\Migration\String_Dictionary;
 use TEC\Events_Pro\Custom_Tables\V1\Series\Post_Type as Series_Post_Type;
 use TEC\Tickets\Flexible_Tickets\CT1_Migration\CT1_Migration_Checks;
 use TEC\Tickets\Flexible_Tickets\Series_Passes;
@@ -25,6 +28,40 @@ use Tribe__Tickets__Global_Stock as Global_Stock;
  */
 trait Ticketed_Recurring_Event_Strategy_Trait {
 	use CT1_Migration_Checks;
+
+	/**
+	 * Ticketed Single and Multi recurrence rule constructor.
+	 *
+	 * since TBD
+	 *
+	 * @param int  $post_id The post ID of the Event to migrate.
+	 * @param bool $dry_run Whether the migration should actually commit information or not.
+	 *
+	 * @throws Migration_Exception If the post is not an Event or the Event is not Recurring, or the Event has no
+	 *                             tickets.
+	 */
+	public function __construct( $post_id, $dry_run ) {
+		parent::__construct( $post_id, $dry_run );
+
+		if ( ! count( $this->get_ticket_ids( $post_id ) ) ) {
+			throw new Migration_Exception( 'Recurring Event has no tickets.' );
+		}
+
+		if ( $dry_run ) {
+			add_action( 'tec_events_custom_tables_v1_migration_after_dry_run', [ $this, 'clean_post_caches' ] );
+		}
+	}
+
+	/**
+	 * A list of post IDs that have been touched by this strategy.
+	 *
+	 * The property is used to ensure the cache is cleared for these posts after a dry run.
+	 *
+	 * @since TBD
+	 *
+	 * @var array<int>
+	 */
+	private array $touched_post_ids = [];
 
 	/**
 	 * Returns a list of meta keys relating a Ticket or an Attende to the Event.
@@ -89,8 +126,8 @@ trait Ticketed_Recurring_Event_Strategy_Trait {
 	 *
 	 * @param int $series_id The ID of the Series to move the Tickets to.
 	 *
-	 * @return array{0: int, 1: array<int,int>} The number of moved Tickets and the number of moved Attendees per Ticket
-	 *                                          indexed by Ticket ID, respectively.
+	 * @return array{0: int, 1: array<int,array<int>>} The list of moved Tickets and the list of moved Attendees,
+	 *                                                 grouped by Ticket.
 	 */
 	protected function move_tickets_to_series( int $series_id ): array {
 		$moved_tickets   = [];
@@ -107,7 +144,7 @@ trait Ticketed_Recurring_Event_Strategy_Trait {
 			update_post_meta( $ticket_id, '_type', Series_Passes::TICKET_TYPE );
 
 			$moved_tickets[]                = $ticket_id;
-			$moved_attendees [ $ticket_id ] = 0;
+			$moved_attendees [ $ticket_id ] = [];
 
 			$attendee_ids = $this->get_attendee_ids( $ticket_id );
 
@@ -123,7 +160,7 @@ trait Ticketed_Recurring_Event_Strategy_Trait {
 				// Attach the Attendee to the Series.
 				foreach ( $meta_keys as $meta_key ) {
 					update_post_meta( $attendee_id, $meta_key, $series_id );
-					$moved_attendees[ $ticket_id ] ++;
+					$moved_attendees[ $ticket_id ][] = $attendee_id;
 				}
 			}
 		}
@@ -173,6 +210,87 @@ trait Ticketed_Recurring_Event_Strategy_Trait {
 			}
 
 			update_post_meta( $series_id, $meta_key, $meta_value );
+		}
+	}
+
+	/**
+	 * Applies the strategy to the given Event and updates the Event_Report.
+	 *
+	 * @since TBD
+	 *
+	 * @param Event_Report $event_report The Event_Report to update.
+	 *
+	 * @throws Migration_Exception If the Series is not found.
+	 */
+	public function apply( Event_Report $event_report ): Event_Report {
+		// Backup the Ticket Provider to restore it after the original migration strategy is applied: it might change it.
+		$provider_meta_key = tribe( 'tickets.handler' )->key_provider_field;
+		$ticket_provider   = get_post_meta( $this->post_id, $provider_meta_key, true );
+
+		parent::apply( $event_report );
+
+		// Restore the Ticket Provider handling namespaced class names (e.g. Commerce).
+		$escaped_ticket_provider = str_replace( '\\', '\\\\', $ticket_provider );
+		update_post_meta( $this->post_id, $provider_meta_key, $escaped_ticket_provider );
+
+		if ( $event_report->status !== 'success' ) {
+			return $event_report;
+		}
+
+		$strings = tribe( String_Dictionary::class );
+
+		$series = tec_series()->where( 'event_post_id', $this->post_id )->first_id();
+
+		if ( $series === null ) {
+			throw new Migration_Exception( sprintf(
+				$strings->get( 'migration-failure-series-not-found' ),
+				$this->get_event_link_markup( $this->post_id )
+			) );
+		}
+
+		$this->ensure_series_ticketable();
+
+		[ $moved_tickets, $moved_attendees ] = $this->move_tickets_to_series( $series );
+
+		// Store the touched Ticket and Attendees IDs to clean their cache after a dry run.
+		$this->touched_post_ids = array_merge( [ $series ], $moved_tickets, ...$moved_attendees );
+
+		$this->set_default_ticket_provider( $series );
+		$this->set_global_capacity( $series );
+
+		$event_report->set( 'moved_tickets', $moved_tickets );
+		$moved_attendees_counts = array_combine(
+			array_keys( $moved_attendees ),
+			array_map( 'count', $moved_attendees )
+		);
+		$event_report->set( 'moved_attendees', $moved_attendees_counts );
+
+		return $event_report;
+	}
+
+	/**
+	 * Cleans the caches of the posts that have been touched by the migration dry-run.
+	 *
+	 * While the database status will be rolled back, persistent object cache that might have been
+	 * altered cannot be rolled back; the next best thing is to clean the cache of the posts that
+	 * have been touched by the migration.
+	 *
+	 * @since TBD
+	 *
+	 * @param int $post_id The migrated Event post ID.
+	 *
+	 * @return void The object cache of the Series, Tickets and Attendees touched by the migration will be flushed.
+	 */
+	public function clean_post_caches( int $post_id ): void {
+		if ( $post_id !== $this->post_id ) {
+			return;
+		}
+
+		// Do not run this a second time, do not hold a reference to the migration strategy object.
+		remove_action( 'tec_events_custom_tables_v1_migration_after_dry_run', [ $this, 'clean_post_caches' ] );
+
+		foreach ( $this->touched_post_ids as $id ) {
+			clean_post_cache( $id );
 		}
 	}
 }
--- src/Tickets/Flexible_Tickets/CT1_Migration/Strategies/Ticketed_Single_Rule_Event_Migration_Strategy.php
+++ src/Tickets/Flexible_Tickets/CT1_Migration/Strategies/Ticketed_Single_Rule_Event_Migration_Strategy.php
@@ -9,14 +9,9 @@
 
 namespace TEC\Tickets\Flexible_Tickets\CT1_Migration\Strategies;
 
-use TEC\Events\Custom_Tables\V1\Migration\Migration_Exception;
-use TEC\Events\Custom_Tables\V1\Migration\Reports\Event_Report;
 use TEC\Events\Custom_Tables\V1\Migration\Strategies\Strategy_Interface;
-use TEC\Events\Custom_Tables\V1\Migration\String_Dictionary;
 use TEC\Events\Custom_Tables\V1\Traits\With_String_Dictionary;
 use TEC\Events_Pro\Custom_Tables\V1\Migration\Strategy\Single_Rule_Event_Migration_Strategy;
-use TEC\Events_Pro\Custom_Tables\V1\Series\Post_Type as Series_Post_Type;
-use WP_Post;
 
 /**
  * Class Ticketed_Single_Rule_Event_Migration_Strategy.
@@ -40,64 +35,5 @@ class Ticketed_Single_Rule_Event_Migration_Strategy
 	 */
 	public static function get_slug() {
 		return 'tec-tickets-recurring-single-rule-with-tickets-strategy';
-	}
-
-	/**
-	 * Ticketed_Single_Rule_Event_Migration_Strategy constructor.
-	 *
-	 * since TBD
-	 *
-	 * @param int  $post_id The post ID of the Event to migrate.
-	 * @param bool $dry_run Whether the migration should actually commit information or not.
-	 *
-	 * @throws Migration_Exception If the post is not an Event or the Event is not Recurring, or the Event has no
-	 *                             tickets.
-	 */
-	public function __construct( $post_id, $dry_run ) {
-		parent::__construct( $post_id, $dry_run );
-
-		if ( ! count( $this->get_ticket_ids( $post_id ) ) ) {
-			throw new Migration_Exception( 'Recurring Event has no tickets.' );
-		}
-	}
-
-	/**
-	 * Applies the strategy to the given Event and updates the Event_Report.
-	 *
-	 * @since TBD
-	 *
-	 * @param Event_Report $event_report The Event_Report to update.
-	 *
-	 * @throws Migration_Exception
-	 */
-	public function apply( Event_Report $event_report ): Event_Report {
-		parent::apply( $event_report );
-
-		if ( $event_report->status !== 'success' ) {
-			return $event_report;
-		}
-
-		$strings = tribe( String_Dictionary::class );
-
-		$series = tec_series()->where( 'event_post_id', $this->post_id )->first_id();
-
-		if ( $series === null ) {
-			throw new Migration_Exception( sprintf(
-				$strings->get( 'migration-failure-series-not-found' ),
-				$this->get_event_link_markup( $this->post_id )
-			) );
-		}
-
-		$this->ensure_series_ticketable();
-
-		[ $moved_tickets, $moved_attendees ] = $this->move_tickets_to_series( $series );
-
-		$this->set_default_ticket_provider( $series );
-		$this->set_global_capacity( $series );
-
-		$event_report->set( 'moved_tickets', $moved_tickets );
-		$event_report->set( 'moved_attendees', $moved_attendees );
-
-		return $event_report;
 	}
 }
