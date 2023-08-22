6296b7a3363d2ffd413bbc6b64550029696a6096
Luca Tumedei <luca@theaveragedev.com>
2023-08-22 15:51:50 +0200

    test(ft_smoketest) update expectation

--- tests/ft_smoketest/SettingsCest.php
+++ tests/ft_smoketest/SettingsCest.php
@@ -1,19 +1,18 @@
 <?php
 
 use FT_Smoketester as Tester;
+use TEC\Events_Pro\Custom_Tables\V1\Series\Post_Type as Series_Post_Type;
 
 class SettingsCest {
 	/**
-	 * It should allow users to set Series as a Post Type that can have tickets
+	 * It should not allow controlling the ticketability of Series
 	 *
 	 * @test
 	 */
-	public function should_allow_setting_series_to_have_tickets( Tester $I ): void {
+	public function should_not_allow_controlling_the_ticketability_of_series( Tester $I ): void {
 		$I->loginAsAdmin();
 
 		$I->amOnAdminPage( '/admin.php?page=tec-tickets-settings' );
-		$I->seeElement('label[title="Series"]');
-		$I->seeElement('input[value="tribe_event_series"]');
+		$I->dontSeeElement( 'input[value="' . Series_Post_Type::POSTTYPE . '"]' );
 	}
-
 }
