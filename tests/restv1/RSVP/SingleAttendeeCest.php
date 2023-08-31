10a98a066e16cc2c9fdc552c7bb0ce545507985b
rafsuntaskin <rafsun.aiub@gmail.com>
2023-08-31 13:21:04 +0600

    tests added for check_in validation with RSVP

--- tests/restv1/RSVP/SingleAttendeeCest.php
+++ tests/restv1/RSVP/SingleAttendeeCest.php
@@ -30,12 +30,57 @@ class SingleAttendeeCest extends BaseRestCest {
 			'optout'      => false,
 		] );
 
-		$attendee_id     = $attendees[ 0 ]['attendee_id'];
+		$attendee_id     = $attendees[0]['attendee_id'];
 		$ticket_rest_url = $this->attendees_url . "/{$attendee_id}";
 
 		$I->sendGET( $ticket_rest_url );
 
 		$I->seeResponseCodeIs( 200 );
 		$I->seeResponseIsJson();
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_not_allow_check_in_for_non_complete_order_status( Restv1Tester $I ) {
+		$post_id     = $I->havePostInDatabase( [ 'post_content' => '[tribe_attendees_list]' ] );
+		$ticket_id   = $this->create_rsvp_ticket( $post_id );
+		$attendee_id = $this->create_attendee_for_ticket( $ticket_id, $post_id, [
+			'rsvp_status' => 'no',
+			'optout'      => false,
+		] );
+
+		$I->generate_nonce_for_role( 'administrator' );
+		$ticket_rest_url = $this->attendees_url . "/{$attendee_id}";
+
+		$I->sendPost( $ticket_rest_url, [ 'check_in' => 1 ] );
+		$I->seeResponseCodeIs( 400 );
+		$I->seeResponseIsJson();
+
+		$expected_json = [
+			'code'    => 'tec-et-attendee-invalid-check-in',
+			'message' => 'Attendee Order status is not authorized for check-in.',
+		];
+		$I->seeResponseContainsJson( $expected_json );
+	}
+
+	/**
+	 * @test
+	 */
+	public function test_should_allow_check_in_for_complete_order_status( Restv1Tester $I ) {
+		$post_id     = $I->havePostInDatabase();
+		$ticket_id   = $this->create_rsvp_ticket( $post_id );
+		$attendee_id = $this->create_attendee_for_ticket( $ticket_id, $post_id, [
+			'rsvp_status' => 'yes',
+			'optout'      => false,
+		] );
+
+		$I->generate_nonce_for_role( 'administrator' );
+		$ticket_rest_url = $this->attendees_url . "/{$attendee_id}";
+
+		$I->sendPost( $ticket_rest_url, [ 'check_in' => 1 ] );
+		$I->seeResponseCodeIs( 201 );
+		$I->seeResponseIsJson();
+		$I->seeResponseContainsJson( [ 'checked_in' => true ] );
 	}
 }
