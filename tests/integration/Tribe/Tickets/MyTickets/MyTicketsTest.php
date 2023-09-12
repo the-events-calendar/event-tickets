358a6441922155f1f02bdd17803912ba99d47b76
rafsuntaskin <rafsun.aiub@gmail.com>
2023-09-13 01:34:40 +0600

    tests(integration) updated snapshots with more scenario

--- tests/integration/Tribe/Tickets/MyTickets/MyTicketsTest.php
+++ tests/integration/Tribe/Tickets/MyTickets/MyTicketsTest.php
@@ -30,30 +30,89 @@ class MyTicketsTest extends WPTestCase {
 	}
 
 	public function get_purchased_ticket_data(): Generator {
-		yield 'post with no rsvp or tickets purchased' => [ function (): array {
-			$post_id = $this->factory()->post->create();
-			//create a rsvp ticket for that post.
-			$rsvp_ticket_id = $this->create_rsvp_ticket( $post_id );
-			return [ $post_id ];
-		} ];
+		yield 'post with no rsvp or tickets purchased' => [
+			function (): array {
+				$post_id = $this->factory()->post->create();
+				//create a rsvp ticket for that post.
+				$rsvp_ticket_id = $this->create_rsvp_ticket( $post_id );
 
-		yield 'post with 1 rsvp purchased' => [ function (): array {
-			$post_id = $this->factory()->post->create();
-			//create a rsvp ticket for that post.
-			$rsvp_ticket_id = $this->create_rsvp_ticket( $post_id );
+				return [ $post_id ];
+			}
+		];
 
-			$this->create_attendee_for_ticket( $rsvp_ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
-			return [ $post_id ];
-		} ];
+		yield 'post with 1 rsvp purchased' => [
+			function (): array {
+				$post_id = $this->factory()->post->create();
+				//create a rsvp ticket for that post.
+				$rsvp_ticket_id = $this->create_rsvp_ticket( $post_id );
+				$attendee       = $this->create_attendee_for_ticket( $rsvp_ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
 
-		yield 'post with 3 rsvp purchased' => [ function (): array {
-			$post_id = $this->factory()->post->create();
-			//create a rsvp ticket for that post.
-			$rsvp_ticket_id = $this->create_rsvp_ticket( $post_id );
+				return [ $post_id ];
+			}
+		];
 
-			$this->create_many_attendees_for_ticket( 3, $rsvp_ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
-			return [ $post_id ];
-		} ];
+		yield 'post with 3 rsvp purchased' => [
+			function (): array {
+				$post_id = $this->factory()->post->create();
+				//create a rsvp ticket for that post.
+				$rsvp_ticket_id = $this->create_rsvp_ticket( $post_id );
+				$this->create_many_attendees_for_ticket( 3, $rsvp_ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
+
+				return [ $post_id ];
+			}
+		];
+
+		yield 'post with 1 ticket purchased' => [
+			function (): array {
+				$post_id = $this->factory()->post->create();
+				// create a tc ticket..
+				$ticket_id = $this->create_tc_ticket( $post_id, 10 );
+				$attendee  = $this->create_attendee_for_ticket( $ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
+
+				return [ $post_id ];
+			}
+		];
+
+		yield 'post with 3 tickets purchased' => [
+			function (): array {
+				$post_id = $this->factory()->post->create();
+				// create a tc ticket..
+				$ticket_id = $this->create_tc_ticket( $post_id, 10 );
+				$attendee  = $this->create_attendee_for_ticket( $ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
+
+				return [ $post_id ];
+			}
+		];
+
+		yield 'post with 1 ticket and 1 RSVP purchased' => [
+			function (): array {
+				$post_id = $this->factory()->post->create();
+				// create a tc ticket..
+				$ticket_id = $this->create_tc_ticket( $post_id, 10 );
+				$attendee  = $this->create_attendee_for_ticket( $ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
+
+				// create rsvp ticket.
+				$rsvp_ticket_id = $this->create_rsvp_ticket( $post_id );
+				$attendee       = $this->create_attendee_for_ticket( $rsvp_ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
+
+				return [ $post_id ];
+			}
+		];
+
+		yield 'post with 2 ticket and 3 RSVP purchased' => [
+			function (): array {
+				$post_id = $this->factory()->post->create();
+				// create a tc ticket.
+				$ticket_id = $this->create_tc_ticket( $post_id, 10 );
+				$attendee  = $this->create_many_attendees_for_ticket( 2, $ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
+
+				// create rsvp ticket.
+				$rsvp_ticket_id = $this->create_rsvp_ticket( $post_id );
+				$attendee       = $this->create_many_attendees_for_ticket( 3, $rsvp_ticket_id, $post_id, [ 'user_id' => get_current_user_id() ] );
+
+				return [ $post_id ];
+			}
+		];
 	}
 
 	/**
@@ -68,7 +127,7 @@ class MyTicketsTest extends WPTestCase {
 
 		/** @var \Tribe__Tickets__Editor__Template $template */
 		$template = tribe( 'tickets.editor.template' );
-		$html = $template->template( 'blocks/attendees/view-link', [ 'post_id' => $post->ID ], false );
+		$html     = $template->template( 'blocks/attendees/view-link', [ 'post_id' => $post->ID ], false );
 
 		$this->assertMatchesHtmlSnapshot( $html );
 	}
