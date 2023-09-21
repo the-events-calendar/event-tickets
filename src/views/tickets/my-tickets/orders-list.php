c309ded385273f14545c3f759bc951a24011766f
Mike Cotton <codingmusician@gmail.com>
2023-09-21 15:19:30 -0500

    tweak

--- src/views/tickets/my-tickets/orders-list.php
+++ src/views/tickets/my-tickets/orders-list.php
@@ -33,12 +33,10 @@
 						'order'     => $order,
 						'attendees' => $attendees,
 					] );
-				?>
-				<?php 
+
 					// @todo Need to determine title based on ticket type. RIght now, it's being passed into the main template.
 					$this->template( 'tickets/my-tickets/title' ); 
-				?>
-				<?php 
+
 					$this->template( 'tickets/my-tickets/tickets-list', [
 						'order'     => $order,
 						'attendees' => $attendees,
