c281eb179b76f0a5cbfc7990e878b34fb5596236
Luca Tumedei <luca@theaveragedev.com>
2023-09-27 10:22:56 +0200

    test(ft_integration) re-enable all Attendee page tests

--- tests/ft_integration/Tribe/Tickets/AttendeesTest.php
+++ tests/ft_integration/Tribe/Tickets/AttendeesTest.php
@@ -58,66 +58,66 @@ class AttendeesTest extends WPTestCase {
 	}
 
 	public function display_provider(): Generator {
-//		yield 'single event without attendees' => [
-//			function (): array {
-//				$event_id = tribe_events()->set_args( [
-//					'title'      => 'Event with no attendees',
-//					'status'     => 'publish',
-//					'start_date' => '2020-01-01 00:00:00',
-//					'duration'   => 2 * HOUR_IN_SECONDS,
-//				] )->create()->ID;
-//				$ticket_id = $this->create_tc_ticket( $event_id );
-//
-//				return [ $event_id, [ $event_id, $ticket_id ] ];
-//			}
-//		];
-//
-//		yield 'single event with Single Ticket attendees' => [
-//			function (): array {
-//				$event_id = tribe_events()->set_args( [
-//					'title'      => 'Event with no attendees',
-//					'status'     => 'publish',
-//					'start_date' => '2020-01-01 00:00:00',
-//					'duration'   => 2 * HOUR_IN_SECONDS,
-//				] )->create()->ID;
-//				$ticket_id = $this->create_tc_ticket( $event_id );
-//				$this->create_order( [ $ticket_id => 3 ] );
-//
-//				return [ $event_id, [ $event_id, $ticket_id ] ];
-//			}
-//		];
-//
-//		yield 'single event with RSVP attendees' => [
-//			function (): array {
-//				$event_id = tribe_events()->set_args( [
-//					'title'      => 'Event with no attendees',
-//					'status'     => 'publish',
-//					'start_date' => '2020-01-01 00:00:00',
-//					'duration'   => 2 * HOUR_IN_SECONDS,
-//				] )->create()->ID;
-//				$ticket_id = $this->create_rsvp_ticket( $event_id );
-//				$this->create_many_attendees_for_ticket( 3, $ticket_id, $event_id );
-//
-//				return [ $event_id, [ $event_id, $ticket_id ] ];
-//			}
-//		];
-//
-//		yield 'single event with Single Tickets and RSVP Attendees' => [
-//			function (): array {
-//				$event_id = tribe_events()->set_args( [
-//					'title'      => 'Event with no attendees',
-//					'status'     => 'publish',
-//					'start_date' => '2020-01-01 00:00:00',
-//					'duration'   => 2 * HOUR_IN_SECONDS,
-//				] )->create()->ID;
-//				$ticket_id = $this->create_tc_ticket( $event_id );
-//				$this->create_order( [ $ticket_id => 3 ] );
-//				$rsvp_ticket_id = $this->create_rsvp_ticket( $event_id );
-//				$this->create_many_attendees_for_ticket( 3, $rsvp_ticket_id, $event_id );
-//
-//				return [ $event_id, [ $event_id, $ticket_id, $rsvp_ticket_id ] ];
-//			}
-//		];
+		yield 'single event without attendees' => [
+			function (): array {
+				$event_id = tribe_events()->set_args( [
+					'title'      => 'Event with no attendees',
+					'status'     => 'publish',
+					'start_date' => '2020-01-01 00:00:00',
+					'duration'   => 2 * HOUR_IN_SECONDS,
+				] )->create()->ID;
+				$ticket_id = $this->create_tc_ticket( $event_id );
+
+				return [ $event_id, [ $event_id, $ticket_id ] ];
+			}
+		];
+
+		yield 'single event with Single Ticket attendees' => [
+			function (): array {
+				$event_id = tribe_events()->set_args( [
+					'title'      => 'Event with no attendees',
+					'status'     => 'publish',
+					'start_date' => '2020-01-01 00:00:00',
+					'duration'   => 2 * HOUR_IN_SECONDS,
+				] )->create()->ID;
+				$ticket_id = $this->create_tc_ticket( $event_id );
+				$this->create_order( [ $ticket_id => 3 ] );
+
+				return [ $event_id, [ $event_id, $ticket_id ] ];
+			}
+		];
+
+		yield 'single event with RSVP attendees' => [
+			function (): array {
+				$event_id = tribe_events()->set_args( [
+					'title'      => 'Event with no attendees',
+					'status'     => 'publish',
+					'start_date' => '2020-01-01 00:00:00',
+					'duration'   => 2 * HOUR_IN_SECONDS,
+				] )->create()->ID;
+				$ticket_id = $this->create_rsvp_ticket( $event_id );
+				$this->create_many_attendees_for_ticket( 3, $ticket_id, $event_id );
+
+				return [ $event_id, [ $event_id, $ticket_id ] ];
+			}
+		];
+
+		yield 'single event with Single Tickets and RSVP Attendees' => [
+			function (): array {
+				$event_id = tribe_events()->set_args( [
+					'title'      => 'Event with no attendees',
+					'status'     => 'publish',
+					'start_date' => '2020-01-01 00:00:00',
+					'duration'   => 2 * HOUR_IN_SECONDS,
+				] )->create()->ID;
+				$ticket_id = $this->create_tc_ticket( $event_id );
+				$this->create_order( [ $ticket_id => 3 ] );
+				$rsvp_ticket_id = $this->create_rsvp_ticket( $event_id );
+				$this->create_many_attendees_for_ticket( 3, $rsvp_ticket_id, $event_id );
+
+				return [ $event_id, [ $event_id, $ticket_id, $rsvp_ticket_id ] ];
+			}
+		];
 
 		yield 'single event in series with Single Tickets, RSVPs and Series Passes' => [
 			function (): array {
