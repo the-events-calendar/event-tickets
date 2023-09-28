d4045b4794a8b551710463424be6c7a8a659affd
Brian Krane <redscar@gmail.com>
2023-09-28 14:04:37 -0400

    `Move_tickets` is working properly right now.

--- tests/commerce_integration/TEC/Admin/MoveTicketsTest.php
+++ tests/commerce_integration/TEC/Admin/MoveTicketsTest.php
@@ -11,80 +11,70 @@ use TEC\Tickets\Commerce\Provider;
 use TEC\Tickets\Commerce\Status\Completed;
 use TEC\Tickets\Commerce\Status\Pending;
 use Tribe\Events\Test\Factories\Event;
+use Tribe\Tickets\Test\Commerce\TicketsCommerce\Order_Maker;
 use Tribe\Tickets\Test\Commerce\TicketsCommerce\Ticket_Maker;
-use Tribe\Tickets\Test\Commerce\Attendee_Maker;
 use Tribe__Tickets__Tickets;
 
 class MoveTicketsTest extends \Codeception\TestCase\WPTestCase {
 	use Ticket_Maker;
-	use Attendee_Maker;
+	use Order_Maker;
 
 	/**
-	 * @inheritDoc
+	 * Test the shared capacity purchase functionality.
 	 */
-	public static function setUpBeforeClass() {
-		parent::setUpBeforeClass();
-
-		tribe_register_provider( Provider::class );
-
-		add_filter( 'tribe_tickets_ticket_object_is_ticket_cache_enabled', '__return_false' );
-
-		//Set our default provider to TC so the logic below works properly.
-		add_filter( 'tribe_tickets_get_default_module', function ( $default_module, $modules ) {
-			return 'TEC\Tickets\Commerce\Module';
-		},          10, 2 );
-
-	}
-
 	public function test_tc_shared_capacity_purchase() {
 
-		$maker = new Event();
-		$event_1_id= $maker->create();
+		// Create two new events.
+		$maker      = new Event();
+		$event_1_id = $maker->create();
 		$event_2_id = $maker->create();
 
-		$overrides = [
+		// Create a ticket for the first event with capped stock mode and specific capacities.
+		$overrides      = [
 			'tribe-ticket' => [
 				'mode'           => \Tribe__Tickets__Global_Stock::CAPPED_STOCK_MODE,
 				'event_capacity' => 50,
 				'capacity'       => 30,
 			],
 		];
-		$event_1_ticket   = $this->create_tc_ticket( $event_1_id, 10, $overrides );
+		$event_1_ticket = $this->create_tc_ticket( $event_1_id, 10, $overrides );
 
-		$overrides = [
+		// Create a ticket for the second event with global stock mode and specific capacities.
+		$overrides      = [
 			'tribe-ticket' => [
 				'mode'           => \Tribe__Tickets__Global_Stock::GLOBAL_STOCK_MODE,
 				'event_capacity' => 50,
 				'capacity'       => 50,
 			],
 		];
-		$event_2_ticket   = $this->create_tc_ticket( $event_2_id, 20, $overrides );
+		$event_2_ticket = $this->create_tc_ticket( $event_2_id, 20, $overrides );
 
-		$attendees_for_event_1 = $this->create_many_attendees_for_ticket( 5, $event_1_ticket, $event_1_id );
+		// Create an order for 5 tickets of the first event.
+		$order = $this->create_order( [ $event_1_ticket => 5 ] );
 
+		// Fetch the attendees for the first event ticket.
+		$attendees_objects = tribe_tickets_get_ticket_provider( $event_1_ticket )->get_attendees_by_id( $event_1_ticket );
 
-		codecept_debug($attendees_for_event_1);
+		// Assert that the attendees_objects array is not empty.
+		$this->assertNotEmpty( $attendees_objects, 'The attendees_objects array should not be empty.' );
+
+		// Assert that the first item in the array has the 'ID' key.
+		$this->assertArrayHasKey( 'ID', $attendees_objects[0], 'The first item in the array should have an "ID" key.' );
+
 		/**
-		 * Our goal is to move a single ticket from Event 0 to Event 1.
+		 * Our goal is to move a single ticket from Event 1 to Event 2.
 		 */
 
-		/**
-		 * action    "move_tickets"
-		 * src_post_id    "14067" Event 1
-		 * target_post_id    "14070" Event 2
-		 * check    "227674bfdc"
-		 * ticket_ids[]    "14082" ID of Attendee
-		 * target_type_id    "14071" Event 2 Ticket
-		 */
+		// Move the first ticket from the first event to the second event.
+		$successful_moves = tribe( 'Tribe__Tickets__Admin__Move_Tickets' )->move_tickets(
+			[ $attendees_objects[0]['ID'] ],
+			$event_2_ticket,
+			$event_1_id,
+			$event_2_id
+		);
 
-		// Now that our scenario is set up, lets move ticket_a to the second event.
-		$successful_moves = tribe( 'Tribe__Tickets__Admin__Move_Tickets' )->move_tickets( [ $attendees_for_event_1 ],
-		                                                                                  $event_2_ticket,
-		                                                                                  $event_1_id,
-		                                                                                  $event_2_id );
-		codecept_debug("Moved ". $successful_moves . " tickets");
-
-
+		// Assert that the move was successful.
+		$this->assertEquals(1, $successful_moves, 'The ticket move operation should be successful.' );
 	}
 
 }
