c19263c41fa0e9e26246497b0d9325f319138776
rafsuntaskin <rafsun.aiub@gmail.com>
2023-10-04 23:04:37 +0600

    tests add OrderReport tests

---
+++ tests/integration/Tribe/Admin/OrderReportTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Tribe\Admin;
+
+use Closure;
+use Generator;
+use Codeception\TestCase\WPTestCase;
+use tad\Codeception\SnapshotAssertions\SnapshotAssertions;
+use Tribe\Tests\Traits\With_Uopz;
+use TEC\Tickets\Commerce\Reports\Orders as Order_Report;
+use Tribe\Tickets\Test\Commerce\TicketsCommerce\Order_Maker;
+use Tribe\Tickets\Test\Commerce\TicketsCommerce\Ticket_Maker;
+
+class OrderReportTest extends WPTestCase {
+
+	use SnapshotAssertions;
+	use With_Uopz;
+	use Ticket_Maker;
+	use Order_Maker;
+
+	public function placehold_post_ids( string $snapshot, array $ids ): string {
+		return str_replace(
+			$ids,
+			array_fill( 0, count( $ids ), '{{ID}}' ),
+			$snapshot
+		);
+	}
+
+	public function tc_order_report_data_provider(): Generator {
+		yield 'event with no orders' => [
+			function (): array {
+				$event_id  = tribe_events()->set_args( [
+					'title'      => 'Event with no attendees',
+					'status'     => 'publish',
+					'start_date' => '2020-01-01 00:00:00',
+					'duration'   => 2 * HOUR_IN_SECONDS,
+				] )->create()->ID;
+				$ticket_id = $this->create_tc_ticket( $event_id );
+
+				return [ $event_id, [ $event_id, $ticket_id ] ];
+			}
+		];
+
+		yield 'event with one order' => [
+			function (): array {
+				$event_id  = tribe_events()->set_args( [
+					'title'      => 'Event with no attendees',
+					'status'     => 'publish',
+					'start_date' => '2020-01-01 00:00:00',
+					'duration'   => 2 * HOUR_IN_SECONDS,
+				] )->create()->ID;
+				$ticket_id = $this->create_tc_ticket( $event_id );
+
+				$this->set_fn_return( 'current_time', '2020-02-22 22:22:22' );
+				$order     = $this->create_order( [ $ticket_id => 1 ], [ 'purchaser_email' => 'purchaser@test.com' ] );
+
+				return [ $event_id, [ $event_id, $ticket_id, $order->ID ] ];
+			}
+		];
+	}
+
+	/**
+	 * @dataProvider tc_order_report_data_provider
+	 */
+	public function test_tc_order_report_display( Closure $fixture ) {
+		// The global hook suffix is used to set the table static cache, randomize it to avoid collisions with other tests.
+		$GLOBALS['hook_suffix'] = uniqid( 'tec-tc-reports-orders', true );
+		// Ensure we're using a user that can manage posts.
+		wp_set_current_user( static::factory()->user->create( [ 'role' => 'administrator' ] ) );
+
+		[ $post_id, $post_ids ] = $fixture();
+		$this->set_fn_return( 'wp_create_nonce', '1234567890' );
+
+		$_GET['event_id'] = $post_id;
+		$_GET['search']   = '';
+
+		$order_report = tribe( Order_Report::class );
+		$order_report->attendees_page_screen_setup();
+
+		ob_start();
+		$order_report->render_page();
+		$html = ob_get_clean();
+
+		$html = $this->placehold_post_ids( $html, $post_ids );
+
+		$this->assertMatchesHtmlSnapshot( $html );
+	}
+}
