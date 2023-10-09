32d57460977235280bbf912a44e7f66baa225dc2
rafsuntaskin <rafsun.aiub@gmail.com>
2023-10-10 05:48:08 +0600

    feat - update order summary template with simplified version

--- src/admin-views/commerce/reports/orders/summary.php
+++ src/admin-views/commerce/reports/orders/summary.php
@@ -12,12 +12,9 @@
  * @var array $event_data A list of PayPal tickets that have at least one sale.
  * @var Order_Summary $order_summary The data object.
  */
-
 use TEC\Tickets\Commerce\Reports\Data\Order_Summary;
-use \TEC\Tickets\Commerce\Status\Completed;
-use \TEC\Tickets\Commerce\Status\Pending;
-use TEC\Tickets\Commerce\Utils\Value;
 
+$sales_totals = $order_summary->get_event_sales_data();
 ?>
 <div id="tribe-order-summary" class="welcome-panel tribe-report-panel">
 	<div class="welcome-panel-content">
@@ -85,7 +82,7 @@ use TEC\Tickets\Commerce\Utils\Value;
 						<div class="tec-tickets__admin-orders-report-overview-ticket-type-label">
 							<?php echo esc_html( $order_summary->get_label_for_type( $type ) ); ?>
 						</div>
-						<div class="tec-tickets__admin-orders-report-overview-ticket-type-border"></div>
+						<div class="tec-tickets__admin-orders-report-overview--border"></div>
 				</div>
 				<ul class="tec-tickets__admin-orders-report-overview-ticket-type-list">
 					<?php foreach ( $items as $item ): ?>
@@ -96,101 +93,34 @@ use TEC\Tickets\Commerce\Utils\Value;
 								</span>
 							</div>
 							<div class="tec-tickets__admin-orders-report-overview-ticket-type-list-item-stat">
-								<?php echo esc_html( $item['qty_string'] ) ?>
+								<?php echo esc_html( $item['qty_by_status'] ) ?>
 							</div>
 						</li>
 					<?php endforeach; ?>
 				</ul>
 					<?php endforeach; ?>
-				<ul>
-					<?php
-					/**
-					 * @todo @juanfra We need to determine what counts as "sale" we have all the statuses here, I am currently only using
-					 *       pending and completed, but we need to make sure user stories here.
-					 * @todo @juanfra Raw HTML here, we need to modify the styling and add some classes.
-					 */
-					foreach ( $tickets as $ticket ) :
-						$data = $tickets_data[ $ticket->ID ];
-						$total = Value::create();
-						$total->total( [
-							$data['total_by_status'][ Completed::SLUG ],
-							$data['total_by_status'][ Pending::SLUG ]
-						] );
-						$ticket_sales = sprintf(
-							'%1$s: %2$s (%3$s)',
-							$ticket->name,
-							$total->get_currency(),
-							$data['qty_by_status'][ Completed::SLUG ] + $data['qty_by_status'][ Pending::SLUG ]
-						);
-						?>
-						<li>
-							<?php echo esc_html( $ticket_sales ); ?>
-						</li>
-					<?php endforeach; ?>
-				</ul>
 			</div>
 			<div class="welcome-panel-column welcome-panel-last alternate">
-				<div class="totals-header">
-					<h3>
-						<?php
-						$text_total_sales = sprintf(
-							esc_html__( 'Total %s Sales', 'event-tickets' ),
-							tribe_get_ticket_label_singular( 'total_sales' )
-						);
-						$total_sales_value = Value::create( $event_data['total_by_status'][ Completed::SLUG ] );
-
-						$totals_header = sprintf(
-							'%1$s: %2$s (%3$s)',
-							$text_total_sales,
-							$total_sales_value->get_currency(),
-							$event_data['qty_by_status'][ Completed::SLUG ]
-						);
-						echo esc_html( $totals_header );
-						?>
-					</h3>
-
-					<div class="order-total">
-						<?php
-						$text_total_ordered = sprintf(
-							esc_html__( 'Total %s Ordered', 'event-tickets' ),
-							tribe_get_ticket_label_plural( 'total_ordered' )
-						);
-
-						$total = Value::create();
-						$total->total( [
-								$data['total_by_status'][ Completed::SLUG ],
-								$data['total_by_status'][ Pending::SLUG ]
-						] );
-
-						$totals_header = sprintf(
-							'%1$s: %2$s (%3$s)',
-							$text_total_ordered,
-							$total->get_currency(),
-							$event_data['qty_by_status'][ Completed::SLUG ] + $event_data['qty_by_status'][ Pending::SLUG ]
-						);
-						echo esc_html( $totals_header );
-						?>
+				<div class="tec-tickets__admin-orders-report__sales-overview__title">
+					<h3><?php echo esc_html__( 'Sales Totals', 'event-tickets' ); ?></h3>
+				</div>
+				<div class="tec-tickets__admin-orders-report__sales-overview__by-status">
+				<?php foreach ( $sales_totals['by_status'] as $status ): ?>
+					<div class="tec-tickets__admin-orders-report__sales-overview__list__item">
+						<div class="tec-tickets__admin-orders-report__sales-overview__list__item-label"><?php echo esc_html( $status['label'] ); ?></div>
+						<div class="tec-tickets__admin-orders-report__sales-overview__list__item-amount"><?php echo esc_html( sprintf( '%1$s (%2$s)', $status['total_sales_price'], $status['qty_sold'] ) )?></div>
+					</div>
+				<?php endforeach; ?>
+				</div>
+				<div class="tec-tickets__admin-orders-report-overview--border"></div>
+				<div class="tec-tickets__admin-orders-report__sales-overview__total">
+					<div class="tec-tickets__admin-orders-report__sales-overview__list__item">
+						<div class="tec-tickets__admin-orders-report__sales-overview__list__item-label">
+							<div class="tec-tickets__admin-orders-report__sales-overview__total-label"><?php echo esc_html__( 'Total Tickets ordered', 'event-tickets' ); ?></div>
+						</div>
+						<div class="tec-tickets__admin-orders-report__sales-overview__list__item-amount"><?php echo esc_html( sprintf( '%1$s (%2$s)', $sales_totals['total_sales']['price'], $sales_totals['total_sales']['qty'] ) ); ?></div>
 					</div>
 				</div>
-
-				<ul id="sales_breakdown_wrapper" class="tribe-event-meta-note">
-					<?php
-					// Loop on all status to get items
-					foreach ( $event_data['qty_by_status'] as $status_slug => $quantity ) :
-						$status = tribe( \TEC\Tickets\Commerce\Status\Status_Handler::class )->get_by_slug( $status_slug );
-						$total = Value::create( $event_data['total_by_status'][ $status_slug ] );
-						// do not show status if no tickets
-						if ( 0 >= (int) $quantity ) {
-							continue;
-						}
-						?>
-						<li>
-							<strong><?php echo esc_html( $status->get_name() ) ?>:</strong>
-							<?php echo esc_html( $total->get_currency() ); ?>
-							<span id="total_issued">(<?php echo esc_html( $quantity ); ?>)</span>
-						</li>
-					<?php endforeach; ?>
-				</ul>
 			</div>
 		</div>
 	</div>
