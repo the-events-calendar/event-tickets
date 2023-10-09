fb25ebf3f555949c0644bfe0b061751cba12b26a
rafsuntaskin <rafsun.aiub@gmail.com>
2023-10-10 05:47:17 +0600

    feat updated data class to handle event sales data

--- src/Tickets/Commerce/Reports/Data/Order_Summary.php
+++ src/Tickets/Commerce/Reports/Data/Order_Summary.php
@@ -1,8 +1,10 @@
 <?php
 namespace TEC\Tickets\Commerce\Reports\Data;
 
+use TEC\Tickets\Commerce\Status\Status_Handler;
 use TEC\Tickets\Commerce\Ticket;
 use TEC\Tickets\Commerce\Utils\Value;
+use Tribe__Tickets__Ticket_Object as Ticket_Object;
 use Tribe__Tickets__Tickets;
 
 class Order_Summary {
@@ -19,6 +21,12 @@ class Order_Summary {
 	private array $tickets_by_type;
 
 	/**
+	 * @var array|array[]
+	 */
+	private array $event_sales_by_status = [];
+	private array $total_sales = [];
+
+	/**
 	 * Order_Summary constructor.
 	 *
 	 * @param int $post_id
@@ -33,14 +41,19 @@ class Order_Summary {
 		$this->tickets  = $provider->get_tickets( $this->post_id );
 		foreach ( $this->tickets as $ticket ) {
 			$quantities = array_filter( tribe( Ticket::class )->get_status_quantity( $ticket->ID ) );
+
+			// Process the event sales data.
+			$this->process_event_sales_data( $quantities, $ticket );
+
+			// We need to show the total available for each ticket type.
 			$quantities[ $ticket->availability_slug() ] = $ticket->available();
 
 			$ticket_data = [
-				'ticket'     => $ticket,
-				'label'      => sprintf( '%1$s %2$s', $ticket->name, Value::create( $ticket->price )->get_currency() ),
-				'type'       => $ticket->type(),
-				'quantities' => $quantities,
-				'qty_string' => implode( ' | ', array_map( fn( $k, $v ) => "$v $k", array_keys( $quantities ), $quantities ) ),
+				'ticket'        => $ticket,
+				'label'         => sprintf( '%1$s %2$s', $ticket->name, Value::create( $ticket->price )->get_currency() ),
+				'type'          => $ticket->type(),
+				'qty_data'      => $quantities,
+				'qty_by_status' => implode( ' | ', array_map( fn( $k, $v ) => "$v $k", array_keys( $quantities ), $quantities ) ),
 			];
 
 			$this->tickets_by_type[ $ticket->type ][] = $ticket_data;
@@ -89,5 +102,47 @@ class Order_Summary {
 		 * @param int    $post_id The post ID.
 		 */
 		return apply_filters( 'tec_tickets_commerce_order_report_summary_label_for_type', $type, $this->post_id );
+	}
+
+	protected function process_event_sales_data( array $quantity_by_status, Ticket_Object $ticket ): void {
+		foreach ( $quantity_by_status as $status_slug => $quantity ) {
+			if ( ! isset( $this->event_sales_by_status[ $status_slug ] ) ) {
+				$status = tribe( Status_Handler::class )->get_by_slug( $status_slug );
+
+				// This is the first time we've seen this status, so initialize it.
+				$this->event_sales_by_status[ $status_slug ] = [
+					'label'              => $status->get_name(),
+					'qty_sold'           => 0,
+					'total_sales_amount' => 0,
+					'total_sales_price'  => Value::create()->get_currency(),
+				];
+			}
+			$this->event_sales_by_status[ $status_slug ]['qty_sold']           += $quantity;
+			$this->event_sales_by_status[ $status_slug ]['total_sales_amount'] += $quantity * $ticket->price;
+			$this->event_sales_by_status[ $status_slug ]['total_sales_price']  = Value::create( $this->event_sales_by_status[ $status_slug ]['total_sales_amount'] )->get_currency();
+
+			// process the total sales data.
+			$this->total_sales['qty']    += $this->event_sales_by_status[ $status_slug ]['qty_sold'];
+			$this->total_sales['amount'] += $this->event_sales_by_status[ $status_slug ]['total_sales_amount'];
+			$this->total_sales['price']  = Value::create( $this->total_sales['amount'] )->get_currency();
+		}
+	}
+
+	public function get_event_sales_data(): array {
+
+		$data = [
+			'by_status'   => $this->event_sales_by_status,
+			'total_sales' => $this->total_sales,
+		];
+
+		/**
+		 * Filters the event sales data in the order summary report.
+		 *
+		 * @since TBD
+		 *
+		 * @param array $event_sales_data The event sales data.
+		 * @param int   $post_id The post ID.
+		 */
+		return apply_filters( 'tec_tickets_commerce_order_report_summary_event_sales_data', $data, $this->post_id );
 	}
 }
