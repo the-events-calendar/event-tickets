2462cd7d4faa6969cab602ee20aecb1c4969013b
rafsuntaskin <rafsun.aiub@gmail.com>
2023-10-11 15:03:04 +0600

    feat Order summary docs updated

--- src/Tickets/Commerce/Reports/Data/Order_Summary.php
+++ src/Tickets/Commerce/Reports/Data/Order_Summary.php
@@ -8,34 +8,52 @@ use TEC\Tickets\Commerce\Utils\Value;
 use Tribe__Tickets__Ticket_Object as Ticket_Object;
 use Tribe__Tickets__Tickets;
 
+/**
+ * Class Order_Summary.
+ *
+ * @since TBD
+ *
+ * @package TEC\Tickets\Commerce\Reports\Data
+ */
 class Order_Summary {
+	/**
+	 * @var int The post ID.
+	 */
 	private int $post_id;
 
 	/**
-	 * @var Ticket_Object[]
+	 * @var Ticket_Object[] The tickets.
 	 */
 	private array $tickets;
 
 	/**
-	 * @var array|array[]
+	 * @var array|array[] The tickets by type.
 	 */
 	private array $tickets_by_type;
 
 	/**
-	 * @var array|array[]
+	 * @var array|array[] The event sales by status.
 	 */
 	private array $event_sales_by_status = [];
+
+	/**
+	 * @var array|array[] The total sales.
+	 */
 	private array $total_sales = [];
+
+	/**
+	 * @var array|array[] The total ordered.
+	 */
 	private array $total_ordered = [];
 
 	/**
 	 * Order_Summary constructor.
 	 *
-	 * @param int $post_id
+	 * @param int $post_id The post ID.
 	 */
 	public function __construct( int $post_id ) {
-		$this->post_id = $post_id;
-		$this->total_sales = [
+		$this->post_id       = $post_id;
+		$this->total_sales   = [
 			'qty'    => 0,
 			'amount' => 0,
 			'price'  => Value::create()->get_currency(),
@@ -45,13 +63,18 @@ class Order_Summary {
 			'amount' => 0,
 			'price'  => Value::create()->get_currency(),
 		];
+
 		$this->build_data();
 	}
 
-	public function build_data(): void {
-		$provider = Tribe__Tickets__Tickets::get_event_ticket_provider_object( $this->post_id );
-		$this->tickets  = $provider->get_tickets( $this->post_id );
-		foreach ( $this->tickets as $ticket ) {
+	/**
+	 * Build the data.
+	 *
+	 * @since TBD
+	 *
+	 */
+	protected function build_data(): void {
+		foreach ( $this->get_tickets() as $ticket ) {
 			$quantities = array_filter( tribe( Ticket::class )->get_status_quantity( $ticket->ID ) );
 
 			// Process the event sales data.
@@ -73,11 +96,33 @@ class Order_Summary {
 	}
 
 	/**
-	 * Get the tickets by type.
+	 * Get the tickets.
 	 *
-	 * @return Ticket_Object[]
 	 * @since TBD
 	 *
+	 * @return array
+	 */
+	public function get_tickets(): array {
+		$provider      = Tribe__Tickets__Tickets::get_event_ticket_provider_object( $this->post_id );
+		$this->tickets = $provider->get_tickets( $this->post_id );
+
+		/**
+		 * Filters the tickets in the order summary report.
+		 *
+		 * @since TBD
+		 *
+		 * @param array $tickets The tickets.
+		 * @param Order_Summary $this The order summary object.
+		 */
+		return apply_filters( 'tec_tickets_commerce_order_report_summary_tickets', $this->tickets, $this );
+	}
+
+	/**
+	 * Get the tickets by type.
+	 *
+	 * @since TBD
+	 *
+	 * @return array
 	 */
 	public function get_tickets_by_type(): array {
 		/**
@@ -86,9 +131,9 @@ class Order_Summary {
 		 * @since TBD
 		 *
 		 * @param array $tickets_by_type The tickets by type.
-		 * @param int   $post_id The post ID.
+		 * @param Order_Summary $this The order summary object.
 		 */
-		return apply_filters( 'tec_tickets_commerce_order_report_summary_tickets_by_type', $this->tickets_by_type, $this->post_id );
+		return apply_filters( 'tec_tickets_commerce_order_report_summary_tickets_by_type', $this->tickets_by_type, $this );
 	}
 
 	/**
@@ -96,9 +141,9 @@ class Order_Summary {
 	 *
 	 * @since TBD
 	 *
-	 * @param string $type
+	 * @param string $type The ticket type.
 	 *
-	 * @return string
+	 * @return string The ticket type label.
 	 */
 	public function get_label_for_type( string $type ): string {
 		if ( 'default' === $type ) {
@@ -111,11 +156,19 @@ class Order_Summary {
 		 * @since TBD
 		 *
 		 * @param string $type The ticket type.
-		 * @param int    $post_id The post ID.
+		 * @param Order_Summary $this The post ID.
 		 */
-		return apply_filters( 'tec_tickets_commerce_order_report_summary_label_for_type', $type, $this->post_id );
+		return apply_filters( 'tec_tickets_commerce_order_report_summary_label_for_type', $type, $this );
 	}
 
+	/**
+	 * Process the event sales data.
+	 *
+	 * @since TBD
+	 *
+	 * @param array $quantity_by_status The quantity by status.
+	 * @param Ticket_Object $ticket The ticket object.
+	 */
 	protected function process_event_sales_data( array $quantity_by_status, Ticket_Object $ticket ): void {
 		foreach ( $quantity_by_status as $status_slug => $quantity ) {
 			if ( ! isset( $this->event_sales_by_status[ $status_slug ] ) ) {
