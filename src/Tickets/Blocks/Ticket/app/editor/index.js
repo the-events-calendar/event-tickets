851068945c0fbf3b8b76ecb253d779fa7a04ecc7
Luca Tumedei <luca@theaveragedev.com>
2023-11-01 12:12:45 +0100

    refactor(Blocks/Ticket) remove duplicate content from registerBlockType call

--- src/Tickets/Blocks/Ticket/app/editor/index.js
+++ src/Tickets/Blocks/Ticket/app/editor/index.js
@@ -18,18 +18,7 @@ import { Tickets as TicketsIcon } from '@moderntribe/tickets/icons';
 import Ticket from './container';
 
 const block = {
-	title: __ ( 'Event Ticket', 'event-tickets' ),
-	description: __ ( 'A single configured ticket type.', 'event-tickets' ),
 	icon: <TicketsIcon/>,
-	category: 'tribe-tickets',
-	keywords: [ 'event', 'event-tickets', 'tribe' ],
-
-	parent: [ 'tribe/tickets' ],
-
-	supports: {
-		html: false,
-		customClassName: false,
-	},
 
 	attributes: {
 		hasBeenCreated: {
