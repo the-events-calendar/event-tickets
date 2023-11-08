e79416cc63a7ae889735bff33504b252bebf7e38
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-11-08 00:48:52 -0500

    Attendees Tab docblocks.

--- src/Tickets/Commerce/Reports/Attendees_Tab.php
+++ src/Tickets/Commerce/Reports/Attendees_Tab.php
@@ -6,20 +6,32 @@ namespace TEC\Tickets\Commerce\Reports;
  * Class Attendees_Tab
  *
  * Models the tab for the Attendees report on a post with tickets.
+ *
+ * @since 5.6.8
  */
 class Attendees_Tab extends \Tribe__Tabbed_View__Tab {
 
 	/**
-	 * @var bool
+	 * @inerhitDoc
+	 * @since 5.6.8
 	 */
 	protected $visible = true;
 
 	/**
-	 * @var string
+	 * @inheritDoc
+	 *
+	 * @since 5.6.8
 	 */
 	protected $slug = 'tickets-attendees';
 
+	/**
+	 * @inheritDoc
+	 *
+	 * @since 5.6.8
+	 *
+	 * @return string|null
+	 */
 	public function get_label() {
-		return __( 'Attendees', 'event-tickets' );
+		return esc_html__( 'Attendees', 'event-tickets' );
 	}
 }
