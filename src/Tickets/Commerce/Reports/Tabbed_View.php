fdcaa00daddf7a3c94f9303467d4c7d98a723ba9
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-11-08 02:03:49 -0500

    Avoid Query Args and Clean up Tabbed View with proper docblocks and new filters

--- src/Tickets/Commerce/Reports/Tabbed_View.php
+++ src/Tickets/Commerce/Reports/Tabbed_View.php
@@ -2,38 +2,60 @@
 
 namespace TEC\Tickets\Commerce\Reports;
 
-use TEC\Tickets\Commerce\Reports\Orders;
 use TEC\Tickets\Commerce\Module;
+
+use Tribe__Tickets__Attendees;
+use Tribe__Tabbed_View;
+use WP_Post;
 
 /**
  * Class Tabbed_View
  *
- * @since TBD
+ * @since 5.6.8
  */
 class Tabbed_View {
 
 	/**
+	 * @since 5.6.8
+	 *
 	 * @var string
 	 */
-	public $active_tab_slug;
+	public string $active_tab_slug;
 
 	/**
+	 * @since 5.6.8
+	 *
 	 * @var array A map that binds requested pages to tabs.
 	 */
-	protected $tab_map = [
+	protected array $tab_map = [
 		'tickets-attendees' => 'tribe-tickets-attendance-report',
 	];
 
 	/**
+	 * Renders the tabbed view for the current post.
+	 *
+	 * @since 5.6.8
+	 *
+	 * @return void
+	 */
+	public function register(): void {
+		add_filter( 'tec_tickets_commerce_reports_tabbed_view_tab_map', [ $this, 'include_order_tab' ] );
+		add_action( 'tec_tickets_commerce_reports_tabbed_view_before_register_tab', [ $this, 'register_tabs' ], 10, 2 );
+
+		// Legacy compatibility with Attendees page which is not part of Tickets Commerce.
+		add_action( 'tribe_tickets_orders_tabbed_view_register_tab_right', [ $this, 'register_tabs' ], 10, 2 );
+	}
+
+	/**
 	 * Adds the Tickets Commerce orders tab slug to the tab slug map.
 	 *
-	 * @since TBD
+	 * @since 5.6.8
 	 *
 	 * @param array $tab_map
 	 *
-	 * @return array
+	 * @return array<string,string>
 	 */
-	public function filter_tickets_orders_tabbed_view_tab_map( array $tab_map = [] ) {
+	public function include_order_tab( array $tab_map = [] ): array {
 		$tab_map[ Orders::$page_slug ] = Orders::$tab_slug;
 
 		return $tab_map;
@@ -42,13 +64,14 @@ class Tabbed_View {
 	/**
 	 * Registers the Ticket Commerce orders tab among those the tabbed view should render.
 	 *
-	 * @since TBD
+	 * @since 5.6.8
 	 *
 	 * @param Tribe__Tabbed_View $tabbed_view
 	 * @param WP_Post            $post
+	 *
+	 * @return void
 	 */
-	public function register_orders_tab( \Tribe__Tabbed_View $tabbed_view, \WP_Post $post ) {
-		/** @var \Module $commerce */
+	public function register_tabs( Tribe__Tabbed_View $tabbed_view, WP_Post $post ): void {
 		$commerce = tribe( Module::class );
 
 		if (
@@ -58,7 +81,7 @@ class Tabbed_View {
 			return;
 		}
 
-		$post_id = \Tribe__Utils__Array::get( $_GET, 'event_id', \Tribe__Utils__Array::get( $_GET, 'post_id', false ), false );
+		$post_id = tribe_get_request_var( 'event_id', tribe_get_request_var( 'post_id' ) );
 
 		if ( empty( $post_id ) || ! $post = get_post( $post_id ) ) {
 			return;
@@ -72,27 +95,38 @@ class Tabbed_View {
 		$tabbed_view->register( $orders_report );
 	}
 
-	public function render() {
-		$post_id = \Tribe__Utils__Array::get( $_GET, 'event_id', \Tribe__Utils__Array::get( $_GET, 'post_id', false ), false );
+	/**
+	 * Renders the tabbed view for the current report.
+	 *
+	 * @since 5.6.8
+	 *
+	 * @return void
+	 */
+	public function render(): void {
+		$post_id = tribe_get_request_var( 'event_id', tribe_get_request_var( 'post_id' ) );
 
 		if ( empty( $post_id ) || ! $post = get_post( $post_id ) ) {
 			return;
 		}
 
+		$current_page = tribe_get_request_var( 'page' );
+
 		$tabbed_view = new \Tribe__Tabbed_View();
 		$tabbed_view->set_label( $this->get_title( $post_id ) );
-		$query_string = empty( $_SERVER['QUERY_STRING'] ) ? '' : '?' . $_SERVER['QUERY_STRING'];
-		$request_uri  = 'edit.php' . $query_string;
+
+		$request_uri = add_query_arg( [
+			'post_id' => $post_id,
+			'page' => $current_page,
+			'post_type' => tribe_get_request_var( 'post_type' ),
+		], admin_url( 'edit.php' ) );
+
 		$tabbed_view->set_url( remove_query_arg( 'tab', $request_uri ) );
 
 		$tab_map = $this->get_tab_map();
 
 		// try to set the active tab from the requested page
-		$active_string = empty( $_SERVER['QUERY_STRING'] ) ? '' : $_SERVER['QUERY_STRING'];
-		wp_parse_str( $active_string, $query_args );
-		if ( ! empty( $query_args['page'] ) && isset( $tab_map[ $query_args['page'] ] ) ) {
-			$active = $tab_map[ $query_args['page'] ];
-			$tabbed_view->set_active( $active );
+		if ( ! empty( $current_page ) && isset( $tab_map[ $current_page ] ) ) {
+			$tabbed_view->set_active( $tab_map[ $current_page ] );
 		}
 
 		/**
@@ -101,21 +135,20 @@ class Tabbed_View {
 		 * Note that the tabbed view will not render if only a tab is registered; tabs registered during this action will
 		 * appear right (after) the default ones.
 		 *
-		 * @since TBD
+		 * @since 5.6.8
 		 *
 		 * @param Tribe__Tabbed_View $tabbed_view The tabbed view that is rendering.
 		 * @param WP_Post            $post        The post orders should be shown for.
-		 * @param string|null        $active      The currently active tab, use the `tribe_tickets_orders_tabbed_view_tab_map` filter
+		 * @param string|null        $active      The currently active tab, use the `tec_tickets_commerce_reports_tabbed_view_tab_map` filter
 		 *                                        to add tabs registered here to the map that will allow them to be activated.
 		 */
-		do_action( 'tribe_tickets_orders_tabbed_view_register_tab_right', $tabbed_view, $post );
-
-		// Register the Attendees tab.
-		$attendees_report = new Attendees_Tab( $tabbed_view );
+		do_action( 'tec_tickets_commerce_reports_tabbed_view_before_register_tab', $tabbed_view, $post );
 
 		/** @var Tribe__Tickets__Attendees $attendees */
 		$attendees = tribe( 'tickets.attendees' );
 
+		// Register the Attendees tab.
+		$attendees_report = new Attendees_Tab( $tabbed_view );
 		$attendees_report->set_url( $attendees->get_report_link( $post ) );
 		$tabbed_view->register( $attendees_report );
 
@@ -125,14 +158,14 @@ class Tabbed_View {
 		 * Note that the tabbed view will not render if only a tab is registered; tabs registered during this action will
 		 * appear left (before) the default ones.
 		 *
-		 * @since TBD
+		 * @since 5.6.8
 		 *
 		 * @param Tribe__Tabbed_View $tabbed_view The tabbed view that is rendering.
 		 * @param WP_Post            $post        The post orders should be shown for.
-		 * @param string|null        $active      The currently active tab, use the `tribe_tickets_orders_tabbed_view_tab_map` filter
+		 * @param string|null        $active      The currently active tab, use the `tec_tickets_commerce_reports_tabbed_view_tab_map` filter
 		 *                                        to add tabs registered here to the map that will allow them to be activated.
 		 */
-		do_action( 'tribe_tickets_orders_tabbed_view_register_tab_left', $tabbed_view, $post );
+		do_action( 'tec_tickets_commerce_reports_tabbed_view_after_register_tab', $tabbed_view, $post );
 
 		// if there is only one tab registered then do not show the tabbed view
 		if ( count( $tabbed_view->get() ) <= 1 ) {
@@ -147,41 +180,15 @@ class Tabbed_View {
 	}
 
 	/**
-	 * Renders the tabbed view for the current post.
-	 *
-	 * @since TBD
-	 */
-	public function register() {
-		add_filter( 'tribe_tickets_orders_tabbed_view_tab_map', [ $this, 'filter_tickets_orders_tabbed_view_tab_map' ] );
-		add_action( 'tribe_tickets_orders_tabbed_view_register_tab_right', [ $this, 'register_orders_tab' ], 10, 2 );
-	}
-
-	/**
 	 * Generates the title based on the page type and post ID.
 	 *
-	 * @since 5.6.2
+	 * @since 5.6.8
 	 *
 	 * @param int $post_id The post ID.
 	 *
 	 * @return string The generated title.
 	 */
 	public function get_title( int $post_id ): string {
-		/**
-		 * Filters whether to show the view title.
-		 *
-		 * @since 5.0.1
-		 *
-		 * @deprecated 5.7.2
-		 *
-		 * @param bool 	$show_title Whether to show the view title.
-		 * @param int 	$post_id The post ID.
-		 */
-		$show_title = apply_filters_deprecated( 'tribe_tickets_attendees_show_view_title', [ true, $post_id ], '5.6.2' );
-
-		if ( ! $show_title ) {
-			return '';
-		}
-
 		$page_type = tribe_get_request_var( 'page' );
 
 		// Check $page_type to confirm if we are on Order or Attendees page.
@@ -198,26 +205,24 @@ class Tabbed_View {
 		/**
 		 * Filters the title on the Attendees, and Order list page.
 		 *
-		 * @since 5.6.2
+		 * @since 5.6.8
 		 *
 		 * @param string 	$view_title The view title.
 		 * @param int 		$post_id The post ID.
 		 * @param string 	$page_type Possible values `tickets-attendees` or `tickets-orders`.
 		 */
-		return apply_filters( 'tec_tickets_attendees_order_view_title', $view_title, $post_id, $page_type );
-
+		return apply_filters( 'tec_tickets_commerce_reports_tabbed_page_title', $view_title, $post_id, $page_type );
 	}
 
 	/**
 	 * Returns the attendee and orders tabbed view tabs to map the tab request slug to
 	 * the registered tabs.
 	 *
-	 * @since TBD
+	 * @since 5.6.8
 	 *
 	 * @return array $tab_map An associative array in the [ <query_var> => <tab_slug> ] format.
-	 *
 	 */
-	protected function get_tab_map() {
+	protected function get_tab_map(): array {
 		/**
 		 * Filters the attendee and orders tabbed view tabs to map the tab request slug to
 		 * the registered tabs.
@@ -229,7 +234,7 @@ class Tabbed_View {
 		 * @param array $tab_map An associative array in the [ <query_var> => <tab_slug> ] format.
 		 *
 		 */
-		$tab_map = apply_filters( 'tribe_tickets_orders_tabbed_view_tab_map', $this->tab_map );
+		$tab_map = (array) apply_filters( 'tec_tickets_commerce_reports_tabbed_view_tab_map', $this->tab_map );
 
 		return $tab_map;
 	}
@@ -237,11 +242,13 @@ class Tabbed_View {
 	/**
 	 * Sets the currently active tab slug.
 	 *
-	 * @since TBD
+	 * @since 5.6.8
 	 *
 	 * @param string $tab_slug
+	 *
+	 * @return void
 	 */
-	public function set_active( $tab_slug ) {
+	public function set_active( string $tab_slug ): void {
 		$this->active_tab_slug = $tab_slug;
 	}
 }
