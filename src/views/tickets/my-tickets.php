dbcc13a4c4715d76fdd909d5f215d3af9922f94b
Gustavo Bordoni <bordoni.dev@gmail.com>
2023-11-14 10:19:15 -0500

    Update TBDs:  `src/views/tickets/my-tickets.php`

--- src/views/tickets/my-tickets.php
+++ src/views/tickets/my-tickets.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * My Tickets Page
- * 
+ *
  * Override this template in your own theme by creating a file at [your-theme]/tribe-events/tickets/tickets/my-tickets.php
- * 
- * @since TBD
- * 
- * @version TBD
+ *
+ * @since 5.6.7
+ *
+ * @version 5.6.7
  *
  * @var  array  $orders              The orders for the current user.
  * @var  int    $post_id             The ID of the post the tickets are for.
@@ -16,7 +16,7 @@
 
 ?>
 <div class="tribe-tickets">
-	<?php 
-		$this->template( 'tickets/my-tickets/orders-list' ); 
+	<?php
+		$this->template( 'tickets/my-tickets/orders-list' );
 	?>
 </div>
