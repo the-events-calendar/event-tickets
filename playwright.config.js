68850ffc2b200bfe87cadf49ab140d7475694948
Luca Tumedei <luca@theaveragedev.com>
2024-01-08 14:13:38 +0100

    test(playwright.config.js) set outputDir to tests/_output

--- playwright.config.js
+++ playwright.config.js
@@ -12,6 +12,7 @@ const { defineConfig, devices } = require('@playwright/test');
  */
 module.exports = defineConfig({
 	testDir: './tests/end-to-end',
+	outputDir: './tests/_output',
 	/* Run tests in files in parallel */
 	fullyParallel: true,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
