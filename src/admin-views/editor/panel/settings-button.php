15327ebccb444756d0c39c47f55857074d55a279
rafsuntaskin <rafsun.aiub@gmail.com>
2024-01-08 19:58:45 +0600

    feat - add settings button template

---
+++ src/admin-views/editor/panel/settings-button.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Render the settings button for the ticket editor.
+ *
+ * @since TBD
+ *
+ * @version TBD
+ */
+?>
+<button id="settings_form_toggle" class="button-secondary tribe-button-icon tribe-button-icon-settings">
+    <?php esc_html_e( 'Settings', 'event-tickets' ); ?>
+</button>
