84d1e2568a52fbc5db187dd70104540570013c4f
Gustavo Bordoni <bordoni.dev@gmail.com>
2024-01-16 01:58:08 -0500

    TBD to 5.8.0 `src/Tickets/Blocks/Ticket/app/editor/container-content/type/container.js`

--- src/Tickets/Blocks/Ticket/app/editor/container-content/type/container.js
+++ src/Tickets/Blocks/Ticket/app/editor/container-content/type/container.js
@@ -64,7 +64,7 @@ const mapStateToProps = (state, ownProps) => {
 	/**
 	 * Filters the properties mapped from the state for the Ticket Type component.
 	 *
-	 * @since TBD
+	 * @since 5.8.0
 	 *
 	 * @type {Object} mappedProps The properties mapped from the state for the Ticket Type component.
 	 * @type {Object} context.state The current state.
