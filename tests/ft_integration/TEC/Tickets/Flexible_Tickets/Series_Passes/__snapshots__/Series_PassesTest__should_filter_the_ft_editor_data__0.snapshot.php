69c8ee3f1a0ddc578f6ba2cdb4d3083da174caca
rafsuntaskin <rafsun.aiub@gmail.com>
2024-01-19 04:07:19 +0600

    tests - snapshots updated

--- tests/ft_integration/TEC/Tickets/Flexible_Tickets/Series_Passes/__snapshots__/Series_PassesTest__should_filter_the_ft_editor_data__0.snapshot.php
+++ tests/ft_integration/TEC/Tickets/Flexible_Tickets/Series_Passes/__snapshots__/Series_PassesTest__should_filter_the_ft_editor_data__0.snapshot.php
@@ -1,3 +1,3 @@
 array (
-  'defaultTicketTypeEventInSeriesDescriptionTemplate' => 'A single ticket is specific to this event. You can add a Series Pass from the %s Series page.',
+  'defaultTicketTypeEventInSeriesDescriptionTemplate' => 'A standard ticket is specific to this event. You can add a Series Pass from the %s Series page.',
 )
