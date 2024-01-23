7e088e83c57712edc6312655bbbce481629de033
rafsuntaskin <rafsun.aiub@gmail.com>
2024-01-23 21:05:41 +0600

    tests - updated warning notice related tests

--- tests/wpunit/Tribe/Editor/__snapshots__/WarningsTest__render_notice_display_correctly with data set Test with all values filled__1.php
+++ tests/wpunit/Tribe/Editor/__snapshots__/WarningsTest__render_notice_display_correctly with data set Test with all values filled__1.php
@@ -1,5 +1,5 @@
-<?php return '		<div  class="ticket-editor-notice warning tribe-dependent class1 class2" 							data-depends="#element-id"
+<?php return '		<div  class="ticket-editor-notice warning tribe-dependent" 							data-depends="#element-id"
 						data-condition-is-checked		>
-			<span class="message">Another message</span>
+						<span class="message">Another message</span>
 		</div>
 		';
