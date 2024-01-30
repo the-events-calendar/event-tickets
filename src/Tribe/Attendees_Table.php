2a7c9896edcc62a13a7552c4905bf5e182b1825e
Brian Krane <redscar@gmail.com>
2024-01-30 15:20:42 -0500

    Added filter to extend WooCommerce URL.

--- src/Tribe/Attendees_Table.php
+++ src/Tribe/Attendees_Table.php
@@ -307,10 +307,23 @@ class Tribe__Tickets__Attendees_Table extends WP_List_Table {
 	public function get_order_id_url( array $item ) {
 		// Backwards compatibility.
 		if ( empty( $item['order_id_url'] ) ) {
+			// @todo fix this piece
 			$item['order_id_url'] = get_edit_post_link( $item['order_id'], true );
 		}
 
-		return $item['order_id_url'];
+		/**
+		 * Filter the order ID URL.
+		 *
+		 * @since TBD
+		 *
+		 * @param string $order_id_url The URL of the order ID.
+		 * @param array  $item The item array.
+		 */
+		return apply_filters(
+			'tec_attendees_table_order_id_url',
+			$item['order_id_url'],
+			$item
+		);
 	}
 
 	/**
