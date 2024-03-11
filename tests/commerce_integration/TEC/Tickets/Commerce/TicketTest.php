7ef54fb5114bbb1a7b06a8912b552066bd9c4a6e
rafsuntaskin <rafsun.aiub@gmail.com>
2024-03-11 14:03:40 +0600

    tests - cover sale price saving

--- tests/commerce_integration/TEC/Tickets/Commerce/TicketTest.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/TicketTest.php
@@ -3,6 +3,7 @@ namespace TEC\Tickets\Commerce;
 
 use Codeception\TestCase\WPTestCase;
 use TEC\Tickets\Commerce\Ticket;
+use TEC\Tickets\Commerce\Utils\Value;
 use Tribe\Tickets\Test\Commerce\TicketsCommerce\Ticket_Maker;
 class TicketTest extends WPTestCase {
 	use Ticket_Maker;
@@ -179,6 +180,8 @@ class TicketTest extends WPTestCase {
 	/**
 	 * @test
 	 * @dataProvider is_on_sale_provider
+	 *
+	 * @covers    Ticket::is_on_sale
 	 */
 	public function test_is_on_sale( \Closure $ticket_data_provider ): void {
 		[ $post_id, $ticket_id, $expected ] = $ticket_data_provider();
@@ -188,5 +191,159 @@ class TicketTest extends WPTestCase {
 		$ticket_class = tribe( Ticket::class );
 		
 		$this->assertEquals( $expected, $ticket_class->is_on_sale( $ticket ) );
+	}
+	
+	/**
+	 * Provides data for the process_sale_price_data test.
+	 *
+	 * @return \Generator
+	 */
+	public function process_sale_price_data_provider(): \Generator {
+		yield 'sale price data not added' => [
+			function (): array {
+				$post_id   = static::factory()->post->create();
+				$ticket_id = $this->create_tc_ticket(
+					$post_id,
+					20,
+				);
+				
+				return [
+					$post_id,
+					$ticket_id,
+					false,
+					false,
+					false,
+					false,
+				];
+			},
+		];
+		
+		yield 'sale price data added' => [
+			function (): array {
+				$post_id   = static::factory()->post->create();
+				$ticket_id = $this->create_tc_ticket(
+					$post_id,
+					20,
+					[
+						'ticket_add_sale_price'  => 'on',
+						'ticket_sale_price'      => 10,
+						'ticket_sale_start_date' => '2010-03-01',
+						'ticket_sale_end_date'   => '2040-03-01',
+					]
+				);
+				
+				return [
+					$post_id,
+					$ticket_id,
+					true,
+					Value::create( 10 ),
+					'2010-03-01',
+					'2040-03-01',
+				];
+			},
+		];
+		
+		yield 'sale price data added but sale price option unchecked' => [
+			function (): array {
+				$post_id   = static::factory()->post->create();
+				$ticket_id = $this->create_tc_ticket(
+					$post_id,
+					20,
+					[
+						'ticket_sale_price'      => 10,
+						'ticket_sale_start_date' => '2010-03-01',
+						'ticket_sale_end_date'   => '2040-03-01',
+					]
+				);
+				
+				return [
+					$post_id,
+					$ticket_id,
+					false,
+					false,
+					false,
+					false,
+				];
+			},
+		];
+		
+		yield 'sale price data is greater than regular price' => [
+			function (): array {
+				$post_id   = static::factory()->post->create();
+				$ticket_id = $this->create_tc_ticket(
+					$post_id,
+					20,
+					[
+						'ticket_add_sale_price'  => 'on',
+						'ticket_sale_price'      => 30,
+						'ticket_sale_start_date' => '2010-03-01',
+						'ticket_sale_end_date'   => '2040-03-01',
+					]
+				);
+				
+				// The sale price is greater than the regular price, so it should be ignored and not saved.
+				return [
+					$post_id,
+					$ticket_id,
+					true,
+					false,
+					'2010-03-01',
+					'2040-03-01',
+				];
+			},
+		];
+		
+		yield 'added a ticket with sale price then saved again with unchecked sale price' => [
+			function (): array {
+				$post_id   = static::factory()->post->create();
+				$ticket_id = $this->create_tc_ticket(
+					$post_id,
+					20,
+					[
+						'ticket_add_sale_price'  => 'on',
+						'ticket_sale_price'      => 10,
+						'ticket_sale_start_date' => '2010-03-01',
+						'ticket_sale_end_date'   => '2040-03-01',
+					]
+				);
+				
+				// Save the ticket again with the sale price option unchecked, this should remove all previously saved sale price data.
+				$this->create_tc_ticket(
+					$post_id,
+					20,
+					[
+						'ticket_id'              => $ticket_id,
+						'ticket_sale_price'      => 10,
+						'ticket_sale_start_date' => '2010-03-01',
+						'ticket_sale_end_date'   => '2040-03-01',
+					]
+				);
+				
+				return [
+					$post_id,
+					$ticket_id,
+					false,
+					false,
+					false,
+					false,
+				];
+			},
+		];
+	}
+	
+	/**
+	 * @test
+	 * @covers \TEC\Tickets\Commerce\Ticket::process_sale_price_data
+	 * @covers \TEC\Tickets\Commerce\Ticket::process_sale_price_dates
+	 *
+	 * @dataProvider process_sale_price_data_provider
+	 */
+	public function test_process_sale_price_data( \Closure $data ): void {
+		[ $post_id, $ticket_id, $expected_checked, $expected_price, $expected_start_date, $expected_end_date ] = $data();
+		
+		$this->assertEquals( $expected_checked, get_post_meta( $ticket_id, Ticket::$sale_price_checked_key, true ) );
+		$this->assertEquals( $expected_price, get_post_meta( $ticket_id, Ticket::$sale_price_key, true ) );
+		$this->assertEquals( $expected_start_date, get_post_meta( $ticket_id, Ticket::$sale_price_start_date_key, true ) );
+		$this->assertEquals( $expected_end_date, get_post_meta( $ticket_id, Ticket::$sale_price_end_date_key, true ) );
 	}
 }
