e04f6115502f51ccc50efc0c7fc3c8acb26f09c5
Brian Krane <redscar@gmail.com>
2024-04-02 15:38:00 -0400

    Added pot file changelog.

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -25,7 +25,7 @@ class Tribe__Tickets__Main {
 	public $latest_version_slug = 'latest_event_tickets_version';
 
 	/**
-	* Min Version of WordPress
+	* Min Version of WordPress.
 	*
 	* @since 4.10
 	*/
