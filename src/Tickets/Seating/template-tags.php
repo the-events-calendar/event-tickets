7ca9c1fb1c270709c2815c798dac0cdf7d93f9dc
rafsuntaskin <rafsun.aiub@gmail.com>
2024-05-09 20:35:37 +0600

    update logic

--- src/Tickets/Seating/template-tags.php
+++ src/Tickets/Seating/template-tags.php
@@ -18,7 +18,6 @@ if ( ! function_exists( 'tec_tickets_seating_enabled' ) ) {
 	 * @return bool Whether the event is using assigned seating.
 	 */
 	function tec_tickets_seating_enabled( int $post_id ): bool {
-		// @toDo: remove the inversion once the meta key is updated from editor.
-		return ! tribe_is_truthy( get_post_meta( $post_id, Meta::META_KEY_LAYOUT_ID, true ) );
+		return tribe_is_truthy( get_post_meta( $post_id, Meta::META_KEY_LAYOUT_ID, true ) );
 	}
 }
