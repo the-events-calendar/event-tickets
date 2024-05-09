0b737a5cb98328710f48956cb495f1d047c60242
rafsuntaskin <rafsun.aiub@gmail.com>
2024-05-09 15:03:19 +0600

    use tribe dialog in seat form

--- src/views/seating/tickets-block.php
+++ src/views/seating/tickets-block.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * My Tickets: Attendee Label
+ * Seating tickets block template.
  *
  * Override this template in your own theme by creating a file at
  * [your-theme]/tribe/tickets-seating/tickets-block.php
  *
- * @since   TBD
+ * @since TBD
  *
  * @version TBD
  *
@@ -18,22 +18,26 @@
 <div class="tribe-common event-tickets tribe-tickets__tickets-wrapper">
 	<div class="tribe-tickets__tickets-form tec-tickets-sld__tickets-block">
 		<h2 class="tribe-common-h4 tribe-common-h--alt tribe-tickets__tickets-title">
-			<?php echo esc_html( tribe_get_ticket_label_plural( 'purchase-form' ) ); ?>
+			<?php echo esc_html( tribe_get_ticket_label_plural( 'seat-form' ) ); ?>
 		</h2>
 		<div class="tec-tickets-seating__tickets-block__information">
 			<span><?php echo esc_html( $cost_range ); ?></span>
-			<span class="tec-tickets-seating__tickets-block__inventory"><?php echo esc_html( $inventory ) . esc_html__( ' available', 'event-tickets' ); ?> </span>
+			<span class="tec-tickets-seating__tickets-block__inventory">
+				<?php echo esc_html( sprintf( '%s %s', $inventory, __( 'available', 'event-tickets' ) ) ); ?>
+			</span>
 		</div>
-
 		<div class="tec-tickets-seating__tickets-block__action">
-			<button
-				type="submit"
-				class="tribe-common-c-btn tribe-common-c-btn--small tribe-tickets__attendee-tickets-submit tribe-tickets__attendee-tickets-footer-checkout-button tribe-validation-submit"
-				name="find-seats-button"
-			>
-				<?php esc_html_e( 'Find Seats', 'event-tickets' ); ?>
-			</button>
+		<?php
+			/** @var Tribe\Dialog\View $dialog_view */
+			$dialog_view = tribe( 'dialog.view' );
+			$content     = '<p>test</p>';
+			$args        = [
+				'button_text'    => esc_html_x( 'Find Seats', 'Find seats button text', 'event-tickets' ),
+				'button_classes' => [ 'tribe-common-c-btn', 'tribe-common-c-btn--small' ],
+				'append_target'  => '.tec-tickets-seating__tickets-block__information',
+			];
+			$dialog_view->render_modal( $content, $args, 'tec-tickets-seating__tickets-block__action--submit' );
+			?>
 		</div>
-
 	</div>
 </div>
