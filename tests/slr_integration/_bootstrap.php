b0be8efd7ba8fd773297dd19b9c193230087f459
rafsuntaskin <rafsun.aiub@gmail.com>
2024-05-10 16:52:56 +0600

    updated bootstrap to enable tc

--- tests/slr_integration/_bootstrap.php
+++ tests/slr_integration/_bootstrap.php
@@ -4,6 +4,15 @@
 use Codeception\Util\Autoload;
 use TEC\Tickets\Seating\Tables\Layouts;
 use TEC\Tickets\Seating\Tables\Seat_Types;
+use \TEC\Tickets\Commerce\Provider as Commerce_Provider;
+
+$tec_support = dirname( __DIR__, 3 ) . '/the-events-calendar/tests/_support';
+Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', $tec_support );
+
+// Let's make sure Commerce is enabled.
+putenv( 'TEC_TICKETS_COMMERCE=1' );
+putenv( 'TEC_DISABLE_LOGGING=1' );
+tribe_register_provider( Commerce_Provider::class );
 
 tribe()->get( \TEC\Tickets\Seating\Service\Service::class );
 
