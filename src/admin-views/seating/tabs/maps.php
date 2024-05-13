089e0d1ea270a93609118d350605e22e7f4957d0
rafsuntaskin <rafsun.aiub@gmail.com>
2024-05-14 02:57:14 +0600

    phpcbf

--- src/admin-views/seating/tabs/maps.php
+++ src/admin-views/seating/tabs/maps.php
@@ -12,35 +12,58 @@
 
 <div class="tec-tickets__tab-heading__wrapper"><h2
 		class="tec-tickets__tab-heading">
-		<?php echo esc_html_x( 'Seating Maps',
+		<?php 
+		echo esc_html_x(
+			'Seating Maps',
 			'Controller maps tab title',
-			'event-tickets' ); ?>
+			'event-tickets' 
+		); 
+		?>
 	</h2>
 	<a class="button button-secondary tec-tickets__tab-heading__button"
-	   type="button"
-	   href="<?php echo esc_url( $add_new_url ); ?>">
+		type="button"
+		href="<?php echo esc_url( $add_new_url ); ?>">
 		<?php echo esc_html_x( 'Add New', 'Add new seating configuration button', 'event-tickets' ); ?>
 	</a>
 	<div class="tec-tickets__tab-heading__description">
 		<p>
-			<?php echo wp_kses(
+			<?php 
+			echo wp_kses(
 				sprintf(
 				/* translators: %1$s: Documentation link */
-					__( 'Build different seat layouts on top of your configurations to create different sections and pricing tiers for use with tickets. %1$s',
-						'event-tickets' ),
+					__(
+						'Build different seat layouts on top of your configurations to create different sections and pricing tiers for use with tickets. %1$s',
+						'event-tickets' 
+					),
 					'<a href="https://evnt.is" target="_blank">'
 					. __( 'Learn more', 'event-tickets' )
-					. '</a>' ),
-				[ 'a' => [ 'href' => [], 'target' => [], 'title' => [] ] ] ); ?>
+					. '</a>' 
+				),
+				[
+					'a' => [
+						'href'   => [],
+						'target' => [],
+						'title'  => [],
+					],
+				] 
+			); 
+			?>
 		</p>
 	</div>
 </div>
 <div class="tec-tickets__tab-content__wrapper">
 	<?php if ( empty( $cards ) ) : ?>
 		<p><?php esc_html_e( 'No seating maps to show.', 'event-tickets' ); ?></p>
-	<?php else : foreach ( $cards as $card ) {
-		$this->template->template( 'components/layout-card', $card );
-	} ?>
+		<?php 
+	else :
+		?>
+		<div class="tec-tickets__tab-content__cards">
+		<?php
+		foreach ( $cards as $card ) {
+			$this->template( 'components/layout-card', [ 'card' => $card ] );
+		} 
+		?>
+		</div>
 	<?php endif; ?>
 </div>
 
