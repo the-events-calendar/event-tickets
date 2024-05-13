dbad38d2351c607cafe7ed4c96613f408919a48d
Luca Tumedei <luca@theaveragedev.com>
2024-05-13 11:09:39 +0200

    test(slr_integration) ensure meta is set/unset

--- tests/slr_integration/Frontend_Test.php
+++ tests/slr_integration/Frontend_Test.php
@@ -2,32 +2,33 @@
 
 namespace TEC\Tickets\Seating\Frontend;
 
-use TEC\Tickets\Seating\Frontend;
-use TEC\Common\Tests\Provider\Controller_Test_Case;
 use tad\Codeception\SnapshotAssertions\SnapshotAssertions;
+use TEC\Common\Tests\Provider\Controller_Test_Case;
+use TEC\Tickets\Commerce\Tickets_View;
 use TEC\Tickets\Flexible_Tickets\Test\Traits\Series_Pass_Factory;
+use TEC\Tickets\Seating\Frontend;
 use TEC\Tickets\Seating\Meta;
 use Tribe\Tickets\Test\Commerce\TicketsCommerce\Ticket_Maker;
-use TEC\Tickets\Commerce\Tickets_View;
 
 class Frontend_Test extends Controller_Test_Case {
 	use SnapshotAssertions;
 	use Ticket_Maker;
 	use Series_Pass_Factory;
-	
+
 	protected string $controller_class = Frontend::class;
-	
+
 	/**
 	 * it should_display_ticket_block_when_seating_is_enabled
 	 *
 	 * @test
 	 */
 	public function should_display_ticket_block_when_seating_is_not_enabled() {
-		$post_id  = static::factory()->post->create(
+		$post_id = static::factory()->post->create(
 			[
 				'post_type' => 'page',
-			] 
+			]
 		);
+		delete_post_meta( $post_id, Meta::META_KEY_LAYOUT_ID );
 		$ticket_1 = $this->create_tc_ticket( $post_id, 10 );
 		$ticket_2 = $this->create_tc_ticket( $post_id, 30 );
 		// Sort the tickets "manually".
@@ -35,40 +36,41 @@ class Frontend_Test extends Controller_Test_Case {
 			[
 				'ID'         => $ticket_1,
 				'menu_order' => 1,
-			] 
+			]
 		);
 		wp_update_post(
 			[
 				'ID'         => $ticket_2,
 				'menu_order' => 2,
-			] 
+			]
 		);
-		
+
 		$this->make_controller()->register();
-		
+
 		$html = tribe( Tickets_View::class )->get_tickets_block( $post_id );
-		
+
 		// Replace the ticket IDs with placeholders.
 		$html = str_replace(
 			[ $post_id, $ticket_1, $ticket_2 ],
 			[ '{{post_id}}', '{{ticket_1}}', '{{ticket_2}}' ],
 			$html
 		);
-		
+
 		$this->assertMatchesHtmlSnapshot( $html );
 	}
-	
+
 	/**
 	 * it should_replace_ticket_block_when_seating_is_enabled
 	 *
 	 * @test
 	 */
 	public function should_replace_ticket_block_when_seating_is_enabled() {
-		$post_id  = static::factory()->post->create(
+		$post_id = static::factory()->post->create(
 			[
 				'post_type' => 'page',
 			]
 		);
+		update_post_meta( $post_id, Meta::META_KEY_LAYOUT_ID, 'some-layout-uuid' );
 		$ticket_1 = $this->create_tc_ticket( $post_id, 20 );
 		$ticket_2 = $this->create_tc_ticket( $post_id, 50 );
 		// Sort the tickets "manually".
@@ -84,20 +86,20 @@ class Frontend_Test extends Controller_Test_Case {
 				'menu_order' => 2,
 			]
 		);
-		
+
 		update_post_meta( $post_id, Meta::META_KEY_LAYOUT_ID, 'yes' );
-		
+
 		$this->make_controller()->register();
-		
+
 		$html = tribe( Tickets_View::class )->get_tickets_block( $post_id );
-		
+
 		// Replace the ticket IDs with placeholders.
 		$html = str_replace(
 			[ $post_id, $ticket_1, $ticket_2 ],
 			[ '{{post_id}}', '{{ticket_1}}', '{{ticket_2}}' ],
 			$html
 		);
-		
+
 		$this->assertMatchesHtmlSnapshot( $html );
 	}
 }
