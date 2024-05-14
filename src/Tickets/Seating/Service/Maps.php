7eb37a35ebd3418e607e5be0510469e0f4405d3d
rafsuntaskin <rafsun.aiub@gmail.com>
2024-05-14 15:27:27 +0600

    doc blocks updated

--- src/Tickets/Seating/Service/Maps.php
+++ src/Tickets/Seating/Service/Maps.php
@@ -11,7 +11,22 @@ namespace TEC\Tickets\Seating\Service;
 
 use TEC\Tickets\Seating\Tables\Maps as Maps_Table;
 
+/**
+ * Class Maps.
+ *
+ * @since TBD
+ *
+ * @package TEC\Controller\Service;
+ */
 class Maps {
+	
+	/**
+	 * Fetches all the Maps from the database.
+	 *
+	 * @since TBD
+	 *
+	 * @return array<string, array{id: string, name: string, seats: int}> The map rows in array format.
+	 */
 	public static function fetch_all() {
 		return Maps_Table::fetch_all();
 	}
