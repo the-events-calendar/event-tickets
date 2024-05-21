4c26aeab158008cb94e5f55a5ec84c7e9048f8a6
rafsuntaskin <rafsun.aiub@gmail.com>
2024-05-21 23:35:26 +0600

    phpcbf

--- src/admin-views/seating/tabs/layouts.php
+++ src/admin-views/seating/tabs/layouts.php
@@ -12,9 +12,13 @@
 
 <div class="tec-tickets__tab-heading__wrapper"><h2
 		class="tec-tickets__tab-heading">
-		<?php echo esc_html_x( 'Seat Layouts',
+		<?php 
+		echo esc_html_x(
+			'Seat Layouts',
 			'Seat layouts tab title',
-			'event-tickets' ); ?>
+			'event-tickets' 
+		); 
+		?>
 	</h2>
 
 	<div class="tec-tickets-seating-modal-wrapper tribe-common tec-tickets__tab-heading__button">
@@ -24,33 +28,48 @@
 		$dialog_view = tribe( 'dialog.view' );
 		$content     = '<p>test</p>';
 		$args        = [
-			'button_text' => esc_html_x( 'Add New', 'Add new seat layout button text','event-tickets' ),
-			'button_classes' => ['button-secondary','tec-tickets-seating-modal__button'],
-			'append_target' => '.tec-tickets-seating-layouts-modal__anchor',
+			'button_text'    => esc_html_x( 'Add New', 'Add new seat layout button text', 'event-tickets' ),
+			'button_classes' => [ 'button-secondary', 'tec-tickets-seating-modal__button' ],
+			'append_target'  => '.tec-tickets-seating-layouts-modal__anchor',
 		];
-		$dialog_view->render_modal( $content, $args, 'tec-tickets-seating-layouts-modal');
+		$dialog_view->render_modal( $content, $args, 'tec-tickets-seating-layouts-modal' );
 		?>
 	</div>
 
 	<div class="tec-tickets__tab-heading__description">
 		<p>
-			<?php echo wp_kses(
+			<?php 
+			echo wp_kses(
 				sprintf(
 				/* translators: %1$s: Documentation link */
-					__( 'Create a seating configuration that represents your room or venue. Configurations are used to create layouts that allow purchasers choose specific seats and ticketing tiers. %1$s',
-						'event-tickets' ),
+					__(
+						'Create a seating configuration that represents your room or venue. Configurations are used to create layouts that allow purchasers choose specific seats and ticketing tiers. %1$s',
+						'event-tickets' 
+					),
 					'<a href="https://evnt.is" target="_blank">'
 					. __( 'Learn more', 'event-tickets' )
-					. '</a>' ),
-				[ 'a' => [ 'href' => [], 'target' => [], 'title' => [] ] ] ); ?>
+					. '</a>' 
+				),
+				[
+					'a' => [
+						'href'   => [],
+						'target' => [],
+						'title'  => [],
+					],
+				] 
+			); 
+			?>
 		</p>
 	</div>
 </div>
 <div class="tec-tickets__tab-content__wrapper">
 	<?php if ( empty( $cards ) ) : ?>
 		<p><?php esc_html_e( 'No seat layouts to show.', 'event-tickets' ); ?></p>
-	<?php else : foreach ( $cards as $card ) {
-		$this->template->template( 'components/map-card', $card );
-	} ?>
+		<?php 
+	else :
+		foreach ( $cards as $card ) {
+			$this->template->template( 'components/map-card', $card );
+		} 
+		?>
 	<?php endif; ?>
 </div>
