b300602946a24e3d1afad3c7f95e541b5e3345d7
rafsuntaskin <rafsun.aiub@gmail.com>
2024-06-01 03:24:05 +0600

    save attendee seat meta

--- src/Tickets/Seating/Orders/Controller.php
+++ src/Tickets/Seating/Orders/Controller.php
@@ -10,7 +10,10 @@
 namespace TEC\Tickets\Seating\Orders;
 
 use TEC\Common\Contracts\Provider\Controller as Controller_Contract;
-
+use TEC\Tickets\Commerce\Attendee;
+use TEC\Tickets\Commerce\Status\Status_Interface;
+use TEC\Tickets\Seating\Meta;
+use Tribe__Utils__Array as Arr;
 /**
  * Class Controller
  *
@@ -29,6 +32,7 @@ class Controller extends Controller_Contract {
 	 */
 	protected function do_register(): void {
 		add_filter( 'tec_tickets_commerce_cart_prepare_data', [ $this, 'handle_seat_selection' ] );
+		add_action( 'tec_tickets_commerce_flag_action_generated_attendee', [ $this, 'save_seat_data_for_attendee' ], 10, 7 );
 	}
 	
 	/**
@@ -40,6 +44,7 @@ class Controller extends Controller_Contract {
 	 */
 	public function unregister(): void {
 		remove_filter( 'tec_tickets_commerce_cart_prepare_data', [ $this, 'handle_seat_selection' ] );
+		remove_action( 'tec_tickets_commerce_flag_action_generated_attendee', [ $this, 'save_seat_data_for_attendee' ] );
 	}
 	
 	/**
@@ -63,5 +68,26 @@ class Controller extends Controller_Contract {
 		}
 		
 		return $data;
+	}
+	
+	/**
+	 * Saves the seat data for the attendee.
+	 *
+	 * @param Attendee                 $attendee               The generated attendee.
+	 * @param \Tribe__Tickets__Tickets $ticket The ticket the attendee is generated for.
+	 * @param \WP_Post                 $order              The order the attendee is generated for.
+	 * @param Status_Interface         $new_status      New post status.
+	 * @param Status_Interface|null    $old_status Old post status.
+	 * @param array                    $item Which cart item this was generated for.
+	 * @param int                      $i      Which Attendee index we are generating.
+	 */
+	public function save_seat_data_for_attendee( $attendee, $ticket, $order, $new_status, $old_status, $item, $i ) {
+		$seats = Arr::get( $item, [ 'extra', 'seats' ], false );
+		
+		if ( empty( $seats ) || ! isset( $seats[ $i ] ) ) {
+			return;
+		}
+		
+		update_post_meta( $attendee->ID, Meta::META_KEY_ATTENDEE_SEAT_LABEL, $seats[ $i ] );
 	}
 }
