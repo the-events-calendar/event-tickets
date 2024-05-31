b6b0f203aef42b9984653a9c9d27eeb3d6bc72a6
Dimitrios Pantazis <dpanta94@gmail.com>
2024-05-31 16:12:28 +0300

    refactor JS

--- src/resources/js/admin/tickets-commerce-settings.js
+++ src/resources/js/admin/tickets-commerce-settings.js
@@ -233,6 +233,43 @@ tribe.tickets.admin.commerceSettings = {};
 			} );
 	};
 
+	obj.onWebHookSetUpLinkClick = function( evt ) {
+		evt.preventDefault();
+		const target = $( evt.target );
+
+		$document.find( '#tec-tickets-webhook-creation-update' ).remove();
+
+		$(
+			`<p id="tec-tickets-webhook-creation-update" class="tooltip description">
+			<span class="dashicons dashicons-update"></span>
+			<span>${ target.attr( 'data-loading-text' ) }</span>
+			</p>`,
+		).insertAfter( target.closest( 'p' ) );
+
+		fetch( target.attr( 'href' ) ).then( function( res ) {
+			return res.json();
+		} ).then( function( res ) {
+			const updateEle = $( '#tec-tickets-webhook-creation-update' );
+			const updateDashIcon = updateEle.find( 'span.dashicons' );
+			const updateText = updateEle.find( 'span:not(.dashicons)' );
+
+			updateText.text( res.data.status );
+
+			updateDashIcon.removeClass( 'dashicons-update' );
+
+			if ( true !== res.success ) {
+				updateDashIcon.addClass( 'dashicons-no' );
+				return;
+			}
+
+			updateDashIcon.addClass( 'dashicons-yes' );
+
+			setTimeout( function() {
+				window.location.reload();
+			}, 700 );
+		} );
+	};
+
 	/**
 	 * Handles the initialization of the gateway settings when Document is ready.
 	 *
@@ -242,6 +279,7 @@ tribe.tickets.admin.commerceSettings = {};
 	obj.ready = function() {
 		obj.onBoardingButton = $( obj.selectors.connectButton );
 		obj.disconnectButton = $( obj.selectors.disconnectPayPalAccountButton );
+		obj.setUpWebhookLink = $( obj.selectors.webHookSetUpLink );
 
 		if ( obj.onBoardingButton[ 0 ] ) {
 			obj.onBoardingButton.on( 'click', obj.handleConnectClick );
@@ -251,42 +289,9 @@ tribe.tickets.admin.commerceSettings = {};
 			obj.disconnectButton.on( 'click', obj.handleDisconnectClick );
 		}
 
-		$document.on( 'click', obj.selectors.webHookSetUpLink, function( evt ) {
-			evt.preventDefault();
-			const target = $( evt.target );
-
-			$document.find( '#tec-tickets-webhook-creation-update' ).remove();
-
-			$(
-				`<p id="tec-tickets-webhook-creation-update" class="tooltip description">
-				<span class="dashicons dashicons-update"></span>
-				<span>${ target.attr( 'data-loading-text' ) }</span>
-				</p>`,
-			).insertAfter( target.closest( 'p' ) );
-
-			fetch( target.attr( 'href' ) ).then( function( res ) {
-				return res.json();
-			} ).then( function( res ) {
-				const updateEle = $( '#tec-tickets-webhook-creation-update' );
-				const updateDashIcon = updateEle.find( 'span.dashicons' );
-				const updateText = updateEle.find( 'span:not(.dashicons)' );
-
-				updateText.text( res.data.status );
-
-				updateDashIcon.removeClass( 'dashicons-update' );
-
-				if ( true !== res.success ) {
-					updateDashIcon.addClass( 'dashicons-no' );
-					return;
-				}
-
-				updateDashIcon.addClass( 'dashicons-yes' );
-
-				setTimeout( function() {
-					window.location.reload();
-				}, 700 );
-			} );
-		} );
+		if ( obj.setUpWebhookLink[ 0 ] ) {
+			obj.setUpWebhookLink.on( 'click', obj.onWebHookSetUpLinkClick );
+		}
 
 		obj.maybeShowModalAfterConnection();
 	};
