85a5129ede54b925802364e1fe3c70ff7c31948b
rafsuntaskin <rafsun.aiub@gmail.com>
2024-06-04 18:07:20 +0600

    render seat column on event attendee page

--- src/Tickets/Seating/Orders/Controller.php
+++ src/Tickets/Seating/Orders/Controller.php
@@ -14,6 +14,8 @@ use TEC\Tickets\Commerce\Attendee;
 use TEC\Tickets\Commerce\Status\Status_Interface;
 use TEC\Tickets\Seating\Meta;
 use Tribe__Utils__Array as Arr;
+use Tribe__Main as Common;
+
 /**
  * Class Controller
  *
@@ -33,6 +35,10 @@ class Controller extends Controller_Contract {
 	protected function do_register(): void {
 		add_filter( 'tec_tickets_commerce_cart_prepare_data', [ $this, 'handle_seat_selection' ] );
 		add_action( 'tec_tickets_commerce_flag_action_generated_attendee', [ $this, 'save_seat_data_for_attendee' ], 10, 7 );
+		
+		// Add attendee seat data column to the attendee list.
+		add_filter( 'tribe_tickets_attendee_table_columns', [ $this, 'add_attendee_seat_column' ], 10, 2 );
+		add_filter( 'tribe_events_tickets_attendees_table_column', [ $this, 'render_seat_column' ], 10, 3 );
 	}
 	
 	/**
@@ -45,6 +51,10 @@ class Controller extends Controller_Contract {
 	public function unregister(): void {
 		remove_filter( 'tec_tickets_commerce_cart_prepare_data', [ $this, 'handle_seat_selection' ] );
 		remove_action( 'tec_tickets_commerce_flag_action_generated_attendee', [ $this, 'save_seat_data_for_attendee' ] );
+		
+		// Remove attendee seat data column from the attendee list.
+		remove_filter( 'tribe_tickets_attendee_table_columns', [ $this, 'add_attendee_seat_column' ] );
+		remove_filter( 'tribe_events_tickets_attendees_table_column', [ $this, 'render_seat_column' ] );
 	}
 	
 	/**
@@ -96,5 +106,54 @@ class Controller extends Controller_Contract {
 		$event_id  = get_post_meta( $ticket->ID, Attendee::$event_relation_meta_key, true );
 		$layout_id = get_post_meta( $event_id, Meta::META_KEY_LAYOUT_ID, true );
 		update_post_meta( $attendee->ID, Meta::META_KEY_LAYOUT_ID, $layout_id );
+	}
+	
+	/**
+	 * Adds the attendee seat column to the attendee list.
+	 *
+	 * @since TBD
+	 *
+	 * @param array<string,mixed> $columns The columns for the Attendees table.
+	 * @param int                 $event_id The event ID.
+	 *
+	 * @return array The filtered columns for the Attendees table.
+	 */
+	public function add_attendee_seat_column( array $columns, int $event_id ): array {
+		$event_layout_id = get_post_meta( $event_id, Meta::META_KEY_LAYOUT_ID, true );
+		
+		if ( empty( $event_layout_id ) ) {
+			return $columns;
+		}
+		
+		return Common::array_insert_after_key(
+			'ticket',
+			$columns,
+			[ 'seat' => esc_html_x( 'Seat', 'attendee table seat column header', 'event-tickets' ) ]
+		);
+	}
+	
+	/**
+	 * Renders the seat column for the attendee list.
+	 *
+	 * @since TBD
+	 *
+	 * @param string              $value  Row item value.
+	 * @param array<string,mixed> $item   Row item data.
+	 * @param string              $column Column name.
+	 *
+	 * @return string The rendered column.
+	 */
+	public function render_seat_column( $value, $item, $column ) {
+		if ( 'seat' !== $column ) {
+			return $value;
+		}
+		
+		$seat_label = get_post_meta( $item['ID'], Meta::META_KEY_ATTENDEE_SEAT_LABEL, true );
+		
+		if ( empty( $seat_label ) ) {
+			return '';
+		}
+		
+		return $seat_label;
 	}
 }
