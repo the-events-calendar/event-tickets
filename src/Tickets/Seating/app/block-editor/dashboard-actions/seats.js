d144b028384bc2f615a1fcc3318f57f73fb0495c
rafsuntaskin <rafsun.aiub@gmail.com>
2024-06-25 04:23:07 +0600

    added seat action component

---
+++ src/Tickets/Seating/app/block-editor/dashboard-actions/seats.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { ActionButton } from "@moderntribe/tickets/elements";
+import { getLink, getLocalizedString } from '@tec/tickets/seating/utils';
+import { Seat } from '@moderntribe/tickets/icons';
+
+const getString = (key) => getLocalizedString( key, 'dashboard' );
+
+const Seats = () => {
+	return (
+		<ActionButton
+			asLink={ true }
+			href={ getLink('layout-edit') }
+			icon={ <Seat /> }
+			target="_blank"
+		>
+		{ getString( 'seats-action-label' ) }
+		</ActionButton>
+	);
+};
+
+export default Seats;
