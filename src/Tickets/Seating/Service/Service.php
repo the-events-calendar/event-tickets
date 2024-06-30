1187eb0ed24d8dce3f6f1b11a312f39ff2855c91
rafsuntaskin <rafsun.aiub@gmail.com>
2024-06-30 21:52:47 +0600

    Add the map edit embed url

--- src/Tickets/Seating/Service/Service.php
+++ src/Tickets/Seating/Service/Service.php
@@ -239,7 +239,7 @@ class Service {
 				'token' => urlencode( $token ),
 				'mapId' => urlencode( $map_id ),
 			],
-			$this->get_frontend_url( '/embed/seating-map/' )
+			$this->get_frontend_url( '/embed/edit-seat-map/' )
 		);
 	}
 
