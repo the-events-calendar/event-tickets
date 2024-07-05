5b40fe4adfddd471a9910273ce200123e47f2463
rafsuntaskin <rafsun.aiub@gmail.com>
2024-07-06 03:44:36 +0600

    add map actions

---
+++ src/Tickets/Seating/app/admin/maps/actions.js
@@ -0,0 +1,51 @@
+import {ajaxUrl, ajaxNonce} from "../../service/service-api/externals";
+
+const onReady = (domReadyCallback) => {
+	if (document.readyState !== 'loading') {
+		domReadyCallback();
+	} else {
+		document.addEventListener('DOMContentLoaded', domReadyCallback);
+	}
+};
+
+const register_delete_action = () => {
+	// add click listener to all links with class 'delete'
+	document.querySelectorAll('.delete-map').forEach((link) => {
+		link.addEventListener('click', async ( event ) => {
+			event.preventDefault();
+			await handleDelete( event );
+		});
+	});
+}
+
+const handleDelete = async ( event ) => {
+	// get the data-map-id from the link.
+	const mapId = event.target.getAttribute('data-map-id');
+	if ( confirm('Are you sure you want to delete this map?') ) {
+		const result = await delete_map( mapId );
+		if ( result ) {
+			window.location.reload();
+		} else {
+			console.log('failed to delete');
+			alert( 'Failed to delete the map' );
+		}
+	} else {
+		console.log('not deleted');
+	}
+}
+
+const delete_map = async (mapId) => {
+	const url = new URL(ajaxUrl);
+	url.searchParams.set('_ajax_nonce', ajaxNonce);
+	url.searchParams.set('mapId', mapId);
+	url.searchParams.set(
+		'action',
+		'tec_tickets_seating_service_delete_map'
+	);
+	const response = await fetch( url.toString(), { method: 'POST' } );
+
+	return response.status === 200;
+}
+onReady(() => {
+	register_delete_action();
+});
