a0eb710860eb2c692dcae1d2861847363db31a94
rafsuntaskin <rafsun.aiub@gmail.com>
2024-07-10 04:17:46 +0600

    update JS to send seat types data

--- src/Tickets/Seating/app/admin/seats-report/index.js
+++ src/Tickets/Seating/app/admin/seats-report/index.js
@@ -1,4 +1,39 @@
 import './style.pcss';
 import { initServiceIframe, getIframeElement } from '@tec/tickets/seating/iframe';
+import { onReady } from '@tec/tickets/seating/utils';
+import {
+	INBOUND_APP_READY_FOR_DATA,
+	OUTBOUND_SEAT_TYPE_TICKETS,
+	removeAction,
+	registerAction,
+	sendPostMessage,
+} from '@tec/tickets/seating/service';
+const { seatTypeMap } = window?.tec?.tickets?.seating?.seatsReport?.data;
 
-initServiceIframe(getIframeElement());
+
+/**
+ * Registers the handlers for the messages received from the service.
+ *
+ * @since TBD
+ *
+ * @param {HTMLElement} iframe The service iframe element to listen to.
+ */
+function registerActions(iframe) {
+	// When the service is ready for data, send the seat type map to the iframe.
+	registerAction(INBOUND_APP_READY_FOR_DATA, () => {
+		removeAction(INBOUND_APP_READY_FOR_DATA);
+		sendPostMessage(iframe, OUTBOUND_SEAT_TYPE_TICKETS, seatTypeMap);
+	});
+}
+
+onReady( async () => {
+	const iframe = getIframeElement();
+	if (!iframe) {
+		console.error('Iframe element not found.');
+		return false;
+	}
+
+	// Register the actions before initializing the iframe to avoid race conditions.
+	registerActions(iframe);
+	await initServiceIframe(iframe);
+})
