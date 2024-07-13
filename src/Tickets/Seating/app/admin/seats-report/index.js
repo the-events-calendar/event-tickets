bfc6fd456b9c567d668c655d68f9d304fde5ae98
rafsuntaskin <rafsun.aiub@gmail.com>
2024-07-13 16:30:01 +0600

    added attendee fetch

--- src/Tickets/Seating/app/admin/seats-report/index.js
+++ src/Tickets/Seating/app/admin/seats-report/index.js
@@ -4,12 +4,44 @@ import { onReady } from '@tec/tickets/seating/utils';
 import {
 	INBOUND_APP_READY_FOR_DATA,
 	OUTBOUND_SEAT_TYPE_TICKETS,
+	OUTBOUND_EVENT_ATTENDEES,
 	removeAction,
 	registerAction,
 	sendPostMessage,
 } from '@tec/tickets/seating/service/api';
-const { seatTypeMap } = window?.tec?.tickets?.seating?.seatsReport?.data;
+import {ajaxUrl, ajaxNonce} from "@tec/tickets/seating/service";
 
+const { seatTypeMap, postId } = window?.tec?.tickets?.seating?.seatsReport?.data;
+let seatTypeMapSent = false;
+
+async function fetchAttendees() {
+	const url = new URL(ajaxUrl);
+	url.searchParams.set('_ajax_nonce', ajaxNonce);
+	url.searchParams.set('postId', postId);
+	url.searchParams.set(
+		'action',
+		'tec_tickets_seating_fetch_attendees'
+	);
+	const response = await fetch(
+		url.toString(),
+		{
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+			},
+		},
+		);
+
+	const json = await response.json();
+
+	if (response.status !== 200) {
+		throw new Error(
+			`Failed to fetch attendees for post ID ${postId}. Status: ${response.status} - ${json?.data?.error}`,
+		);
+	}
+
+	return json?.data || [];
+}
 /**
  * Registers the handlers for the messages received from the service.
  *
@@ -19,9 +51,13 @@ const { seatTypeMap } = window?.tec?.tickets?.seating?.seatsReport?.data;
  */
 function registerActions(iframe) {
 	// When the service is ready for data, send the seat type map to the iframe.
-	registerAction(INBOUND_APP_READY_FOR_DATA, () => {
-		removeAction(INBOUND_APP_READY_FOR_DATA);
-		sendPostMessage(iframe, OUTBOUND_SEAT_TYPE_TICKETS, seatTypeMap);
+	registerAction(INBOUND_APP_READY_FOR_DATA, async () => {
+		if ( ! seatTypeMapSent ) {
+			sendPostMessage(iframe, OUTBOUND_SEAT_TYPE_TICKETS, seatTypeMap);
+			seatTypeMapSent = true;
+		}
+		const attendees = await fetchAttendees();
+		sendPostMessage(iframe, OUTBOUND_EVENT_ATTENDEES, attendees);
 	});
 }
 
