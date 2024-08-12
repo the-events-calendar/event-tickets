678b4fa57be0753475eb0062d7db32daf9671c4e
Dimitrios Pantazis <dpanta94@gmail.com>
2024-08-13 00:00:29 +0300

    simplify delete_text logic

--- src/admin-views/commerce/orders/single/order-actions-metabox.php
+++ src/admin-views/commerce/orders/single/order-actions-metabox.php
@@ -55,13 +55,10 @@ $possible_statuses = tribe( Status_Handler::class )->get_orders_possible_status(
 		<div id="delete-action">
 			<?php
 			if ( current_user_can( 'delete_post', $order->ID ) ) {
-				if ( ! EMPTY_TRASH_DAYS ) {
-					$delete_text = __( 'Delete permanently', 'event-tickets' );
-				} else {
-					$delete_text = __( 'Move to Trash', 'event-tickets' );
-				}
 				?>
-				<a class="submitdelete deletion" href="<?php echo esc_url( get_delete_post_link( $order->ID ) ); ?>"><?php echo esc_html( $delete_text ); ?></a>
+				<a class="submitdelete deletion" href="<?php echo esc_url( get_delete_post_link( $order->ID ) ); ?>">
+					<?php echo ! EMPTY_TRASH_DAYS ? esc_html__( 'Delete permanently', 'event-tickets' ) : esc_html__( 'Move to Trash', 'event-tickets' ); ?>
+				</a>
 				<?php
 			}
 			?>
