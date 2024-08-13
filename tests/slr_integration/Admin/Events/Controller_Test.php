5f2d26d196f7ad0c65939981005ebb836255022b
rafsuntaskin <rafsun.aiub@gmail.com>
2024-08-13 18:10:00 +0600

    tests -stabilize snapshots

--- tests/slr_integration/Admin/Events/Controller_Test.php
+++ tests/slr_integration/Admin/Events/Controller_Test.php
@@ -8,13 +8,20 @@ use TEC\Tickets\Seating\Admin\Events\Controller;
 use TEC\Tickets\Seating\Service\Layouts as Layouts_Service;
 use TEC\Tickets\Seating\Meta;
 use TEC\Tickets\Seating\Tables\Layouts as Layouts_Table;
+use Tribe\Tests\Traits\With_Uopz;
 
 class Controller_Test extends Controller_Test_Case {
 	use SnapshotAssertions;
+	use With_Uopz;
 	
 	protected string $controller_class = Controller::class;
 	
-	public function test_rendering_associated_events_list() {
+	/**
+	 * @before
+	 */
+	public function setup_conditions() {
+		$this->set_fn_return( 'wp_create_nonce', 'xxxxxx' );
+		
 		Layouts_Service::insert_rows_from_service(
 			[
 				[
@@ -28,14 +35,22 @@ class Controller_Test extends Controller_Test_Case {
 				[
 					'id'            => 'some-layout-2',
 					'name'          => 'Some Layout 2',
-					'seats'         => 20,
+					'seats'         => 10,
 					'createdDate'   => time() * 1000,
 					'mapId'         => 'some-map-2',
 					'screenshotUrl' => 'https://example.com/some-layouts-2.png',
 				],
 			]
 		);
-		
+	}
+	
+	public function tearDown() {
+		parent::tearDown();
+		Layouts_Service::invalidate_cache();
+		unset( $_GET['layout'] );
+	}
+
+	public function test_rendering_associated_events_list() {
 		$event_id = tribe_events()->set_args(
 			[
 				'title'         => 'Event with layout',
@@ -46,11 +61,11 @@ class Controller_Test extends Controller_Test_Case {
 				'post_date_gmt' => '2020-01-01 00:00:00',
 			]
 		)->create()->ID;
-		
+
 		$post_id = self::factory()->post->create(
 			[ 'post_title' => 'Post with layout' ]
 		);
-		
+
 		wp_update_post(
 			[
 				'ID'            => $post_id,
@@ -58,52 +73,37 @@ class Controller_Test extends Controller_Test_Case {
 				'post_date_gmt' => '2020-01-02 00:00:00',
 			]
 		);
-		
+
 		update_post_meta( $event_id, Meta::META_KEY_ENABLED, true );
 		update_post_meta( $event_id, Meta::META_KEY_LAYOUT_ID, 'some-layout-1' );
 		update_post_meta( $post_id, Meta::META_KEY_ENABLED, true );
 		update_post_meta( $post_id, Meta::META_KEY_LAYOUT_ID, 'some-layout-1' );
-		
+
 		$_GET['layout'] = 'some-layout-1';
-		
+
 		ob_start();
 		$this->make_controller()->render();
-		
 		$html = ob_get_clean();
 		
 		$html = str_replace( [ $event_id, $post_id ], [ '{{EVENT_ID}}', '{{POST_ID}}' ], $html );
-		
+
 		$this->assertMatchesHtmlSnapshot( $html );
-		
-		Layouts_Table::truncate();
 	}
-	
+
 	public function test_associated_events_list_without_layout() {
+		$this->make_controller()->register();
 		ob_start();
 		$this->make_controller()->render();
 		$html = ob_get_clean();
-		
+
 		$this->assertMatchesHtmlSnapshot( $html );
 	}
 	
 	public function test_list_with_layout_without_association() {
-		Layouts_Service::insert_rows_from_service(
-			[
-				[
-					'id'            => 'some-layout-2',
-					'name'          => 'Some Layout 2',
-					'seats'         => 10,
-					'createdDate'   => time() * 1000,
-					'mapId'         => 'some-map-2',
-					'screenshotUrl' => 'https://example.com/some-layouts-2.png',
-				],
-			]
-		);
-		
 		$post_id = self::factory()->post->create(
 			[ 'post_title' => 'Post with layout' ]
 		);
-		
+
 		wp_update_post(
 			[
 				'ID'            => $post_id,
@@ -111,15 +111,14 @@ class Controller_Test extends Controller_Test_Case {
 				'post_date_gmt' => '2020-01-02 00:00:00',
 			]
 		);
-		
+
 		$_GET['layout'] = 'some-layout-2';
-		
+		$this->make_controller()->register();
+
 		ob_start();
 		$this->make_controller()->render();
 		$html = ob_get_clean();
-		
+
 		$this->assertMatchesHtmlSnapshot( $html );
-		
-		Layouts_Table::truncate();
 	}
 }
