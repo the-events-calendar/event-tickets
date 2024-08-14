a889cd3ff94c5690085118974d3d89275d0fda2b
Luca Tumedei <luca@theaveragedev.com>
2024-08-14 11:20:03 +0200

    refactor(Seating) udpate OAuth_Token trait to read/write from Uplink

--- src/Tickets/Seating/Service/OAuth_Token.php
+++ src/Tickets/Seating/Service/OAuth_Token.php
@@ -9,6 +9,11 @@
 
 namespace TEC\Tickets\Seating\Service;
 
+use RuntimeException;
+use TEC\Common\StellarWP\Uplink\Resources\Resource;
+use function TEC\Common\StellarWP\Uplink\get_authorization_token;
+use function TEC\Common\StellarWP\Uplink\get_resource;
+
 /**
  * Trait OAuth_Token.
  *
@@ -24,19 +29,21 @@ trait OAuth_Token {
 	 *
 	 * @return string|null The OAuth token, or `null` if there is no OAuth token.
 	 */
-	private function get_oauth_token(): ?string {
-		return tribe_get_option( self::get_oauth_token_option_name(), null );
-	}
+	protected function get_oauth_token(): ?string {
+		$memoize      = tribe_cache();
+		$cache_key    = 'tec_seating_access_token';
+		$access_token = $memoize[ $cache_key ];
 
-	/**
-	 * Returns the option name used to store the Service oAuth access token.
-	 *
-	 * @since TBD
-	 *
-	 * @return string
-	 */
-	public static function get_oauth_token_option_name(): string {
-		return 'events_tickets_seating_access_token';
+		if ( ! ( $access_token && is_string( $access_token ) ) ) {
+			try {
+				$access_token = get_authorization_token( 'tec-seating' );
+			} catch ( RuntimeException $e ) {
+				$access_token = null;
+			}
+			$memoize[ $cache_key ] = $access_token;
+		}
+
+		return $access_token;
 	}
 
 	/**
@@ -49,6 +56,12 @@ trait OAuth_Token {
 	 * @return void
 	 */
 	protected function set_oauth_token( string $token ): void {
-		tribe_update_option( self::get_oauth_token_option_name(), $token );
+		try {
+			/** @var Resource|null $resource */
+			$resource = get_resource( 'tec-seating' );
+			$resource->store_token( $token );
+		} catch ( RuntimeException $e ) {
+			return;
+		}
 	}
 }
