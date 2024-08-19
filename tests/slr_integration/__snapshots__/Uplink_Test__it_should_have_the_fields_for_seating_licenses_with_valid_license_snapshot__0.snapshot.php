88f3a577c668c9f25b10596577882d537f3b2776
Luca Tumedei <luca@theaveragedev.com>
2024-08-19 15:39:45 +0200

    test(slr_integration) move left-over Seating tests to slr_integration

--- tests/slr_integration/__snapshots__/Uplink_Test__it_should_have_the_fields_for_seating_licenses_with_valid_license_snapshot__0.snapshot.php
+++ tests/slr_integration/__snapshots__/Uplink_Test__it_should_have_the_fields_for_seating_licenses_with_valid_license_snapshot__0.snapshot.php
@@ -1,14 +1,4 @@
-<?php return array (
-  'stellarwp-uplink_tec-seating-heading' => 
-  array (
-    'type' => 'heading',
-    'label' => 'Seat Layouts & Reservations',
-  ),
-  'stellarwp-uplink_tec-seating' => 
-  array (
-    'type' => 'html',
-    'label' => '',
-    'html' => '
+a:2:{s:36:"stellarwp-uplink_tec-seating-heading";a:2:{s:4:"type";s:7:"heading";s:5:"label";s:7:"Seating";}s:28:"stellarwp-uplink_tec-seating";a:3:{s:4:"type";s:4:"html";s:5:"label";s:0:"";s:4:"html";s:334:"
 <div class="uplink-authorize-container">
 	<a href="http://wordpress.test/wp-admin/?uplink_disconnect=1&#038;uplink_slug=tec-seating&#038;uplink_cache=bb0d77ac221024633cd38c6ebbe813936b85ede435c76e49c400f10f6a3dee87&#038;_wpnonce=12345678"
 	   target="_self"
@@ -16,6 +6,4 @@
 		Disconnect	</a>
 </div>
 
-',
-  ),
-);
+";}}
