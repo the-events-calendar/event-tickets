89ce3be4b46359dbdee4235c71587e75f0e28f07
Mike Cotton <codingmusician@gmail.com>
2024-08-20 12:23:48 -0500

    tidy up the templates

--- src/admin-views/all-tickets/filters.php
+++ src/admin-views/all-tickets/filters.php
@@ -7,11 +7,13 @@
  * @var \Tribe__Template  $this            Current template object.
  * @var string[]          $select_options  The list table for the All Tickets screen.
  * @var string            $current_filter  Currently selected filter.
+ * @var string            $search_id       ID for the search input element.
+ * @var string            $search_value    Current search value.
  */
 
 ?>
 <div class="alignleft actions">
 	<?php $this->template( 'all-tickets/filters/select-box' ); ?>
 	<?php $this->template( 'all-tickets/filters/search-input' ); ?>
-	<?php submit_button( esc_html__( 'Filter', 'event-tickets' ), 'button', false, false, [ 'id' => 'tec-tickets-all-tickets-select-filter' ] ); ?>
+	<?php submit_button( esc_html__( 'Show Tickets', 'event-tickets' ), 'button', false, false, [ 'id' => 'tec-tickets-all-tickets-select-filter' ] ); ?>
 </div>
--- src/admin-views/all-tickets/filters/search-input.php
+++ src/admin-views/all-tickets/filters/search-input.php
@@ -5,15 +5,14 @@
  * @since TBD
  *
  * @var \Tribe__Template  $this                Current template object.
- * @var string            $search_id           The list table for the All Tickets screen.
- * @var string            $search_placeholder  Currently selected filter.
- * @var string            $search_value        Currently selected filter.
+ * @var string            $search_id           ID for the search input element.
+ * @var string            $search_value        Current search value.
  */
 
 ?>
 <input
 	type="search"
-	placeholder="<?php echo esc_attr( $search_placeholder ); ?>"
+	placeholder="<?php echo esc_attr__( 'Ticket or Event Name', 'event-tickets' ); ?>"
 	id="<?php echo esc_attr( $search_id ); ?>"
 	name="s"
 	value="<?php echo esc_attr( $search_value ); ?>"
