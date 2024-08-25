7c746f2adb41db804a0c029f697a80021211e0bf
rafsuntaskin <rafsun.aiub@gmail.com>
2024-08-26 02:13:27 +0600

    jest tests updated

--- tests/slr_jest/admin/action-handlers.spec.js
+++ tests/slr_jest/admin/action-handlers.spec.js
@@ -2,11 +2,13 @@ import {
 	ACTION_DELETE_RESERVATIONS,
 	ACTION_RESERVATIONS_UPDATED_FROM_SEAT_TYPES,
 	ACTION_SEAT_TYPES_UPDATED,
+	ACTION_SEAT_TYPE_DELETED,
 } from '@tec/tickets/seating/ajax';
 import {
 	handleReservationsDeleted,
 	handleReservationsUpdatedFollowingSeatTypes,
 	handleSeatTypesUpdated,
+	handleSeatTypeDeleted,
 } from '@tec/tickets/seating/admin/action-handlers';
 
 require('jest-fetch-mock').enableMocks();
@@ -287,6 +289,58 @@ describe('action handlers', () => {
 			expect(result).toMatchObject({
 				updatedAttendees: 6,
 			});
+		});
+	});
+
+	describe('handleSeatTypeDeleted', () => {
+		it('should not send fetch request if data empty', async () => {
+			fetch.mockIf(
+				/^https:\/\/wordpress\.test\/wp-admin\/admin-ajax\.php?.*$/,
+				JSON.stringify({ success: true,
+					data: { seatTypesUpdated: true } }),
+				{ status: 200 }
+			);
+
+			const result = await handleSeatTypeDeleted({});
+			expect(fetch).not.toBeCalled();
+			expect(result).toBe(false);
+		});
+
+		it('should send proper fetch request', () => {
+			const payload = {
+				"deletedId": 'some-seat-type-2',
+				"transferTo": {
+					id: 'some-seat-type-1',
+					name: 'Some Seat Type 1',
+					mapId: 'some-map-id',
+					layoutId: 'some-layout-id',
+					description: 'Seat Type 1 description',
+					seatsCount: 50,
+				}
+			};
+
+			fetch.mockIf(
+				/^https:\/\/wordpress\.test\/wp-admin\/admin-ajax\.php?.*$/,
+				JSON.stringify({
+					success: true,
+					data: {
+						updatedSeatTypes: 2,
+						updatedTickets: 4,
+						updatedPosts: 6,
+					},
+				}),
+				{ status: 200 }
+			);
+
+			const result = handleSeatTypeDeleted( payload );
+
+			expect(fetch).toBeCalledWith(
+				`https://wordpress.test/wp-admin/admin-ajax.php?_ajax_nonce=1234567890&action=${ACTION_SEAT_TYPE_DELETED}`,
+				{
+					method: 'POST',
+					body: JSON.stringify(payload),
+				}
+			);
 		});
 	});
 });
