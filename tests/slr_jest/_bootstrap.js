0b63e8817e11fa5b92a431f738b6ab6f0a5d0948
rafsuntaskin <rafsun.aiub@gmail.com>
2024-08-29 14:40:31 +0600

    update test document name

--- tests/slr_jest/_bootstrap.js
+++ tests/slr_jest/_bootstrap.js
@@ -178,7 +178,7 @@ global.getTestDocument = function (documentName, transformer) {
 			'/../slr_integration/Orders/__snapshots__/Seats_Report_Test__test_render_page__2_tickets_3_attendees__0.snapshot.html',
 		'seats-selection':
 			'/../slr_integration/__snapshots__/Frontend_Test__should_replace_ticket_block_when_seating_is_enabled__two tickets__0.snapshot.html',
-		'maps-edit':
+		'maps-list':
 			'/../slr_integration/Admin/__snapshots__/Maps_Layout_Homepage_Test__test_maps_tab_card_listing__0.snapshot.html',
 	};
 
