3ac194e42ba6e145aa21e3d5ab5f88a5f82410eb
rafsuntaskin <rafsun.aiub@gmail.com>
2024-08-29 14:22:28 +0600

    updated map edit with init

--- src/Tickets/Seating/app/admin/map-edit/index.js
+++ src/Tickets/Seating/app/admin/map-edit/index.js
@@ -1,7 +1,27 @@
 import './style.pcss';
+import { onReady } from '@tec/tickets/seating/utils';
 import {
 	initServiceIframe,
 	getIframeElement,
 } from '@tec/tickets/seating/service/iframe';
+import {
+	registerAction,
+} from '@tec/tickets/seating/service/api';
 
-initServiceIframe(getIframeElement());
+/**
+ * Initializes iframe and the communication with the service.
+ *
+ * @since TBD
+ *
+ * @param {HTMLDocument|null} dom The document to use to search for the iframe element.
+ *
+ * @return {Promise<void>} A promise that resolves when the iframe is initialized.
+ */
+export async function init(dom) {
+	dom = dom || document;
+	await initServiceIframe(getIframeElement(dom));
+}
+onReady(() => {
+	init(document);
+});
+
