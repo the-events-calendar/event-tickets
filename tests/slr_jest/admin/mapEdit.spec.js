8fbedcbb573e34ff4686b9a961786d80133f937b
rafsuntaskin <rafsun.aiub@gmail.com>
2024-08-29 15:11:58 +0600

    updated mapEdit with resize handler test

--- tests/slr_jest/admin/mapEdit.spec.js
+++ tests/slr_jest/admin/mapEdit.spec.js
@@ -1,8 +1,13 @@
 import { init } from '@tec/tickets/seating/admin/layoutEdit';
 import { reset } from '@tec/tickets/seating/service/api/state';
 import { getIframeElement } from '@tec/tickets/seating/service/iframe';
+import {
+	getHandlerForAction,
+	INBOUND_SET_ELEMENT_HEIGHT,
+} from '@tec/tickets/seating/service/api';
 
 const iframeModule = require('@tec/tickets/seating/service/iframe');
+const actionHandlersModule = require('@tec/tickets/seating/admin/action-handlers');
 
 jest.mock( '@tec/tickets/seating/utils', () => ({
 	redirectTo: jest.fn(),
@@ -24,7 +29,7 @@ describe('Maps Edit', () => {
 
 	describe('init', () => {
 		it('should initialize the iframe', async () => {
-			const dom = getTestDocument('layout-edit');
+			const dom = getTestDocument('map-edit');
 			const iframe = getIframeElement(dom);
 			expect(iframe).toBeInstanceOf(HTMLIFrameElement);
 			iframeModule.initServiceIframe = jest.fn();
@@ -34,4 +39,26 @@ describe('Maps Edit', () => {
 			expect(iframeModule.initServiceIframe).toHaveBeenCalledWith(iframe);
 		});
 	});
+
+	describe('handleResize', () => {
+		it('should be registered and resize the iframe', async () => {
+			const dom = getTestDocument('map-edit');
+			const iframe = getIframeElement(dom);
+			expect(iframe).toBeInstanceOf(HTMLIFrameElement);
+
+			iframeModule.initServiceIframe = jest.fn();
+			actionHandlersModule.handleReservationsDeleted = jest.fn();
+
+			await init(dom);
+
+			// Get the registered resize handler.
+			const resizeHandler = getHandlerForAction(INBOUND_SET_ELEMENT_HEIGHT);
+
+			resizeHandler({height: 100}, dom);
+			expect(iframe.style.height).toBe( 100 + 'px' );
+
+			resizeHandler({height: 200}, dom);
+			expect(iframe.style.height).toBe( 200 + 'px' );
+		});
+	})
 });
