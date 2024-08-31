25a0fe8db267374f8a6abc5b4b343a6c8c14a876
rafsuntaskin <rafsun.aiub@gmail.com>
2024-08-31 15:40:43 +0600

    add basic modal view

--- src/Tickets/Seating/app/blockEditor/settings/layoutSelect.js
+++ src/Tickets/Seating/app/blockEditor/settings/layoutSelect.js
@@ -2,7 +2,7 @@ import {LabeledItem, Select} from '@moderntribe/common/elements';
 import PropTypes from 'prop-types';
 import {Fragment, useState} from 'react';
 import {getLink, getLocalizedString} from '@tec/tickets/seating/utils';
-import {Modal} from '@wordpress/components';
+import {Modal, Dashicon} from '@wordpress/components';
 
 const getString = (key) => getLocalizedString(key, 'capacity-form');
 
@@ -17,6 +17,19 @@ const LayoutSelect = ({
 	}
 
 	const [activeLayout, setActiveLayout] = useState(getCurrentLayoutOption(currentLayout, layouts));
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const handleLayoutChange = (selectedLayout) => {
+		if ( selectedLayout === activeLayout ) {
+			return;
+		}
+
+		setIsModalOpen(true);
+	};
+
+	const closeModal = () => {
+		setIsModalOpen(false);
+	}
 
 	return (
 		<div className="tec-tickets-seating__settings_layout--wrapper">
@@ -25,8 +38,24 @@ const LayoutSelect = ({
 				id="tec-tickets-seating__settings_layout-select"
 				value={activeLayout}
 				options={layouts}
+				onChange={handleLayoutChange}
 			/>
 			<p>Changing the event's layout will impact all existing tickets and attendees.</p>
+
+			{ isModalOpen &&
+				<Modal
+					title="Confirm Seat Layout Change"
+					isDismissible={true}
+					onRequestClose={closeModal}
+					size="medium"
+				>
+					<div>
+						<Dashicon icon="warning" size={20} />
+						Caution
+					</div>
+
+				</Modal>
+			}
 		</div>
 	);
 }
