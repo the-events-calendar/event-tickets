3c3ca693c016b055af807e03db1c3120b53508a6
George Pattichis <georgepattichis@stellarwp.com>
2024-09-04 01:24:37 +0300

    Fix moved attendees stock issue

--- src/Tribe/Tickets_Handler.php
+++ src/Tribe/Tickets_Handler.php
@@ -880,8 +880,12 @@ class Tribe__Tickets__Tickets_Handler {
 
 		$totals = array_map( 'intval', $totals );
 
-		// Remove Pending from total
+		// Remove Pending from total.
 		$totals['sold'] -= $totals['pending'];
+
+		// Update Stock.
+		$totals['stock'] = tribe_tickets_get_capacity( $ticket->ID ) - $totals['sold'];
+		update_post_meta( $ticket->ID, '_stock', $totals['stock'] );
 
 		return $totals;
 	}
