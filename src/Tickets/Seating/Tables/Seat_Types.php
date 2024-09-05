968494c6001fd4eae2e94255f901a9911503abaf
rafsuntaskin <rafsun.aiub@gmail.com>
2024-09-06 05:21:57 +0600

    store with order of creation to get the default seatType first for layouts

--- src/Tickets/Seating/Tables/Seat_Types.php
+++ src/Tickets/Seating/Tables/Seat_Types.php
@@ -82,12 +82,14 @@ class Seat_Types extends Table {
 
 		return "
 			CREATE TABLE `{$table_name}` (
+				`order` int(11) NOT NULL AUTO_INCREMENT,
 				`id` varchar(36) NOT NULL,
 				`name` varchar(255) NOT NULL,
 				`map` varchar(36) NOT NULL,
 				`layout` varchar(36) NOT NULL,
 				`seats` int(11) NOT NULL DEFAULT '0',
-				PRIMARY KEY (`id`)
+				PRIMARY KEY (`order`),
+				UNIQUE KEY (`id`)
 			) {$charset_collate};
 		";
 	}
