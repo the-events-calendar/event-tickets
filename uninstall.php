4e3b0ff13b008207947cb8bd5379ad82132c1bf4
Jeremy Pry <jeremy.pry@gmail.com>
2024-09-17 19:24:29 -0500

    Update uninstall script loader

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,7 @@
 declare( strict_types=1 );
 
 use TEC\Tickets\Order_Modifiers\Controller as Order_Modifier_Controller;
+use Tribe__Tickets__Main as Main;
 
 // Ensure the uninstall script is not called directly.
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
@@ -21,6 +22,26 @@ if ( ! $should_uninstall ) {
 }
 
 // Get the custom table controller class instance.
+$main_instance = Main::instance();
+
+// Remove the actions that were added in the constructor.
+remove_action( 'plugins_loaded', [ $main_instance, 'should_autoload' ], -1 );
+remove_action( 'plugins_loaded', [ $main_instance, 'plugins_loaded' ], 0 );
+
+// Determine should_autoload().
+$main_instance->should_autoload();
+
+// Init the autoloader.
+$init_autoloader = Closure::bind(
+	function () {
+		$this->init_autoloading();
+	},
+	$main_instance,
+	$main_instance
+);
+$init_autoloader();
+
+// Get the instance of the order modifier controller.
 tribe_register_provider( Order_Modifier_Controller::class );
 $instance = tribe( Order_Modifier_Controller::class );
 
