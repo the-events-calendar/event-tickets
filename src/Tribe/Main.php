83ee8666a6037a6f656cd2c1efd91d7b494d25fb
bordoni <236579+bordoni@users.noreply.github.com>
2024-09-27 19:30:59 +0000

    Version bump for release/T24.dev.005
    This is an automated PR created by bordoni.
    Generated by: https://github.com/the-events-calendar/event-tickets/actions/runs/11076610939

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -15,7 +15,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin.
 	 */
-	const VERSION = '5.13.4';
+	const VERSION = '5.13.5';
 
 	/**
 	 * Used to store the version history.
