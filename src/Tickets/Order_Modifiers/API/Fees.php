b4983e7cc69684bbb3b92ae7e5369de77b00c7eb
Jeremy Pry <jeremy.pry@gmail.com>
2024-10-03 18:21:14 -0500

    Add fees response, and include fees in regular response

--- src/Tickets/Order_Modifiers/API/Fees.php
+++ src/Tickets/Order_Modifiers/API/Fees.php
@@ -9,12 +9,15 @@ declare( strict_types=1 );
 
 namespace TEC\Tickets\Order_Modifiers\API;
 
+use TEC\Tickets\Order_Modifiers\Modifiers\Fee;
+use TEC\Tickets\Order_Modifiers\Modifiers\Modifier_Manager as Manager;
 use TEC\Tickets\Order_Modifiers\Repositories\Order_Modifier_Relationship as Relationships;
 use TEC\Tickets\Order_Modifiers\Repositories\Order_Modifiers as Modifiers;
 use TEC\Tickets\Order_Modifiers\Traits\Fee_Types;
 use TEC\Tickets\Registerable;
 use WP_REST_Request as Request;
 use WP_REST_Response as Response;
+use WP_REST_Server as Server;
 
 /**
  * Class Fees
@@ -24,6 +27,13 @@ use WP_REST_Response as Response;
 class Fees implements Registerable {
 
 	use Fee_Types;
+
+	/**
+	 * The namespace for the API.
+	 *
+	 * @var string
+	 */
+	protected string $namespace = 'tribe/tickets/v1';
 
 	/**
 	 * The repository for interacting with the order modifiers relationships.
@@ -51,11 +61,13 @@ class Fees implements Registerable {
 	 * @return void The method does not return any value.
 	 */
 	public function register(): void {
-		add_action(
-			'rest_api_init',
-			function () {
-				$this->register_routes();
-			}
+		add_action( 'rest_api_init', fn() => $this->register_routes() );
+
+		add_filter(
+			'tribe_rest_single_ticket_data',
+			fn( array $data, Request $request ) => $this->add_fees_to_ticket_data( $data, $request ),
+			20,
+			2
 		);
 	}
 
@@ -67,13 +79,23 @@ class Fees implements Registerable {
 	 * @return void
 	 */
 	protected function register_routes() {
-		tribe_register_rest_route(
-			'event-tickets/v1',
+		register_rest_route(
+			$this->namespace,
 			'/fees',
 			[
-				'methods'             => 'GET',
-				'callback'            => fn( Request $request ) => $this->get_fees( $request ),
-				'permission_callback' => fn() => current_user_can( 'manage_options' ),
+				'methods'             => Server::READABLE,
+				'callback'            => fn( Request $request ) => $this->get_fees_response( $request ),
+				'permission_callback' => $this->get_permission_callback(),
+			]
+		);
+
+		register_rest_route(
+			$this->namespace,
+			'/tickets/(?P<id>\\d+)/fees',
+			[
+				'methods'             => Server::READABLE,
+				'callback'            => fn( Request $request ) => $this->get_fees_for_ticket_response( $request ),
+				'permission_callback' => $this->get_permission_callback(),
 			]
 		);
 	}
@@ -89,7 +111,7 @@ class Fees implements Registerable {
 	 *
 	 * @return Response
 	 */
-	protected function get_fees( Request $request ): Response {
+	protected function get_fees_response( Request $request ): Response {
 		$all = $this->get_all_fees();
 
 		return rest_ensure_response(
@@ -99,5 +121,72 @@ class Fees implements Registerable {
 				'selectable' => $this->get_selectable_fees( $all ),
 			]
 		);
+	}
+
+	/**
+	 * Get the fees for a specific ticket.
+	 *
+	 * @since TBD
+	 *
+	 * @param Request $request The request object.
+	 *
+	 * @return Response The response object.
+	 */
+	protected function get_fees_for_ticket_response( Request $request ): Response {
+		$ticket_id = (int) $request->get_param( 'id' );
+
+		return rest_ensure_response( $this->get_fees_for_ticket( $ticket_id ) );
+	}
+
+	/**
+	 * Get the fees for a specific ticket.
+	 *
+	 * @since TBD
+	 *
+	 * @param int $ticket_id The ticket ID.
+	 *
+	 * @return array The fees for the ticket.
+	 */
+	protected function get_fees_for_ticket( int $ticket_id ) {
+		$ticket_fees = $this->relationships->find_by_post_id( $ticket_id );
+		$ticket_fees ??= [];
+
+		$automatic_fees = $this->get_automatic_fees( $this->get_all_fees() );
+
+		return [
+			'fees'      => $ticket_fees,
+			'automatic' => $automatic_fees,
+		];
+	}
+
+	/**
+	 * Get the permission callback.
+	 *
+	 * @since TBD
+	 *
+	 * @return callable The permission callback.
+	 */
+	protected function get_permission_callback() {
+		return static function () {
+			return current_user_can( 'manage_options' );
+		};
+	}
+
+	/**
+	 * Add fees to the ticket data.
+	 *
+	 * @since TBD
+	 *
+	 * @param array   $data    The ticket data.
+	 * @param Request $request The request object.
+	 *
+	 * @return array The ticket data with fees.
+	 */
+	protected function add_fees_to_ticket_data( array $data, Request $request ): array {
+		$ticket_id    = (int) $request->get_param( 'id' );
+		$ticket_fees  = $this->get_fees_for_ticket( $ticket_id );
+		$data['fees'] = $ticket_fees;
+
+		return $data;
 	}
 }
