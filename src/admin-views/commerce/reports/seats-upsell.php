4d3713484a05da3046bfca7e2559cbeabafe0197
Dimitrios Pantazis <dpanta94@gmail.com>
2024-10-04 23:20:32 +0300

    Update upsell's markup

--- src/admin-views/commerce/reports/seats-upsell.php
+++ src/admin-views/commerce/reports/seats-upsell.php
@@ -11,31 +11,33 @@ use Tribe__Tickets__Main as Tickets_Main;
 
 $main = Tickets_Main::instance();
 ?>
-<div class="tec-tickets-seating-upsell">
-	<div class="tec-tickets-seating-upsell__content">
-		<div class="tec-tickets-seating-upsell__title">
-			<img
-				src="<?php echo esc_url( tribe_resource_url( 'icons/seating-icon.svg', false, null, $main ) ); ?>"
-				alt="<?php esc_attr_e( 'Seating Icon', 'event-tickets' ); ?>"
-			>
-			<h3>
-				<?php esc_html_e( 'Seating', 'event-tickets' ); ?>
-			</h3>
-		</div>
-		<p>
-			<?php esc_html_e( 'Maximize event revenue and improve the attendee experience by adding assigned seats with this innovative add-on.', 'event-tickets' ); ?>
-		</p>
-		<div class="tec-tickets-seating-upsell__btn">
-			<a href="https://evnt.is/get-seating" target="_blank" rel="noopener noreferrer">
+
+<div class="tec-admin__upsell-container">
+	<div class="tec-admin__upsell-banner">
+		<div class="tec-admin__upsell-banner-content">
+			<div class="tec-admin__upsell-banner-header">
+				<img
+					src="<?php echo esc_url( tribe_resource_url( 'icons/seating-icon.svg', false, null, $main ) ); ?>"
+					class="tec-admin__upsell-banner-logo"
+					role="presentation"
+					alt="<?php esc_attr_e( 'Seating Icon', 'event-tickets' ); ?>"
+				/>
+				<h3 class="tec-admin__upsell-banner-title">
+					<?php esc_html_e( 'Seating', 'event-tickets' ); ?>
+				</h3>
+			</div>
+			<p>
+				<?php esc_html_e( 'Maximize event revenue and improve the attendee experience by adding assigned seats with this innovative add-on.', 'event-tickets' ); ?>
+			</p>
+			<a href="https://evnt.is/get-seating" class="tec-admin__upsell-banner-btn" target="_blank" rel="noopener noreferrer">
 				<?php esc_html_e( 'Check out Seating for Event Tickets', 'event-tickets' ); ?>
 			</a>
 		</div>
-	</div>
-
-	<div class="tec-tickets-seating-upsell__icon">
 		<img
+			class="tec-admin__upsell-banner-image"
 			src="<?php echo esc_url( tribe_resource_url( 'icons/seating-banner.png', false, null, $main ) ); ?>"
 			alt="<?php esc_attr_e( 'Seating Banner Icon', 'event-tickets' ); ?>"
-		>
+		/>
 	</div>
 </div>
+
