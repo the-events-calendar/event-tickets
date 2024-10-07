27e009875a3e3b9237b147ebcb941718fa327b32
rafsuntaskin <rafsun.aiub@gmail.com>
2024-10-06 20:54:05 -0700

    add upsell component

---
+++ src/Tickets/Seating/app/blockEditor/settings/upSell.js
@@ -0,0 +1,13 @@
+const UpSell = () => {
+
+	return (
+		<div className="tec-tickets-seating__settings_layout--wrapper">
+			<div className={"tec-tickets-seating__settings_layout_upsell--header"}>
+				<span className="tec-tickets-seating__settings_layout--title">
+					{ _x( 'Seat Layout', 'Seat layout upsell title', 'event-tickets' ) }
+				</span>
+			</div>
+		</div>
+	);
+}
+export default UpSell;
