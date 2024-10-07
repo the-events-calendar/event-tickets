9c508e6c2babdd17e4833882157c69bfde02eb3b
rafsuntaskin <rafsun.aiub@gmail.com>
2024-10-06 22:22:31 -0700

    updated upsell component

--- src/Tickets/Seating/app/blockEditor/settings/upSell.js
+++ src/Tickets/Seating/app/blockEditor/settings/upSell.js
@@ -1,4 +1,9 @@
+import { ECP as PlusIcon } from '@moderntribe/tickets/icons';
+import { _x } from '@wordpress/i18n';
 const UpSell = () => {
+	const anchorStyle = {
+		color: 'var(--tec-color-link-accent)',
+	};
 
 	return (
 		<div className="tec-tickets-seating__settings_layout--wrapper">
@@ -6,7 +11,32 @@ const UpSell = () => {
 				<span className="tec-tickets-seating__settings_layout--title">
 					{ _x( 'Seat Layout', 'Seat layout upsell title', 'event-tickets' ) }
 				</span>
+				<PlusIcon/>
 			</div>
+
+			<span className={"tec-tickets-seating__settings_layout--description"}>
+				{
+					_x(
+						'Allow purchasers to select seats with',
+						'Seat layout upsell description start',
+						'event-tickets'
+					)
+				}{ ' ' }
+				<a
+					href="https://evnt.is/add-seating"
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					{ _x( 'Seating', 'Seat layout upsell link label', 'event-tickets' ) }
+				</a>{' '}
+				{
+					_x(
+						'for Event Tickets.',
+						'Seat layout upsell description end',
+						'event-tickets'
+					)
+				}
+			</span>
 		</div>
 	);
 }
