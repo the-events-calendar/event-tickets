0d5b8058e6b4a43eb562677ed54c44eff96d7587
Luca Tumedei <luca@theaveragedev.com>
2024-10-07 16:18:58 +0200

    fix(Seating) correctly map over reservation IDs on interrupt

--- src/Tickets/Seating/Frontend/Timer.php
+++ src/Tickets/Seating/Frontend/Timer.php
@@ -533,7 +533,10 @@ class Timer extends Controller_Contract {
 
 		// Cancel the reservations for the post ID and token, remove the session associated with the token from the database.
 		if ( ! (
-			$this->reservations->cancel( $post_id, $this->sessions->get_reservations_for_token( $token ) )
+			$this->reservations->cancel(
+				$post_id,
+				array_column( $this->sessions->get_reservations_for_token( $token ), 'reservation_id' )
+			)
 			&& $this->sessions->delete_token_session( $token )
 		) ) {
 			wp_send_json_error(
