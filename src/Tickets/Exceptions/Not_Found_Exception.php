4c6a5dbce8648264ab5e127989f2386db494ee0a
Jeremy Pry <jeremy.pry@gmail.com>
2024-10-07 15:08:27 -0500

    Create a Not found exception class

---
+++ src/Tickets/Exceptions/Not_Found_Exception.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Exception when something is not found
+ *
+ * @since TBD
+ */
+
+declare( strict_types=1 );
+
+namespace TEC\Tickets\Exceptions;
+
+use Exception;
+
+/**
+ * Class Not_Found_Exception
+ *
+ * @since TBD
+ */
+class Not_Found_Exception extends Exception {}
