91a2a1bac614c10d8df7d2d4bca5bf39d2eda44c
Brian Krane <redscar@gmail.com>
2024-10-16 16:04:56 -0400

    Moved snapshots to Coupon & Fee classes.

--- tests/integration/TEC/Tickets/Order_Modifiers/Create_Order_Modifiers_Abstract.php
+++ tests/integration/TEC/Tickets/Order_Modifiers/Create_Order_Modifiers_Abstract.php
@@ -3,6 +3,7 @@
 namespace TEC\Tickets\Tests\Integration\Order_Modifiers;
 
 use Codeception\TestCase\WPTestCase;
+use tad\Codeception\SnapshotAssertions\SnapshotAssertions;
 use TEC\Tickets\Order_Modifiers\Modifier_Admin_Handler;
 use TEC\Tickets\Order_Modifiers\Modifiers\Modifier_Abstract;
 use TEC\Tickets\Order_Modifiers\Table_Views\Order_Modifier_Table;
@@ -12,6 +13,7 @@ use Tribe\Tickets\Test\Traits\Order_Modifiers;
 abstract class Create_Order_Modifiers_Abstract extends WPTestCase {
 	use Order_Modifiers;
 	use With_Uopz;
+	use SnapshotAssertions;
 
 	/**
 	 * The type of order modifier being tested ($this->modifier_type).
@@ -228,9 +230,6 @@ abstract class Create_Order_Modifiers_Abstract extends WPTestCase {
 		$this->assertEquals( $update_data['order_modifier_display_name'], $retrieved_modifier->display_name ); // Name should be updated.
 	}
 
-	/**
-	 * @test
-	 */
 	public function does_edit_form_display_properly_with_no_data() {
 		$modifier_admin_handler = new Modifier_Admin_Handler();
 		$_POST                  = [
@@ -242,13 +241,9 @@ abstract class Create_Order_Modifiers_Abstract extends WPTestCase {
 		$modifier_admin_handler->render_tec_order_modifiers_page();
 		$test = ob_get_contents();
 		ob_end_flush();
-		$this->assertMatchesHtmlSnapshot( $test );
+		return $test;
 	}
 
-	/**
-	 * @test
-	 * @dataProvider modifier_edit_form_data_provider
-	 */
 	public function does_edit_form_display_properly_with_data( array $insert_data, array $post_data ) {
 		// Step 1: Insert a new modifier.
 		$inserted_modifier = $this->upsert_order_modifier_for_test( $insert_data );
@@ -268,8 +263,7 @@ abstract class Create_Order_Modifiers_Abstract extends WPTestCase {
 		$output = ob_get_contents();
 		ob_end_clean();
 
-		// Assert that the form renders correctly by comparing to the snapshot.
-		$this->assertMatchesHtmlSnapshot( $output );
+		return $output;
 	}
 
 	/**
@@ -367,5 +361,32 @@ abstract class Create_Order_Modifiers_Abstract extends WPTestCase {
 				'edit'     => 1,
 			],
 		];
+	}
+
+
+	protected function get_table_display() {
+		$this->clear_all_modifiers( $this->modifier_type );
+		for ( $i = 0; $i < 20; $i++ ) {
+			// Step 1: Insert a new modifier.
+			$insert_data = [
+				'modifier'                    => $this->modifier_type,
+				'order_modifier_amount'       => (float) $i,
+				'order_modifier_sub_type'     => $i % 2 ? 'percent' : 'flat',
+				'order_modifier_slug'         => sprintf( 'test_%1$s_%2$02d', $this->modifier_type, $i ),
+				'order_modifier_display_name' => "Test {$this->modifier_type} Insert",
+			];
+			$this->upsert_order_modifier_for_test( $insert_data );
+		}
+
+		$modifier_admin_handler = new Modifier_Admin_Handler();
+		$_POST = [
+			'modifier' => $this->modifier_type,
+		];
+		ob_start();
+		$modifier_admin_handler->render_tec_order_modifiers_page();
+		$test = ob_get_contents();
+		ob_end_flush();
+
+		return $test;
 	}
 }
--- tests/integration/TEC/Tickets/Order_Modifiers/Fees/Create_Fees_Modifiers_Test.php
+++ tests/integration/TEC/Tickets/Order_Modifiers/Fees/Create_Fees_Modifiers_Test.php
@@ -2,13 +2,9 @@
 
 namespace TEC\Tickets\Order_Modifiers\Fees;
 
-use tad\Codeception\SnapshotAssertions\SnapshotAssertions;
-use TEC\Tickets\Order_Modifiers\Modifier_Admin_Handler;
 use TEC\Tickets\Tests\Integration\Order_Modifiers\Create_Order_Modifiers_Abstract;
 
 class Create_Fees_Modifiers_Test extends Create_Order_Modifiers_Abstract {
-
-	use SnapshotAssertions;
 
 	/**
 	 * The type of order modifier being tested (fee).
@@ -19,29 +15,30 @@ class Create_Fees_Modifiers_Test extends Create_Order_Modifiers_Abstract {
 
 	/**
 	 * @test
+	 *
+	 * @return void
+	 * @throws \Gajus\Dindent\Exception\InvalidArgumentException
 	 */
-	public function does_table_display_properly() {
-		$this->clear_all_modifiers( $this->modifier_type );
-		for ( $i = 0; $i < 20; $i++ ) {
-			// Step 1: Insert a new modifier.
-			$insert_data = [
-				'modifier'                    => $this->modifier_type,
-				'order_modifier_amount'       => (float) $i,
-				'order_modifier_sub_type'     => $i % 2 ? 'percent' : 'flat',
-				'order_modifier_slug'         => "test_{$this->modifier_type}_{$i}",
-				'order_modifier_display_name' => "Test {$this->modifier_type} Insert",
-			];
-			$this->upsert_order_modifier_for_test( $insert_data );
-		}
+	public function does_table_render_correctly() {
+		$this->assertMatchesHtmlSnapshot( $this->get_table_display() );
+	}
 
-		$modifier_admin_handler = new Modifier_Admin_Handler();
-		$_POST                  = [
-			'modifier' => $this->modifier_type,
-		];
-		ob_start();
-		$modifier_admin_handler->render_tec_order_modifiers_page();
-		$test = ob_get_contents();
-		ob_end_flush();
-		$this->assertMatchesHtmlSnapshot( $test );
+	/**
+	 * @test
+	 * @dataProvider modifier_edit_form_data_provider
+	 */
+	public function does_edit_screen_render_correctly( array $insert_data, array $post_data ) {
+		$snapshot = $this->does_edit_form_display_properly_with_data( $insert_data, $post_data );
+		$this->assertMatchesHtmlSnapshot( $snapshot );
+	}
+
+	/**
+	 * @test
+	 * @return void
+	 * @throws \Gajus\Dindent\Exception\InvalidArgumentException
+	 */
+	public function does_edit_screen_render_with_no_data() {
+		$snapshot = $this->does_edit_form_display_properly_with_no_data();
+		$this->assertMatchesHtmlSnapshot( $snapshot );
 	}
 }
