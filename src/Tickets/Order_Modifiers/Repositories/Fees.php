fe03c4c36b96865129ecc33db86459d5e0ae1f08
Jeremy Pry <jeremy.pry@gmail.com>
2024-10-17 15:48:30 -0500

    Add method to get all automatically-applied fees

--- src/Tickets/Order_Modifiers/Repositories/Fees.php
+++ src/Tickets/Order_Modifiers/Repositories/Fees.php
@@ -9,6 +9,8 @@ declare( strict_types=1 );
 
 namespace TEC\Tickets\Order_Modifiers\Repositories;
 
+use stdClass;
+
 /**
  * Class Fees
  *
@@ -23,5 +25,21 @@ class Fees extends Order_Modifiers {
 	 */
 	public function __construct() {
 		parent::__construct( 'fee' );
+	}
+
+	/**
+	 * Get all automatic fees.
+	 *
+	 * @since TBD
+	 *
+	 * @return stdClass[] The array of fee objects from the database.
+	 */
+	public function get_all_automatic_fees() {
+		return $this->find_by_modifier_type_and_meta(
+			'fee_applied_to',
+			[ 'all' ],
+			'fee_applied_to',
+			'all'
+		);
 	}
 }
