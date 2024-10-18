ff89fe7b072e118d1b067e409dda817be47fa2f1
Jeremy Pry <jeremy.pry@gmail.com>
2024-10-18 16:52:19 -0500

    Add static from_number() method to float class

--- src/Tickets/Order_Modifiers/Values/Float_Value.php
+++ src/Tickets/Order_Modifiers/Values/Float_Value.php
@@ -25,8 +25,31 @@ class Float_Value extends Base_Value {
 	 *
 	 * @return float
 	 */
-	public function get() {
+	public function get(): float {
 		return $this->value;
+	}
+
+	/**
+	 * Create a new instance from a numeric value.
+	 *
+	 * @param float|int|string $value The value to store. Can be a float, int, or numeric string.
+	 *
+	 * @return static
+	 */
+	public static function from_number( $value ): Float_Value {
+		if ( ! is_numeric( $value ) ) {
+			throw new InvalidArgumentException( 'Value must be a number.' );
+		}
+
+		if ( 'NAN' === (string) $value ) {
+			throw new InvalidArgumentException( 'NAN is by definition not a number.' );
+		}
+
+		if ( 'INF' === (string) $value ) {
+			throw new InvalidArgumentException( 'Infinity is too big for us to work with.' );
+		}
+
+		return new static( (float) $value );
 	}
 
 	/**
