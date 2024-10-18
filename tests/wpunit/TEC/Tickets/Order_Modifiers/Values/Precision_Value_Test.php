7c46c501863acd852c54a3ec3e57b3977d17ae5c
Jeremy Pry <jeremy.pry@gmail.com>
2024-10-18 14:29:28 -0500

    Add unit test for Precision_Value class

---
+++ tests/wpunit/TEC/Tickets/Order_Modifiers/Values/Precision_Value_Test.php
@@ -0,0 +1,60 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace TEC\Tickets\Tests\Unit\Order_Modifiers\Values;
+
+use Codeception\TestCase\WPTestCase;
+use InvalidArgumentException;
+use stdClass;
+use TEC\Tickets\Order_Modifiers\Values\Positive_Integer_Value;
+use TEC\Tickets\Order_Modifiers\Values\Precision_Value;
+
+class Precision_Value_Test extends WPTestCase {
+
+	/**
+	 * Test get.
+	 *
+	 * @dataProvider get_data_provider
+	 * @test
+	 */
+	public function value_is_returned_correctly( $raw_value, $precision, $expected ) {
+		$value = new Precision_Value( $raw_value, new Positive_Integer_Value( $precision ) );
+		$this->assertSame( $expected, $value->get() );
+	}
+
+	/**
+	 * Test validate.
+	 *
+	 * @dataProvider validate_data_provider
+	 * @test
+	 */
+	public function validation_fails_for_invalid_types( $raw_value ) {
+		$this->expectException( InvalidArgumentException::class );
+		$this->expectExceptionMessage( 'Value must be numeric.' );
+		new Precision_Value( $raw_value );
+	}
+
+	protected function get_data_provider() {
+		// raw value, precision, expected value
+		return [
+			[ 1.234, 2, 1.23 ],
+			[ 1.236, 2, 1.24 ],
+			[ pi(), 5, 3.14159 ],
+			[ 1, 2, 1.00 ],
+			[ '1.234', 2, 1.23 ],
+			[ 100, 0, (float) 100 ],
+		];
+	}
+
+	protected function validate_data_provider() {
+		return [
+			[ 'foo' ],
+			[ [] ],
+			[ new stdClass() ],
+			[ 'abc123' ],
+			[ null ],
+			[ NAN ],
+		];
+	}
+}
