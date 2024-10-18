8b89228b71b4ed1b99d90a213ce255a2faec5da9
Jeremy Pry <jeremy.pry@gmail.com>
2024-10-18 17:52:16 -0500

    Update the table column to decimal

--- src/Tickets/Order_Modifiers/Custom_Tables/Order_Modifiers.php
+++ src/Tickets/Order_Modifiers/Custom_Tables/Order_Modifiers.php
@@ -69,7 +69,7 @@ class Order_Modifiers extends Abstract_Custom_Table {
 				`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
 				`modifier_type` VARCHAR(255) NOT NULL,
 				`sub_type` VARCHAR(255) NOT NULL,
-				`raw_amount` INT NOT NULL,
+				`raw_amount` DECIMAL(18,6) NOT NULL,
 				`slug` VARCHAR(255) NOT NULL,
 				`display_name` VARCHAR(255) NOT NULL,
 				`status` VARCHAR(20) NOT NULL DEFAULT 'draft',
