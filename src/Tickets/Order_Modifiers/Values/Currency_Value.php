3bf1e3fd75c370e347394428631ce5e10e0fab17
Jeremy Pry <jeremy.pry@gmail.com>
2024-10-18 13:59:11 -0500

    Create the currency value class

---
+++ src/Tickets/Order_Modifiers/Values/Currency_Value.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * Currency Value
+ *
+ * @since TBD
+ */
+
+declare( strict_types=1 );
+
+namespace TEC\Tickets\Order_Modifiers\Values;
+
+/**
+ * Class Currency_Value
+ *
+ * @since TBD
+ */
+class Currency_Value implements Value_Interface {
+
+	/**
+	 * The value.
+	 *
+	 * @var Precision_Value
+	 */
+	protected Precision_Value $value;
+
+	/**
+	 * The currency symbol.
+	 *
+	 * @var string
+	 */
+	protected $currency_symbol;
+
+	/**
+	 * The currency symbol position.
+	 *
+	 * @var string
+	 */
+	protected $currency_symbol_position;
+
+	/**
+	 * The thousands separator.
+	 *
+	 * @var string
+	 */
+	protected $thousands_separator;
+
+	/**
+	 * The decimal separator.
+	 *
+	 * @var string
+	 */
+	protected $decimal_separator;
+
+	/**
+	 * Default values.
+	 *
+	 * @var array
+	 */
+	protected static array $defaults = [
+		'currency_symbol'          => '$',
+		'thousands_separator'      => ',',
+		'decimal_separator'        => '.',
+		'currency_symbol_position' => 'before',
+	];
+
+	/**
+	 * Currency_Value constructor.
+	 *
+	 * @since TBD
+	 *
+	 * @param Precision_Value $value                    The value to store.
+	 * @param string          $currency_symbol          The currency symbol.
+	 * @param string          $thousands_separator      The thousands separator.
+	 * @param string          $decimal_separator        The decimal separator.
+	 * @param string          $currency_symbol_position The currency symbol position.
+	 */
+	public function __construct(
+		Precision_Value $value,
+		string $currency_symbol = '$',
+		string $thousands_separator = ',',
+		string $decimal_separator = '.',
+		string $currency_symbol_position = 'before'
+	) {
+		$this->value                    = $value;
+		$this->currency_symbol          = $currency_symbol;
+		$this->thousands_separator      = $thousands_separator;
+		$this->decimal_separator        = $decimal_separator;
+		$this->currency_symbol_position = $currency_symbol_position;
+	}
+
+	/**
+	 * Get the formatted value.
+	 *
+	 * @since TBD
+	 *
+	 * @return string The value.
+	 */
+	public function get(): string {
+		$formatted = number_format(
+			$this->value->get(),
+			$this->value->get_precision()->get(),
+			$this->decimal_separator,
+			$this->thousands_separator
+		);
+
+		switch ( $this->currency_symbol_position ) {
+			case 'after':
+				return "{$formatted}{$this->currency_symbol}";
+
+			case 'before':
+			default:
+				return "{$this->currency_symbol}{$formatted}";
+		}
+	}
+
+	/**
+	 * Get the raw value.
+	 *
+	 * This returns a clone of the value to prevent mutation.
+	 *
+	 * @since TBD
+	 *
+	 * @return Precision_Value The raw value.
+	 */
+	public function get_raw_value(): Precision_Value {
+		return clone $this->value;
+	}
+
+	/**
+	 * The __toString method allows a class to decide how it will react when it is converted to a string.
+	 *
+	 * @since TBD
+	 *
+	 * @return string
+	 */
+	public function __toString(): string {
+		return $this->get();
+	}
+
+	/**
+	 * Create a new instance of the class.
+	 *
+	 * @since TBD
+	 *
+	 * @param Precision_Value $value The value to store.
+	 *
+	 * @return Currency_Value
+	 */
+	public static function create( Precision_Value $value ): self {
+		return new self(
+			$value,
+			self::$defaults['currency_symbol'],
+			self::$defaults['thousands_separator'],
+			self::$defaults['decimal_separator'],
+			self::$defaults['currency_symbol_position']
+		);
+	}
+
+	/**
+	 * Set the default values for the class.
+	 *
+	 * Use this to allow for setting default values for all instances of this class
+	 * that are created with the create() method.
+	 *
+	 * @since TBD
+	 *
+	 * @param ?string $currency_symbol
+	 * @param ?string $thousands_separator
+	 * @param ?string $decimal_separator
+	 * @param ?string $currency_symbol_position
+	 *
+	 * @return void
+	 */
+	public static function set_defaults(
+		?string $currency_symbol = null,
+		?string $thousands_separator = null,
+		?string $decimal_separator = null,
+		?string $currency_symbol_position = null
+	) {
+		self::$defaults = [
+			'currency_symbol'          => $currency_symbol ?? self::$defaults['currency_symbol'],
+			'thousands_separator'      => $thousands_separator ?? self::$defaults['thousands_separator'],
+			'decimal_separator'        => $decimal_separator ?? self::$defaults['decimal_separator'],
+			'currency_symbol_position' => $currency_symbol_position ?? self::$defaults['currency_symbol_position'],
+		];
+	}
+}
