9821885a79841987200fe8c16d48ba3c6398d70e
Jeremy Pry <jeremy.pry@gmail.com>
2024-10-22 19:16:52 -0500

    Run footer entry points, and update docblock

--- src/views/v2/commerce/checkout/cart/footer.php
+++ src/views/v2/commerce/checkout/cart/footer.php
@@ -13,30 +13,36 @@
  *
  * @version 5.1.9
  *
- * @var \Tribe__Template $this                  [Global] Template object.
- * @var Module           $provider              [Global] The tickets provider instance.
- * @var string           $provider_id           [Global] The tickets provider class name.
- * @var array[]          $items                 [Global] List of Items on the cart to be checked out.
- * @var bool             $must_login            [Global] Whether login is required to buy tickets or not.
- * @var string           $login_url             [Global] The site's login URL.
- * @var string           $registration_url      [Global] The site's registration URL.
- * @var bool             $is_tec_active         [Global] Whether `The Events Calendar` is active or not.
- * @var array[]          $gateways              [Global] An array with the gateways.
- * @var int              $gateways_active       [Global] The number of active gateways.
+ * @var Tribe__Template $this             [Global] Template object.
+ * @var Module          $provider         [Global] The tickets provider instance.
+ * @var string          $provider_id      [Global] The tickets provider class name.
+ * @var array[]         $items            [Global] List of Items on the cart to be checked out.
+ * @var bool            $must_login       [Global] Whether login is required to buy tickets or not.
+ * @var string          $login_url        [Global] The site's login URL.
+ * @var string          $registration_url [Global] The site's registration URL.
+ * @var bool            $is_tec_active    [Global] Whether `The Events Calendar` is active or not.
+ * @var array[]         $gateways         [Global] An array with the gateways.
+ * @var int             $gateways_active  [Global] The number of active gateways.
  */
+
+use TEC\Tickets\Commerce\Module;
 
 $classes = [
 	'tribe-tickets__commerce-checkout-cart-footer',
 	'tribe-common-b1',
 ];
+
 ?>
 
 <footer <?php tribe_classes( $classes ); ?>>
 	<?php
-	do_action( 'tec_tickets_commerce_checkout_cart_before_footer_quantity', $post, $items, $this );
+
+	// Run the entry point hooks.
+	$this->do_entry_point( 'tec_tickets_commerce_checkout_cart_before_footer_quantity' );
+
+	// Do the footer templates.
+	$this->template( 'checkout/cart/footer/quantity' );
+	$this->template( 'checkout/cart/footer/total' );
+
 	?>
-	<?php $this->template( 'checkout/cart/footer/quantity' ); ?>
-
-	<?php $this->template( 'checkout/cart/footer/total' ); ?>
-
 </footer>
