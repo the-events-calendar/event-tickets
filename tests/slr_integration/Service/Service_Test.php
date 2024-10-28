5f5a0c55ac0544897f7c859788074d015d49b379
Luca Tumedei <luca@theaveragedev.com>
2024-10-28 17:41:32 +0100

    test(slr_integration) update snapshots

--- tests/slr_integration/Service/Service_Test.php
+++ tests/slr_integration/Service/Service_Test.php
@@ -13,13 +13,13 @@ class Service_Test extends WPTestCase {
 	public function it_should_get_frontend_url() {
 		$service = tribe( Service::class );
 
-		$this->assertEquals( 'https://evnt.is', $service->get_frontend_url() );
-		$this->assertEquals( 'https://evnt.is/test/path', $service->get_frontend_url( '/test/path/' ) );
+		$this->assertEquals( 'https://seating.theeventscalendar.com', $service->get_frontend_url() );
+		$this->assertEquals( 'https://seating.theeventscalendar.com/test/path', $service->get_frontend_url( '/test/path/' ) );
 		$_GET['isNew'] = 0;
-		$this->assertEquals( 'https://evnt.is/test/path/2', $service->get_frontend_url( '/test/path/2' ) );
+		$this->assertEquals( 'https://seating.theeventscalendar.com/test/path/2', $service->get_frontend_url( '/test/path/2' ) );
 		$_GET['isNew'] = 'tree';
-		$this->assertEquals( 'https://evnt.is/test/path/2', $service->get_frontend_url( '/test/path/2' ) );
+		$this->assertEquals( 'https://seating.theeventscalendar.com/test/path/2', $service->get_frontend_url( '/test/path/2' ) );
 		$_GET['isNew'] = '1';
-		$this->assertEquals( 'https://evnt.is/test/path/2?isNew=1', $service->get_frontend_url( '/test/path/2' ) );
+		$this->assertEquals( 'https://seating.theeventscalendar.com/test/path/2?isNew=1', $service->get_frontend_url( '/test/path/2' ) );
 	}
 }
