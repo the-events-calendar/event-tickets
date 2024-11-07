55978799d2c7f351b645187191c122617e6da420
Jeremy Pry <jeremy.pry@gmail.com>
2024-11-06 19:51:55 -0600

    Update Fee api

--- src/Tickets/Commerce/Order_Modifiers/API/Fees.php
+++ src/Tickets/Commerce/Order_Modifiers/API/Fees.php
@@ -9,11 +9,13 @@ declare( strict_types=1 );
 
 namespace TEC\Tickets\Commerce\Order_Modifiers\API;
 
+use Exception;
 use TEC\Tickets\Commerce\Order_Modifiers\Modifiers\Fee;
 use TEC\Tickets\Commerce\Order_Modifiers\Modifiers\Modifier_Manager as Manager;
 use TEC\Tickets\Commerce\Order_Modifiers\Repositories\Order_Modifier_Relationship as Relationships;
 use TEC\Tickets\Commerce\Order_Modifiers\Repositories\Order_Modifiers as Modifiers;
 use TEC\Tickets\Commerce\Order_Modifiers\Traits\Fee_Types;
+use WP_Error;
 use WP_REST_Request as Request;
 use WP_REST_Response as Response;
 use WP_REST_Server as Server;
@@ -83,6 +85,19 @@ class Fees extends Base_API {
 			20,
 			2
 		);
+
+		add_filter(
+			'tec_tickets_commerce_rest_ticket_archive_data',
+			function ( array $tickets, Request $request ) {
+				foreach ( $tickets as $key => $ticket ) {
+					$tickets[ $key ] = $this->add_fees_to_ticket_data( $ticket, $request );
+				}
+
+				return $tickets;
+			},
+			10,
+			2
+		);
 	}
 
 	/**
@@ -120,7 +135,16 @@ class Fees extends Base_API {
 				'methods'             => Server::EDITABLE,
 				'callback'            => fn( Request $request ) => $this->update_fees_for_ticket_response( $request ),
 				'permission_callback' => $this->get_permission_callback(),
-				'args'                => [], // @todo Add args.
+				'args'                => [
+					'selected_fees' => [
+						'description' => __( 'The selected fees for the ticket.', 'event-tickets' ),
+						'type'        => 'array',
+						'required'    => true,
+						'items'       => [
+							'type' => 'integer',
+						],
+					],
+				],
 			]
 		);
 	}
@@ -137,15 +161,25 @@ class Fees extends Base_API {
 	 * @return Response
 	 */
 	protected function get_fees_response( Request $request ): Response {
-		$all = $this->get_all_fees();
+		try {
+			$all = $this->get_all_fees();
 
-		return rest_ensure_response(
-			[
-				'fees'       => $all,
-				'automatic'  => $this->get_automatic_fees( $all ),
-				'selectable' => $this->get_selectable_fees( $all ),
-			]
-		);
+			return rest_ensure_response(
+				[
+					'all_fees'        => $all,
+					'automatic_fees'  => $this->get_automatic_fees( $all ),
+					'selectable_fees' => $this->get_selectable_fees( $all ),
+				]
+			);
+		} catch ( Exception $e ) {
+			return $this->convert_error_to_response(
+				new WP_Error(
+					'tickets_commerce_get_fees_error',
+					$e->getMessage(),
+					[ 'status' => $e->getCode() ?: 500 ]
+				)
+			);
+		}
 	}
 
 	/**
@@ -158,9 +192,19 @@ class Fees extends Base_API {
 	 * @return Response The response object.
 	 */
 	protected function get_fees_for_ticket_response( Request $request ): Response {
-		$ticket_id = (int) $request->get_param( 'id' );
+		try {
+			$ticket_id = (int) $request->get_param( 'id' );
 
-		return rest_ensure_response( $this->get_fees_for_ticket( $ticket_id ) );
+			return rest_ensure_response( $this->get_fees_for_ticket( $ticket_id ) );
+		} catch ( Exception $e ) {
+			return $this->convert_error_to_response(
+				new WP_Error(
+					'tickets_commerce_get_fees_for_ticket_error',
+					$e->getMessage(),
+					[ 'status' => $e->getCode() ?: 500 ]
+				)
+			);
+		}
 	}
 
 	/**
@@ -173,14 +217,16 @@ class Fees extends Base_API {
 	 * @return array The fees for the ticket.
 	 */
 	protected function get_fees_for_ticket( int $ticket_id ) {
+		$all_fees    = $this->get_all_fees();
 		$ticket_fees = $this->relationships->find_by_post_id( $ticket_id );
 		$ticket_fees ??= [];
 
-		$automatic_fees = $this->get_automatic_fees( $this->get_all_fees() );
+		$automatic_fees = $this->get_automatic_fees( $all_fees );
 
 		return [
-			'fees'      => $ticket_fees,
-			'automatic' => $automatic_fees,
+			'available_fees' => $this->get_selectable_fees( $all_fees ),
+			'selected_fees'  => $ticket_fees,
+			'automatic_fees' => $automatic_fees,
 		];
 	}
 
@@ -195,11 +241,18 @@ class Fees extends Base_API {
 	 * @return array The ticket data with fees.
 	 */
 	protected function add_fees_to_ticket_data( array $data, Request $request ): array {
-		$ticket_id    = (int) $request->get_param( 'id' );
-		$ticket_fees  = $this->get_fees_for_ticket( $ticket_id );
-		$data['fees'] = $ticket_fees;
+		// Only add fees to the default ticket type.
+		if ( array_key_exists( 'type', $data ) && 'default' !== $data['type'] ) {
+			return $data;
+		}
 
-		return $data;
+		try {
+			$ticket_id    = (int) ( $data['id'] ?? $request->get_param( 'id' ) );
+			$ticket_fees  = $this->get_fees_for_ticket( $ticket_id );
+			$data['fees'] = $ticket_fees;
+		} finally {
+			return $data;
+		}
 	}
 
 	/**
@@ -212,15 +265,46 @@ class Fees extends Base_API {
 	 * @return Response
 	 */
 	protected function update_fees_for_ticket_response( Request $request ) {
-		$ticket_id = (int) $request->get_param( 'id' );
-		$fees      = $request->get_param( 'fees' );
+		try {
+			$ticket_id = (int) $request->get_param( 'id' );
+			$fees      = $request->get_param( 'selected_fees' );
 
-		$this->manager->delete_relationships_by_post( $ticket_id );
+			// Validate that the fees are actually selectable.
+			$all_fees        = $this->get_all_fees();
+			$selectable_fees = wp_list_pluck( $this->get_selectable_fees( $all_fees ), 'id', 'id' );
+			$invalid_fees    = [];
+			foreach ( $fees as $fee ) {
+				if ( ! array_key_exists( $fee, $selectable_fees ) ) {
+					$invalid_fees[] = $fee;
+				}
+			}
 
-		$fee_ids = array_map( 'absint', $fees );
+			if ( ! empty( $invalid_fees ) ) {
+				throw new Exception(
+					sprintf(
+						/* translators: %s: The invalid fees. */
+						__( 'The following fees are not selectable: %s', 'event-tickets' ),
+						implode( ', ', $invalid_fees )
+					),
+					400
+				);
+			}
 
-		$this->manager->sync_modifier_relationships( $fee_ids, [ $ticket_id ] );
+			$this->manager->delete_relationships_by_post( $ticket_id );
 
-		return rest_ensure_response( $this->get_fees_for_ticket( $ticket_id ) );
+			$fee_ids = array_map( 'absint', $fees );
+
+			$this->manager->sync_modifier_relationships( $fee_ids, [ $ticket_id ] );
+
+			return rest_ensure_response( $this->get_fees_for_ticket( $ticket_id ) );
+		} catch ( Exception $e ) {
+			return $this->convert_error_to_response(
+				new WP_Error(
+					'tickets_commerce_update_fees_for_ticket_error',
+					$e->getMessage(),
+					[ 'status' => $e->getCode() ?: 500 ]
+				)
+			);
+		}
 	}
 }
