fe392b17d6f57e6be468ec430a637331f830d19d
Jeremy Pry <jeremy.pry@gmail.com>
2024-11-11 23:43:20 -0600

    Update the template for displaying fees

--- src/Tickets/Blocks/Ticket/app/editor/container-content/fees/template.js
+++ src/Tickets/Blocks/Ticket/app/editor/container-content/fees/template.js
@@ -1,58 +1,143 @@
 /**
  * External dependencies
  */
-import React, { Fragment } from 'react';
+import classNames from 'classnames';
+import React, { Fragment, PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { LabeledItem, Select } from '@moderntribe/common/elements';
+import {
+	Checkbox,
+	LabeledItem,
+	Select
+} from '@moderntribe/common/elements';
+import { __ } from '@wordpress/i18n';
 
-const FeesSelector = ({
-						  selectedFees,
-						  activeFees,
-						  onSelectedFeesChange,
-					  }) => (
-	<Fragment>
-		{/* Active Fees Section */}
-		<div className="tribe-editor__ticket__active-fees">
-			<LabeledItem
-				className="tribe-editor__ticket__active-fees-label"
-				label="Active Fees"
-			/>
-			<ul>
-				{activeFees.length > 0 ? (
-					activeFees.map((fee) => (
-						<li key={fee.value}>
-							{fee.label}
-						</li>
-					))
-				) : (
-					<li>No active fees.</li>
-				)}
-			</ul>
-		</div>
 
-		{/* Selected Fees Section */}
-		<div className="tribe-editor__ticket__order_modifier_fees">
-			<LabeledItem
-				className="tribe-editor__ticket__selected-fees-label"
-				label="Selected Fees"
-			/>
-			<Select
-				id="tec-ticket-selected-fees-select"
-				placeholder="Select Fees"
-				options={activeFees}
-				value={selectedFees}
-				onChange={onSelectedFeesChange}
-				isMulti={true}
-				noOptionsMessage={() => 'No selectable fees available'}
-			/>
-		</div>
-	</Fragment>
-);
+const mapFeeToItem = ( isDisabled ) => {
+	return ( fee ) => {
+		// We shouldn't have these here, but just in case skip anything not active.
+		if ( fee.status !== 'active' ) {
+			return null;
+		}
 
-FeesSelector.propTypes = {
-	selectedFees: PropTypes.array.isRequired,
-	activeFees: PropTypes.array.isRequired,
-	onSelectedFeesChange: PropTypes.func.isRequired,
+		// Todo: the precision should be determined by settings.
+		const amount = Number.parseFloat( fee.raw_amount ).toFixed( 2 );
+
+		let feeLabel;
+		if ( fee.sub_type === 'percent' ) {
+			feeLabel = `${ fee.display_name } (${ amount }%)`;
+		} else {
+			feeLabel = `${ fee.display_name } ($${ amount })`;
+		}
+
+		return (
+			<FeeItem
+				key={ fee.id }
+				isDisabled={ isDisabled }
+				isSelected={ true }
+				label={ feeLabel }
+				value={ fee.id }
+				classes={ [ 'tribe-editor__ticket__fee-checkbox' ] }
+			/>
+		);
+	}
 };
 
-export default FeesSelector;
+class FeeItem extends PureComponent {
+	static propTypes = {
+		isDisabled: PropTypes.bool.isRequired,
+		isSelected: PropTypes.bool.isRequired,
+		label: PropTypes.string.isRequired,
+		value: PropTypes.string.isRequired,
+		classes: PropTypes.array.isRequired,
+	};
+
+	constructor( props ) {
+		super( props );
+	}
+
+	render() {
+		const {
+			isDisabled,
+			isSelected,
+			label,
+			value,
+			classes,
+		} = this.props;
+
+		const name = `tec-ticket-fee-${ value }`;
+
+		return (
+			<Checkbox
+				checked={ isSelected }
+				className={ classes.join( ' ' ) }
+				disabled={ false }
+				id={ name }
+				name={ name }
+				label={ label }
+				value={ value }
+			/>
+		);
+	}
+}
+
+class FeesSection extends PureComponent {
+	static propTypes = {
+		feesSelected: PropTypes.array.isRequired,
+		feesAvailable: PropTypes.array.isRequired,
+		feesAutomatic: PropTypes.array.isRequired,
+		onSelectedFeesChange: PropTypes.func.isRequired,
+		shouldDisplay: PropTypes.bool.isRequired,
+	};
+
+	constructor( props ) {
+		super( props );
+	}
+
+	render() {
+		const {
+			feesSelected,
+			feesAvailable,
+			feesAutomatic,
+			onSelectedFeesChange,
+			shouldDisplay,
+		} = this.props;
+
+		// If we shouldn't display this component, return null.
+		if ( ! shouldDisplay ) {
+			return null;
+		}
+
+		const hasItemsToDisplay = feesAutomatic.length > 0 || feesAvailable.length > 0;
+
+		return (
+			<div
+				className={ classNames(
+					'tribe-editor__ticket__fees',
+					'tribe-editor__ticket__content-row',
+					'tribe-editor__ticket__content-row--fees'
+				) }
+			>
+				<LabeledItem
+					className="tribe-editor__ticket__active-fees-label"
+					label={ __( 'Ticket Fees', 'event-tickets' ) }
+				/>
+
+				<div className="tribe-editor__ticket__order_modifier_fees">
+					{ feesAutomatic.length > 0 ? (
+						feesAutomatic.map( mapFeeToItem( { isDisabled: true } ) )
+					) : null }
+
+					{ feesAvailable.length > 0 ? (
+						feesAvailable.map( mapFeeToItem( { isDisabled: false } ) )
+					) : null }
+
+					{ ! hasItemsToDisplay ? (
+						<p>{ __( 'No available fees.', 'event-tickets' ) }</p>
+					) : null }
+				</div>
+			</div>
+		);
+	}
+}
+
+export default FeesSection;
