29ebfa36c06e98dcf4d8ad91620429e7eb4829e8
Dimitrios Pantazis <dpanta94@gmail.com>
2024-11-23 01:34:34 +0200

    editor script - added conditional loading

--- src/Tickets/Commerce/Order_Modifiers/Admin/Editor.php
+++ src/Tickets/Commerce/Order_Modifiers/Admin/Editor.php
@@ -10,12 +10,11 @@ declare( strict_types=1 );
 namespace TEC\Tickets\Commerce\Order_Modifiers\Admin;
 
 use TEC\Common\Contracts\Provider\Controller as Controller_Contract;
-use TEC\Common\Contracts\Container;
 use TEC\Common\StellarWP\Assets\Assets;
 use TEC\Tickets\Commerce\Order_Modifiers\Traits\Asset_Build;
-use Tribe__Tickets__Main as Tickets;
 use Tribe__Main as Common;
 use TEC\Tickets\Commerce\Order_Modifiers\API\Fees;
+use WP_Post;
 
 /**
  * Class Editor
@@ -25,16 +24,6 @@ use TEC\Tickets\Commerce\Order_Modifiers\API\Fees;
 class Editor extends Controller_Contract {
 
 	use Asset_Build;
-
-	/**
-	 * ServiceProvider constructor.
-	 *
-	 * @param Container $container The DI container.
-	 */
-	public function __construct( Container $container ) {
-		parent::__construct( $container );
-		$this->plugin = Tickets::instance();
-	}
 
 	/**
 	 * Registers the filters and actions hooks added by the controller.
@@ -102,6 +91,30 @@ class Editor extends Controller_Contract {
 			)
 			->add_localize_script( 'tec.tickets.orderModifiers.blockEditor', [ $this, 'get_store_data' ] )
 			->enqueue_on( 'enqueue_block_editor_assets' )
+			->set_condition( [ $this, 'should_enqueue_assets' ] )
 			->register();
+	}
+
+	/**
+	 * Checks if the current context is the Block Editor and the post type is ticket-enabled.
+	 *
+	 * @since TBD
+	 *
+	 * @return bool Whether the assets should be enqueued or not.
+	 */
+	public function should_enqueue_assets() {
+		$ticketable_post_types = (array) tribe_get_option( 'ticket-enabled-post-types', [] );
+
+		if ( empty( $ticketable_post_types ) ) {
+			return false;
+		}
+
+		$post = get_post();
+
+		if ( ! $post instanceof WP_Post ) {
+			return false;
+		}
+
+		return is_admin() && in_array( $post->post_type, $ticketable_post_types, true );
 	}
 }
