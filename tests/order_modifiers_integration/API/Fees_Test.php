1972a777bbdd1fa6768527b08ba9d16db152e6e6
Dimitrios Pantazis <dpanta94@gmail.com>
2024-11-22 22:42:05 +0200

    Fix more namespaces

--- tests/order_modifiers_integration/API/Fees_Test.php
+++ tests/order_modifiers_integration/API/Fees_Test.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace TEC\Tickets\Commerce\Order_Modifiers;
+namespace TEC\Tickets\Commerce\Order_Modifiers\API;
 
 use TEC\Common\Tests\Provider\Controller_Test_Case;
-use TEC\Tickets\Commerce\Order_Modifiers\API\Fees;
 use Tribe\Tests\Traits\With_Uopz;
 
 class Fees_Test extends Controller_Test_Case {
