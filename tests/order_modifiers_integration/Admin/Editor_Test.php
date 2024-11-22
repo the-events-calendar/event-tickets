7f20b1375077d2d0df02c7a7030572513bd519c2
Brian Krane <redscar@gmail.com>
2024-11-22 13:33:44 -0500

    Added new test testing if `tec-tickets-order-modifiers-block-editor` enqueues properly.

--- tests/order_modifiers_integration/Admin/Editor_Test.php
+++ tests/order_modifiers_integration/Admin/Editor_Test.php
@@ -6,6 +6,8 @@ use TEC\Common\Tests\Provider\Controller_Test_Case;
 use TEC\Tickets\Commerce\Order_Modifiers\Admin\Editor;
 use Tribe\Tests\Traits\With_Uopz;
 use TEC\Common\StellarWP\Assets\Assets;
+use Closure;
+use Generator;
 
 class Editor_Test extends Controller_Test_Case {
 	use With_Uopz;
@@ -33,6 +35,60 @@ class Editor_Test extends Controller_Test_Case {
 
 		foreach ( $assets as $slug => $path ) {
 			yield $slug => [ $slug, $path ];
+		}
+	}
+
+	public function should_enqueue_block_editor_assets_data_provider(): Generator {
+		yield 'Assets should not enqueue on incorrect action' => [
+			function (): bool {
+				// Simulate an action where the assets should NOT enqueue.
+				do_action( 'some_other_action' );
+
+				return false;
+			},
+		];
+
+		yield 'Assets should enqueue on block editor action' => [
+			function (): bool {
+				// Simulate the block editor action.
+				do_action( 'enqueue_block_editor_assets' );
+
+				return true;
+			},
+		];
+	}
+
+	/**
+	 * @dataProvider should_enqueue_block_editor_assets_data_provider
+	 */
+	public function test_should_enqueue_block_editor_assets( Closure $fixture ): void {
+		$this->make_controller()->register();
+		// Setup: Mock WordPress enqueue functions to capture enqueued scripts.
+		$enqueued_assets = [];
+		$this->set_fn_return(
+			'wp_enqueue_script',
+			function ( $handle ) use ( &$enqueued_assets ) {
+				$enqueued_assets[] = $handle;
+			},
+			true
+		);
+
+		// Execute the fixture to simulate the action.
+		$should_enqueue_assets = $fixture();
+
+		// Assert whether the asset is enqueued based on the condition.
+		if ( $should_enqueue_assets ) {
+			$this->assertContains(
+				'tec-tickets-order-modifiers-block-editor',
+				$enqueued_assets,
+				'The block editor asset should be enqueued.'
+			);
+		} else {
+			$this->assertNotContains(
+				'tec-tickets-order-modifiers-block-editor',
+				$enqueued_assets,
+				'The block editor asset should not be enqueued.'
+			);
 		}
 	}
 }
