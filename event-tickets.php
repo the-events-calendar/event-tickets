89514d83afb29823042f791fb14b8258fc34c1c7
Camwyn <stephen@theeventscalendar.com>
2024-12-17 14:25:09 -0500

    Fix min WP version

--- event-tickets.php
+++ event-tickets.php
@@ -4,7 +4,7 @@
  * Plugin URI:  https://evnt.is/1acb
  * Description: Event Tickets allows you to sell basic tickets and collect RSVPs from any post, page, or event.
  * Version: 5.18.0
- * Requires at least: 6.3
+ * Requires at least: 6.5
  * Requires PHP: 7.4
  * Author: The Events Calendar
  * Author URI: https://evnt.is/1aor
