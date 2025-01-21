df78ef5b0fdeda62be603ea5e2cf4f1e54b10fcb
Jeremy Pry <jeremy.pry@gmail.com>
2025-01-21 16:54:20 -0600

    Replace inheritdoc comments with actual comments

--- src/Tickets/Commerce/Cart/Unmanaged_Cart.php
+++ src/Tickets/Commerce/Cart/Unmanaged_Cart.php
@@ -25,21 +25,34 @@ class Unmanaged_Cart extends Abstract_Cart {
 	protected $items = null;
 
 	/**
-	 * {@inheritDoc}
+	 * Determines if this instance of the cart has a public page.
+	 *
+	 * @since 5.1.9
+	 *
+	 * @return bool
 	 */
 	public function has_public_page() {
 		return false;
 	}
 
 	/**
-	 * {@inheritDoc}
+	 * Gets the Cart mode based.
+	 *
+	 * @since 5.1.9
+	 *
+	 * @return string
 	 */
 	public function get_mode() {
 		return \TEC\Tickets\Commerce\Cart::REDIRECT_MODE;
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Sets the cart hash.
+	 *
+	 * @since 5.1.9
+	 * @since 5.2.0 Renamed to set_hash instead of set_id
+	 *
+	 * @param string $hash
 	 */
 	public function set_hash( $hash ) {
 		/**
@@ -54,7 +67,11 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Gets the cart hash.
+	 *
+	 * @since 5.2.0
+	 *
+	 * @return string
 	 */
 	public function get_hash() {
 		/**
@@ -69,7 +86,12 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Saves the cart.
+	 *
+	 * This method should include any persistence, request and redirection required
+	 * by the cart implementation.
+	 *
+	 * @since 5.1.9
 	 */
 	public function save() {
 		$cart_hash = tribe( Commerce\Cart::class )->get_cart_hash( true );
@@ -90,7 +112,13 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Gets the cart items from the cart.
+	 *
+	 * This method should include any persistence by the cart implementation.
+	 *
+	 * @since 5.1.9
+	 *
+	 * @return array
 	 */
 	public function get_items() {
 		if ( null !== $this->items ) {
@@ -113,7 +141,10 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Clears the cart of its contents and persists its new state.
+	 *
+	 * This method should include any persistence, request and redirection required
+	 * by the cart implementation.
 	 */
 	public function clear() {
 		$cart_hash = tribe( Commerce\Cart::class )->get_cart_hash();
@@ -132,7 +163,11 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Whether a cart exists meeting the specified criteria.
+	 *
+	 * @since 5.1.9
+	 *
+	 * @param array $criteria The criteria to check for.
 	 */
 	public function exists( array $criteria = [] ) {
 		$cart_hash = tribe( Commerce\Cart::class )->get_cart_hash();
@@ -145,7 +180,12 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Whether the cart contains items or not.
+	 *
+	 * @since 5.1.9
+	 *
+	 * @return bool|int The number of products in the cart (regardless of the products quantity) or `false`
+	 *
 	 */
 	public function has_items() {
 		$items = $this->get_items();
@@ -154,7 +194,13 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Whether an item is in the cart or not.
+	 *
+	 * @since 5.1.9
+	 *
+	 * @param string $item_id
+	 *
+	 * @return bool|int Either the quantity in the cart for the item or `false`.
 	 */
 	public function has_item( $item_id ) {
 		$items = $this->get_items();
@@ -163,7 +209,13 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Adds a specified quantity of the item to the cart.
+	 *
+	 * @since 5.1.9
+	 *
+	 * @param int|string $item_id    The item ID.
+	 * @param int        $quantity   The quantity to remove.
+	 * @param array      $extra_data Extra data to save to the item.
 	 */
 	public function add_item( $item_id, $quantity, array $extra_data = [] ) {
 		$current_quantity = $this->has_item( $item_id );
@@ -188,7 +240,12 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritdoc}
+	 * Removes an item from the cart.
+	 *
+	 * @since 5.1.9
+	 *
+	 * @param int|string $item_id  The item ID.
+	 * @param null|int   $quantity The quantity to remove.
 	 */
 	public function remove_item( $item_id, $quantity = null ) {
 		if ( null !== $quantity ) {
@@ -259,7 +316,13 @@ class Unmanaged_Cart extends Abstract_Cart {
 	}
 
 	/**
-	 * {@inheritDoc}
+	 * Prepare the data for cart processing.
+	 *
+	 * @since 5.1.10
+	 *
+	 * @param array $data To be processed by the cart.
+	 *
+	 * @return array
 	 */
 	public function prepare_data( array $data = [] ) {
 		return $data;
