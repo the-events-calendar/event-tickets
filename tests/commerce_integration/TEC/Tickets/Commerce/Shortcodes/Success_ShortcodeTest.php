7f56cdbfc6168f4a48abe4d965ea90ce635601b2
rafsuntaskin <rafsun.aiub@gmail.com>
2025-01-21 19:36:54 +0600

    phpcbf

--- tests/commerce_integration/TEC/Tickets/Commerce/Shortcodes/Success_ShortcodeTest.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Shortcodes/Success_ShortcodeTest.php
@@ -27,15 +27,21 @@ class Success_Shortcode_Test extends WPTestCase {
 		parent::setUp();
 
 		// Enable post as ticket type.
-		add_filter( 'tribe_tickets_post_types', function () {
-			return [ 'post' ];
-		} );
+		add_filter(
+			'tribe_tickets_post_types',
+			function () {
+				return [ 'post' ];
+			} 
+		);
 
 		// Enable Tickets Commerce as the default provider.
-		add_filter( 'tribe_tickets_get_modules', function ( $modules ) {
-			$modules[ Module::class ] = Module::class;
-			return $modules;
-		} );
+		add_filter(
+			'tribe_tickets_get_modules',
+			function ( $modules ) {
+				$modules[ Module::class ] = Module::class;
+				return $modules;
+			} 
+		);
 
 		tribe_update_option( 'tickets-commerce-currency-code', 'USD' );
 		tribe_update_option( 'tickets-commerce-currency-decimal-separator', '.' );
@@ -76,31 +82,43 @@ class Success_Shortcode_Test extends WPTestCase {
 	 */
 	public function create_order_with_ticket_and_attendee( $gateway_order_id ) {
 		// Create a post and a ticket for it.
-		$post_id = $this->factory()->post->create();
-		$tickets_commerce_ticket_id = $this->create_tc_ticket( $post_id, 10, [
-			'ticket_name'        => "Test TC ticket",
-			'ticket_description' => "Test TC ticket description",
-		] );
+		$post_id                    = $this->factory()->post->create();
+		$tickets_commerce_ticket_id = $this->create_tc_ticket(
+			$post_id,
+			10,
+			[
+				'ticket_name'        => 'Test TC ticket',
+				'ticket_description' => 'Test TC ticket description',
+			] 
+		);
 
 		// Create an order for one ticket.
-		$order = $this->create_order( [ $tickets_commerce_ticket_id => 1 ], [
-			'purchaser_email' => 'purchaser_email@test.com',
-		] );
+		$order = $this->create_order(
+			[ $tickets_commerce_ticket_id => 1 ],
+			[
+				'purchaser_email' => 'purchaser_email@test.com',
+			] 
+		);
 
 		// Update order info for normalization.
 		update_post_meta( $order->ID, Order::$gateway_order_id_meta_key, $gateway_order_id );
 		$fake_date = '1974-11-20 21:57:56';
-		wp_update_post( [
-			'ID' => $order->ID,
-			'post_date' => $fake_date,
-			'post_date_gmt' => $fake_date,
-			'post_modified' => $fake_date,
-			'post_modified_gmt' => $fake_date,
-		] );
+		wp_update_post(
+			[
+				'ID'                => $order->ID,
+				'post_date'         => $fake_date,
+				'post_date_gmt'     => $fake_date,
+				'post_modified'     => $fake_date,
+				'post_modified_gmt' => $fake_date,
+			] 
+		);
 
-		$attendee_id = $this->create_attendee_for_ticket( $tickets_commerce_ticket_id, $post_id, [
-			'order_id' => $order->ID,
-		] );
+		$attendee_id = $this->create_attendee_for_ticket(
+			$tickets_commerce_ticket_id,
+			$post_id,
+			[
+				'order_id' => $order->ID,
+			] 
+		);
 	}
 }
-
