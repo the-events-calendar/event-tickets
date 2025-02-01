42397a1f72d58ab3f8dcdc9453c28dd198a1f155
Jeremy Pry <jeremy.pry@gmail.com>
2025-01-31 22:18:49 -0600

    Update agnostic cart functionality

--- src/Tickets/Commerce/Cart/Agnostic_Cart.php
+++ src/Tickets/Commerce/Cart/Agnostic_Cart.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *
+ * A cart that is agnostic to what types of items it contains.
  *
  * @since TBD
  */
@@ -11,6 +11,11 @@ namespace TEC\Tickets\Commerce\Cart;
 
 use TEC\Tickets\Commerce\Cart;
 use TEC\Tickets\Commerce\Traits\Cart as Cart_Trait;
+use TEC\Tickets\Commerce\Utils\Value;
+use Tribe__Tickets__REST__V1__Messages as Messages;
+use Tribe__Tickets__Ticket_Object as Ticket_Object;
+use Tribe__Tickets__Tickets as Tickets;
+use Tribe__Tickets__Tickets_Handler as Tickets_Handler;
 
 /**
  * Class Agnostic_Cart
@@ -27,27 +32,29 @@ class Agnostic_Cart extends Abstract_Cart {
 	protected array $items = [];
 
 	/**
-	 * Gets the Cart mode based.
-	 *
-	 * @since 5.1.9
-	 *
-	 * @return string
-	 */
-	public function get_mode() {
-		// TODO: Implement get_mode() method.
-	}
-
-	/**
 	 * Gets the cart items from the cart.
 	 *
 	 * This method should include any persistence by the cart implementation.
 	 *
-	 * @since 5.1.9
+	 * @since TBD
 	 *
 	 * @return array
 	 */
 	public function get_items() {
-		// TODO: Implement get_items() method.
+		if ( ! empty( $this->items ) ) {
+			return $this->items;
+		}
+
+		if ( ! $this->exists() ) {
+			return [];
+		}
+
+		$items = get_transient( $this->get_transient_key( $this->get_hash() ) );
+		if ( is_array( $items ) && ! empty( $items ) ) {
+			$this->items = $items;
+		}
+
+		return $this->items;
 	}
 
 	/**
@@ -56,10 +63,27 @@ class Agnostic_Cart extends Abstract_Cart {
 	 * This method should include any persistence, request and redirection required
 	 * by the cart implementation.
 	 *
-	 * @since 5.1.9
+	 * @since TBD
 	 */
 	public function save() {
-		// TODO: Implement save() method.
+		$cart_hash = tribe( Cart::class )->get_cart_hash( true );
+
+		if ( false === $cart_hash ) {
+			return false;
+		}
+
+		$this->set_hash( $cart_hash );
+
+		if ( ! $this->has_items() ) {
+			$this->clear();
+
+			return false;
+		}
+
+		set_transient( $this->get_transient_key( $cart_hash ), $this->items, DAY_IN_SECONDS );
+		tribe( Cart::class )->set_cart_hash_cookie( $cart_hash );
+
+		return true;
 	}
 
 	/**
@@ -69,25 +93,43 @@ class Agnostic_Cart extends Abstract_Cart {
 	 * by the cart implementation.
 	 */
 	public function clear() {
-		// TODO: Implement clear() method.
+		$cart_hash = tribe( Cart::class )->get_cart_hash() ?? '';
+
+		if ( false === $cart_hash ) {
+			return;
+		}
+
+		$this->set_hash( null );
+		delete_transient( $this->get_transient_key( $cart_hash ) );
+		tribe( Cart::class )->set_cart_hash_cookie( null );
+
+		// clear cart items data.
+		$this->items      = [];
+		$this->cart_total = null;
 	}
 
 	/**
 	 * Whether a cart exists meeting the specified criteria.
 	 *
-	 * @since 5.1.9
+	 * @since TBD
 	 *
 	 * @param array $criteria
+	 *
+	 * @return bool Whether the cart exists or not.
 	 */
 	public function exists( array $criteria = [] ) {
-		$hash = $this->get_cart_object()->get_cart_hash();
+		$hash = $this->get_hash();
+		if ( empty( $hash ) ) {
+			return false;
+		}
 
+		return (bool) get_transient( $this->get_transient_key( $hash ) );
 	}
 
 	/**
 	 * Whether the cart contains items or not.
 	 *
-	 * @since 5.1.9
+	 * @since TBD
 	 *
 	 * @return bool|int The number of products in the cart (regardless of the products quantity) or `false`
 	 *
@@ -101,7 +143,7 @@ class Agnostic_Cart extends Abstract_Cart {
 	/**
 	 * Whether an item is in the cart or not.
 	 *
-	 * @since 5.1.9
+	 * @since TBD
 	 *
 	 * @param string $item_id
 	 *
@@ -116,72 +158,221 @@ class Agnostic_Cart extends Abstract_Cart {
 			return false;
 		}
 
-		return $this->items[ $item_id ][ 'quantity' ] ?? false;
+		return $this->items[ $item_id ]['quantity'] ?? false;
 	}
 
 	/**
 	 * Adds a specified quantity of the item to the cart.
 	 *
-	 * @since 5.1.9
+	 * @since TBD
 	 *
 	 * @param int|string $item_id    The item ID.
 	 * @param int        $quantity   The quantity to remove.
 	 * @param array      $extra_data Extra data to save to the item.
 	 */
 	public function add_item( $item_id, $quantity, array $extra_data = [] ) {
-		// TODO: Implement add_item() method.
+		// Allow for the type of item to be passed in.
+		if ( array_key_exists( 'type', $extra_data ) ) {
+			$type = $extra_data['type'];
+			unset( $extra_data['type'] );
+		} else {
+			$type = 'ticket';
+		}
+
+		// Set the ID key based on the type of item, defaulting to ticket types.
+		$id_key = "{$type}_id";
+
+		// If the item is already in the cart, update the quantity.
+		$current_quantity = $this->has_item( $item_id );
+		if ( false !== $current_quantity ) {
+			$this->update_item( $item_id, $quantity, $extra_data );
+
+			return;
+		}
+
+		$this->items[ $item_id ] = [
+			$id_key    => $item_id,
+			'quantity' => $quantity,
+			'extra'    => $extra_data,
+		];
 	}
 
 	/**
-	 * Determines if this instance of the cart has a public page.
+	 * Update an item in the cart.
 	 *
-	 * @since 5.1.9
+	 * @param string $item_id    The item ID.
+	 * @param int    $quantity   The quantity to update.
+	 * @param array  $extra_data Extra data to save to the item.
 	 *
-	 * @return bool
+	 * @return void
 	 */
-	public function has_public_page() {
-		return false;
+	protected function update_item( $item_id, int $quantity, array $extra_data = [] ): void {
+		$item_data    = $this->items[ $item_id ];
+		$new_quantity = $item_data['quantity'] + $quantity;
+
+		// If the quantity is less than 1, remove the item from the cart.
+		if ( $new_quantity < 1 ) {
+			$this->remove_item( $item_id );
+
+			return;
+		}
+
+		// Maybe update the extra data.
+		if ( ! empty( $extra_data ) ) {
+			$item_data['extra'] = array_merge( $item_data['extra'], $extra_data );
+		}
+
+		$this->items[ $item_id ] = $item_data;
 	}
 
 	/**
 	 * Removes an item from the cart.
 	 *
-	 * @since 5.1.9
+	 * @since TBD
 	 *
 	 * @param int|string $item_id  The item ID.
 	 * @param null|int   $quantity The quantity to remove.
 	 */
 	public function remove_item( $item_id, $quantity = null ) {
-		// TODO: Implement remove_item() method.
+		if ( null === $quantity ) {
+			unset( $this->items[ $item_id ] );
+
+			return;
+		}
+
+		$this->update_item( $item_id, -abs( (int) $quantity ) );
 	}
 
 	/**
 	 * Process the items in the cart.
 	 *
-	 * @since 5.1.10
+	 * @since TBD
 	 *
 	 * @param array $data to be processed by the cart.
 	 *
-	 * @return array
+	 * @return array|bool
 	 */
 	public function process( array $data = [] ) {
-		// TODO: Implement process() method.
+		if ( empty( $data ) ) {
+			return false;
+		}
+
+		// Reset the contents of the cart.
+		$this->clear();
+
+		/** @var Messages $messages */
+		$messages = tribe( 'tickets.rest-v1.messages' );
+
+		/** @var Tickets_Handler $tickets_handler */
+		$tickets_handler = tribe( 'tickets.handler' );
+
+		// Prepare the error message array.
+		$errors = [];
+
+		// Natively handle adding tickets as items to the cart.
+		foreach ( $data['tickets'] as $ticket ) {
+			$available = $tickets_handler->get_ticket_max_purchase( $ticket['ticket_id'] );
+
+			// Bail if ticket does not have enough available capacity.
+			if ( ( -1 !== $available && $available < $ticket['quantity'] ) || ! $ticket['obj']->date_in_range() ) {
+				$error_code = 'ticket-capacity-not-available';
+
+				$errors[] = new \WP_Error(
+					$error_code, sprintf( $messages->get_message( $error_code ), $ticket['obj']->name ), [
+					'ticket'        => $ticket,
+					'max_available' => $available,
+				]
+				);
+				continue;
+			}
+
+			// Enforces that the min to add is 1.
+			$ticket['quantity'] = max( 1, (int) $ticket['quantity'] );
+
+			// Add to / update quantity in cart.
+			$this->add_item( $ticket['ticket_id'], $ticket['quantity'], $ticket['extra'] );
+		}
+
+		/**
+		 * Fires after the ticket data has been processed.
+		 *
+		 * This allows for further processing of data within the $data array.
+		 *
+		 * @since TBD
+		 *
+		 * @param Unmanaged_Cart $cart The cart object.
+		 * @param array          $data The data to be processed by the cart.
+		 */
+		do_action( 'tec_tickets_commerce_cart_process', $this, $data );
+
+		// Saved added items to the cart.
+		$this->save();
+
+		if ( ! empty( $errors ) ) {
+			return $errors;
+		}
+
+		return true;
 	}
 
 	/**
-	 * Prepare the data for cart processing.
+	 * Add the full set of parameters to the items in the cart.
 	 *
-	 * @since 5.1.10
+	 * @param array $items The items in the cart.
 	 *
-	 * @param array $data To be processed by the cart.
-	 *
-	 * @return array
+	 * @return array The items in the cart with the full set of parameters.
 	 */
-	public function prepare_data( array $data = [] ) {
-		// TODO: Implement prepare_data() method.
+	protected function add_full_item_params( array $items ): array {
+		return array_map(
+			function ( $item ) {
+				$type = $item['type'] ?? 'ticket';
+				switch ( $type ) {
+					case 'ticket':
+						return $this->add_ticket_params( $item );
+
+					default:
+						/**
+						 * Filter the full item parameters for the cart.
+						 *
+						 * This allows for further processing of the item parameters.
+						 *
+						 * If the item shouldn't be processed, `null` should be returned. Otherwise,
+						 * an array of the full item parameters should be returned.
+						 *
+						 * @since TBD
+						 *
+						 * @param array|null $params The full item parameters for the cart.
+						 * @param array      $item   The item in the cart.
+						 * @param string     $type   The type of item.
+						 */
+						return apply_filters( 'tec_tickets_commerce_cart_add_full_item_params', null, $item, $type );
+				}
+			},
+			$items
+		);
 	}
 
-	protected function get_cart_object(): Cart {
-		return tribe( Cart::class );
+	/**
+	 * Add the ticket parameters to the item in the cart.
+	 *
+	 * @param array $item The item in the cart.
+	 *
+	 * @return ?array The item in the cart with the full set of parameters.
+	 */
+	protected function add_ticket_params( $item ) {
+		// Try to get the ticket object, and if it's not valid, remove it from the cart.
+		$item['obj'] = Tickets::load_ticket_object( $item['ticket_id'] );
+		if ( ! $item['obj'] instanceof Ticket_Object ) {
+			return null;
+		}
+
+		$sub_total_value = Value::create();
+		$sub_total_value->set_value( $item['obj']->price );
+
+		$item['event_id']  = $item['obj']->get_event_id();
+		$item['sub_total'] = $sub_total_value->sub_total( $item['quantity'] );
+		$item['type']      = 'ticket';
+
+		return $item;
 	}
 }
