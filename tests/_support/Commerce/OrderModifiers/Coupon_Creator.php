76c31955a614c475f9a0689ce5b3fbf805a3fbdf
Jeremy Pry <jeremy.pry@gmail.com>
2025-02-18 21:28:46 -0600

    Add initial tests for the Coupons API

--- tests/_support/Commerce/OrderModifiers/Coupon_Creator.php
+++ tests/_support/Commerce/OrderModifiers/Coupon_Creator.php
@@ -17,6 +17,15 @@ trait Coupon_Creator {
 	 */
 	static int $coupon_counter = 0;
 
+	/**
+	 * Create a coupon.
+	 *
+	 * @since TBD
+	 *
+	 * @param array $args The coupon arguments.
+	 *
+	 * @return Coupon The coupon.
+	 */
 	protected function create_coupon( array $args = [] ): Coupon {
 		if ( isset( $args['raw_amount'] ) && is_numeric( $args['raw_amount'] ) ) {
 			$args['raw_amount'] = Float_Value::from_number( $args['raw_amount'] );
@@ -37,5 +46,25 @@ trait Coupon_Creator {
 		$args = wp_parse_args( $args, $defaults );
 
 		return Coupon::create( $args );
+	}
+
+	/**
+	 * Create multiple coupons.
+	 *
+	 * @since TBD
+	 *
+	 * @param int   $how_many The number of coupons to create.
+	 * @param array $args     The coupon arguments.
+	 *
+	 * @return Coupon[] The coupons.
+	 */
+	protected function create_coupons( int $how_many, array $args = [] ): array {
+		$coupons = [];
+
+		for ( $i = 0; $i < $how_many; $i++ ) {
+			$coupons[] = $this->create_coupon( $args );
+		}
+
+		return $coupons;
 	}
 }
