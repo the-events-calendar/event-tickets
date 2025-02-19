6ca4b5fbc49dbbe5a86ccdaa70113ffb3c69f41e
Gustavo Bordoni <bordoni.dev@gmail.com>
2025-02-19 14:04:47 -0500

    Resolve problems around bad strings

--- src/views/tickets/my-tickets/user-details.php
+++ src/views/tickets/my-tickets/user-details.php
@@ -21,17 +21,17 @@
 		printf(
 			// Translators: 1: order number, 2: count of attendees in the order, 3: ticket label (dynamically singular or plural), 4: purchaser name, 5: linked purchaser email, 6: date of purchase.
 			esc_html__( 'Order #%1$s: %2$d %3$s reserved by %4$s (%5$s) on %6$s', 'event-tickets' ),
-			esc_html( $order_id ),
-			count( $attendees ),
+			esc_html( (string) $order_id ),
+			count( $attendees ),2
 			_n(
 				esc_html( tribe_get_ticket_label_singular( 'orders_tickets' ) ),
 				esc_html( tribe_get_ticket_label_plural( 'orders_tickets' ) ),
 				count( $attendees ),
 				'event-tickets'
 			),
-			esc_attr( $order['purchaser_name'] ),
-			'<a href="mailto:' . esc_url( $order['purchaser_email'] ) . '">' . esc_html( $order['purchaser_email'] ) . '</a>',
-			date_i18n( tribe_get_date_format( true ), strtotime( esc_attr( $order['purchase_time'] ) ) )
+			esc_attr( (string) $order['purchaser_name'] ),
+			'<a href="mailto:' . esc_url( (string) $order['purchaser_email'] ) . '">' . esc_html( (string) $order['purchaser_email'] ) . '</a>',
+			date_i18n( tribe_get_date_format( true ), strtotime( esc_attr( (string) $order['purchase_time'] ) ) )
 		);
 
 		/**
