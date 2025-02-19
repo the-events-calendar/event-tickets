7865cc998bc7adfd08c8a181ede16d32f39c143a
Jeremy Pry <jeremy.pry@gmail.com>
2025-02-19 13:19:10 -0600

    Update cart HTML to display a coupon if the cart already has one applied

--- src/views/v2/commerce/checkout/order-modifiers/coupons.php
+++ src/views/v2/commerce/checkout/order-modifiers/coupons.php
@@ -6,7 +6,11 @@
  * @since   TBD Updated with actual coupon logic.
  *
  * @package TEC\Tickets\Commerce\Order_Modifiers\Checkout
+ *
+ * @var array[] $items [Global] List of Items on the cart to be checked out.
  */
+
+use TEC\Tickets\Commerce\Utils\Value;
 
 $coupon_input_classes = [
 	'tribe-tickets__commerce-checkout-cart-coupons__input',
@@ -19,9 +23,23 @@ $apply_button_classes = [
 	'tribe-common-c-btn',
 ];
 
+$coupon_items = array_filter( $items, fn( $item ) => 'coupon' === ( $item['type'] ?? '' ) );
+
+// If we have any coupons, always use the first one. There *shouldn't* be more than one.
+$coupon = ! empty( $coupon_items ) ? $coupon_items[ array_key_first( $coupon_items ) ] : [];
+
+// Determine the discount to display.
+$discount = '';
+if ( isset( $coupon['sub_total'] ) && $coupon['sub_total'] instanceof Value ) {
+	$discount = $coupon['sub_total']->get_currency();
+}
+
+$hide_input = ! empty( $coupon );
+$hide_applied = empty( $coupon );
+
 ?>
 <div class="tribe-tickets__form tribe-tickets__commerce-checkout-coupons-wrapper tribe-common-b2">
-	<div class="tec-tickets__commerce-checkout-cart-coupons">
+	<div class="tec-tickets__commerce-checkout-cart-coupons" <?php echo $hide_input ? 'style="display: none;"' : ''; ?>>
 		<input
 			type="text"
 			class="<?php tribe_classes( $coupon_input_classes ); ?>"
@@ -30,6 +48,7 @@ $apply_button_classes = [
 			aria-describedby="coupon_error"
 			aria-label="<?php esc_attr_e( 'Enter coupon code', 'event-tickets' ); ?>"
 			placeholder="<?php esc_attr_e( 'Enter coupon code', 'event-tickets' ); ?>"
+			value="<?php echo esc_attr( $coupon['slug'] ?? '' ); ?>"
 		/>
 		<button
 			id="coupon_apply"
@@ -37,14 +56,21 @@ $apply_button_classes = [
 		>
 			<?php echo esc_html_x( 'Apply', 'button to apply a coupon code', 'event-tickets' ); ?>
 		</button>
-		<p id="coupon_error" class="tec-tickets__commerce-checkout-cart-coupons__error" style="display: none; color: red;" aria-live="polite" role="alert">
-			<?php esc_html_e( 'Invalid coupon code', 'event-tickets' ); ?>
-		</p>
 	</div>
-	<div class="tec-tickets__commerce-checkout-cart-coupons__applied" style="display: none;">
-	<span class="tec-tickets__commerce-checkout-cart-coupons__applied-text">
-		<?php esc_html_e( 'Coupon:', 'event-tickets' ); ?> <span class="tec-tickets__commerce-checkout-cart-coupons__applied-value"></span> - <?php esc_html_e( 'Discount:', 'event-tickets' ); ?> <span class="tec-tickets__commerce-checkout-cart-coupons__applied-discount"></span>
-	</span>
+	<p id="coupon_error" class="tec-tickets__commerce-checkout-cart-coupons__error" style="display: none; color: red;" aria-live="polite" role="alert">
+		<?php esc_html_e( 'Invalid coupon code', 'event-tickets' ); ?>
+	</p>
+	<div class="tec-tickets__commerce-checkout-cart-coupons__applied" <?php echo $hide_applied ? 'style="display: none;"' : ''; ?>>
+		<span class="tec-tickets__commerce-checkout-cart-coupons__applied-text">
+			<?php esc_html_e( 'Coupon:', 'event-tickets' ); ?>
+			<span class="tec-tickets__commerce-checkout-cart-coupons__applied-value">
+				<?php echo esc_html( $coupon['display_name'] ?? '' ); ?>
+			</span>
+			- <?php esc_html_e( 'Discount:', 'event-tickets' ); ?>
+			<span class="tec-tickets__commerce-checkout-cart-coupons__applied-discount">
+				<?php echo esc_html( $discount ); ?>
+			</span>
+		</span>
 		<button class="tec-tickets__commerce-checkout-cart-coupons__remove-button" type="button">
 			<?php echo esc_html_x( 'X', 'text for button to remove an applied coupon', 'event-tickets' ); // @todo use an ICON instead. @see https://github.com/the-events-calendar/event-tickets/pull/3430#discussion_r1872545653 ?>
 		</button>
