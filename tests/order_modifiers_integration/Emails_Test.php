084fd19fade05d7ac664e3164b1f87f56e629f4c
Dimitrios Pantazis <dpanta94@gmail.com>
2025-02-21 18:58:04 +0200

    Get snapshots into a healthy state

--- tests/order_modifiers_integration/Emails_Test.php
+++ tests/order_modifiers_integration/Emails_Test.php
@@ -1,24 +1,32 @@
 <?php
 
-namespace TEC\Tickets\Commerce\Order_Modifiers\Fees;
+namespace TEC\Tickets\Commerce\Order_Modifiers;
 
 use Tribe\Tickets\Test\Commerce\OrderModifiers\Fee_Creator;
 use Tribe\Tickets\Test\Commerce\TicketsCommerce\Order_Maker;
 use Tribe\Tickets\Test\Commerce\TicketsCommerce\Ticket_Maker;
-use Codeception\TestCase\WPTestCase;
+use TEC\Common\Tests\Provider\Controller_Test_Case;
 use tad\Codeception\SnapshotAssertions\SnapshotAssertions;
 use Tribe\Tests\Traits\With_Uopz;
 use Tribe__Tickets__Tickets as Tickets;
 use Tribe\Tests\Traits\With_Clock_Mock;
 use Tribe__Date_Utils as Dates;
+use TEC\Tickets\Commerce\Order_Modifiers\Controller as Order_Modifiers_Controller;
+use TEC\Tickets\Commerce\Order_Modifiers\Checkout\Fees as Checkout_Fees;
+use TEC\Tickets\Commerce\Order_Modifiers\Checkout\Gateway\PayPal\Fees as Checkout_PayPal_Fees;
+use TEC\Tickets\Commerce\Order_Modifiers\Checkout\Gateway\Stripe\Fees as Checkout_Stripe_Fees;
+use TEC\Tickets\Commerce\Order_Modifiers\API\Fees as API_Fees;
+use TEC\Tickets\Commerce\Order;
 
-class Emails_Test extends WPTestCase {
+class Emails_Test extends Controller_Test_Case {
 	use Ticket_Maker;
 	use Order_Maker;
 	use Fee_Creator;
 	use With_Uopz;
 	use SnapshotAssertions;
 	use With_Clock_Mock;
+
+	protected string $controller_class = Emails::class;
 
 	protected static array $store = [];
 
@@ -30,7 +38,7 @@ class Emails_Test extends WPTestCase {
 		$this->set_fn_return(
 			'wp_mail',
 			function ( $to, $subject, $content, $headers, $attachments ) use ( &$store ) {
-				$store = compact( 'to', 'subject', 'content', 'headers', 'attachments' );
+				$store[] = compact( 'to', 'subject', 'content', 'headers', 'attachments' );
 				return true;
 			},
 			true
@@ -53,10 +61,11 @@ class Emails_Test extends WPTestCase {
 	 */
 	public function it_should_include_fees_in_purchase_receipt() {
 		$this->freeze_time( Dates::immutable( '2025-02-20 10:00:00' ) );
-		$post      = static::factory()->post->create( [ 'post_name' => 'Event post' ] );
+		$this->make_controller()->register();
+		$post      = static::factory()->post->create( [ 'post_title' => 'Event post' ] );
 		$ticket_id = $this->create_tc_ticket( $post, 50 );
 
-		$fee = $this->create_fee_for_ticket(
+		$this->create_fee_for_ticket(
 			$ticket_id,
 			[
 				'raw_amount' => 5,
@@ -66,17 +75,13 @@ class Emails_Test extends WPTestCase {
 
 		$this->set_class_fn_return( Tickets::class, 'generate_security_code', 'attendee-security-code' );
 
-		$order           = $this->create_order( [ $ticket_id => 2 ], [ 'purchaser_email' => 'sam@tec.com' ] );
-		$refreshed_order = tec_tc_get_order( $order->ID );
-
-		// Trigger email generation
-		do_action( 'tec_tickets_commerce_send_email_purchase_receipt', $refreshed_order );
+		$order = $this->create_order( [ $ticket_id => 2 ], [ 'purchaser_email' => 'sam@tec.com' ] );
 
 		// Ensure emails were sent
 		$this->assertEmailSent();
 
 		// Ensure snapshot correctness
-		$this->assertMatchesHtmlSnapshot( str_replace( [ $post, $ticket_id, $order->ID ], [ '{POST_ID}', '{TICKET_ID}', '{ORDER_ID}' ], self::$store['content'] ) );
+		$this->assertMatchesHtmlSnapshot( str_replace( [ $post, $ticket_id, $order->ID ], [ '{POST_ID}', '{TICKET_ID}', '{ORDER_ID}' ], self::$store['2']['content'] ) );
 	}
 
 	/**
@@ -84,10 +89,11 @@ class Emails_Test extends WPTestCase {
 	 */
 	public function it_should_include_fees_in_order_completed() {
 		$this->freeze_time( Dates::immutable( '2025-02-20 10:00:00' ) );
-		$post      = static::factory()->post->create( [ 'post_name' => 'Event post' ] );
+		$this->make_controller()->register();
+		$post      = static::factory()->post->create( [ 'post_title' => 'Event post' ] );
 		$ticket_id = $this->create_tc_ticket( $post, 50 );
 
-		$fee = $this->create_fee_for_ticket(
+		$this->create_fee_for_ticket(
 			$ticket_id,
 			[
 				'raw_amount' => 5,
@@ -96,18 +102,15 @@ class Emails_Test extends WPTestCase {
 		);
 
 		$this->set_class_fn_return( Tickets::class, 'generate_security_code', 'attendee-security-code' );
+		$this->set_class_fn_return( Order::class, 'generate_order_key', 'paypal-gateway-id' );
 
-		$order           = $this->create_order( [ $ticket_id => 2 ], [ 'purchaser_email' => 'sam@tec.com' ] );
-		$refreshed_order = tec_tc_get_order( $order->ID );
-
-		// Trigger email generation
-		do_action( 'tec_tickets_commerce_send_email_completed_order', $refreshed_order );
+		$order = $this->create_order( [ $ticket_id => 2 ], [ 'purchaser_email' => 'sam@tec.com' ] );
 
 		// Ensure emails were sent
 		$this->assertEmailSent();
 
 		// Ensure snapshot correctness
-		$this->assertMatchesHtmlSnapshot( str_replace( [ $post, $ticket_id, $order->ID ], [ '{POST_ID}', '{TICKET_ID}', '{ORDER_ID}' ], self::$store['content'] ) );
+		$this->assertMatchesHtmlSnapshot( str_replace( [ $post, $ticket_id, $order->ID ], [ '{POST_ID}', '{TICKET_ID}', '{ORDER_ID}' ], self::$store['1']['content'] ) );
 	}
 
 	/**
@@ -115,7 +118,8 @@ class Emails_Test extends WPTestCase {
 	 */
 	public function it_should_not_break_template_when_adding_multiple_fees_to_same_ticket() {
 		$this->freeze_time( Dates::immutable( '2025-02-20 10:00:00' ) );
-		$post      = static::factory()->post->create( [ 'post_name' => 'Event post' ] );
+		$this->make_controller()->register();
+		$post      = static::factory()->post->create( [ 'post_title' => 'Event post' ] );
 		$ticket_id = $this->create_tc_ticket( $post, 50 );
 
 		// Add 10 fees to the ticket
@@ -131,24 +135,22 @@ class Emails_Test extends WPTestCase {
 
 		$this->set_class_fn_return( Tickets::class, 'generate_security_code', 'attendee-security-code' );
 
-		$order           = $this->create_order( [ $ticket_id => 2 ], [ 'purchaser_email' => 'sam@tec.com' ] );
-		$refreshed_order = tec_tc_get_order( $order->ID );
-
-		// Trigger email generation
-		do_action( 'tec_tickets_commerce_send_email_purchase_receipt', $refreshed_order );
+		$order = $this->create_order( [ $ticket_id => 2 ], [ 'purchaser_email' => 'sam@tec.com' ] );
 
 		// Ensure emails were sent
 		$this->assertEmailSent();
 
 		// Ensure snapshot correctness
-		$this->assertMatchesHtmlSnapshot( str_replace( [ $post, $ticket_id, $order->ID ], [ '{POST_ID}', '{TICKET_ID}', '{ORDER_ID}' ], self::$store['content'] ) );
+		$this->assertMatchesHtmlSnapshot( str_replace( [ $post, $ticket_id, $order->ID ], [ '{POST_ID}', '{TICKET_ID}', '{ORDER_ID}' ], self::$store['2']['content'] ) );
 	}
 
 	/**
 	 * @test
 	 */
 	public function it_should_allow_multiple_fees_of_different_types_on_the_same_ticket() {
-		$post      = static::factory()->post->create( [ 'post_name' => 'Event post' ] );
+		$this->freeze_time( Dates::immutable( '2025-02-20 10:00:00' ) );
+		$this->make_controller()->register();
+		$post      = static::factory()->post->create( [ 'post_title' => 'Event post' ] );
 		$ticket_id = $this->create_tc_ticket( $post, 50 );
 
 		// Add multiple fees of different types to the same ticket
@@ -186,16 +188,12 @@ class Emails_Test extends WPTestCase {
 
 		$this->set_class_fn_return( Tickets::class, 'generate_security_code', 'attendee-security-code' );
 
-		$order           = $this->create_order( [ $ticket_id => 2 ], [ 'purchaser_email' => 'sam@tec.com' ] );
-		$refreshed_order = tec_tc_get_order( $order->ID );
-
-		// Trigger email generation
-		do_action( 'tec_tickets_commerce_send_email_purchase_receipt', $refreshed_order );
+		$order = $this->create_order( [ $ticket_id => 2 ], [ 'purchaser_email' => 'sam@tec.com' ] );
 
 		// Ensure emails were sent
 		$this->assertEmailSent();
 
 		// Ensure snapshot correctness
-		$this->assertMatchesHtmlSnapshot( str_replace( [ $post, $ticket_id, $order->ID ], [ '{POST_ID}', '{TICKET_ID}', '{ORDER_ID}' ], self::$store['content'] ) );
+		$this->assertMatchesHtmlSnapshot( str_replace( [ $post, $ticket_id, $order->ID ], [ '{POST_ID}', '{TICKET_ID}', '{ORDER_ID}' ], self::$store['2']['content'] ) );
 	}
 }
