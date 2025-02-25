85f1484976440a779bece696d58a15a32f6c27ff
Dimitrios Pantazis <dpanta94@gmail.com>
2025-02-25 20:13:29 +0200

    test coverage for handle_checkout_request

--- src/Tickets/Commerce/Gateways/Stripe/Hooks.php
+++ src/Tickets/Commerce/Gateways/Stripe/Hooks.php
@@ -360,7 +360,7 @@ class Hooks extends \TEC\Common\Contracts\Service_Provider {
 		}
 
 		// Invalid payment intent i guess?
-		if ( $payment_intent['client_secret'] !== $payment_intent_client_secret ) {
+		if ( empty( $payment_intent['client_secret'] ) || $payment_intent['client_secret'] !== $payment_intent_client_secret ) {
 			return;
 		}
 
--- tests/commerce_integration/TEC/Tickets/Commerce/Gateways/Stripe/Hooks_Test.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Gateways/Stripe/Hooks_Test.php
@@ -11,6 +11,10 @@ use TEC\Tickets\Commerce\Status\Created;
 use TEC\Tickets\Commerce\Status\Completed;
 use TEC\Tickets\Commerce\Status\Status_Handler;
 use TEC\Tickets\Commerce\Order;
+use Closure;
+use Generator;
+use TEC\Tickets\Commerce\Gateways\Stripe\Payment_Intent_Handler;
+use TEC\Tickets\Commerce\Status\Denied;
 
 class Hooks_Test extends WPTestCase {
 	use Ticket_Maker;
@@ -162,5 +166,266 @@ class Hooks_Test extends WPTestCase {
 		$this->assertSame( $wp_status_slug_from_slug( Completed::SLUG ), $refreshed_order->post_status );
 
 		$this->assertEmpty( tribe( Webhooks::class )->get_pending_webhooks( $order->ID ) );
+	}
+
+	public function checkout_request_data_provider(): Generator {
+		yield 'empty request' => [
+			function() {
+				$post = self::factory()->post->create(
+					[
+						'post_type' => 'page',
+					]
+				);
+				$ticket_id_1 = $this->create_tc_ticket( $post, 10 );
+				$order = $this->create_order( [ $ticket_id_1 => 1 ], [ 'order_status' => Pending::SLUG ] );
+
+				return [ $order->ID, false ];
+			},
+		];
+
+		yield 'with request params - no pi' => [
+			function() {
+				$post = self::factory()->post->create(
+					[
+						'post_type' => 'page',
+					]
+				);
+				$ticket_id_1 = $this->create_tc_ticket( $post, 10 );
+				$order = $this->create_order( [ $ticket_id_1 => 1 ], [ 'order_status' => Pending::SLUG ] );
+				$_REQUEST['payment_intent'] = 'pi_123';
+				$_REQUEST['payment_intent_client_secret'] = 'pi_123_secret';
+				return [ $order->ID, false ];
+			},
+		];
+
+		yield 'with request params - pi in memory' => [
+			function() {
+				$post = self::factory()->post->create(
+					[
+						'post_type' => 'page',
+					]
+				);
+				$ticket_id_1 = $this->create_tc_ticket( $post, 10 );
+				$order = $this->create_order( [ $ticket_id_1 => 1 ], [ 'order_status' => Pending::SLUG ] );
+				$_REQUEST['payment_intent'] = 'pi_123';
+				$_REQUEST['payment_intent_client_secret'] = 'pi_123_secret';
+				$pi = [
+					'id' => 'pi_123',
+					'client_secret' => 'pi_123_secret',
+					'status' => 'canceled',
+				];
+
+				tec_tc_orders()
+					->by_args(
+						[
+							'id' => $order->ID,
+						]
+					)
+					->set_args(
+						[
+							'gateway_payload'  => $pi,
+							'gateway_order_id' => $pi['id'],
+						]
+					)
+					->save();
+
+				tribe( Payment_Intent_Handler::class )->set( $pi );
+				return [ $order->ID, Denied::SLUG ];
+			},
+		];
+
+		yield 'with request params - pi in memory' => [
+			function() {
+				$post = self::factory()->post->create(
+					[
+						'post_type' => 'page',
+					]
+				);
+				$ticket_id_1 = $this->create_tc_ticket( $post, 10 );
+				$order = $this->create_order( [ $ticket_id_1 => 1 ], [ 'order_status' => Pending::SLUG ] );
+				$_REQUEST['payment_intent'] = 'pi_123';
+				$_REQUEST['payment_intent_client_secret'] = 'pi_123_secret';
+				$pi = [
+					'id'            => 'pi_123',
+					'client_secret' => 'pi_123_secret',
+					'status'        => 'canceled',
+				];
+
+				tec_tc_orders()
+					->by_args(
+						[
+							'id' => $order->ID,
+						]
+					)
+					->set_args(
+						[
+							'gateway_payload'  => $pi,
+							'gateway_order_id' => $pi['id'],
+						]
+					)
+					->save();
+
+				tribe( Payment_Intent_Handler::class )->set( $pi );
+				return [ $order->ID, Denied::SLUG ];
+			},
+		];
+
+		yield 'with request params - pi from stripe' => [
+			function() {
+				$post = self::factory()->post->create(
+					[
+						'post_type' => 'page',
+					]
+				);
+				$ticket_id_1 = $this->create_tc_ticket( $post, 10 );
+				$order = $this->create_order( [ $ticket_id_1 => 1 ], [ 'order_status' => Pending::SLUG ] );
+				$_REQUEST['payment_intent'] = 'pi_123';
+				$_REQUEST['payment_intent_client_secret'] = 'pi_123_secret';
+				$pi = [
+					'id'            => 'pi_123',
+					'client_secret' => 'pi_123_secret',
+					'status'        => 'canceled',
+				];
+
+				tec_tc_orders()
+					->by_args(
+						[
+							'id' => $order->ID,
+						]
+					)
+					->set_args(
+						[
+							'gateway_payload'  => $pi,
+							'gateway_order_id' => $pi['id'],
+						]
+					)
+					->save();
+
+				$this->set_class_fn_return( Payment_Intent::class, 'get', $pi );
+				return [ $order->ID, Denied::SLUG ];
+			},
+		];
+
+		yield 'with request params - pi in memory - with redirect' => [
+			function() {
+				$post = self::factory()->post->create(
+					[
+						'post_type' => 'page',
+					]
+				);
+				$ticket_id_1 = $this->create_tc_ticket( $post, 10 );
+				$order = $this->create_order( [ $ticket_id_1 => 1 ], [ 'order_status' => Pending::SLUG ] );
+				$_REQUEST['payment_intent'] = 'pi_123';
+				$_REQUEST['payment_intent_client_secret'] = 'pi_123_secret';
+				$pi = [
+					'id'            => 'pi_123',
+					'client_secret' => 'pi_123_secret',
+					'status'        => 'succeeded',
+				];
+
+				tec_tc_orders()
+					->by_args(
+						[
+							'id' => $order->ID,
+						]
+					)
+					->set_args(
+						[
+							'gateway_payload'  => $pi,
+							'gateway_order_id' => $pi['id'],
+						]
+					)
+					->save();
+
+				tribe( Payment_Intent_Handler::class )->set( $pi );
+				return [ $order->ID, Completed::SLUG ];
+			},
+		];
+
+		yield 'with request params - pi from stripe' => [
+			function() {
+				$post = self::factory()->post->create(
+					[
+						'post_type' => 'page',
+					]
+				);
+				$ticket_id_1 = $this->create_tc_ticket( $post, 10 );
+				$order = $this->create_order( [ $ticket_id_1 => 1 ], [ 'order_status' => Pending::SLUG ] );
+				$_REQUEST['payment_intent'] = 'pi_123';
+				$_REQUEST['payment_intent_client_secret'] = 'pi_123_secret';
+				$pi = [
+					'id'            => 'pi_123',
+					'client_secret' => 'pi_123_secret',
+					'status'        => 'succeeded',
+				];
+
+				tec_tc_orders()
+					->by_args(
+						[
+							'id' => $order->ID,
+						]
+					)
+					->set_args(
+						[
+							'gateway_payload'  => $pi,
+							'gateway_order_id' => $pi['id'],
+						]
+					)
+					->save();
+
+				$this->set_class_fn_return( Payment_Intent::class, 'get', $pi );
+				return [ $order->ID, Completed::SLUG ];
+			},
+		];
+	}
+
+	/**
+	 * @dataProvider checkout_request_data_provider
+	 */
+	public function test_the_checkout_request_is_parsed_as_indented( Closure $fixture ): void {
+		$hooks = tribe( Hooks::class );
+
+		[ $order_id, $final_status_slug ] = $fixture();
+
+		$storage = [];
+
+		$this->set_fn_return( 'tribe_exit', true );
+		$this->set_fn_return(
+			'wp_safe_redirect',
+			function ( $url ) use ( &$storage ) {
+				$storage['url'] = $url;
+				return true;
+			},
+			true
+		);
+
+		$hooks->handle_checkout_request();
+
+		$order = tec_tc_get_order( $order_id );
+		if ( ! $final_status_slug ) {
+			$this->assertEquals( Pending::SLUG, str_replace( 'tec-tc-', '', $order->post_status ) );
+			return;
+		}
+
+		$this->assertEquals( $final_status_slug, str_replace( 'tec-tc-', '', $order->post_status ) );
+
+		if ( Completed::SLUG !== $final_status_slug ) {
+			$this->assertEmpty( $storage );
+			return;
+		}
+
+		$this->assertEquals( home_url( '?tc-order-id=pi_123' ), $storage['url'] );
+	}
+
+	/**
+	 * @after
+	 */
+	public function request_cleaner(): void {
+		unset(
+			$_REQUEST['payment_intent'],
+			$_REQUEST['payment_intent_client_secret'],
+		);
+
+		tribe( Payment_Intent_Handler::class )->set( [ 'id' => true ] );
 	}
 }
