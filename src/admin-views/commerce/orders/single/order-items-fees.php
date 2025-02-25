676a91b7e4c645e311ebe3f362391313e923a989
Jeremy Pry <jeremy.pry@gmail.com>
2025-02-24 18:49:03 -0600

    Add the fees template file to the single order template

---
+++ src/admin-views/commerce/orders/single/order-items-fees.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Single order - Fees section
+ *
+ * @since TBD
+ *
+ * @version TBD
+ *
+ * @var array   $fees  The fees for the order.
+ * @var WP_Post $order The current post object (with added properties).
+ */
+
+declare( strict_types=1 );
+
+// If we don't have any fees, we don't need to display anything.
+if ( empty( $fees ) ) {
+	return;
+}
+
+?>
+<tr class="tec-tickets-commerce__fees-section">
+	<td colspan="5"><h2><?php esc_html_e( 'Fees', 'event-tickets' ); ?></h2></td>
+</tr>
+<?php foreach ( $fees as $fee ) : ?>
+	<tr class="tec-tickets-commerce-single-order--items--table--row">
+		<td><?php echo esc_html( $fee['display_name'] ); ?></td>
+		<td></td>
+		<td class="tec-tickets-commerce-single-order--items--table--row--info-column"></td>
+		<td style="padding-left: 0;">
+			<div class="tec-tickets-commerce-price-container">
+				<ins>
+					<span class="tec-tickets-commerce-price">
+						<?php echo esc_html( $fee['sub_total']->get_currency() ); ?>
+					</span>
+				</ins>
+			</div>
+		</td>
+		<td></td>
+	</tr>
+<?php endforeach; ?>
