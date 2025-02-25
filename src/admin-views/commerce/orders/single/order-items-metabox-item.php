5eb558d0799b556a28107bf071e1d805b7836bdd
Jeremy Pry <jeremy.pry@gmail.com>
2025-02-25 09:00:26 -0600

    Use stylesheet for padding, and use colspan instead of empty <td> elements

--- src/admin-views/commerce/orders/single/order-items-metabox-item.php
+++ src/admin-views/commerce/orders/single/order-items-metabox-item.php
@@ -18,7 +18,7 @@ use TEC\Tickets\Commerce\Order;
 	<td><?php echo esc_html( $ticket->name ); ?></td>
 	<td class="tribe-desktop-only"><?php echo 'series_pass' === $ticket->type ? esc_html__( 'Series Pass', 'event-tickets' ) : esc_html__( 'Standard Ticket', 'event-tickets' ); ?></td>
 	<td class="tec-tickets-commerce-single-order--items--table--row--info-column"><?php /* @todo dpan: this is were Refunded would go */ ?></td>
-	<td style="padding-left:0;">
+	<td class="tec-tickets-commerce-single-order--items--table--row--price-column">
 		<?php
 		$current  = tribe( Order::class )->get_item_value( $item );
 		$original = tribe( Order::class )->get_item_value( $item, true );
