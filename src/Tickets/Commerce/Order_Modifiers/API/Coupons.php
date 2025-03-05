ecfbedd436f418c4ccd0989ae46cbdadf86dfc7f
Jeremy Pry <jeremy.pry@gmail.com>
2025-03-05 11:24:05 -0600

    Remove unused import

--- src/Tickets/Commerce/Order_Modifiers/API/Coupons.php
+++ src/Tickets/Commerce/Order_Modifiers/API/Coupons.php
@@ -20,7 +20,6 @@ use TEC\Tickets\Commerce\Order_Modifiers\Modifiers\Coupon_Modifier_Manager as Ma
 use TEC\Tickets\Commerce\Order_Modifiers\Repositories\Coupons as Coupons_Repository;
 use TEC\Tickets\Commerce\Order_Modifiers\Traits\Coupons as CouponsTrait;
 use TEC\Tickets\Commerce\Values\Currency_Value;
-use TEC\Tickets\Commerce\Traits\Type;
 use WP_Error;
 use WP_REST_Request as Request;
 use WP_REST_Response as Response;
