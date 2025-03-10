0a3ee9f4414a7bb36d7c602aaddfd41853a1c5db
Jeremy Pry <jeremy.pry@gmail.com>
2025-03-10 18:51:24 -0500

    Update properties for modifier meta

--- src/Tickets/Commerce/Order_Modifiers/Models/Order_Modifier_Meta.php
+++ src/Tickets/Commerce/Order_Modifiers/Models/Order_Modifier_Meta.php
@@ -39,8 +39,8 @@ class Order_Modifier_Meta extends Model implements ModelCrud, ModelFromQueryBuil
 		'id'                => 'int',
 		'order_modifier_id' => 'int',
 		'meta_key'          => 'string',
-		'meta_value'        => 'string',
-		'priority'          => 'int',
+		'meta_value'        => 'string|int',
+		'priority'          => [ 'int', 0 ],
 		'created_at'        => 'string',
 	];
 
