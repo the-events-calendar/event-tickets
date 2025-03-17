a2827c2fd81ffd03b6daae4264d506287fa9d36b
Camwyn <stephen@theeventscalendar.com>
2025-03-17 10:31:31 -0400

    Don't move to a template

--- src/Tickets/Admin/Tickets/Page.php
+++ src/Tickets/Admin/Tickets/Page.php
@@ -418,7 +418,19 @@ class Page extends Abstract_Admin_Page {
 	 * @return void
 	 */
 	protected function render_ticket_table_tab_content(): void {
-		include __DIR__ . '/views/ticket-table.php';
+		tribe_asset_enqueue_group( 'event-tickets-admin-tickets' );
+
+		/** @var Tribe__Tickets__Admin__Views $admin_views */
+		$admin_views = tribe( 'tickets.admin.views' );
+
+		$context = [
+			'tickets_table'  => tribe( List_Table::class ),
+			'page_slug'      => Page::$page_slug,
+			'tickets_exist'  => Page::tickets_exist(),
+			'edit_posts_url' => $this->get_link_to_edit_posts(),
+		];
+
+		$admin_views->template( 'admin-tickets', $context );
 	}
 
 	/**
