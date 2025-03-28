fe847d88c82ffde41f82ef9b47af5bf49592374b
Jeremy Pry <jeremy.pry@gmail.com>
2025-03-28 18:07:47 -0500

    Add is_active() check with tec_using_classy_editor()

--- tests/integration/TEC/Tickets/Blocks/ControllerTest.php
+++ tests/integration/TEC/Tickets/Blocks/ControllerTest.php
@@ -3,7 +3,20 @@
 namespace TEC\Tickets\Blocks;
 
 use TEC\Common\Tests\Provider\Controller_Test_Case;
+use Tribe\Tests\Traits\With_Uopz;
 
 class ControllerTest extends Controller_Test_Case {
+
+	use With_Uopz;
+
 	protected string $controller_class = Controller::class;
+
+	/**
+	 * @test
+	 */
+	public function it_should_not_register_with_classy_active() {
+		$this->set_fn_return( 'tec_using_classy_editor', true );
+		$this->make_controller()->register();
+		$this->assertFalse( $this->controller_class::is_registered(), 'Controller should not be registered with Classy active' );
+	}
 }
