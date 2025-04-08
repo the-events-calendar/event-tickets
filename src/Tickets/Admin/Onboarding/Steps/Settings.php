ea4eb13e3f748e28a65f74f39fbcaa4de9f15a06
Camwyn <stephen@theeventscalendar.com>
2025-04-07 21:25:39 -0400

    missing docblocks

--- src/Tickets/Admin/Onboarding/Steps/Settings.php
+++ src/Tickets/Admin/Onboarding/Steps/Settings.php
@@ -11,7 +11,7 @@ namespace TEC\Tickets\Admin\Onboarding\Steps;
 
 use TEC\Common\Admin\Onboarding\Steps\Abstract_Step;
 use WP_REST_Response;
-
+use WP_REST_Request;
 /**
  * Settings step for the onboarding wizard.
  *
@@ -21,8 +21,17 @@ use WP_REST_Response;
  */
 class Settings extends Abstract_Step {
 
+	/**
+	 * Process the step.
+	 *
+	 * @since TBD
+	 *
+	 * @param WP_REST_Response $response The response object.
+	 * @param WP_REST_Request  $request  The request object.
+	 *
+	 * @return WP_REST_Response
+	 */
 	public static function process( $response, $request ): WP_REST_Response {
 		return new WP_REST_Response( [ 'success' => true ] );
 	}
-
 }
