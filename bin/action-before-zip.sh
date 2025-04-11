dbf01944da48ada60bda9c1540059334a69c3129
George Pattichis <gpattihis@gmail.com>
2025-04-12 02:05:28 +0300

    Alternate build mode: development

--- bin/action-before-zip.sh
+++ bin/action-before-zip.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# Default to production mode if not specified
+BUILD_MODE=${1:-production}
+
+# If 'dev' is passed, set to development mode
+if [ "$1" = "dev" ]; then
+    BUILD_MODE="development"
+    DEVTOOL="--devtool=source-map"
+else
+    DEVTOOL=""
+fi
+
 echo "Moving the Webpack 4 directory out of the way to prevent conflicts."
 mv node_modules/webpack /tmp/_webpack
 mv node_modules/webpack-cli /tmp/_webpack-cli
@@ -6,12 +19,12 @@ echo "Moving configuration files out of the way."
 mv webpack.config.js _webpack.config.js
 mv babel.config.json _babel.config.json
 
-echo "Building the JS with the correct Webpack version."
+echo "Building the JS with the correct Webpack version in $BUILD_MODE mode."
 npx \
 	--package="@tanstack/react-query" \
 	--package="@wordpress/scripts@27.9.0" \
 	--yes -- \
-	wp-scripts build --webpack-src-dir=src/resources/packages/wizard/ --output-path=build/wizard
+	wp-scripts build --webpack-src-dir=src/resources/packages/wizard/ --output-path=build/wizard --mode=$BUILD_MODE $DEVTOOL
 
 echo "Moving the Webpack 4 directory back to node_modules."
 mv /tmp/_webpack node_modules/webpack
