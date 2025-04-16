22269ba57384a2719468f4fff167ed7ad07f54b7
jesseeproductions <info@jesseeproductions.com>
2025-04-15 22:36:03 -0400

    ETP-1003 - prevent notice 2

--- src/Tribe/RSVP.php
+++ src/Tribe/RSVP.php
@@ -2320,9 +2320,9 @@ class Tribe__Tickets__RSVP extends Tribe__Tickets__Tickets {
 		}
 
 		$checkin_details = [
-			'date'      => $details['timestamp'] ?: current_time( 'mysql' ),
-			'source' => ! empty( $qr ) ? 'app' : 'site',
-			'author' => get_current_user_id(),
+			'date'      => ! empty( $details['timestamp'] ) ?: current_time( 'mysql' ),
+			'source'    => ! empty( $qr ) ? 'app' : 'site',
+			'author'    => get_current_user_id(),
 			'device_id' => $details['device_id'] ?? null,
 		];
 
