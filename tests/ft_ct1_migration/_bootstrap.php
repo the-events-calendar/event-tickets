7deb148ebce3b4f2bc3665facae73ea714110dfa
Camwyn <stephen@theeventscalendar.com>
2025-04-22 16:59:51 -0400

    remove a few more unnecessary changes

--- tests/ft_ct1_migration/_bootstrap.php
+++ tests/ft_ct1_migration/_bootstrap.php
@@ -80,11 +80,9 @@ $clean_after_test = static function () {
 };
 addListener( Codeception\Events::TEST_AFTER, $clean_after_test );
 
-// Move the CT1 setup to plugins_loaded action
 addListener( Codeception\Events::SUITE_BEFORE, static function () {
 	putenv( 'TEC_CUSTOM_TABLES_V1_DISABLED=0' );
 	$_ENV['TEC_CUSTOM_TABLES_V1_DISABLED'] = 0;
-
 	add_filter( 'tec_events_custom_tables_v1_enabled', '__return_true' );
 	tribe()->register( TEC\Events\Custom_Tables\V1\Provider::class );
 	tribe()->register( TEC\Events_Pro\Custom_Tables\V1\Provider::class );
