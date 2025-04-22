d859e08ec704aeeb0d46b929fc13668454552df9
Camwyn <stephen@theeventscalendar.com>
2025-04-22 16:12:37 -0400

    not working, revert bootstrap change

--- tests/ft_ct1_migration/_bootstrap.php
+++ tests/ft_ct1_migration/_bootstrap.php
@@ -81,7 +81,6 @@ $clean_after_test = static function () {
 addListener( Codeception\Events::TEST_AFTER, $clean_after_test );
 
 addListener( Codeception\Events::SUITE_BEFORE, static function () {
-	require_once __DIR__ . '/../../common/src/Tribe/Container.php';
 	putenv( 'TEC_CUSTOM_TABLES_V1_DISABLED=0' );
 	$_ENV['TEC_CUSTOM_TABLES_V1_DISABLED'] = 0;
 	add_filter( 'tec_events_custom_tables_v1_enabled', '__return_true' );
