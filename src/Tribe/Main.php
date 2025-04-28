088d8b8940d34a1ba8d2cab9e88cbfcc711e673e
Camwyn <stephen@theeventscalendar.com>
2025-04-28 12:24:25 -0400

    version

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -18,7 +18,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin.
 	 */
-	const VERSION = '5.21.1';
+	const VERSION = '5.23.0';
 
 	/**
 	 * Used to store the version history.
