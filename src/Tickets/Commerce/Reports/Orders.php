015554c61c5f27470682830a6a2859184a3b2cc5
Andras Guseo <andras@theeventscalendar.com>
2025-05-13 14:48:23 +0200

    Hook into the page_row_actions filter to add a link to the orders from the page list

--- src/Tickets/Commerce/Reports/Orders.php
+++ src/Tickets/Commerce/Reports/Orders.php
@@ -189,6 +189,7 @@ class Orders extends Report_Abstract {
 	 */
 	public function hook() {
 		add_filter( 'post_row_actions', [ $this, 'add_orders_row_action' ], 10, 2 );
+		add_filter( 'page_row_actions', [ $this, 'add_orders_row_action' ], 10, 2 );
 		// Register before the default priority of 10 to avoid submenu hook issues.
 		add_action( 'admin_menu', [ $this, 'register_orders_page' ], 5 );
 
