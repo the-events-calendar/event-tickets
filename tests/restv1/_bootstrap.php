faaa6b12d5239931f3ee8d7af0b0f0fe3be09a4c
Brian Krane <redscar@gmail.com>
2025-05-14 10:31:26 -0400

    Unhook `tribe_tickets_plugin_loaded` during the restv1 test.

--- tests/restv1/_bootstrap.php
+++ tests/restv1/_bootstrap.php
@@ -1,1 +1,10 @@
 <?php
+
+/**
+ * Remove the deprecated 'tribe_tickets_plugin_loaded' hook to prevent
+ * deprecated warnings during REST API tests that interfere with response headers.
+ * The replacement hook 'tec_tickets_fully_loaded' should be used instead.
+ *
+ * @since TBD
+ */
+remove_all_actions( 'tribe_tickets_plugin_loaded' );
