93b56ae354049f5737bf0e03e8423cc93e103801
Dimitrios Pantazis <dpanta94@gmail.com>
2025-05-21 20:04:26 +0300

    Fix commerce integration suite

--- tests/_support/Commerce/TicketsCommerce/Order_Maker.php
+++ tests/_support/Commerce/TicketsCommerce/Order_Maker.php
@@ -4,6 +4,8 @@ namespace Tribe\Tickets\Test\Commerce\TicketsCommerce;
 
 use TEC\Tickets\Commerce\Cart;
 use TEC\Tickets\Commerce\Gateways\PayPal\Gateway;
+use TEC\Tickets\Commerce\Gateways\Stripe\Gateway as Stripe_Gateway;
+use TEC\Tickets\Commerce\Gateways\Square\Gateway as Square_Gateway;
 use TEC\Tickets\Commerce\Order;
 use TEC\Tickets\Commerce\Status\Completed;
 use TEC\Tickets\Commerce\Status\Pending;
@@ -48,6 +50,34 @@ trait Order_Maker {
 		$cart->clear_cart();
 
 		return $order;
+	}
+
+	/**
+	 * Create an order through Stripe.
+	 *
+	 * @param array $items     An array with the item ID as the key, and data as value.
+	 * @param array $overrides An array of overrides for the purchaser.
+	 *
+	 * @return false|WP_Post The order post object or false if the order could not be created.
+	 */
+	protected function create_order_through_stripe( array $items, array $overrides = [] ) {
+		$gateway = tribe( Stripe_Gateway::class );
+
+		return $this->create_order( $items, array_merge( $overrides, [ 'gateway' => $gateway ] ) );
+	}
+
+	/**
+	 * Create an order through Square.
+	 *
+	 * @param array $items     An array with the item ID as the key, and data as value.
+	 * @param array $overrides An array of overrides for the purchaser.
+	 *
+	 * @return false|WP_Post The order post object or false if the order could not be created.
+	 */
+	protected function create_order_through_square( array $items, array $overrides = [] ) {
+		$gateway = tribe( Square_Gateway::class );
+
+		return $this->create_order( $items, array_merge( $overrides, [ 'gateway' => $gateway ] ) );
 	}
 
 	/**
