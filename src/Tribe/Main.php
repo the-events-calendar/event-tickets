a4a54b885ca229339fa0d357f70866ffe103a575
Camwyn <929375+Camwyn@users.noreply.github.com>
2025-05-22 12:35:09 +0000

    Version bump for release/T25.elongatedman
    This is an automated PR created by Camwyn.
    Generated by: https://github.com/the-events-calendar/event-tickets/actions/runs/15186708647

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -18,7 +18,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin.
 	 */
-	const VERSION = '5.23.0';
+	const VERSION = '5.23.1';
 
 	/**
 	 * Used to store the version history.
