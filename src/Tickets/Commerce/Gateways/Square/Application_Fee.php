5ce96106f58f055dc824b01b82fdf119dcc2428f
Gustavo Bordoni <bordoni.dev@gmail.com>
2025-05-27 00:24:57 -0400

    Add application fee to the Price.

---
+++ src/Tickets/Commerce/Gateways/Square/Application_Fee.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace TEC\Tickets\Commerce\Gateways\Square;
+
+use TEC\Tickets\Commerce\Settings;
+use TEC\Tickets\Commerce\Utils\Value;
+
+/**
+ * The Square Application_Fee class
+ *
+ * @package TEC\Tickets\Commerce\Gateways\Square
+ */
+class Application_Fee {
+
+	/**
+	 * The percentage applied to Square transactions. Currently set at 2%.
+	 *
+	 * @since TBD
+	 *
+	 * @var float
+	 */
+	const FIXED_FEE = 0.02;
+
+	/**
+	 * Calculate the fee value that needs to be applied to the PaymentIntent.
+	 *
+	 * @since TBD
+	 *
+	 * @param Value $value the value over which to calculate the fee.
+	 *
+	 * @return Value;
+	 */
+	public static function calculate( Value $value ) {
+		if ( Settings::is_licensed_plugin( true ) ) {
+			return Value::create();
+		}
+
+		// Otherwise, calculate it over the total value.
+		return Value::create( $value->get_decimal() * static::get_application_fee_percentage() );
+	}
+
+	/**
+	 * Returns the application fee percentage value.
+	 *
+	 * @since TBD
+	 *
+	 * @return float
+	 */
+	private static function get_application_fee_percentage() {
+		return static::FIXED_FEE;
+	}
+}
