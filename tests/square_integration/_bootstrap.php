5b4209bac179109717e848d9d4014cd93da13dee
Dimitrios Pantazis <dpanta94@gmail.com>
2025-05-28 13:42:09 +0300

    added bootstrap file

---
+++ tests/square_integration/_bootstrap.php
@@ -0,0 +1,26 @@
+<?php
+use TEC\Common\StellarWP\DB\DB;
+use TEC\Tickets\Commerce\Provider as Commerce_Provider;
+use Tribe\Tickets\Promoter\Triggers\Dispatcher;
+
+$tec_support = dirname( __DIR__, 3 ) . '/the-events-calendar/tests/_support';
+Codeception\Util\Autoload::addNamespace( 'Tribe\Events\Test', $tec_support );
+
+// Let's make sure Commerce is enabled.
+putenv( 'TEC_TICKETS_COMMERCE=1' );
+putenv( 'TEC_DISABLE_LOGGING=1' );
+tribe_register_provider( Commerce_Provider::class );
+// Ensure `post` is a ticketable post type.
+$ticketable   = tribe_get_option( 'ticket-enabled-post-types', [] );
+$ticketable[] = 'post';
+tribe_update_option( 'ticket-enabled-post-types', array_values( array_unique( $ticketable ) ) );
+
+define( 'JSON_SNAPSHOT_OPTIONS', JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES );
+
+// Start the posts auto-increment from a high number to make it easier to replace the post IDs in HTML snapshots.
+DB::query( DB::prepare( "ALTER TABLE %i AUTO_INCREMENT = 5096", DB::prefix( 'posts' ) ) );
+
+// Disconnect Promoter to avoid license-related notices.
+remove_action( 'tribe_tickets_promoter_trigger', [ tribe( Dispatcher::class ), 'trigger' ] );
+
+tec_tickets_tests_fake_transactions_enable();
