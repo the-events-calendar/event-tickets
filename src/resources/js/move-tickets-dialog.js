9f7e5e92345ab28a5d6c007c4c6e5c4712dea933
Sami Dokus <sami2464@gmail.com>
2025-05-30 14:55:54 -0600

    Formatting fixes

--- src/resources/js/move-tickets-dialog.js
+++ src/resources/js/move-tickets-dialog.js
@@ -246,38 +246,38 @@ var tribe_move_tickets = tribe_move_tickets || {};
 					return;
 				}
 
-				// Clear the existing list
+				// Clear the existing list.
 				$post_choices.html( '' );
-				var total_posts = 0;
-                
-                // Convert the object to a sortable array
-                var posts_array = [];
-                for (var key in response.data.posts) {
-                    posts_array.push({
-                        id: key,
-                        title: response.data.posts[key]
-                    });
-                }
-                
-                // Sort by title alphabetically
-                posts_array.sort(function(a, b) {
-                    return a.title.localeCompare(b.title);
-                });
+				let total_posts = 0;
+				
+				// Convert the object to a sortable array.
+				let posts_array = [];
+				for ( let key in response.data.posts ) {
+					posts_array.push( {
+						id: key,
+						title: response.data.posts[key]
+					} );
+				}
+				
+				// Sort by title alphabetically.
+				posts_array.sort( function( a, b ) {
+					return a.title.localeCompare( b.title );
+				} );
 
-                // Use the sorted array to build the options
-                for (var i = 0; i < posts_array.length; i++) {
-                    var post_id = parseInt(posts_array[i].id, 10);
-                    var title = posts_array[i].title;
-                    total_posts++;
+				// Use the sorted array to build the options.
+				for ( let i = 0; i < posts_array.length; i++ ) {
+					const post_id = parseInt( posts_array[i].id, 10 );
+					const title = posts_array[i].title;
+					total_posts++;
 
-                    $post_choices.append(
-                        '<label> <input type="radio" value="' + post_id + '" name="post-choice">' + title + '</label>'
-                    );
-                }
+					$post_choices.append(
+						`<label> <input type="radio" value="${post_id}" name="post-choice">${title}</label>`
+					);
+				}
 
 				if ( ! total_posts ) {
 					$post_choices.append(
-						'<label>' + tribe_move_tickets_data.no_posts_found + '</label>'
+						`<label>${tribe_move_tickets_data.no_posts_found}</label>`
 					);
 				}
 
