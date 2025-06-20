05b117d78ba7cbb34c338cca4bfa10c8907edab5
Jeremy Pry <jeremy.pry@gmail.com>
2025-06-20 18:37:10 -0500

    Add initial TicketUpsertModal to Tickets field

--- src/resources/packages/classy/fields/Tickets/Tickets.tsx
+++ src/resources/packages/classy/fields/Tickets/Tickets.tsx
@@ -2,27 +2,59 @@ import * as React from 'react';
 import { Fragment, useEffect, useState } from 'react';
 import { _x } from '@wordpress/i18n';
 import {
-	AddTicket
+	AddTicket,
+	TicketUpsertModal,
 } from '../../components';
+import { Ticket as TicketData } from '../../types/Ticket';
 
 type TicketsProps = {
 	tickets: []
+};
+
+const defaultTicket: TicketData = {
+	name: 'Default Ticket',
+	description: '',
+	price: '',
+	hasSalePrice: false,
+	salePrice: '',
+	capacityType: 'general-admission',
+	selectedFees: [],
+	displayedFees: [],
+	capacity: '',
+	capacityShared: false
 };
 
 export default function Tickets( props: TicketsProps ): JSX.Element {
 
 	const { tickets } = props;
 
-	const [ hasTickets, setHasTickets ] = React.useState( tickets.length > 0 );
+	const [ hasTickets, setHasTickets ] = useState( false );
+
+	const [ isUpserting, setIsUpserting ] = useState( false );
+
+	const onTicketAddedClicked = () => {
+		console.log( 'Ticket added clicked' );
+		setIsUpserting( true );
+	}
 
 	return (
-		<Fragment>
+		<div className="classy-field classy-field--tickets">
 			{ ! hasTickets && (
 				<AddTicket
 					buttonText={ _x( 'Add Tickets', 'Button text to add a new ticket', 'event-tickets' ) }
-					onClick={ () => console.log( 'Add ticket clicked' ) }
+					onClick={ onTicketAddedClicked }
 				/>
 			) }
-		</Fragment>
+
+			{ isUpserting && (
+				<TicketUpsertModal
+					isUpdate={ ! hasTickets }
+					onCancel={ () => setIsUpserting( false ) }
+					onClose={ () => setIsUpserting( false ) }
+					onSave={ () => {} }
+					values={ defaultTicket }
+				/>
+			) }
+		</div>
 	);
 }
