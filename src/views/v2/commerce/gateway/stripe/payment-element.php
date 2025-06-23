5891db4a5bcc16a0971da437ecbd3ce6b0c11bf4
rafsuntaskin <rafsun.aiub@gmail.com>
2025-06-24 00:45:43 +0600

    update template to avoid undefined warning

--- src/views/v2/commerce/gateway/stripe/payment-element.php
+++ src/views/v2/commerce/gateway/stripe/payment-element.php
@@ -7,21 +7,28 @@
  *
  * See more documentation about our views templating system.
  *
- * @link    https://evnt.is/1amp Help article for RSVP & Ticket template files.
+ * @link https://evnt.is/1amp Help article for RSVP & Ticket template files.
  *
  * @since 5.3.0
+ * @since TBD Add hidden class var to avoid undefined warnings.
  *
- * @version 5.3.0
+ * @version TBD
+ *
+ * @var Checkout_Shortcode $shortcode [Global] The checkout shortcode instance.
  * @var bool $must_login      [Global] Whether login is required to buy tickets or not.
  * @var bool $payment_element [Global] Whether to load the Stripe Payment Element.
  */
 
+use TEC\Tickets\Commerce\Shortcodes\Checkout_Shortcode;
+
 if ( $must_login || ! $payment_element ) {
 	return;
 }
+
+$hidden = $shortcode->should_display_billing_info() ? 'tribe-common-a11y-hidden': '';
 ?>
 <div id="tec-tc-gateway-stripe-payment-element" class="tribe-tickets__commerce-checkout-stripe-payment-element"></div>
-<button id="tec-tc-gateway-stripe-checkout-button" class="tribe-common-c-btn tribe-tickets__commerce-checkout-form-submit-button <?php echo $show_address ? 'tribe-common-a11y-hidden' : ''; ?>">
+<button id="tec-tc-gateway-stripe-checkout-button" class="tribe-common-c-btn tribe-tickets__commerce-checkout-form-submit-button <?php echo esc_attr( $hidden ) ?>">
 	<div class="spinner hidden" id="spinner"></div>
 	<span id="button-text">
 		<?php
