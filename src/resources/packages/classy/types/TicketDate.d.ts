1a20d2e34d47b379f4de95285da7cc7f97243eaf
Jeremy Pry <jeremy.pry@gmail.com>
2025-06-25 15:21:32 -0500

    Use Days type from common for TicketDate

--- src/resources/packages/classy/types/TicketDate.d.ts
+++ src/resources/packages/classy/types/TicketDate.d.ts
@@ -1,3 +1,4 @@
+import { Days } from '@tec/common/classy/types/Days';
 import { Hours } from '@tec/common/classy/types/Hours';
 import { Minutes } from '@tec/common/classy/types/Minutes';
 import { Months } from '@tec/common/classy/types/Months';
@@ -7,7 +8,7 @@ type Seconds = Minutes;
 export type TicketDate = {
 	year: number;
 	month: Months;
-	day: number;
+	day: Days;
 	hour?: Hours;
 	minute?: Minutes;
 	second?: Seconds;
