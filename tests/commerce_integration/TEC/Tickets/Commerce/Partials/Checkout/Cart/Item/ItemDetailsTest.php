e893c19bd6a566c9cd552970c2c29755ead5f5b8
Sami Dokus <sami2464@gmail.com>
2025-06-30 16:22:33 -0600

    Slight tweaks to test

--- tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/Item/ItemDetailsTest.php
+++ tests/commerce_integration/TEC/Tickets/Commerce/Partials/Checkout/Cart/Item/ItemDetailsTest.php
@@ -9,17 +9,19 @@ class ItemDetailsTest extends TicketsCommerceSnapshotTestCase {
 
 	protected $partial_path = 'checkout/cart/item/details';
 
+	/**
+	 * Test that toggle and description ARE rendered when ticket has a description and should show it.
+	 *
+	 * @since TBD
+	 */
 	public function test_should_render_cart_item_details() {
-
-		$ticket_id        = $this->get_mock_thing( 'tickets/1.json' );
-		$ticket['obj'] = tribe( Ticket::class )->get_ticket( $ticket_id );
+		$ticket_id           = $this->get_mock_thing( 'tickets/1.json' );
+		$ticket['obj']       = tribe( Ticket::class )->get_ticket( $ticket_id );
 		$ticket['ticket_id'] = $ticket_id;
 
 		$this->assertMatchesHtmlSnapshot( $this->get_partial_html( [
-				'item' => $ticket,
-			]
-		) );
-
+			'item' => $ticket,
+		] ) );
 	}
 
 	/**
@@ -28,19 +30,18 @@ class ItemDetailsTest extends TicketsCommerceSnapshotTestCase {
 	 * @since TBD
 	 */
 	public function test_should_render_cart_item_details_without_toggle_when_no_description() {
-		$ticket_id = $this->get_mock_thing( 'tickets/1.json' );
-		$ticket_obj = tribe( Ticket::class )->get_ticket( $ticket_id );
+		$ticket_id           = $this->get_mock_thing( 'tickets/1.json' );
+		$ticket_obj          = tribe( Ticket::class )->get_ticket( $ticket_id );
 		
 		// Remove the description to test the conditional logic.
 		$ticket_obj->description = '';
 		
-		$ticket['obj'] = $ticket_obj;
+		$ticket['obj']       = $ticket_obj;
 		$ticket['ticket_id'] = $ticket_id;
 
 		$this->assertMatchesHtmlSnapshot( $this->get_partial_html( [
-				'item' => $ticket,
-			]
-		) );
+			'item' => $ticket,
+		] ) );
 	}
 
 	/**
@@ -49,22 +50,21 @@ class ItemDetailsTest extends TicketsCommerceSnapshotTestCase {
 	 * @since TBD
 	 */
 	public function test_should_render_cart_item_details_without_toggle_when_show_description_false() {
-		$ticket_id = $this->get_mock_thing( 'tickets/1.json' );
-		$ticket_obj = tribe( Ticket::class )->get_ticket( $ticket_id );
+		$ticket_id           = $this->get_mock_thing( 'tickets/1.json' );
+		$ticket_obj          = tribe( Ticket::class )->get_ticket( $ticket_id );
 		
 		// Create a mock that returns false for show_description().
-		$mock_ticket = $this->createMock( get_class( $ticket_obj ) );
+		$mock_ticket              = $this->createMock( get_class( $ticket_obj ) );
 		$mock_ticket->method( 'show_description' )->willReturn( false );
 		$mock_ticket->description = 'This ticket has a description but should not show it';
-		$mock_ticket->post_title = $ticket_obj->post_title;
-		$mock_ticket->ID = $ticket_obj->ID;
+		$mock_ticket->post_title  = $ticket_obj->post_title;
+		$mock_ticket->ID          = $ticket_obj->ID;
 		
-		$ticket['obj'] = $mock_ticket;
+		$ticket['obj']       = $mock_ticket;
 		$ticket['ticket_id'] = $ticket_id;
 
 		$this->assertMatchesHtmlSnapshot( $this->get_partial_html( [
-				'item' => $ticket,
-			]
-		) );
+			'item' => $ticket,
+		] ) );
 	}
 }
