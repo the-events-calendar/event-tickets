76df789de75c8d66be3976cbcbb242f786a759dd
Jeremy Pry <jeremy.pry@gmail.com>
2025-07-11 17:29:51 -0500

    Add docs to the Api types

--- src/resources/packages/classy/types/Api.d.ts
+++ src/resources/packages/classy/types/Api.d.ts
@@ -1,11 +1,21 @@
-import { Ticket } from "./Ticket";
+import { Ticket } from './Ticket';
 
+/**
+ * Parameters to be used when fetching tickets from the API.
+ *
+ * @since TBD
+ */
 export type TicketsApiParams = {
 	include_post?: number[];
 	per_page?: number;
 	page?: number;
 }
 
+/**
+ * Response structure for retrieving multiple tickets from the API.
+ *
+ * @since TBD
+ */
 export type GetTicketsApiResponse = {
 	rest_url: string;
 	total: number;
