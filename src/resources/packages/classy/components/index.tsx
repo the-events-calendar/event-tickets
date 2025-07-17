2846933d6105914bacb101216981ef94639544a8
Jeremy Pry <jeremy.pry@gmail.com>
2025-07-16 19:57:23 -0500

    Copy Clock icon from legacy blocks

--- src/resources/packages/classy/components/index.tsx
+++ src/resources/packages/classy/components/index.tsx
@@ -1,5 +1,6 @@
 export { AddTicket } from './AddTicket';
 export { CurrencyInput } from './CurrencyInput';
+export * from './Icons';
 export { TicketTable } from './TicketTable';
 export { TicketUpsert } from './TicketUpsert';
 export { TicketUpsertModal } from './TicketUpsertModal';
