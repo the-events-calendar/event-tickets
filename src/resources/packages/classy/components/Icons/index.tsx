2a521af5fce33a718960a7fba219bbe00e564c80
Jeremy Pry <jeremy.pry@gmail.com>
2025-07-18 16:43:51 -0500

    Create ChevronUp/Down icons

--- src/resources/packages/classy/components/Icons/index.tsx
+++ src/resources/packages/classy/components/Icons/index.tsx
@@ -1,2 +1,4 @@
+export { default as ChevronDownIcon } from './ChevronDown';
+export { default as ChevronUpIcon } from './ChevronUp';
 export { default as ClipboardIcon } from './Clipboard';
 export { default as ClockIcon } from './Clock';
