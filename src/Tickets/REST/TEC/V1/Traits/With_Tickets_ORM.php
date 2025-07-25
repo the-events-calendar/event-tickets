7f974c5605975a86fa882862f69049d1a3fa8ffe
Dimitrios Pantazis <dpanta94@gmail.com>
2025-07-25 13:31:25 +0300

    Tickets ORM trait

---
+++ src/Tickets/REST/TEC/V1/Traits/With_Tickets_ORM.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Trait to provide tickets ORM access.
+ *
+ * @since TBD
+ *
+ * @package TEC\Tickets\REST\TEC\V1\Traits
+ */
+
+declare( strict_types=1 );
+
+namespace TEC\Tickets\REST\TEC\V1\Traits;
+
+use TEC\Tickets\Commerce\Repositories\Tickets_Repository;
+use Tribe__Repository__Interface;
+
+/**
+ * Trait With_Tickets_ORM.
+ *
+ * @since TBD
+ *
+ * @package TEC\Tickets\REST\TEC\V1\Traits
+ */
+trait With_Tickets_ORM {
+	/**
+	 * Returns a repository instance.
+	 *
+	 * @since TBD
+	 *
+	 * @return Tribe__Repository__Interface The repository instance.
+	 */
+	public function get_orm(): Tribe__Repository__Interface {
+		return tribe( Tickets_Repository::class );
+	}
+}
