fbd202067620330b3206bdaf59f8bd679ab28cbd
Dimitrios Pantazis <dpanta94@gmail.com>
2025-07-31 15:42:40 +0300

    No experimental endpoints during tests

--- tests/rest_tec_v1_integration/_bootstrap.php
+++ tests/rest_tec_v1_integration/_bootstrap.php
@@ -11,5 +11,7 @@ $ticketable[] = 'post';
 $ticketable[] = 'page';
 tribe_update_option( 'ticket-enabled-post-types', array_values( array_unique( $ticketable ) ) );
 
+add_filter( 'tec_rest_experimental_endpoint', '__return_false' );
+
 // Enable fake transactions for testing
 tec_tickets_tests_fake_transactions_enable();
