44a2b4016d4de3a1e73ed7219f6b38f93f31736f
Stephen Page <stephen@theeventscalendar.com>
2025-08-12 09:40:26 -0400

    Bump the TEC required version to 6.25.0.

--- src/Tribe/Plugin_Register.php
+++ src/Tribe/Plugin_Register.php
@@ -25,7 +25,7 @@ class Tribe__Tickets__Plugin_Register extends Tribe__Abstract_Plugin_Register {
 		'addon-dependencies' => [
 			'Tribe__Tickets_Plus__Main'               => '6.7.0-dev',
 			'Tribe__Events__Community__Tickets__Main' => '4.9.3-dev',
-			'Tribe__Events__Main'                     => '6.13.2-dev',
+			'Tribe__Events__Main'                     => '6.15.0-dev',
 		],
 	];
 
