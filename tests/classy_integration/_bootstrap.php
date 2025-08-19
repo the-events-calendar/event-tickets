2edbcd7d97a84db7645fc82c7b7a10e8007c1ee9
Jeremy Pry <jeremy.pry@gmail.com>
2025-08-18 21:13:36 -0500

    Ensure the Classy editor is enabled in the bootstrap

--- tests/classy_integration/_bootstrap.php
+++ tests/classy_integration/_bootstrap.php
@@ -1,2 +1,6 @@
 <?php
-assert( tec_using_classy_editor() === true, 'Classy editor must be enabled.' );
+
+declare( strict_types=1 );
+
+// Classy needs to be enabled for the tests to run.
+add_filter( 'tec_using_classy_editor', '__return_true' );
