5ebb6629598a20ef3b8dea59c2ae3a008a90d1a5
dpanta94 <29694484+dpanta94@users.noreply.github.com>
2025-09-01 17:11:42 +0000

    Version bump for release/T25.huntress
    This is an automated PR created by dpanta94.
    Generated by: https://github.com/the-events-calendar/event-tickets/actions/runs/17383716455

--- src/Tribe/Main.php
+++ src/Tribe/Main.php
@@ -18,7 +18,7 @@ class Tribe__Tickets__Main {
 	/**
 	 * Current version of this plugin.
 	 */
-	const VERSION = '5.26.1';
+	const VERSION = '5.26.2';
 
 	/**
 	 * Used to store the version history.
