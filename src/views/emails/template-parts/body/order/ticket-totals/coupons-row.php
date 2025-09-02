e92a9a6dbbd313cf9f82fb9e8ec3e52f9595c738
Sam Dokus <95599199+sdokus@users.noreply.github.com>
2025-09-02 16:52:08 -0600

    Update src/views/emails/template-parts/body/order/ticket-totals/coupons-row.php
    Co-authored-by: Jeremy Pry <JPry@users.noreply.github.com>

--- src/views/emails/template-parts/body/order/ticket-totals/coupons-row.php
+++ src/views/emails/template-parts/body/order/ticket-totals/coupons-row.php
@@ -13,14 +13,14 @@
  *
  * @since TBD
  *
- * @var Tribe__Template                    $this               Current template object.
- * @var Email_Abstract $email              The email object.
- * @var string                             $heading            The email heading.
- * @var string                             $title              The email title.
- * @var bool                               $preview            Whether the email is in preview mode or not.
- * @var string                             $additional_content The email additional content.
- * @var bool                               $is_tec_active      Whether `The Events Calendar` is active or not.
- * @var \WP_Post                           $order              The order object.
+ * @var Tribe__Template $this               Current template object.
+ * @var Email_Abstract  $email              The email object.
+ * @var string          $heading            The email heading.
+ * @var string          $title              The email title.
+ * @var bool            $preview            Whether the email is in preview mode or not.
+ * @var string          $additional_content The email additional content.
+ * @var bool            $is_tec_active      Whether `The Events Calendar` is active or not.
+ * @var \WP_Post        $order              The order object.
  */
 
 declare( strict_types=1 );
