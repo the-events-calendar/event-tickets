e7364b5c191f19ab54e24ccfa4e547fa91936a60
Sami Dokus <sami2464@gmail.com>
2025-09-03 12:37:55 -0600

    update snapshots again

--- tests/integration/TEC/Tickets/Emails/Admin/__snapshots__/Preview_ModalTest__it_should_match_snapshot_for_individual_email_preview with data set rsvp-not-going__1.php
+++ tests/integration/TEC/Tickets/Emails/Admin/__snapshots__/Preview_ModalTest__it_should_match_snapshot_for_individual_email_preview with data set rsvp-not-going__1.php
@@ -399,7 +399,8 @@
 			padding: 15px 0 30px 0 !important;
 		}
 	}
-</style><div class="tec-tickets__email-body">
+</style>
+<div class="tec-tickets__email-body">
 
 		
 		<table role="presentation" class="tec-tickets__email-table-main">
