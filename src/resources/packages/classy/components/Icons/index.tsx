b962567c49e0a480fb278cc988050e5a0c069269
Jeremy Pry <jeremy.pry@gmail.com>
2025-10-24 09:56:28 -0500

    Add the Timer icon

--- src/resources/packages/classy/components/Icons/index.tsx
+++ src/resources/packages/classy/components/Icons/index.tsx
@@ -2,3 +2,4 @@ export { default as ChevronDownIcon } from './ChevronDown';
 export { default as ChevronUpIcon } from './ChevronUp';
 export { default as ClipboardIcon } from './Clipboard';
 export { default as ClockIcon } from './Clock';
+export { default as TimerIcon } from './Timer';
