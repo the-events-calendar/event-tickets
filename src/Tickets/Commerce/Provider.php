2a4ba4ada99fa28b6b6ef09d373822bf15c4ba00
Dimitrios Pantazis <dpanta94@gmail.com>
2025-11-18 15:11:06 +0200

    Enable shepherd cleanup when ET is present

--- src/Tickets/Commerce/Provider.php
+++ src/Tickets/Commerce/Provider.php
@@ -111,6 +111,10 @@ class Provider extends Service_Provider {
 
 		// Cache invalidation.
 		add_filter( 'tec_cache_listener_save_post_types', [ $this, 'filter_cache_listener_save_post_types' ] );
+
+		add_action( 'wp_loaded', function () {
+			remove_filter( 'shepherd_tec_schedule_cleanup_task_every', '__return_zero' );
+		}, 15 );
 	}
 
 	/**
