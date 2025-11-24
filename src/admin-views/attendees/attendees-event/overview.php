27c757964ccdcabde3af0f4322a7b807bc8d0a74
DAnn2012 <dannunziata@hotmail.it>
2025-11-24 13:27:43 +0100

    Update overview.php

--- src/admin-views/attendees/attendees-event/overview.php
+++ src/admin-views/attendees/attendees-event/overview.php
@@ -56,7 +56,7 @@
 							<?php esc_html_e( $ticket->name ); ?>
 						</span>
 						<span class="tec-tickets__admin-attendees-overview-ticket-type-list-item-ticket-id">
-							<?php echo (int) $ticket->ID; ?>
+							<?php echo '#' . (int) $ticket->ID; ?>
 						</span>
 					</div>
 					<div class="tec-tickets__admin-attendees-overview-ticket-type-list-item-stat">
