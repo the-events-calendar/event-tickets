b2a6626c5134f820b039d85d74b2af8fb4b6a81a
Luca Tumedei <luca@theaveragedev.com>
2025-12-16 10:46:27 +0100

    test: clean up tests
    Remove tests that would cover again what is already covered in the
    controller test case

--- tests/wpunit/TEC/Tickets/RSVP/V2/Controller_Test.php
+++ tests/wpunit/TEC/Tickets/RSVP/V2/Controller_Test.php
@@ -15,45 +15,4 @@ use Codeception\TestCase\WPTestCase;
  * @since TBD
  */
 class Controller_Test extends WPTestCase {
-
-	/**
-	 * Test that register works without errors.
-	 *
-	 * @test
-	 */
-	public function test_register_works_without_errors(): void {
-		$controller = new Controller( tribe() );
-		$controller->register();
-
-		// If we get here without exception, test passes.
-		$this->assertTrue( true );
-	}
-
-	/**
-	 * Test that unregister does not throw.
-	 *
-	 * @test
-	 */
-	public function test_unregister_does_not_throw(): void {
-		$controller = new Controller( tribe() );
-		$controller->unregister();
-
-		// If we get here without exception, test passes.
-		$this->assertTrue( true );
-	}
-
-	/**
-	 * Test that Constants are registered as singleton.
-	 *
-	 * @test
-	 */
-	public function test_registers_constants_singleton(): void {
-		$controller = new Controller( tribe() );
-		$controller->register();
-
-		$constants1 = tribe( Constants::class );
-		$constants2 = tribe( Constants::class );
-
-		$this->assertSame( $constants1, $constants2 );
-	}
 }
