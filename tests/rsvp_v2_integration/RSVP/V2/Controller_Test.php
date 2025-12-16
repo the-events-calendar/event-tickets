4919e9ff6d3a3ddd0f230448c970f33dd90745ef
Luca Tumedei <luca@theaveragedev.com>
2025-12-16 16:27:56 +0100

    test: clean up test and remove useless tests

--- tests/rsvp_v2_integration/RSVP/V2/Controller_Test.php
+++ tests/rsvp_v2_integration/RSVP/V2/Controller_Test.php
@@ -9,7 +9,7 @@
 
 namespace TEC\Tickets\RSVP\V2;
 
-use Codeception\TestCase\WPTestCase;
+use TEC\Common\Tests\Provider\Controller_Test_Case;
 
 /**
  * Class Controller_Test
@@ -18,50 +18,6 @@ use Codeception\TestCase\WPTestCase;
  *
  * @package TEC\Tickets\RSVP\V2
  */
-class Controller_Test extends WPTestCase {
-
-	/**
-	 * @test
-	 */
-	public function it_should_be_resolved_from_container(): void {
-		$controller = tribe( Controller::class );
-
-		$this->assertInstanceOf( Controller::class, $controller );
-	}
-
-	/**
-	 * @test
-	 */
-	public function it_should_register_without_errors(): void {
-		$controller = tribe( Controller::class );
-
-		// Should not throw.
-		$controller->register();
-
-		$this->assertTrue( true );
-	}
-
-	/**
-	 * @test
-	 */
-	public function it_should_unregister_without_errors(): void {
-		$controller = tribe( Controller::class );
-		$controller->register();
-		$controller->unregister();
-
-		$this->assertTrue( true );
-	}
-
-	/**
-	 * @test
-	 */
-	public function it_should_register_constants_singleton(): void {
-		$controller = tribe( Controller::class );
-		$controller->register();
-
-		$constants_1 = tribe( Constants::class );
-		$constants_2 = tribe( Constants::class );
-
-		$this->assertSame( $constants_1, $constants_2 );
-	}
+class Controller_Test extends Controller_Test_Case {
+	protected string $controller_class = Controller::class;
 }
