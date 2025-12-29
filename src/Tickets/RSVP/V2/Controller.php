edc79f79657facd0fa6e616ed452060fde7a8082
Luca Tumedei <luca@theaveragedev.com>
2025-12-29 13:49:17 +0100

    fix: filter payments tab to hide tc toggle

--- src/Tickets/RSVP/V2/Controller.php
+++ src/Tickets/RSVP/V2/Controller.php
@@ -11,6 +11,7 @@ namespace TEC\Tickets\RSVP\V2;
 
 use TEC\Common\Contracts\Provider\Controller as Controller_Contract;
 use TEC\Tickets\RSVP\RSVP_Controller_Methods;
+use TEC\Tickets\Settings;
 
 /**
  * Class Controller
@@ -364,5 +365,39 @@ class Controller extends Controller_Contract {
 			'tec_tickets_commerce_single_order_details_metabox_after',
 			$this->container->callback( Metabox::class, 'add_rsvp_status_to_single_order_details_metabox' ),
 		);
+	}
+
+	/**
+	 * Filters the fields rendered in the Payments tab to replace the toggle to deactivate Tickets Commerce
+	 * with one that will not allow the user to do that.
+	 *
+	 * @since TBD
+	 *
+	 * @param array<string,mixed> $fields The fields to render in the tab.
+	 *
+	 * @return array<string,mixed> The filtered fields to render in the tab.
+	 */
+	public function change_tickets_commerce_settings( array $fields ): array {
+		if ( ! isset( $fields['tec-settings-payment-enable'] ) ) {
+			return $fields;
+		}
+
+		$is_tickets_commerce_enabled = tec_tickets_commerce_is_enabled();
+
+		$fields['tec-settings-payment-enable'] = [
+			'type' => 'html',
+			'html' => '<div>
+                  <input
+                      type="hidden"
+                      name="' . Settings::$tickets_commerce_enabled . '"
+                      ' . checked( $is_tickets_commerce_enabled, true, false ) . '
+                      id="tickets-commerce-enable-input"
+                      class="tribe-dependency tribe-dependency-verified">
+              </div>
+              <h2 class="tec-tickets__admin-settings-tab-heading">' . esc_html__( 'Tickets Commerce',
+					'event-tickets' ) . '</h2>',
+		];
+
+		return $fields;
 	}
 }
