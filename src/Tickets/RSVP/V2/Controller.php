adbddbc0129f7a6be7890508b0f6722e9587abd2
Luca Tumedei <luca@theaveragedev.com>
2025-12-29 13:49:17 +0100

    fix: filter payments tab to hide tc toggle

--- src/Tickets/RSVP/V2/Controller.php
+++ src/Tickets/RSVP/V2/Controller.php
@@ -11,6 +11,7 @@ namespace TEC\Tickets\RSVP\V2;
 
 use TEC\Common\Contracts\Provider\Controller as Controller_Contract;
 use TEC\Tickets\RSVP\RSVP_Controller_Methods;
+use TEC\Tickets\Settings;
 
 /**
  * Class Controller
@@ -58,6 +59,8 @@ class Controller extends Controller_Contract {
 		$this->container->singleton( Cart\RSVP_Cart::class );
 
 		add_action( 'rest_api_init', [ $this, 'register_rest_endpoints' ] );
+
+		add_filter( 'tec_tickets_commerce_settings_top_level', [ $this, 'change_tickets_commerce_settings' ] );
 	}
 
 	/**
@@ -69,6 +72,7 @@ class Controller extends Controller_Contract {
 	 */
 	public function unregister(): void {
 		remove_action( 'rest_api_init', [ $this, 'register_rest_endpoints' ] );
+		remove_filter( 'tec_tickets_commerce_settings_top_level', [ $this, 'change_tickets_commerce_settings' ] );
 	}
 
 	/**
@@ -80,5 +84,39 @@ class Controller extends Controller_Contract {
 	 */
 	public function register_rest_endpoints(): void {
 		$this->container->make( REST\Order_Endpoint::class )->register();
+	}
+
+	/**
+	 * Filters the fields rendered in the Payments tab to replace the toggle to deactivate Tickets Commerce
+	 * with one that will not allow the user to do that.
+	 *
+	 * @since TBD
+	 *
+	 * @param array<string,mixed> $fields The fields to render in the tab.
+	 *
+	 * @return array<string,mixed> The filtered fields to render in the tab.
+	 */
+	public function change_tickets_commerce_settings( array $fields ): array {
+		if ( ! isset( $fields['tec-settings-payment-enable'] ) ) {
+			return $fields;
+		}
+
+		$is_tickets_commerce_enabled = tec_tickets_commerce_is_enabled();
+
+		$fields['tec-settings-payment-enable'] = [
+			'type' => 'html',
+			'html' => '<div>
+                  <input
+                      type="hidden"
+                      name="' . Settings::$tickets_commerce_enabled . '"
+                      ' . checked( $is_tickets_commerce_enabled, true, false ) . '
+                      id="tickets-commerce-enable-input"
+                      class="tribe-dependency tribe-dependency-verified">
+              </div>
+              <h2 class="tec-tickets__admin-settings-tab-heading">' . esc_html__( 'Tickets Commerce',
+					'event-tickets' ) . '</h2>',
+		];
+
+		return $fields;
 	}
 }
--- tests/rsvp_v2_integration/RSVP/V2/Controller_Test.php
+++ tests/rsvp_v2_integration/RSVP/V2/Controller_Test.php
@@ -2,9 +2,88 @@
 
 namespace TEC\Tickets\RSVP\V2;
 
+use tad\Codeception\SnapshotAssertions\SnapshotAssertions;
 use TEC\Common\Tests\Provider\Controller_Test_Case;
-use TEC\Tickets\RSVP\RSVP_Disabled;
 
 class Controller_Test extends Controller_Test_Case {
+	use SnapshotAssertions;
+
 	protected string $controller_class = Controller::class;
+
+	public static function change_tickets_commerce_settings_provider(): array {
+		return [
+			'empty fields' => [ [], [] ],
+
+			'with fields, but target missing' => [
+				[
+					'tec-settings-payment-something' => [
+						'type' => 'html',
+						'html' => '<div>Test</div>'
+					],
+				],
+				[
+					'tec-settings-payment-something' => [
+						'type' => 'html',
+						'html' => '<div>Test</div>'
+					],
+				],
+			],
+
+			'with fields and target' => [
+				[
+					'tec-settings-payment-enable' => [
+						'type' => 'html',
+						'html' => '<div>Test</div>'
+					],
+				],
+				[
+					'tec-settings-payment-enable' => [
+						'type' => 'html',
+						'html' => '<div>Test</div>'
+					],
+				],
+			]
+		];
+	}
+
+	public function test_change_tickets_commerce_settings_changes_nothing_when_target_missing(): void {
+		$controller = $this->make_controller();
+		$actual     = $controller->change_tickets_commerce_settings(
+			[
+				'tec-settings-payment-something' => [
+					'type' => 'html',
+					'html' => '<div>Test</div>'
+				],
+			]
+		);
+
+		$this->assertEquals( [
+			'tec-settings-payment-something' => [
+				'type' => 'html',
+				'html' => '<div>Test</div>'
+			],
+		], $actual );
+	}
+
+	public function change_tickets_commerce_settings_changes_target(): void {
+		$controller = $this->make_controller();
+		$actual     = $controller->change_tickets_commerce_settings(
+			[
+				'tec-settings-payment-something' => [
+					'type' => 'html',
+					'html' => '<div>Test</div>'
+				],
+				'tec-settings-payment-enable'    => [
+					'type' => 'html',
+					'html' => '<div>Test</div>'
+				],
+			]
+		);
+
+		$this->assertArrayHasKey( 'tec-settings-payment-enable', $fields );
+		$this->assertArrayHasKey( 'type', $fields['tec-settings-payment-enable'] );
+		$this->assertEquals( 'html', $fields['tec-settings-payment-enable']['type'] );
+		$this->assertArrayHasKey( 'html', $fields['tec-settings-payment-enable'] );
+		$this->assertMatchesHtmlSnapshot( $fields['tec-settings-payment-enable']['html'] );
+	}
 }
