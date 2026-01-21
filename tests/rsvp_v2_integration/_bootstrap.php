9ec072a0edb102a3b6cc56cf9d7a6a69bbb6adc3
Luca Tumedei <luca@theaveragedev.com>
2026-01-21 15:31:54 +0100

    tests: auto increment hack for post IDs, update snapshots

--- tests/rsvp_v2_integration/_bootstrap.php
+++ tests/rsvp_v2_integration/_bootstrap.php
@@ -11,3 +11,9 @@ use TEC\Tickets\RSVP\Controller;
 
 // Enable RSVP V2 for all tests in this suite.
 add_filter( 'tec_tickets_rsvp_version', static fn() => Controller::VERSION_2 );
+
+// Start the posts auto-increment from a high number to make it easier to replace the post IDs in HTML snapshots.
+global $wpdb;
+if ( ! $wpdb->query( "ALTER TABLE $wpdb->posts AUTO_INCREMENT = 5096" ) ) {
+	throw new RuntimeException( 'Could not alter posts auto-increment' );
+}
