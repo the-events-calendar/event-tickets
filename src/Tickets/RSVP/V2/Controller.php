8aba94f2ac84ae5e9e29b71529c1306cca752661
Dimitrios Pantazis <dpanta94@gmail.com>
2026-02-10 00:11:04 +0200

    feat: add the attendee's status in RSVP V2 orders

--- src/Tickets/RSVP/V2/Controller.php
+++ src/Tickets/RSVP/V2/Controller.php
@@ -214,6 +214,11 @@ class Controller extends Controller_Contract {
 			'tec_tickets_commerce_attendee_before_delete',
 			$this->container->callback( Attendees::class, 'void_order_after_last_attendee_deleted' )
 		);
+
+		add_action(
+			'tec_tickets_commerce_single_order_details_metabox_after',
+			$this->container->callback( Metabox::class, 'add_rsvp_status_to_single_order_details_metabox' )
+		);
 	}
 
 	/**
