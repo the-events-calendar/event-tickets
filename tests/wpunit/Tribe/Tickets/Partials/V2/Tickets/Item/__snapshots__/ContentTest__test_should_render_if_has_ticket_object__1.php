77f158c24db70600806dd95909aacacc5a1b83fd
Victor Larodiel <victor.larodiel@hotmail.com>
2026-03-04 17:39:44 -0300

    tweak: update ticket snapshot values for consistent rendering in tests

--- tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Item/__snapshots__/ContentTest__test_should_render_if_has_ticket_object__1.php
+++ tests/wpunit/Tribe/Tickets/Partials/V2/Tickets/Item/__snapshots__/ContentTest__test_should_render_if_has_ticket_object__1.php
@@ -1,32 +1,32 @@
 <?php return '<div  class="tribe-tickets__tickets-item-content-title-container" >
 		<div  class="tribe-common-h7 tribe-common-h6--min-medium tribe-tickets__tickets-item-content-title" >
-				Test ticket for {{ID}}	</div>
+				Test ticket for 8	</div>
 </div>
 
 
 <div
-	id="tribe__details__content--{{POST_ID}}"
+	id="tribe__details__content--12953"
 	 class="tribe-common-b2 tribe-common-b3--min-medium tribe-tickets__tickets-item-details-content" >
-	Test ticket description for {{ID}}</div>
+	Test ticket description for 8</div>
 <div  class="tribe-tickets__tickets-item-extra" >
 
 	<div  class="tribe-common-b2 tribe-common-b1--min-medium tribe-tickets__tickets-item-extra-price" >
 		<span class="tribe-tickets__tickets-sale-price">
-
+		
 				<span class="tribe-formatted-currency-wrap tribe-currency-prefix">
 					<span class="tribe-currency-symbol">$</span>
-					<span class="tribe-amount">{{PRICE}}</span>
+					<span class="tribe-amount">6.00</span>
 				</span>
 						</span>
 </div>
 
-
+	
 <div class="tribe-common-b3 tribe-tickets__tickets-item-extra-available">
 
-
+	
 	<span class="tribe-tickets__tickets-item-extra-available-quantity"> 100 </span> available
 </div>
 
-
+	
 </div>
 ';
