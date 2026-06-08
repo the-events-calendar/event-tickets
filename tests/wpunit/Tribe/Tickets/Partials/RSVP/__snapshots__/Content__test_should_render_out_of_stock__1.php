dbc9e5f9918d4a4e162b9add8776fed021a6b531
Dimitrios Pantazis <dpanta94@gmail.com>
2026-06-08 20:12:32 +0200

    tests: update broken snapshots

--- tests/wpunit/Tribe/Tickets/Partials/RSVP/__snapshots__/Content__test_should_render_out_of_stock__1.php
+++ tests/wpunit/Tribe/Tickets/Partials/RSVP/__snapshots__/Content__test_should_render_out_of_stock__1.php
@@ -22,7 +22,80 @@
 	
 <!-- This div is where the AJAX returns the form. -->
 <div class="tribe-block__rsvp__form">
+	<form
+	name="tribe-rsvp-form"
+	data-product-id="12734"
+>
+	<input type="hidden" name="product_id[]" value="12734">
+	<input type="hidden" name="attendee[order_status]" value="yes">
+	<!-- Maybe add nonce over here? Try to leave templates as clean as possible -->
+
+	<div class="tribe-left">
+					<div class="tribe-block__rsvp__number-input">
+	<div class="tribe-block__rsvp__number-input-inner">
+		<button
+	type="button"
+	class="tribe-block__rsvp__number-input-button tribe-block__rsvp__number-input-button--minus"
+></button>
+
+		<label
+	class="screen-reader-text"
+	for="quantity_12734"
+>
+	Quantity</label>
+<input
+	type="number"
+	id="quantity_12734"
+	name="quantity_12734"
+	class="tribe-tickets-quantity"
+	step="1"
+	min="1"
+	value="1"
+	required
+	max="0"
+	/>
+
+		<button
+	type="button"
+	class="tribe-block__rsvp__number-input-button tribe-block__rsvp__number-input-button--plus"
+></button>
 	</div>
+	<span class="tribe-block__rsvp__number-input-label">
+		RSVPs	</span>
+</div>
+			</div>
+
+	<div class="tribe-right">
+		<div class="tribe-block__rsvp__message__error">
+
+	Please fill in the RSVP confirmation name and email fields.
+</div>
+					<input
+	type="text"
+	name="attendee[full_name]"
+	class="tribe-tickets-full-name"
+	placeholder="Full Name"
+	value=""
+	required
+/>
+<input
+	type="email"
+	name="attendee[email]"
+	class="tribe-tickets-email"
+	placeholder="Email"
+	value=""
+	required
+/>
+	<input name="attendee[optout]" value="1" type="hidden" />
+							<button
+	type="submit"
+	name="tickets_process"
+	value="1"
+	class="tribe-block__rsvp__submit-button"
+>
+	Submit RSVP</button>			</div>
+</form>
+</div>
 
 </div>
 ';
--- tests/wpunit/Tribe/Tickets/Partials/RSVP/__snapshots__/Status__test_should_render_status_full__1.php
+++ tests/wpunit/Tribe/Tickets/Partials/RSVP/__snapshots__/Status__test_should_render_status_full__1.php
@@ -1,13 +1,3 @@
 <?php return '<div class="tribe-block__rsvp__status">
-	
-		<span>
-	<button
-	class="tribe-block__rsvp__status-button tribe-block__rsvp__status-button--going"
-		>
-		<span>Going</span>
-		
-	</button>
-</span>
-		
-	</div>
+			RSVP Full	</div>
 ';
