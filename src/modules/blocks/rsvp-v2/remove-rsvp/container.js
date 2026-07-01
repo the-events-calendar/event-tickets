b843e008e7274738e2d5051de27380e8e16bee83
Victor Larodiel <victor.larodiel@hotmail.com>
2026-07-01 00:08:54 -0300

    feat(RSVP): Add V2 rsvp-window, remove-rsvp, and sidebar-controls components

---
+++ src/modules/blocks/rsvp-v2/remove-rsvp/container.js
@@ -0,0 +1,60 @@
+/**
+ * External dependencies
+ */
+import { dispatch as wpDispatch } from '@wordpress/data';
+import { __ } from '@wordpress/i18n';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+
+/**
+ * Internal dependencies
+ */
+import { withStore } from '@moderntribe/common/hoc';
+import { actions, selectors, thunks } from '../../../data/blocks/rsvp-v2';
+import RSVPRemoveRsvp from './template';
+
+const mapStateToProps = ( state ) => ( {
+	created: selectors.getRSVPCreated( state ),
+	isDisabled: selectors.getRSVPSettingsOpen( state ),
+	isLoading: selectors.getRSVPIsLoading( state ),
+	rsvpId: selectors.getRSVPId( state ),
+} );
+
+const mergeProps = ( stateProps, dispatchProps, ownProps ) => {
+	const { dispatch } = dispatchProps;
+
+	return {
+		...ownProps,
+		isDisabled: stateProps.isDisabled,
+		isLoading: stateProps.isLoading,
+		onRemove: async () => {
+			if (
+				! window.confirm(
+					// eslint-disable-line no-alert
+					__( 'Are you sure you want to disable RSVP? This cannot be undone.', 'event-tickets' )
+				)
+			) {
+				return;
+			}
+
+			if ( stateProps.created && stateProps.rsvpId ) {
+				await dispatch( thunks.deleteRSVP( stateProps.rsvpId ) );
+			}
+
+			dispatch( actions.deleteRSVP() );
+			wpDispatch( 'core/block-editor' ).removeBlocks( [ ownProps.clientId ] );
+		},
+	};
+};
+
+const ConnectedRemoveRsvp = compose( withStore(), connect( mapStateToProps, null, mergeProps ) )( RSVPRemoveRsvp );
+
+const RSVPRemoveRsvpContainer = ( { clientId, isSelected, created } ) => {
+	if ( ! created || ! isSelected ) {
+		return null;
+	}
+
+	return <ConnectedRemoveRsvp clientId={ clientId } />;
+};
+
+export default RSVPRemoveRsvpContainer;
