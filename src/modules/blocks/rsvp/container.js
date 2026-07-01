9ff9cdab57cc5467806429a5d538422582f88d75
Victor Larodiel <victor.larodiel@hotmail.com>
2026-07-01 00:06:40 -0300

    feat(RSVP): Refactor RSVP V1 block containers to use shared components
    Update V1 RSVP block containers to import from rsvp-shared:
    - container.js — Use shared createSetInitialState and createCloseBlockOverlays
    - action-dashboard, capacity, duration-picker, inactive-block — Use shared templates
    - container-content, container-header — Use shared counter and button components
    - index.js — Add filters for RSVP block lifecycle
    - filters.js — New filter hooks for RSVP block editor
    
    Part of SOFT-3335

--- src/modules/blocks/rsvp/container.js
+++ src/modules/blocks/rsvp/container.js
@@ -3,12 +3,6 @@
  */
 import { connect } from 'react-redux';
 import { compose } from 'redux';
-import moment from 'moment';
-
-/**
- * WordPress dependencies
- */
-import { select } from '@wordpress/data';
 
 /**
  * Internal dependencies
@@ -18,46 +12,10 @@ import { actions, selectors, thunks } from '../../data/blocks/rsvp';
 import { isModalShowing, getModalTicketId } from '../../data/shared/move/selectors';
 import { withStore } from '@moderntribe/common/hoc';
 import withSaveData from '../hoc/with-save-data';
-import { moment as momentUtil, time } from '@moderntribe/common/utils';
 import { hasRecurrenceRules, noRsvpsOnRecurring } from '@moderntribe/common/utils/recurrence';
-
-const getIsInactive = ( state ) => {
-	const startDateMoment = selectors.getRSVPStartDateMoment( state );
-	const startTime = selectors.getRSVPStartTimeNoSeconds( state );
-	const endDateMoment = selectors.getRSVPEndDateMoment( state );
-	const endTime = selectors.getRSVPEndTimeNoSeconds( state );
-
-	if ( ! startDateMoment || ! endDateMoment ) {
-		return false;
-	}
-
-	const startMoment = momentUtil.setTimeInSeconds(
-		startDateMoment.clone(),
-		time.toSeconds( startTime, time.TIME_FORMAT_HH_MM )
-	);
-	const endMoment = momentUtil.setTimeInSeconds(
-		endDateMoment.clone(),
-		time.toSeconds( endTime, time.TIME_FORMAT_HH_MM )
-	);
-	const currentMoment = moment();
-
-	return ! ( currentMoment.isAfter( startMoment ) && currentMoment.isBefore( endMoment ) );
-};
-
-const setInitialState = ( dispatch, ownProps ) => () => {
-	const postId = select( 'core/editor' ).getCurrentPostId();
-	dispatch( thunks.getRSVP( postId ) );
-	const { attributes = {} } = ownProps;
-	if ( parseInt( attributes.headerImageId, 10 ) ) {
-		dispatch( actions.fetchRSVPHeaderImage( attributes.headerImageId ) );
-	}
-	if ( attributes.goingCount ) {
-		dispatch( actions.setRSVPGoingCount( parseInt( attributes.goingCount, 10 ) ) );
-	}
-	if ( attributes.notGoingCount ) {
-		dispatch( actions.setRSVPNotGoingCount( parseInt( attributes.notGoingCount, 10 ) ) );
-	}
-};
+import { createSetInitialState } from '../rsvp-shared/utils/create-set-initial-state';
+import { createCloseBlockOverlays } from '../rsvp-shared/utils/create-close-block-overlays';
+import './filters';
 
 const mapStateToProps = ( state ) => {
 	const rsvpId = selectors.getRSVPId( state );
@@ -65,21 +23,39 @@ const mapStateToProps = ( state ) => {
 	return {
 		created: selectors.getRSVPCreated( state ),
 		isAddEditOpen: selectors.getRSVPIsAddEditOpen( state ),
-		isInactive: getIsInactive( state ),
+		isInactive: selectors.getRSVPIsInactive( state ),
 		isLoading: selectors.getRSVPIsLoading( state ),
 		isModalShowing: isModalShowing( state ) && getModalTicketId( state ) === rsvpId,
 		isSettingsOpen: selectors.getRSVPSettingsOpen( state ),
 		hasRecurrenceRules: hasRecurrenceRules( state ),
 		noRsvpsOnRecurring: noRsvpsOnRecurring(),
 		rsvpId,
+		goingCount: String( selectors.getRSVPGoingCount( state ) ?? '' ),
+		notGoingCount: String( selectors.getRSVPNotGoingCount( state ) ?? '' ),
 	};
 };
 
 const mapDispatchToProps = ( dispatch, ownProps ) => ( {
 	initializeRSVP: () => dispatch( actions.initializeRSVP() ),
-	onBlockRemoved: () => dispatch( actions.deleteRSVP() ),
-	setInitialState: setInitialState( dispatch, ownProps ),
-	setAddEditClosed: () => dispatch( actions.setRSVPIsAddEditOpen( false ) ),
+	onBlockRemoved: ( props ) => {
+		if ( props.created && props.rsvpId ) {
+			dispatch( thunks.deleteRSVP( props.rsvpId ) );
+		}
+
+		dispatch( actions.deleteRSVP() );
+	},
+	setInitialState: createSetInitialState( {
+		actions,
+		thunks,
+		hydrateHeaderImage: true,
+	} )( dispatch, ownProps ),
+	closeBlockOverlays: createCloseBlockOverlays( { dispatch, actions, closeSettings: true } ),
+	closeBlockOverlaysOnDeselect: createCloseBlockOverlays( {
+		dispatch,
+		actions,
+		closeSettings: true,
+		closeAttendeeModal: false,
+	} ),
 } );
 
 export default compose( withStore(), connect( mapStateToProps, mapDispatchToProps ), withSaveData() )( RSVP );
