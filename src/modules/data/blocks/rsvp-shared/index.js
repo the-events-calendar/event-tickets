a063c3a4acd607ca4edbc1d84031f10139b9532b
Victor Larodiel <victor.larodiel@hotmail.com>
2026-07-02 12:05:37 -0300

    Merge branch 'fix/soft-3335-rsvp-block-ui' into split/soft-3335-03-data-shared-types

---
+++ src/modules/data/blocks/rsvp-shared/index.js
@@ -0,0 +1,11 @@
+/**
+ * Internal dependencies
+ */
+import * as types from './types';
+import * as actions from './actions';
+import * as selectors from './selectors';
+import reducer from './reducer';
+import sagas from './sagas';
+
+export default reducer;
+export { types, actions, sagas, selectors };
