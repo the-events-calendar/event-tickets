edd71bb8283694bef9c3d0bf5e2bcc841fe7bb28
Victor Larodiel <victor.larodiel@hotmail.com>
2026-07-30 00:47:10 -0300

    test: update RSVP snapshots to use dynamic event IDs
    - Modified snapshot test to replace hardcoded event ID with a placeholder for dynamic data representation.
    - Ensured consistency in RSVP rendering tests by using [EVENT_ID] for title and description fields.

--- tests/wpunit/Tribe/Tickets/__snapshots__/RSVPTest__it_should_render_rsvp_step with data set initial state__1.php
+++ tests/wpunit/Tribe/Tickets/__snapshots__/RSVPTest__it_should_render_rsvp_step with data set initial state__1.php
@@ -24,16 +24,16 @@
 
 
 
-	
+
 	<div class="tribe-tickets__rsvp tribe-common-g-row tribe-common-g-row--gutters">
 
 		<div class="tribe-tickets__rsvp-details-wrapper tribe-common-g-col">
 	<div class="tribe-tickets__rsvp-details">
 		<h3 class="tribe-tickets__rsvp-title tribe-common-h2 tribe-common-h4--min-medium">
-	Test RSVP ticket for 14057</h3>
+	Test RSVP ticket for [EVENT_ID]</h3>
 
 		<div class="tribe-tickets__rsvp-description tribe-common-h6 tribe-common-h--alt tribe-common-b3--min-medium">
-	<p>Ticket RSVP ticket excerpt for 14057</p>
+	<p>Ticket RSVP ticket excerpt for [EVENT_ID]</p>
 </div>
 
 		<div class="tribe-tickets__rsvp-attendance">
@@ -44,8 +44,8 @@
 </div>
 
 		<div class="tribe-tickets__rsvp-availability tribe-common-h6 tribe-common-h--alt tribe-common-b3--min-medium">
-			<span class="tribe-tickets__rsvp-availability-quantity tribe-common-b2--bold"> 10 </span> remaining,	
-			
+			<span class="tribe-tickets__rsvp-availability-quantity tribe-common-b2--bold"> 10 </span> remaining,
+
 <span class="tribe-tickets__rsvp-availability-days-to-rsvp">
 	Last day to RSVP</span>
 	</div>
@@ -55,9 +55,9 @@
 		<div class="tribe-tickets__rsvp-actions-wrapper tribe-common-g-col">
 	<div class="tribe-tickets__rsvp-actions">
 
-		
+
 			<div class="tribe-tickets__rsvp-actions-rsvp">
-	
+
 <div class="tribe-tickets__rsvp-actions-rsvp-going">
 	<button
 		class="tribe-common-c-btn tribe-tickets__rsvp-actions-button-going tribe-common-b1 tribe-common-b2--min-medium"
@@ -72,8 +72,8 @@
 
 </div>
 
-		
+
 	</div>
 
-	
+
 ';
