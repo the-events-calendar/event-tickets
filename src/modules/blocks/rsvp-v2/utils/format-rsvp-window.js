f593fd49ca1337684d59d0b9a36ce9f8f474a29b
Victor Larodiel <victor.larodiel@hotmail.com>
2026-07-31 01:42:22 -0300

    fix(rsvp-v2): use momentUtil.toFormat() instead of duplicate PHP_TO_MOMENT_FORMAT table

--- src/modules/blocks/rsvp-v2/utils/format-rsvp-window.js
+++ src/modules/blocks/rsvp-v2/utils/format-rsvp-window.js
@@ -1,49 +1,4 @@
-import { globals } from '@moderntribe/common/utils';
-
-const PHP_TO_MOMENT_FORMAT = {
-	d: 'DD',
-	D: 'ddd',
-	j: 'D',
-	l: 'dddd',
-	N: 'E',
-	S: 'o',
-	w: 'e',
-	z: 'DDD',
-	W: 'W',
-	F: 'MMMM',
-	m: 'MM',
-	M: 'MMM',
-	n: 'M',
-	t: '',
-	L: '',
-	o: 'YYYY',
-	Y: 'YYYY',
-	y: 'YY',
-	a: 'a',
-	A: 'A',
-	B: '',
-	g: 'h',
-	G: 'H',
-	h: 'hh',
-	H: 'HH',
-	i: 'mm',
-	s: 'ss',
-	u: 'SSS',
-	e: 'zz',
-	I: '',
-	O: '',
-	P: '',
-	T: '',
-	Z: '',
-	c: '',
-	r: '',
-	U: 'X',
-};
-
-const convertFormat = ( format ) => {
-	const chars = format.split( '' );
-	return chars.map( ( c ) => ( c in PHP_TO_MOMENT_FORMAT ? PHP_TO_MOMENT_FORMAT[ c ] : c ) ).join( '' );
-};
+import { globals, moment as momentUtil } from '@moderntribe/common/utils';
 
 /**
  * Format RSVP start and end dates for display in the RSVP Window section.
@@ -58,7 +13,7 @@ export const formatRsvpWindow = ( startDateMoment, endDateMoment ) => {
 	}
 
 	const phpFormat = globals.tecDateSettings().datepickerFormat || 'F j, Y';
-	const dateFormat = convertFormat( phpFormat );
+	const dateFormat = momentUtil.toFormat( phpFormat );
 
 	const startFormatted = startDateMoment.format( dateFormat );
 	const endFormatted = endDateMoment.format( dateFormat );
