f743d20c3429c362590aec15e8f20d314f0dd978
Filipe Coutinho <coutinhocouto@hotmail.com>
2026-09-17 09:47:09 -0300

    Revert "Merge branch 'release/M26.octillery' of github.com-coutinhocouto:the-events-calendar/event-tickets into smtnc-2748-event-tickets-commerce-paypal-orders-remain-stuck-in-pending"
    This reverts commit 24c62f6d8fc8263e8727627a94e93a3ec8cc90ee, reversing
    changes made to 523679871361a351e03c31536782820d1308fded.

--- src/Tickets/Commerce/Gateways/PayPal/REST.php
+++ src/Tickets/Commerce/Gateways/PayPal/REST.php
@@ -11,13 +11,8 @@ use WP_REST_Server;
  * @package TEC\Tickets\Commerce\Gateways\PayPal
  */
 class REST extends \TEC\Common\Contracts\Service_Provider {
-	/**
-	 * Registers the endpoint singletons.
-	 *
-	 * @since TBD Registers the webhook endpoint.
-	 */
 	public function register() {
-		$this->container->singleton( REST\Webhook_Endpoint::class );
+//		$this->container->singleton( REST\Webhook_Endpoint::class, [ $this, 'boot_webhook_endpoint' ] );
 		$this->container->singleton( REST\On_Boarding_Endpoint::class );
 		$this->container->singleton( REST\Order_Endpoint::class );
 	}
@@ -26,10 +21,8 @@ class REST extends \TEC\Common\Contracts\Service_Provider {
 	 * Register the endpoints for handling webhooks.
 	 *
 	 * @since 5.1.6
-	 * @since TBD Serves the webhook route, which onboarding already registers with PayPal.
 	 */
 	public function register_endpoints() {
-		$this->container->make( REST\Webhook_Endpoint::class )->register();
 		$this->container->make( REST\On_Boarding_Endpoint::class )->register();
 		$this->container->make( REST\Order_Endpoint::class )->register();
 	}
